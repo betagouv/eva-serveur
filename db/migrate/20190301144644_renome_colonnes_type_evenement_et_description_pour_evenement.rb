0e8e4b536611da5e9ebc5412aecd1f78e4a67ddc
Stéphane Hanser <shanser@captive.fr>
2019-03-18 16:52:06 +0100

    transforme les tabs en espaces

--- db/migrate/20190301144644_renome_colonnes_type_evenement_et_description_pour_evenement.rb
+++ db/migrate/20190301144644_renome_colonnes_type_evenement_et_description_pour_evenement.rb
@@ -1,6 +1,6 @@
 class RenomeColonnesTypeEvenementEtDescriptionPourEvenement < ActiveRecord::Migration[5.2]
   def change
-  	rename_column :evenements, :type_evenement, :nom
-	rename_column :evenements, :description, :donnees
+    rename_column :evenements, :type_evenement, :nom
+    rename_column :evenements, :description, :donnees
   end
 end

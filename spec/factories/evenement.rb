8d38e3b62f69b7bf78f129ff60520239d797a759
François de Metz <francois.de_metz@beta.gouv.fr>
2019-03-26 15:12:04 +0100

    Ajout d'une classe EvaluationInventaire qui retourne quelques informations.

--- spec/factories/evenement.rb
+++ spec/factories/evenement.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# rubocop:disable Metrics/BlockLength
 FactoryBot.define do
   factory :evenement do
     nom { 'ouvertureContenant' }
@@ -7,5 +8,38 @@ FactoryBot.define do
     situation { 'inventaire' }
     session_id { '07319b2485be9ac4850664cd47cede38' }
     date { DateTime.now }
+
+    factory :evenement_demarrage do
+      nom { 'demarrage' }
+      donnees { {} }
+    end
+
+    factory :evenement_saisie_inventaire do
+      nom { 'saisieInventaire' }
+      donnees { {} }
+
+      factory :evenement_saisie_inventaire_ok do
+        donnees do
+          { reussite: true }
+        end
+      end
+
+      factory :evenement_saisie_inventaire_echec do
+        donnees do
+          { reussite: false }
+        end
+      end
+    end
+
+    factory :evenement_ouverture_contenant do
+      nom { 'ouvertureContenant' }
+      donnees { { contenant: 1 } }
+    end
+
+    factory :evenement_stop do
+      nom { 'stop' }
+      donnees { {} }
+    end
   end
 end
+# rubocop:enable Metrics/BlockLength
---
+++ spec/models/evaluation_inventaire_spec.rb
@@ -0,0 +1,118 @@
+require 'rails_helper'
+
+describe EvaluationInventaire do
+  describe EvaluationInventaire::Essai do
+    it 'retourne le temps depuis le début de la situation' do
+      evenements = [
+        build(:evenement_ouverture_contenant, date: 2.minute.ago),
+        build(:evenement_ouverture_contenant, date: 1.minute.ago)
+      ]
+      evaluation = described_class.new(evenements, 5.minute.ago)
+      expect(evaluation.temps_total).to be_within(0.1).of(240)
+    end
+  end
+
+  it 'session_id retourne le session_id' do
+    evenements = [
+      build(:evenement_demarrage, session_id: 'exemple')
+    ]
+    evaluation = described_class.new(evenements)
+    expect(evaluation.session_id).to eql('exemple')
+  end
+
+  it 'est en reussite' do
+    evenements = [
+      build(:evenement_demarrage),
+      build(:evenement_saisie_inventaire_ok)
+    ]
+    evaluation = described_class.new(evenements)
+    expect(evaluation).to be_reussite
+    expect(evaluation).to_not be_abandon
+    expect(evaluation).to_not be_en_cours
+  end
+
+  it 'est en echec' do
+    evenements = [
+      build(:evenement_demarrage),
+      build(:evenement_saisie_inventaire_echec)
+    ]
+    evaluation = described_class.new(evenements)
+    expect(evaluation).to_not be_reussite
+    expect(evaluation).to_not be_abandon
+    expect(evaluation).to_not be_en_cours
+  end
+
+  it 'est en abandon' do
+    evenements = [
+      build(:evenement_demarrage),
+      build(:evenement_stop)
+    ]
+    evaluation = described_class.new(evenements)
+    expect(evaluation).to_not be_reussite
+    expect(evaluation).to be_abandon
+    expect(evaluation).to_not be_en_cours
+  end
+
+  it 'est en cours' do
+    evenements = [
+      build(:evenement_demarrage)
+    ]
+    evaluation = described_class.new(evenements)
+    expect(evaluation).to_not be_reussite
+    expect(evaluation).to_not be_abandon
+    expect(evaluation).to be_en_cours
+  end
+
+  it 'retourne le temps total' do
+    evenements = [
+      build(:evenement_demarrage, date: 10.minutes.ago),
+      build(:evenement_saisie_inventaire_ok, date: 4.minutes.ago)
+    ]
+    evaluation = described_class.new(evenements)
+    expect(evaluation.temps_total).to be_within(0.1).of(360)
+  end
+
+  it "retourne le nombre d'ouverture de contenant" do
+    evenements = [
+      build(:evenement_ouverture_contenant),
+      build(:evenement_ouverture_contenant),
+      build(:evenement_ouverture_contenant)
+    ]
+    evaluation = described_class.new(evenements)
+    expect(evaluation.nombre_ouverture_contenant).to eql(3)
+  end
+
+  it "retourne le nombre d'essai de validation" do
+    evenements = [
+      build(:evenement_saisie_inventaire_echec),
+      build(:evenement_saisie_inventaire_echec),
+      build(:evenement_saisie_inventaire_echec),
+      build(:evenement_saisie_inventaire_ok)
+    ]
+    evaluation = described_class.new(evenements)
+    expect(evaluation.nombre_essais_validation).to eql(4)
+  end
+
+  it 'retourne les essais avec le nombre de click et le temps' do
+    evenements = [
+      build(:evenement_demarrage, date: 10.minutes.ago),
+      build(:evenement_ouverture_contenant, date: 8.minutes.ago),
+      build(:evenement_ouverture_contenant, date: 8.minutes.ago),
+      build(:evenement_ouverture_contenant, date: 8.minutes.ago),
+      build(:evenement_saisie_inventaire_echec, date: 7.minutes.ago),
+      build(:evenement_saisie_inventaire_ok, date: 5.minutes.ago)
+    ]
+    evaluation = described_class.new(evenements)
+    expect(evaluation.essais.size).to eql(2)
+    evaluation.essais.first.tap do |essai|
+      expect(essai).to_not be_reussite
+      expect(essai.nombre_ouverture_contenant).to eql(3)
+      expect(essai.temps_total).to within(0.1).of(180)
+    end
+    evaluation.essais.last.tap do |essai|
+      expect(essai).to be_reussite
+      expect(essai.nombre_ouverture_contenant).to eql(0)
+      expect(essai.temps_total).to within(0.1).of(300)
+    end
+  end
+end

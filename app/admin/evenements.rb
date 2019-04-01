eac3af2efd68ba0f8752914524eeec0a4cf2af65
Clément MORISSET <cmorisset@captive.fr>
2019-04-01 18:53:07 +0200

    empêche de créer/modifier un évenement dans le BO

--- app/admin/evenements.rb
+++ app/admin/evenements.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Evenement do
+  actions :index, :show, :destroy
   permit_params :donnees, :session_id, :situation, :nom, :date
 
   filter :situation, as: :select
@@ -8,26 +9,6 @@ ActiveAdmin.register Evenement do
   filter :date
 
   index do
-    selectable_column
-    column :id
-    column :session_id
-    column :situation
-    column :nom
-    column :donnees
-    column :date
-    column :created_at
-    column :updated_at
-    actions
-  end
-
-  form do |f|
-    f.inputs do
-      f.input :session_id
-      f.input :situation
-      f.input :nom
-      f.input :donnees, as: :text
-      f.input :date, as: :datepicker
-    end
-    f.actions
+    render 'index', context: self
   end
 end

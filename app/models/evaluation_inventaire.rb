f85ad1cb1e77f639c6c3e8f11745970e161a3ec3
Stéphane Hanser <shanser@captive.fr>
2019-04-19 12:58:35 +0200

    simplifie la classe Essai

--- app/models/evaluation_inventaire.rb
+++ app/models/evaluation_inventaire.rb
@@ -7,35 +7,7 @@ class EvaluationInventaire < EvaluationBase
     STOP: 'stop'
   }.freeze
 
-  module Evaluation
-    def reussite?
-      @evenements.last.nom == EVENEMENT[:SAISIE_INVENTAIRE] &&
-        @evenements.last.donnees['reussite']
-    end
-
-    def en_cours?
-      @evenements.last.nom != EVENEMENT[:SAISIE_INVENTAIRE] &&
-        @evenements.last.nom != EVENEMENT[:STOP]
-    end
-
-    def abandon?
-      @evenements.last.nom == EVENEMENT[:STOP]
-    end
-
-    def nombre_ouverture_contenant
-      compte_nom_evenements(EVENEMENT[:OUVERTURE_CONTENANT])
-    end
-
-    def nombre_essais_validation
-      compte_nom_evenements(EVENEMENT[:SAISIE_INVENTAIRE])
-    end
-  end
-
-  include Evaluation
-
-  class Essai < EvaluationBase
-    include Evaluation
-
+  class Essai < EvaluationInventaire
     def initialize(evenements, date_depart_situation)
       super(evenements)
       @date_depart_situation = date_depart_situation
@@ -44,6 +16,28 @@ class EvaluationInventaire < EvaluationBase
     def temps_total
       @evenements.last.date - @date_depart_situation
     end
+  end
+
+  def reussite?
+    @evenements.last.nom == EVENEMENT[:SAISIE_INVENTAIRE] &&
+      @evenements.last.donnees['reussite']
+  end
+
+  def en_cours?
+    @evenements.last.nom != EVENEMENT[:SAISIE_INVENTAIRE] &&
+      @evenements.last.nom != EVENEMENT[:STOP]
+  end
+
+  def abandon?
+    @evenements.last.nom == EVENEMENT[:STOP]
+  end
+
+  def nombre_ouverture_contenant
+    compte_nom_evenements(EVENEMENT[:OUVERTURE_CONTENANT])
+  end
+
+  def nombre_essais_validation
+    compte_nom_evenements(EVENEMENT[:SAISIE_INVENTAIRE])
   end
 
   def temps_total
@@ -56,16 +50,7 @@ class EvaluationInventaire < EvaluationBase
     end
     date_depart = @evenements.first.date
     essais.map do |evenements|
-      cree_essai(evenements, date_depart)
+      Essai.new(evenements, date_depart)
     end
-  end
-
-  private
-
-  def cree_essai(evenements, date_depart)
-    Essai.new(
-      evenements,
-      date_depart
-    )
   end
 end

d759595f15e1d35b1075deebb6c99caabb0bdf7c
François de Metz <francois.de_metz@beta.gouv.fr>
2019-05-02 20:03:27 +0200

    Ajoute l'évaluation de la compétence organisation et méthode de la situation inventaire.

---
+++ spec/models/evaluation/inventaire/organisation_methode_spec.rb
@@ -0,0 +1,56 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Evaluation::Inventaire::OrganisationMethode do
+  let(:evaluation) { double }
+
+  context 'en réussite et moins de 70 ouvertures de contenants' do
+    it 'a le niveau 4' do
+      expect(evaluation).to receive(:reussite?).and_return(true)
+      expect(evaluation).to receive(:nombre_ouverture_contenant).and_return(60)
+      expect(
+        described_class.new(evaluation).niveau
+      ).to eql(Competence::NIVEAU_4)
+    end
+  end
+
+  context 'en réussite et entre de 70 et 120 ouvertures de contenants' do
+    it 'a le niveau 3' do
+      expect(evaluation).to receive(:reussite?).and_return(true)
+      expect(evaluation).to receive(:nombre_ouverture_contenant).and_return(80)
+      expect(
+        described_class.new(evaluation).niveau
+      ).to eql(Competence::NIVEAU_3)
+    end
+  end
+
+  context 'en réussite et entre de 120 et 250 ouvertures de contenants' do
+    it 'a le niveau 2' do
+      expect(evaluation).to receive(:reussite?).and_return(true)
+      expect(evaluation).to receive(:nombre_ouverture_contenant).and_return(200)
+      expect(
+        described_class.new(evaluation).niveau
+      ).to eql(Competence::NIVEAU_2)
+    end
+  end
+
+  context 'en réussite et plus de 250 ouvertures de contenants' do
+    it 'a le niveau 2' do
+      expect(evaluation).to receive(:reussite?).and_return(true)
+      expect(evaluation).to receive(:nombre_ouverture_contenant).and_return(300)
+      expect(
+        described_class.new(evaluation).niveau
+      ).to eql(Competence::NIVEAU_1)
+    end
+  end
+
+  context 'en cas de non reussite' do
+    it 'a le niveau indetermine' do
+      expect(evaluation).to receive(:reussite?).and_return(false)
+      expect(
+        described_class.new(evaluation).niveau
+      ).to eql(Competence::NIVEAU_INDETERMINE)
+    end
+  end
+end

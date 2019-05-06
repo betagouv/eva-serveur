237dcd749826fb964acfecdf01f149fa0be72c50
François de Metz <francois.de_metz@beta.gouv.fr>
2019-05-06 18:01:39 +0200

    Ajoute l'évaluation de la compréhension de la consigne dans le controle.

---
+++ app/models/evaluation/controle/comprehension_consigne.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module Evaluation
+  class Controle
+    class ComprehensionConsigne < Evaluation::Competence::Base
+      def niveau
+        if @evaluation.termine? && @evaluation.nombre_loupees < 16
+          ::Competence::NIVEAU_4
+        elsif @evaluation.termine? || abandon_en_moins_de_11_pieces_avec_erreurs?
+          ::Competence::NIVEAU_1
+        else
+          ::Competence::NIVEAU_INDETERMINE
+        end
+      end
+
+      def abandon_en_moins_de_11_pieces_avec_erreurs?
+        @evaluation.abandon? && nombre_pieces < 11 && @evaluation.nombre_loupees > 0 &&
+          @evaluation.nombre_rejoue_consigne > 0
+      end
+
+      def nombre_pieces
+        @evaluation.nombre_bien_placees + @evaluation.nombre_loupees
+      end
+    end
+  end
+end

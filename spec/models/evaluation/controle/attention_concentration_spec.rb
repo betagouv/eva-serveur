77ad445ce2e0720160e173ad4f90c0d0cb0351f0
François de Metz <francois.de_metz@beta.gouv.fr>
2019-05-09 13:23:03 +0200

    Utilise nombre_loupees.

--- spec/models/evaluation/controle/attention_concentration_spec.rb
+++ spec/models/evaluation/controle/attention_concentration_spec.rb
@@ -5,60 +5,46 @@ require 'rails_helper'
 describe Evaluation::Controle::AttentionConcentration do
   let(:evaluation) { double }
 
-  context "lorsqu'il n'y a pas d'erreurs ou de ratées" do
+  context "lorsqu'il n'y a pas de loupées" do
     it 'a le niveau 4' do
-      expect(evaluation).to receive(:nombre_mal_placees).and_return(0)
-      expect(evaluation).to receive(:nombre_non_triees).and_return(0)
+      expect(evaluation).to receive(:nombre_loupees).and_return(0)
       expect(
         described_class.new(evaluation).niveau
       ).to eql(Competence::NIVEAU_4)
     end
   end
 
-  context "lorsqu'il y a une pièce mal placée" do
+  context "lorsqu'il y a un loupée" do
     it 'a le niveau 3' do
-      expect(evaluation).to receive(:nombre_mal_placees).and_return(1)
-      expect(evaluation).to receive(:nombre_non_triees).and_return(0)
+      expect(evaluation).to receive(:nombre_loupees).and_return(1)
+
       expect(
         described_class.new(evaluation).niveau
       ).to eql(Competence::NIVEAU_3)
     end
   end
 
-  context "lorsqu'il y a une pièce ratée" do
-    it 'a le niveau 3' do
-      expect(evaluation).to receive(:nombre_mal_placees).and_return(0)
-      expect(evaluation).to receive(:nombre_non_triees).and_return(1)
-      expect(
-        described_class.new(evaluation).niveau
-      ).to eql(Competence::NIVEAU_3)
-    end
-  end
-
-  context "lorsqu'il y a deux pièces ratées" do
+  context "lorsqu'il y a deux pièces loupées" do
     it 'a le niveau 2' do
-      expect(evaluation).to receive(:nombre_mal_placees).and_return(0)
-      expect(evaluation).to receive(:nombre_non_triees).and_return(2)
+      expect(evaluation).to receive(:nombre_loupees).and_return(2)
       expect(
         described_class.new(evaluation).niveau
       ).to eql(Competence::NIVEAU_2)
     end
   end
 
-  context "lorsqu'il y a trois pièces ratées" do
+  context "lorsqu'il y a trois pièces loupées" do
     it 'a le niveau 1' do
-      expect(evaluation).to receive(:nombre_mal_placees).and_return(0)
-      expect(evaluation).to receive(:nombre_non_triees).and_return(3)
+      expect(evaluation).to receive(:nombre_loupees).and_return(3)
       expect(
         described_class.new(evaluation).niveau
       ).to eql(Competence::NIVEAU_1)
     end
   end
 
-  context "lorsqu'il y a quatre pièces ratées" do
+  context "lorsqu'il y a quatre pièces loupées" do
     it 'a le niveau 1' do
-      expect(evaluation).to receive(:nombre_mal_placees).and_return(1)
-      expect(evaluation).to receive(:nombre_non_triees).and_return(3)
+      expect(evaluation).to receive(:nombre_loupees).and_return(4)
       expect(
         described_class.new(evaluation).niveau
       ).to eql(Competence::NIVEAU_1)

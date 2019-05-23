3af6a2a49602ff7e21bf28e206f1e00bc324a529
Étienne Charignon <etienne.charignon@gmail.com>
2019-05-23 15:33:14 +0200

    Ajoute de l'expressivité dans cette ré-écriture de test

--- spec/models/evaluation/inventaire/perseverance_spec.rb
+++ spec/models/evaluation/inventaire/perseverance_spec.rb
@@ -2,56 +2,50 @@
 
 require 'rails_helper'
 
+class TestPerseverance
+  def initialize(entrees, rspec)
+    @rspec = rspec
+    @entrees = entrees
+    @entrees[:secondes] = @entrees[:minutes].minutes.to_i if @entrees[:minutes]
+  end
+
+  def message(niveau, entrees)
+    message = entrees[:reussite] ? 'Réussite' : 'Abandon'
+    message += " après #{entrees[:nombre_essais]} essai(s)" if entrees[:nombre_essais]
+    message += " avec #{entrees[:erreurs]} erreur(s)" if entrees[:erreurs]
+    message += " au bout de #{entrees[:minutes]} minutes" if entrees[:minutes]
+    message + " : niveau #{niveau}"
+  end
+
+  def on_evalue_a(niveau)
+    entrees = @entrees
+    @rspec.it message(niveau, @entrees) do
+      expect(evaluation).to receive(:reussite?).and_return(entrees[:reussite])
+      essai = double
+      allow(essai).to receive(:nombre_erreurs).and_return(entrees[:erreurs])
+      allow(evaluation).to receive(:essais).and_return([essai])
+      allow(evaluation).to receive(:nombre_essais_validation).and_return(entrees[:nombre_essais])
+      allow(evaluation).to receive(:temps_total).and_return(entrees[:secondes])
+      expect(described_class.new(evaluation).niveau).to eql(niveau)
+    end
+  end
+end
+
 describe Evaluation::Inventaire::Perseverance do
-  NIMPORTE_QUELLE = "n'importe quelle"
   let(:evaluation) { double }
 
-  def self.test_reussite_au_bout_d_un_temp(temp, niveau)
-    it "en réussite en #{temp} : niveau #{niveau}" do
-      expect(evaluation).to receive(:reussite?).and_return(true)
-      expect(evaluation).to receive(:temps_total).and_return(temp.minutes.to_i)
-      expect(
-        described_class.new(evaluation).niveau
-      ).to eql(niveau)
-    end
+  def self.pour(entrees)
+    TestPerseverance.new(entrees, self)
   end
 
-  test_reussite_au_bout_d_un_temp(23, Competence::NIVEAU_4)
-  test_reussite_au_bout_d_un_temp(22, Competence::NIVEAU_INDETERMINE)
+  pour(reussite: true, minutes: 23).on_evalue_a(Competence::NIVEAU_4)
+  pour(reussite: true, minutes: 22).on_evalue_a(Competence::NIVEAU_INDETERMINE)
 
-  def allow_temps_total(minutes)
-    return if minutes == NIMPORTE_QUELLE
-
-    allow(evaluation).to receive(:temps_total).and_return(minutes.minutes.to_i)
-  end
-
-  def self.message(nombre_essai_validation, minutes, nombre_erreurs, niveau)
-    "abandon après #{nombre_essai_validation} essai(s) \
-avec #{nombre_erreurs} erreur(s) au bout de #{minutes} minutes: niveau #{niveau}"
-  end
-
-  def self.test_non_perseverant(nombre_essai_validation,
-                                minutes,
-                                nombre_erreurs,
-                                niveau)
-
-    it message(nombre_essai_validation, minutes, nombre_erreurs, niveau) do
-      essai = double
-      allow(essai).to receive(:nombre_erreurs).and_return(nombre_erreurs)
-      allow(evaluation).to receive(:essais).and_return([essai])
-      expect(evaluation).to receive(:reussite?).and_return(false)
-      allow_temps_total(minutes)
-      allow(evaluation).to receive(:nombre_essais_validation).and_return(nombre_essai_validation)
-      expect(described_class.new(evaluation).niveau)
-        .to eql(niveau)
-    end
-  end
-
-  test_non_perseverant(1, NIMPORTE_QUELLE,
-                       "n'impote quel nombre d'", Competence::NIVEAU_1)
-  test_non_perseverant(2, 21, 7, Competence::NIVEAU_1)
-  test_non_perseverant(2, 21, 8, Competence::NIVEAU_INDETERMINE)
-  test_non_perseverant(2, 22, 7, Competence::NIVEAU_INDETERMINE)
-  test_non_perseverant(0, NIMPORTE_QUELLE,
-                       "n'impote quel nombre d'", Competence::NIVEAU_INDETERMINE)
+  pour(reussite: false, nombre_essais: 1).on_evalue_a(Competence::NIVEAU_1)
+  pour(reussite: false, nombre_essais: 2, minutes: 21, erreurs: 7).on_evalue_a(Competence::NIVEAU_1)
+  pour(reussite: false, nombre_essais: 2, minutes: 21, erreurs: 8)
+    .on_evalue_a(Competence::NIVEAU_INDETERMINE)
+  pour(reussite: false, nombre_essais: 2, minutes: 22, erreurs: 7)
+    .on_evalue_a(Competence::NIVEAU_INDETERMINE)
+  pour(reussite: false, nombre_essais: 0).on_evalue_a(Competence::NIVEAU_INDETERMINE)
 end

316527aae2deaea0c0f8f50167e9718bf2302252
François de Metz <francois.de_metz@beta.gouv.fr>
2019-05-24 11:15:01 +0200

    Refactore les tests de la compétence persévérance pour être plus rpsec friendly.
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

--- spec/models/evaluation/inventaire/perseverance_spec.rb
+++ spec/models/evaluation/inventaire/perseverance_spec.rb
@@ -2,50 +2,46 @@
 
 require 'rails_helper'
 
-class TestPerseverance
-  def initialize(entrees, rspec)
-    @rspec = rspec
-    @entrees = entrees
-    @entrees[:secondes] = @entrees[:minutes].minutes.to_i if @entrees[:minutes]
+RSpec::Matchers.define :evalue_a do |expected|
+  match do |actual|
+    actual.niveau == expected
   end
 
-  def message(niveau, entrees)
-    message = entrees[:reussite] ? 'Réussite' : 'Abandon'
-    message += " après #{entrees[:nombre_essais]} essai(s)" if entrees[:nombre_essais]
-    message += " avec #{entrees[:erreurs]} erreur(s)" if entrees[:erreurs]
-    message += " au bout de #{entrees[:minutes]} minutes" if entrees[:minutes]
-    message + " : niveau #{niveau}"
-  end
-
-  def on_evalue_a(niveau)
-    entrees = @entrees
-    @rspec.it message(niveau, @entrees) do
-      expect(evaluation).to receive(:reussite?).and_return(entrees[:reussite])
-      essai = double
-      allow(essai).to receive(:nombre_erreurs).and_return(entrees[:erreurs])
-      allow(evaluation).to receive(:essais).and_return([essai])
-      allow(evaluation).to receive(:nombre_essais_validation).and_return(entrees[:nombre_essais])
-      allow(evaluation).to receive(:temps_total).and_return(entrees[:secondes])
-      expect(described_class.new(evaluation).niveau).to eql(niveau)
-    end
+  failure_message do |actual|
+    "expected that #{actual.niveau.inspect} to equal #{expected}"
   end
 end
 
 describe Evaluation::Inventaire::Perseverance do
   let(:evaluation) { double }
 
-  def self.pour(entrees)
-    TestPerseverance.new(entrees, self)
+  def pour(reussite: nil, nombre_essais: nil, minutes: nil, erreurs: nil)
+    secondes = minutes.minutes.to_i if minutes
+    expect(evaluation).to receive(:reussite?).and_return(reussite)
+    essai = double
+    allow(essai).to receive(:nombre_erreurs).and_return(erreurs)
+    allow(evaluation).to receive(:essais).and_return([essai])
+    allow(evaluation).to receive(:nombre_essais_validation).and_return(nombre_essais)
+    allow(evaluation).to receive(:temps_total).and_return(secondes)
+    described_class.new(evaluation)
   end
 
-  pour(reussite: true, minutes: 23).on_evalue_a(Competence::NIVEAU_4)
-  pour(reussite: true, minutes: 22).on_evalue_a(Competence::NIVEAU_INDETERMINE)
+  it { expect(pour(reussite: true, minutes: 23)).to evalue_a(Competence::NIVEAU_4) }
+  it { expect(pour(reussite: true, minutes: 22)).to evalue_a(Competence::NIVEAU_INDETERMINE) }
 
-  pour(reussite: false, nombre_essais: 1).on_evalue_a(Competence::NIVEAU_1)
-  pour(reussite: false, nombre_essais: 2, minutes: 21, erreurs: 7).on_evalue_a(Competence::NIVEAU_1)
-  pour(reussite: false, nombre_essais: 2, minutes: 21, erreurs: 8)
-    .on_evalue_a(Competence::NIVEAU_INDETERMINE)
-  pour(reussite: false, nombre_essais: 2, minutes: 22, erreurs: 7)
-    .on_evalue_a(Competence::NIVEAU_INDETERMINE)
-  pour(reussite: false, nombre_essais: 0).on_evalue_a(Competence::NIVEAU_INDETERMINE)
+  it { expect(pour(reussite: false, nombre_essais: 0)).to evalue_a(Competence::NIVEAU_INDETERMINE) }
+  it { expect(pour(reussite: false, nombre_essais: 1)).to evalue_a(Competence::NIVEAU_1) }
+
+  it {
+    expect(pour(reussite: false, nombre_essais: 2, minutes: 21, erreurs: 7))
+      .to evalue_a(Competence::NIVEAU_1)
+  }
+  it {
+    expect(pour(reussite: false, nombre_essais: 2, minutes: 21, erreurs: 8))
+      .to evalue_a(Competence::NIVEAU_INDETERMINE)
+  }
+  it {
+    expect(pour(reussite: false, nombre_essais: 2, minutes: 22, erreurs: 7))
+      .to evalue_a(Competence::NIVEAU_INDETERMINE)
+  }
 end

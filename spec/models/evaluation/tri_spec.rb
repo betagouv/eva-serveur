23d0de3b7964a1e826d923b7ab75dd595d3de9a0
François de Metz <francois.de_metz@beta.gouv.fr>
2019-05-27 15:27:15 +0200

    Ajoute la classe d'évaluation basique du tri.

---
+++ spec/models/evaluation/tri_spec.rb
@@ -0,0 +1,54 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Evaluation::Tri do
+  let(:evaluation) { described_class.new(evenements) }
+
+  context 'avec toutes les pieces enregistrées' do
+    let(:evenements) do
+      [
+        build(:evenement_demarrage),
+        build(:evenement_piece_mal_placee),
+        *Array.new(Evaluation::Tri::PIECES_TOTAL) do
+          build(:evenement_piece_bien_placee)
+        end
+      ]
+    end
+    it { expect(evaluation).to be_termine }
+  end
+
+  context 'avec pas toutes les pieces enregistrées' do
+    let(:evenements) do
+      [
+        build(:evenement_demarrage),
+        *Array.new(4) do
+          build(:evenement_piece_bien_placee)
+        end
+      ]
+    end
+    it { expect(evaluation).to_not be_termine }
+  end
+
+  context 'compter les pièces bien placées' do
+    let(:evenements) do
+      [
+        build(:evenement_piece_bien_placee),
+        build(:evenement_piece_mal_placee)
+      ]
+    end
+
+    it { expect(evaluation.nombre_bien_placees).to eq(1) }
+  end
+
+  context 'compter les pièces mal placées' do
+    let(:evenements) do
+      [
+        build(:evenement_piece_mal_placee),
+        build(:evenement_piece_mal_placee)
+      ]
+    end
+
+    it { expect(evaluation.nombre_mal_placees).to eq(2) }
+  end
+end

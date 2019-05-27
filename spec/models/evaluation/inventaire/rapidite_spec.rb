8e70f71fd20d41a1bfc0909947cc6232f5ee82a0
Étienne Charignon <etienne.charignon@gmail.com>
2019-05-27 14:09:49 +0200

    [NETTOYAGE] utilise le comparateur `evalue_a` dans les tests rapidité du inventaire

--- spec/models/evaluation/inventaire/rapidite_spec.rb
+++ spec/models/evaluation/inventaire/rapidite_spec.rb
@@ -5,26 +5,20 @@ require 'rails_helper'
 describe Evaluation::Inventaire::Rapidite do
   let(:evaluation) { double }
 
-  def self.reussite_avec_temps(secondes, niveau)
-    it "en réussite et en #{secondes / 60} minutes: niveau #{niveau}" do
-      expect(evaluation).to receive(:reussite?).and_return(true)
-      expect(evaluation).to receive(:temps_total).and_return(secondes.to_i)
-      expect(described_class.new(evaluation).niveau).to eql(niveau)
-    end
+  def pour(reussite: nil, minutes: nil)
+    expect(evaluation).to receive(:reussite?).and_return(reussite)
+    secondes = minutes.minutes.to_i if minutes
+    allow(evaluation).to receive(:temps_total).and_return(secondes)
+    described_class.new(evaluation)
   end
 
-  reussite_avec_temps(0.minutes, Competence::NIVEAU_4)
-  reussite_avec_temps(10.minutes, Competence::NIVEAU_4)
-  reussite_avec_temps(11.minutes, Competence::NIVEAU_3)
-  reussite_avec_temps(15.minutes, Competence::NIVEAU_3)
-  reussite_avec_temps(16.minutes, Competence::NIVEAU_2)
-  reussite_avec_temps(30.minutes, Competence::NIVEAU_2)
-  reussite_avec_temps(31.minutes, Competence::NIVEAU_1)
+  it { expect(pour(reussite: false)).to evalue_a(Competence::NIVEAU_INDETERMINE) }
 
-  it 'en cas de non réussite: niveau indéterminé' do
-    expect(evaluation).to receive(:reussite?).and_return(false)
-    expect(
-      described_class.new(evaluation).niveau
-    ).to eql(Competence::NIVEAU_INDETERMINE)
-  end
+  it { expect(pour(reussite: true, minutes: 0)).to evalue_a(Competence::NIVEAU_4) }
+  it { expect(pour(reussite: true, minutes: 10)).to evalue_a(Competence::NIVEAU_4) }
+  it { expect(pour(reussite: true, minutes: 11)).to evalue_a(Competence::NIVEAU_3) }
+  it { expect(pour(reussite: true, minutes: 15)).to evalue_a(Competence::NIVEAU_3) }
+  it { expect(pour(reussite: true, minutes: 16)).to evalue_a(Competence::NIVEAU_2) }
+  it { expect(pour(reussite: true, minutes: 30)).to evalue_a(Competence::NIVEAU_2) }
+  it { expect(pour(reussite: true, minutes: 31)).to evalue_a(Competence::NIVEAU_1) }
 end

58701e6cfd18e7ea4fd5e56fe26a406fda45ea29
Étienne Charignon <etienne.charignon@gmail.com>
2019-05-27 13:44:32 +0200

    [NETTOYAGE] utilise le comparateur `evalue_a` dans les tests perseverance du contrôle

--- spec/models/evaluation/inventaire/perseverance_spec.rb
+++ spec/models/evaluation/inventaire/perseverance_spec.rb
@@ -2,16 +2,6 @@
 
 require 'rails_helper'
 
-RSpec::Matchers.define :evalue_a do |expected|
-  match do |actual|
-    actual.niveau == expected
-  end
-
-  failure_message do |actual|
-    "expected that #{actual.niveau.inspect} to equal #{expected}"
-  end
-end
-
 describe Evaluation::Inventaire::Perseverance do
   let(:evaluation) { double }
 

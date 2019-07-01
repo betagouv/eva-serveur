7ae8dc345dbe3a8e7d9bcdb80689b3c0ff221110
François de Metz <francois.de_metz@beta.gouv.fr>
2019-07-01 18:49:38 +0200

    Corrige l'affichage de l'efficience dans la barre de progression.

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -13,4 +13,14 @@ describe ApplicationHelper do
         .to eql(I18n.t('admin.evaluations.evaluation.indetermine'))
     end
   end
+
+  describe '#progression_efficience' do
+    it 'retourne la valeur' do
+      expect(helper.progression_efficience(5.3)).to eql(5.3)
+    end
+
+    it "retourne 0 si l'efficience est indéterminée" do
+      expect(helper.progression_efficience(::Competence::NIVEAU_INDETERMINE)).to eql(0)
+    end
+  end
 end

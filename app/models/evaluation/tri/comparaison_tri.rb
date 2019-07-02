b4f23c46d94e4ebfb1b1f6ac6cf854a1481af365
François de Metz <francois.de_metz@beta.gouv.fr>
2019-07-02 09:50:33 +0200

    Vérifie que la situation est terminé pour comparaison et tri.

--- app/models/evaluation/tri/comparaison_tri.rb
+++ app/models/evaluation/tri/comparaison_tri.rb
@@ -4,15 +4,13 @@ module Evaluation
   class Tri
     class ComparaisonTri < Evaluation::Competence::Base
       def niveau
+        return ::Competence::NIVEAU_INDETERMINE unless @evaluation.termine?
+
         case @evaluation.nombre_mal_placees
-        when 0
-          ::Competence::NIVEAU_4
-        when 1..2
-          ::Competence::NIVEAU_3
-        when 3
-          ::Competence::NIVEAU_2
-        else
-          ::Competence::NIVEAU_1
+        when 0 then ::Competence::NIVEAU_4
+        when 1..2 then ::Competence::NIVEAU_3
+        when 3 then ::Competence::NIVEAU_2
+        else ::Competence::NIVEAU_1
         end
       end
     end

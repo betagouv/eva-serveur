e943f5ffafcf776a0f408cb8c83d1ff2fd1daa76
Étienne Charignon <etienne.charignon@gmail.com>
2019-07-05 16:31:40 +0200

    Affiche le nom de l'évalué sur la restitution globale
    Co-authored-by: François de Metz <francois.de_metz@beta.gouv.fr>

--- app/admin/restitutions_globales.rb
+++ app/admin/restitutions_globales.rb
@@ -7,10 +7,11 @@ ActiveAdmin.register_page 'Restitution Globale' do
     helper_method :restitution_globale
 
     def restitution_globale
+      evaluation = Evenement.find(params[:restitution_ids].first).evaluation
       restitutions = params[:restitution_ids].map do |id|
         FabriqueRestitution.depuis_evenement_id id
       end
-      Restitution::Globale.new restitutions: restitutions
+      Restitution::Globale.new restitutions: restitutions, evaluation: evaluation
     end
   end
 

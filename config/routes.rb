d9fdcbd8c69d09d8056eae97fbb32ed924ecfbb3
François de Metz <francois.de_metz@beta.gouv.fr>
2019-07-05 10:42:57 +0200

    Ajoute l'API d'évaluations.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :administrateurs, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   namespace :api do
+    resources :evaluations, only: [:create]
     resources :evenements
   end
 end

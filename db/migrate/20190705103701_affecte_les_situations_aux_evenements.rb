8cf0c67d5ad62c3af83dd39c9be6a73b67b8949c
Stéphane Hanser <shanser@captive.fr>
2019-07-05 12:42:10 +0200

    relie les situations aux événements existants

---
+++ db/migrate/20190705103701_affecte_les_situations_aux_evenements.rb
@@ -0,0 +1,12 @@
+class AffecteLesSituationsAuxEvenements < ActiveRecord::Migration[5.2]
+  def change
+    Evenement.find_each do |evenement|
+      nom_technique = evenement['situation']
+      situation = Situation.where(nom_technique: nom_technique).first_or_create do |situation|
+        situation.libelle = nom_technique
+      end
+      evenement.situation = situation
+      evenement.save!
+    end
+  end
+end

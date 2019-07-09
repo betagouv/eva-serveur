7e91699bb7634facba5421b931679393f3ede7b6
François de Metz <francois.de_metz@beta.gouv.fr>
2019-07-09 09:56:58 +0200

    Associe les anciens événéments a des évaluations.

---
+++ db/migrate/20190709072254_affecte_evaluation_aux_evenements.rb
@@ -0,0 +1,9 @@
+class AffecteEvaluationAuxEvenements < ActiveRecord::Migration[5.2]
+  def change
+    Evenement.select(:utilisateur).distinct.each do |evenement|
+      next if evenement.utilisateur.nil?
+      evaluation = Evaluation.create(nom: evenement.utilisateur)
+      Evenement.where(utilisateur: evenement.utilisateur).update_all(evaluation_id: evaluation.id)
+    end
+  end
+end

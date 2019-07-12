6f3bb9859ae989d1d636d0666c61c19ff40ea37e
Stéphane Hanser <shanser@captive.fr>
2019-07-12 11:38:50 +0200

    génère un code unique pour les campagnes

--- app/models/campagne.rb
+++ app/models/campagne.rb
@@ -4,8 +4,20 @@ class Campagne < ApplicationRecord
   has_many :evaluations
   validates :libelle, presence: true
   validates :code, presence: true, uniqueness: true
+  before_validation :genere_code_unique
 
   def display_name
     libelle
+  end
+
+  private
+
+  def genere_code_unique
+    return if self[:code].present?
+
+    loop do
+      self[:code] = SecureRandom.hex 3
+      break if Campagne.where(code: self[:code]).none?
+    end
   end
 end

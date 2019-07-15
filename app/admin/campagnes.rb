27059333245bd5d819573aa9a9db0694bbae4205
François de Metz <francois.de_metz@beta.gouv.fr>
2019-07-15 18:36:07 +0200

    Ajoute l'édition de SituationConfiguration dans activeadmin.

--- app/admin/campagnes.rb
+++ app/admin/campagnes.rb
@@ -3,6 +3,10 @@
 ActiveAdmin.register Campagne do
   permit_params :libelle, :code, :questionnaire_id
 
+  sidebar 'Configuration', only: %i[show edit] do
+    link_to 'Configuration', admin_campagne_situations_configurations_path(resource)
+  end
+
   show do
     render partial: 'show', locals: { evaluations: resource.evaluations }
   end

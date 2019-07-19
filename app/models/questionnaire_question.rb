2befb9dc05f70736d04b0d878a4f5165c3bd8b5d
François de Metz <francois.de_metz@beta.gouv.fr>
2019-07-19 10:45:47 +0200

    Limite une seule même question par questionnaire.

--- app/models/questionnaire_question.rb
+++ app/models/questionnaire_question.rb
@@ -4,4 +4,6 @@ class QuestionnaireQuestion < ApplicationRecord
   belongs_to :question
 
   acts_as_list scope: :questionnaire_id
+
+  validates :question_id, uniqueness: { scope: :questionnaire_id }
 end
---
+++ spec/models/questionnaire_question_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe QuestionnaireQuestion do
+  context 'validation unicité' do
+    let(:question) { create(:question) }
+
+    subject { described_class.new(question: question) }
+
+    it { should validate_uniqueness_of(:question_id).scoped_to(:questionnaire_id) }
+  end
+end

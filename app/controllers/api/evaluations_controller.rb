03671bf632cdae82167525ba4f23dd523bdd551e
François de Metz <francois.de_metz@beta.gouv.fr>
2019-07-19 10:38:56 +0200

    Ajoute l'api d'évaluation.

--- app/controllers/api/evaluations_controller.rb
+++ app/controllers/api/evaluations_controller.rb
@@ -2,6 +2,10 @@
 
 module Api
   class EvaluationsController < ActionController::API
+    rescue_from ActiveRecord::RecordNotFound do
+      render status: :not_found
+    end
+
     def create
       evaluation = Evaluation.new(EvaluationParams.from(params))
       if evaluation.save
@@ -9,6 +13,17 @@ module Api
       else
         render json: evaluation.errors, status: 422
       end
+    end
+
+    def show
+      evaluation = Evaluation.find(params[:id])
+
+      questions = evaluation.campagne.questionnaire
+                    &.questionnaires_questions
+                    &.joins(:question)
+                    &.map(&:question) || []
+
+      render json: { questions: questions }
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :administrateurs, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
+
   namespace :api do
-    resources :evaluations, only: [:create]
+    resources :evaluations, only: [:create, :show]
     resources :evenements
   end
 end

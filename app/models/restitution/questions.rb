5a65e93bca02911198b08ce8565085697b58df05
François de Metz <francois.de_metz@beta.gouv.fr>
2019-07-22 16:27:01 +0200

    Affiche les réponses et l'état correct ou non.

--- app/models/restitution/questions.rb
+++ app/models/restitution/questions.rb
@@ -7,11 +7,38 @@ module Restitution
     }.freeze
 
     def termine?
-      reponses.size == campagne.questionnaire.questionnaires_questions.size
+      reponses.size == questions.size
+    end
+
+    def questions_et_reponses
+      questions_repondues
+        .map do |question|
+        {
+          question: question,
+          reponse: trouve_reponse(question.id)
+        }
+      end
     end
 
     def reponses
       evenements.find_all { |e| e.nom == EVENEMENT[:REPONSE] }
+    end
+
+    private
+
+    def questions
+      campagne.questionnaire.questions
+    end
+
+    def questions_repondues
+      questions_ids = reponses.collect { |r| r.donnees['question'] }
+      questions.where(id: questions_ids)
+    end
+
+    def trouve_reponse(question_id)
+      reponses.find do |evenement|
+        evenement.donnees['question'] == question_id
+      end.donnees['reponse']
     end
   end
 end

9475cf62778c31b25e93736b8df14120b3272abc
François de Metz <francois.de_metz@beta.gouv.fr>
2019-07-22 11:04:54 +0200

    Serialise l'illustration dans les questions.

--- app/models/question_redaction_note.rb
+++ app/models/question_redaction_note.rb
@@ -4,6 +4,9 @@ class QuestionRedactionNote < Question
   def as_json(_options = nil)
     json = slice(:id, :intitule, :entete_reponse, :expediteur, :message, :objet_reponse)
     json['type'] = 'redaction_note'
+    if illustration.attached?
+      json['illustration'] = Rails.application.routes.url_helpers.url_for(illustration)
+    end
     json
   end
 end

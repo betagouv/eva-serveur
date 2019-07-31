d34b0e43a3d81d9ae125664714491285e6ab792f
Stéphane Hanser <shanser@captive.fr>
2019-07-31 10:18:40 +0200

    déplace la sélection des comptes dans le controller et non plus dans un helper de vue
    Co-authored-by: Clément Morisset <clement.morisset@beta.gouv.fr>

--- app/admin/evaluation.rb
+++ app/admin/evaluation.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 ActiveAdmin.register Evaluation do
-  filter :campagne, collection: proc { evaluations_visibles(current_compte) }
+  filter :campagne, collection: proc { evaluations_visibles }
   filter :created_at
 
   index do
@@ -11,6 +11,16 @@ ActiveAdmin.register Evaluation do
     column '' do |evaluation|
       span link_to t('.restitution'),
                    admin_restitutions_path(evaluation_id: evaluation.id)
+    end
+  end
+
+  controller do
+    helper_method :evaluations_visibles
+
+    private
+
+    def evaluations_visibles
+      current_compte.administrateur? ? Campagne.all : Campagne.where(compte: current_compte)
     end
   end
 end

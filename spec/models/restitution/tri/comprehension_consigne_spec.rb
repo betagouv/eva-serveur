bfd2bf659b4fa7dabfab22dd1cdfef6d7f50740d
Étienne Charignon <etienne.charignon@gmail.com>
2019-08-27 16:45:59 +0200

    [Tri] corrige la compréhension de la consigne
    On considère qu'une personne n'a pas compris si elle ré-écoute au moins une
    fois et qu'elle abandonne rappidement après avoir fait au moins une erreur.

--- spec/models/restitution/tri/comprehension_consigne_spec.rb
+++ spec/models/restitution/tri/comprehension_consigne_spec.rb
@@ -5,8 +5,11 @@ require 'rails_helper'
 describe Restitution::Tri::ComprehensionConsigne do
   let(:restitution) { double }
 
-  def pour(termine: nil, erreurs: nil, relectures: nil)
+  def pour(termine: nil, jouees: nil, erreurs: nil, relectures: nil)
     allow(restitution).to receive(:termine?).and_return(termine)
+    bien_placees = jouees || 0
+    bien_placees -= erreurs if erreurs
+    allow(restitution).to receive(:nombre_bien_placees).and_return(bien_placees)
     allow(restitution).to receive(:nombre_mal_placees).and_return(erreurs)
     allow(restitution).to receive(:nombre_rejoue_consigne).and_return(relectures)
     described_class.new(restitution)
@@ -15,13 +18,24 @@ describe Restitution::Tri::ComprehensionConsigne do
   it { expect(pour(termine: true, erreurs: 7)).to evalue_a(Competence::NIVEAU_4) }
   it { expect(pour(termine: true, erreurs: 8)).to evalue_a(Competence::NIVEAU_INDETERMINE) }
 
-  it { expect(pour(termine: false, erreurs: 7, relectures: 2)).to evalue_a(Competence::NIVEAU_1) }
   it do
-    expect(pour(termine: false, erreurs: 8, relectures: 2))
+    expect(pour(termine: false, jouees: 7, erreurs: 1, relectures: 1))
+      .to evalue_a(Competence::NIVEAU_1)
+  end
+  it do
+    expect(pour(termine: false, jouees: 7, erreurs: 1, relectures: 2))
+      .to evalue_a(Competence::NIVEAU_1)
+  end
+  it do
+    expect(pour(termine: false, jouees: 7, erreurs: 0, relectures: 1))
       .to evalue_a(Competence::NIVEAU_INDETERMINE)
   end
   it do
-    expect(pour(termine: false, erreurs: 7, relectures: 1))
+    expect(pour(termine: false, jouees: 7, erreurs: 1, relectures: 0))
+      .to evalue_a(Competence::NIVEAU_INDETERMINE)
+  end
+  it do
+    expect(pour(termine: false, jouees: 8, erreurs: 1, relectures: 1))
       .to evalue_a(Competence::NIVEAU_INDETERMINE)
   end
 end

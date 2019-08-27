3f1b37ab310f34a2174247c441686df19d9493fc
Étienne Charignon <etienne.charignon@gmail.com>
2019-08-27 16:45:59 +0200

    [Tri] corrige la compréhension de la consigne
    On considère qu'une personne n'a pas compris si elle ré-écoute au moins une
    fois et qu'elle abandonne rappidement après avoir fait au moins une erreur.

--- app/models/restitution/tri/comprehension_consigne.rb
+++ app/models/restitution/tri/comprehension_consigne.rb
@@ -4,16 +4,32 @@ module Restitution
   class Tri
     class ComprehensionConsigne < Restitution::Competence::Base
       attr_reader :restitution
-      delegate :termine?, :nombre_mal_placees, :nombre_rejoue_consigne, to: :restitution
+      delegate :termine?, to: :restitution
+      delegate :nombre_bien_placees, :nombre_mal_placees, to: :restitution
+      delegate :nombre_rejoue_consigne, to: :restitution
 
       def niveau
-        if termine? && nombre_mal_placees < 8
-          ::Competence::NIVEAU_4
-        elsif !termine? && nombre_mal_placees < 8 && nombre_rejoue_consigne >= 2
+        if termine?
+          evalue_termine
+        elsif consigne_mal_comprise
           ::Competence::NIVEAU_1
         else
           ::Competence::NIVEAU_INDETERMINE
         end
+      end
+
+      def evalue_termine
+        if nombre_mal_placees < 8
+          ::Competence::NIVEAU_4
+        else
+          ::Competence::NIVEAU_INDETERMINE
+        end
+      end
+
+      def consigne_mal_comprise
+        nombre_bien_placees + nombre_mal_placees < 8 &&
+          nombre_mal_placees&.positive? &&
+          nombre_rejoue_consigne.positive?
       end
     end
   end

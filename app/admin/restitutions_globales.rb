8d123ab23bc41566540c6fd7d5b3d6dd120e4150
Clément MORISSET <cmorisset@captive.fr>
2019-09-06 14:49:06 +0200

    pouvoir exporter la restitution globale en PDF

--- app/admin/restitutions_globales.rb
+++ app/admin/restitutions_globales.rb
@@ -3,8 +3,12 @@
 ActiveAdmin.register_page 'Restitution Globale' do
   menu false
 
+  action_item :telecharger do
+    link_to 'Exporter en PDF', lien_pdf, target: '_blank'
+  end
+
   controller do
-    helper_method :restitution_globale
+    helper_method :restitution_globale, :lien_pdf
 
     def restitution_globale
       evaluation = Evenement.find(params[:restitution_ids].first).evaluation
@@ -12,6 +16,14 @@ ActiveAdmin.register_page 'Restitution Globale' do
         FabriqueRestitution.depuis_evenement_id id
       end
       Restitution::Globale.new restitutions: restitutions, evaluation: evaluation
+    end
+
+    def restitution_ids
+      params[:restitution_ids].join('-')
+    end
+
+    def lien_pdf
+      "/restitution_globale/show.pdf?restitution_ids=#{restitution_ids}"
     end
   end
 

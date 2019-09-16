dae468aa7c95ffa2d957b8c06b342b64ec26ef2d
Stéphane Hanser <shanser@captive.fr>
2019-09-16 12:03:45 +0200

    affiche la restituation globale dans l'évaluation
    Co-authored-by: Clément Morisset <clement.morisset@beta.gouv.fr>

--- app/admin/evaluation.rb
+++ app/admin/evaluation.rb
@@ -12,15 +12,34 @@ ActiveAdmin.register Evaluation do
       span link_to t('.restitution'),
                    admin_restitutions_path(evaluation_id: evaluation.id)
     end
+    actions
   end
 
   controller do
-    helper_method :evaluations_visibles
+    helper_method :evaluations_visibles, :restitutions, :restitution_globale
 
     private
 
     def evaluations_visibles
       current_compte.administrateur? ? Campagne.all : Campagne.where(compte: current_compte)
     end
+
+    def restitutions
+      Evenement.where(nom: 'demarrage', evaluation_id: params[:id])
+    end
+
+    def restitution_globale
+      restitution_ids = restitutions.pluck(:id)
+      evaluation = Evenement.find(restitution_ids.first).evaluation
+      restitutions = restitution_ids.map do |id|
+        FabriqueRestitution.depuis_evenement_id id
+      end
+      Restitution::Globale.new restitutions: restitutions, evaluation: evaluation
+    end
+  end
+
+  show do
+    default_main_content
+    render partial: 'show'
   end
 end

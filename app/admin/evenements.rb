d12105410d0edfb783e44bf316c22a59ab11f860
Stéphane Hanser <shanser@captive.fr>
2019-09-27 12:38:04 +0200

    trie les événements dans l'admin
    Co-authored-by: Clément Morisset <clement.morisset@beta.gouv.fr>

--- app/admin/evenements.rb
+++ app/admin/evenements.rb
@@ -3,6 +3,7 @@
 ActiveAdmin.register Evenement do
   menu if: proc { can? :manage, Compte }
   permit_params :donnees, :session_id, :situation, :nom, :date
+  config.sort_order = 'created_at_desc'
 
   filter :situation
   filter :session_id

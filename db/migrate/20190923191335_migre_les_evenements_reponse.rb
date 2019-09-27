784890861ff32f476daa73551336d023efef02c4
Stéphane Hanser <shanser@captive.fr>
2019-09-27 12:13:52 +0200

    Migre les événements réponses pour référencer des uid

---
+++ db/migrate/20190923191335_migre_les_evenements_reponse.rb
@@ -0,0 +1,16 @@
+class MigreLesEvenementsReponse < ActiveRecord::Migration[6.0]
+  def change
+    Evenement.where(nom: 'reponse').find_each do |evenement|
+      anciennes_donnees = evenement.donnees
+      nouvelles_donnees = anciennes_donnees.clone
+      if anciennes_donnees['question'].is_a? Integer
+        nouvelles_donnees['question'] = Question.find(anciennes_donnees['question']).uuid
+      end
+      if anciennes_donnees['reponse'].is_a? Integer
+        nouvelles_donnees['reponse'] = Choix.find(anciennes_donnees['reponse']).uuid
+      end
+      evenement.donnees = nouvelles_donnees
+      evenement.save!(validate: false)
+    end
+  end
+end

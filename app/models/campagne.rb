00320aa7022b8acf70eb15cadab0f412748c0338
Étienne Charignon <etienne.charignon@gmail.com>
2019-10-07 10:11:25 +0200

    Trie les évaluations dans la campagne par ordre antéchronologique
    antéchronologique ça veut dire dans l'ordre inverse de la date de création.
    
    Co-authored-by: François de Metz <francois.de_metz@beta.gouv.fr>

--- app/models/campagne.rb
+++ app/models/campagne.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Campagne < ApplicationRecord
-  has_many :evaluations
+  has_many :evaluations, -> { order(created_at: :desc) }
   has_many :situations_configurations, -> { order(position: :asc) }
   has_many :situations, through: :situations_configurations
   belongs_to :questionnaire, optional: true

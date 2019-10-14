d492c65d9b49ad284be426bc5d577f7e57d383bc
Stéphane Hanser <shanser@captive.fr>
2019-10-14 15:13:42 +0200

    Affiche dans l'admin le rapport de la sécurité
    prend en compte pour le moment le nombre de dangers bien qualifiés et le fait que la situation est terminée
    
    Co-authored-by: Clément Morisset <clement.morisset@beta.gouv.fr>

--- app/models/restitution/securite.rb
+++ app/models/restitution/securite.rb
@@ -3,19 +3,32 @@
 module Restitution
   class Securite < Base
     BONNE_REPONSE = 'bonne'
+    DANGERS_TOTAL = 5
 
     EVENEMENT = {
       QUALIFICATION_DANGER: 'qualificationDanger'
     }.freeze
 
-    def nombre_bonnes_qualifications
-      evenements.count { |e| e.nom == EVENEMENT[:QUALIFICATION_DANGER] && bonne_reponse?(e) }
+    def termine?
+      qualifications_par_danger.count == DANGERS_TOTAL
+    end
+
+    def nombre_bien_qualifies
+      qualifications_finales = qualifications_par_danger.map do |_danger, qualifications|
+        qualifications.max_by(&:created_at)
+      end
+      qualifications_finales.count { |e| bonne_reponse?(e) }
     end
 
     private
 
     def bonne_reponse?(evenement)
       evenement.donnees['reponse'] == BONNE_REPONSE
+    end
+
+    def qualifications_par_danger
+      qualifications_dangers = evenements.select { |e| e.nom == EVENEMENT[:QUALIFICATION_DANGER] }
+      qualifications_dangers.group_by { |e| e.donnees['danger'] }
     end
   end
 end

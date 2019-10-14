c5c910ec4c73e05a30521489212288629cb9e37c
François de Metz <francois.de_metz@beta.gouv.fr>
2019-10-14 14:50:29 +0200

    Ajoute des compteurs du nombre d'évaluations dans le dashboard.

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,31 +4,29 @@ ActiveAdmin.register_page 'Dashboard' do
   menu priority: 1, label: proc { I18n.t('active_admin.dashboard') }
 
   content title: proc { I18n.t('active_admin.dashboard') } do
-    div class: 'blank_slate_container', id: 'dashboard_default_message' do
-      span class: 'blank_slate' do
-        span I18n.t('active_admin.dashboard_welcome.welcome')
-        small I18n.t('active_admin.dashboard_welcome.call_to_action')
-      end
-    end
+    campagnes = if current_compte.administrateur?
+                  Campagne.all
+                else
+                  Campagne.where(compte: current_compte)
+                end
 
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    debut_aujourdhui = Date.current.beginning_of_day
+    interval_hier = Date.yesterday.beginning_of_day..debut_aujourdhui
+    nombre_evaluations_total = campagnes.sum(:nombre_evaluations)
+    nombre_evaluations_hier = campagnes
+                              .joins(:evaluations)
+                              .where('evaluations.created_at' => interval_hier)
+                              .count
+    nombre_evaluations_aujourdhui = campagnes
+                                    .joins(:evaluations)
+                                    .where('evaluations.created_at > ?', debut_aujourdhui)
+                                    .count
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+    render partial: 'dashboard',
+           locals: {
+             total: nombre_evaluations_total,
+             hier: nombre_evaluations_hier,
+             aujourdhui: nombre_evaluations_aujourdhui
+           }
   end
 end

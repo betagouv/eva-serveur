8cfe236a0f14a7bd466af80d051b143d139888df
Clément MORISSET <cmorisset@captive.fr>
2019-10-14 11:28:42 +0200

    Ajoute colonne nombre_evaluation à la table Campagne
    Co-authored-by: Stéphane Hanser <stephane.hanser@beta.gouv.fr>

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -2,7 +2,7 @@
 
 class Evaluation < ApplicationRecord
   validates :nom, presence: true
-  belongs_to :campagne
+  belongs_to :campagne, counter_cache: :nombre_evaluations
 
   def display_name
     nom
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_24_080332) do
+ActiveRecord::Schema.define(version: 2019_10_14_091938) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2019_09_24_080332) do
     t.datetime "updated_at", null: false
     t.uuid "questionnaire_id"
     t.uuid "compte_id"
+    t.integer "nombre_evaluations", default: 0
     t.index ["compte_id"], name: "index_campagnes_on_compte_id"
     t.index ["questionnaire_id"], name: "index_campagnes_on_questionnaire_id"
   end

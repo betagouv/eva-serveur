4cba8359544327cc1628ef2fa4015d1525b13360
François de Metz <francois.de_metz@beta.gouv.fr>
2019-10-23 14:40:24 +0200

    Utilise les événements hors entrainements pour les mesures.

--- spec/models/restitution/securite_spec.rb
+++ spec/models/restitution/securite_spec.rb
@@ -14,18 +14,24 @@ describe Restitution::Securite do
 
     context 'tous les dangers qualifiés' do
       let(:evenements) do
-        Array.new(Restitution::Securite::DANGERS_TOTAL) do |index|
-          build(:evenement_qualification_danger, donnees: { danger: "danger-#{index}" })
-        end
+        [
+          build(:evenement_demarrage),
+          Array.new(Restitution::Securite::DANGERS_TOTAL) do |index|
+            build(:evenement_qualification_danger, donnees: { danger: "danger-#{index}" })
+          end
+        ].flatten
       end
       it { expect(restitution).to be_termine }
     end
 
     context 'ignore les requalifications de danger' do
       let(:evenements) do
-        Array.new(Restitution::Securite::DANGERS_TOTAL) do
-          build(:evenement_qualification_danger, donnees: { danger: 'danger' })
-        end
+        [
+          build(:evenement_demarrage),
+          Array.new(Restitution::Securite::DANGERS_TOTAL) do
+            build(:evenement_qualification_danger, donnees: { danger: 'danger' })
+          end
+        ].flatten
       end
       it { expect(restitution).to_not be_termine }
     end
@@ -38,18 +44,25 @@ describe Restitution::Securite do
     end
 
     context 'avec bonne qualification' do
-      let(:evenements) { [build(:evenement_qualification_danger, :bon)] }
+      let(:evenements) do
+        [build(:evenement_demarrage),
+         build(:evenement_qualification_danger, :bon)]
+      end
       it { expect(restitution.nombre_bien_qualifies).to eq 1 }
     end
 
     context 'ignore les mauvaises qualifications' do
-      let(:evenements) { [build(:evenement_qualification_danger, :mauvais)] }
+      let(:evenements) do
+        [build(:evenement_demarrage),
+         build(:evenement_qualification_danger, :mauvais)]
+      end
       it { expect(restitution.nombre_bien_qualifies).to eq 0 }
     end
 
     context 'prend en compte la requalification' do
       let(:evenements) do
-        [build(:evenement_qualification_danger,
+        [build(:evenement_demarrage),
+         build(:evenement_qualification_danger,
                donnees: { reponse: 'mauvais', danger: 'danger' }, created_at: 1.minute.ago),
          build(:evenement_qualification_danger,
                donnees: { reponse: 'bonne', danger: 'danger' }, created_at: 2.minutes.ago)]
@@ -66,21 +79,24 @@ describe Restitution::Securite do
 
     context 'avec bonne identification' do
       let(:evenements) do
-        [build(:evenement_identification_danger, donnees: { reponse: 'oui', danger: 'danger' })]
+        [build(:evenement_demarrage),
+         build(:evenement_identification_danger, donnees: { reponse: 'oui', danger: 'danger' })]
       end
       it { expect(restitution.nombre_dangers_identifies).to eq 1 }
     end
 
     context 'ignore les réponses négatives' do
       let(:evenements) do
-        [build(:evenement_identification_danger, donnees: { reponse: 'non' })]
+        [build(:evenement_demarrage),
+         build(:evenement_identification_danger, donnees: { reponse: 'non' })]
       end
       it { expect(restitution.nombre_dangers_identifies).to eq 0 }
     end
 
     context 'ignore les identifications de zone sans danger' do
       let(:evenements) do
-        [build(:evenement_identification_danger, donnees: { reponse: 'oui' })]
+        [build(:evenement_demarrage),
+         build(:evenement_identification_danger, donnees: { reponse: 'oui' })]
       end
       it { expect(restitution.nombre_dangers_identifies).to eq 0 }
     end
@@ -94,7 +110,8 @@ describe Restitution::Securite do
 
     context 'deux qualifications du même danger' do
       let(:evenements) do
-        [build(:evenement_qualification_danger,
+        [build(:evenement_demarrage),
+         build(:evenement_qualification_danger,
                donnees: { reponse: 'mauvais', danger: 'danger' }),
          build(:evenement_qualification_danger,
                donnees: { reponse: 'bonne', danger: 'danger' })]
@@ -111,7 +128,8 @@ describe Restitution::Securite do
 
     context "avec des dangers identifiés en ayant activé l'aide" do
       let(:evenements) do
-        [build(:evenement_identification_danger,
+        [build(:evenement_demarrage, date: 4.minutes.ago),
+         build(:evenement_identification_danger,
                donnees: { reponse: 'oui', danger: 'danger', nom: 'avant' },
                date: 3.minutes.ago),
          build(:evenement_identification_danger,
@@ -127,7 +145,8 @@ describe Restitution::Securite do
 
     context "avec des dangers identifiés aprés avoir activé l'aide" do
       let(:evenements) do
-        [build(:activation_aide),
+        [build(:evenement_demarrage),
+         build(:activation_aide),
          build(:evenement_identification_danger,
                donnees: { reponse: 'oui', danger: 'danger' },
                date: 2.minutes.from_now)]
@@ -137,7 +156,8 @@ describe Restitution::Securite do
 
     context "avec un danger identifié sans avoir activé l'aide" do
       let(:evenements) do
-        [build(:evenement_identification_danger,
+        [build(:evenement_demarrage),
+         build(:evenement_identification_danger,
                donnees: { reponse: 'oui', danger: 'danger' },
                date: 2.minutes.from_now)]
       end
@@ -189,7 +209,8 @@ describe Restitution::Securite do
 
     context "avec identification du danger sans avoir activé l'aide" do
       let(:evenements) do
-        [build(:evenement_identification_danger,
+        [build(:evenement_demarrage),
+         build(:evenement_identification_danger,
                donnees: { reponse: 'oui', danger: Restitution::Securite::DANGER_VISUO_SPATIAL })]
       end
 
@@ -198,7 +219,8 @@ describe Restitution::Securite do
 
     context "avec identification du danger après avoir activé l'aider" do
       let(:evenements) do
-        [build(:activation_aide, date: 2.minutes.ago),
+        [build(:evenement_demarrage, date: 3.minute.ago),
+         build(:activation_aide, date: 2.minutes.ago),
          build(:evenement_identification_danger,
                donnees: { reponse: 'oui', danger: Restitution::Securite::DANGER_VISUO_SPATIAL },
                date: 1.minute.ago)]
@@ -206,9 +228,10 @@ describe Restitution::Securite do
       it { expect(restitution.attention_visuo_spatiale).to eq Competence::APTE_AVEC_AIDE }
     end
 
-    context "avec identification du danger avant avoir activé l'aider" do
+    context "avec identification du danger avant avoir activé l'aide" do
       let(:evenements) do
-        [build(:evenement_identification_danger,
+        [build(:evenement_demarrage, date: 3.minute.ago),
+         build(:evenement_identification_danger,
                donnees: { reponse: 'oui', danger: Restitution::Securite::DANGER_VISUO_SPATIAL },
                date: 2.minute.ago),
          build(:activation_aide, date: 1.minutes.ago)]

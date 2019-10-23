acc8375b1ad8c12722c3ce2f8b4fae03e7762aa4
François de Metz <francois.de_metz@beta.gouv.fr>
2019-10-23 18:02:21 +0200

    Compte le nombre de réouverture de zones sans dangers.

--- spec/models/restitution/securite_spec.rb
+++ spec/models/restitution/securite_spec.rb
@@ -239,4 +239,39 @@ describe Restitution::Securite do
       it { expect(restitution.attention_visuo_spatiale).to eq Competence::APTE }
     end
   end
+
+  describe '#nombre_reouverture_zone_sans_danger' do
+    context 'sans réouverture' do
+      let(:evenements) do
+        [build(:evenement_demarrage),
+         build(:evenement_ouverture_zone, donnees: { zone: 'zone1' })]
+      end
+
+      it { expect(restitution.nombre_reouverture_zone_sans_danger).to eq 0 }
+    end
+
+    context 'avec une réouverture' do
+      let(:evenements) do
+        [build(:evenement_demarrage),
+         build(:evenement_ouverture_zone, donnees: { zone: 'zone1' }),
+         build(:evenement_ouverture_zone, donnees: { zone: 'zone2' }),
+         build(:evenement_ouverture_zone, donnees: { zone: 'zone1' })]
+      end
+
+      it { expect(restitution.nombre_reouverture_zone_sans_danger).to eq 1 }
+    end
+
+    context "avec une autre réouverture d'une zone avec danger" do
+      let(:evenements) do
+        [build(:evenement_demarrage),
+         build(:evenement_ouverture_zone, donnees: { zone: 'zone1' }),
+         build(:evenement_ouverture_zone, donnees: { zone: 'zone2' }),
+         build(:evenement_ouverture_zone, donnees: { zone: 'zone3', danger: 'danger1' }),
+         build(:evenement_ouverture_zone, donnees: { zone: 'zone3', danger: 'danger1' }),
+         build(:evenement_ouverture_zone, donnees: { zone: 'zone1' })]
+      end
+
+      it { expect(restitution.nombre_reouverture_zone_sans_danger).to eq 1 }
+    end
+  end
 end

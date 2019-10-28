bf853b16853d0316a6f8690a7d6855ae40c8f045
Stéphane Hanser <shanser@captive.fr>
2019-10-28 11:39:04 +0100

    Déplace les tests liés à l'entrainement dans les tests de la restitution avec entrainement
    Co-authored-by: François de Metz <francois.de_metz@beta.gouv.fr>

--- spec/models/restitution/securite_spec.rb
+++ spec/models/restitution/securite_spec.rb
@@ -300,42 +300,4 @@ describe Restitution::Securite do
       it { expect(restitution.nombre_reouverture_zone_sans_danger).to eq 1 }
     end
   end
-
-  describe '#temps_entrainement' do
-    context "lorsqu'il est terminé" do
-      let(:evenements) do
-        [build(:evenement_demarrage_entrainement, date: Time.local(2019, 10, 9, 10, 2)),
-         build(:evenement_identification_danger,
-               donnees: { reponse: 'oui', danger: 'danger' }, date: Time.local(2019, 10, 9, 10, 3)),
-         build(:evenement_qualification_danger, date: Time.local(2019, 10, 9, 10, 7))]
-      end
-
-      it { expect(restitution.temps_entrainement).to eq 5.minutes }
-    end
-
-    context "lorsqu'il n'est pas terminé" do
-      let(:evenements) do
-        [build(:evenement_demarrage_entrainement, date: Time.local(2019, 10, 9, 10, 2)),
-         build(:evenement_identification_danger,
-               donnees: { reponse: 'oui', danger: 'danger' },
-               date: Time.local(2019, 10, 9, 10, 4))]
-      end
-
-      it { expect(restitution.temps_entrainement).to eq 2.minutes }
-    end
-
-    context 'ne prend pas en compte le temps de la situation' do
-      let(:evenements) do
-        [build(:evenement_demarrage_entrainement, date: Time.local(2019, 10, 9, 10, 2)),
-         build(:evenement_identification_danger,
-               donnees: { reponse: 'oui', danger: 'danger' },
-               date: Time.local(2019, 10, 9, 10, 4)),
-         build(:evenement_qualification_danger, date: Time.local(2019, 10, 9, 10, 4)),
-         build(:evenement_demarrage, date: Time.local(2019, 10, 9, 10, 6)),
-         build(:evenement_identification_danger)]
-      end
-
-      it { expect(restitution.temps_entrainement).to eq 2.minutes }
-    end
-  end
 end

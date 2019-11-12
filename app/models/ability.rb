9eb8c6898be9e2c45955f6ae96a03cadb8804b2b
François de Metz <francois.de_metz@beta.gouv.fr>
2019-11-12 11:22:37 +0100

    Refactor les droits pour avoir ne pas tout autoriser puis interdire.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,54 +4,54 @@ class Ability
   include CanCan::Ability
 
   def initialize(compte)
-    droits_generiques
+    droits_generiques compte
     droit_campagne compte
     droit_evaluation compte
     droit_evenement compte
-    droit_situation compte
-    droit_question compte
-    droit_questionnaire compte
-    cannot :manage, Compte unless compte.administrateur?
+    droit_restitution compte
+    droit_situation
+    droit_question
+    droit_questionnaire
   end
 
   private
 
   def droit_campagne(compte)
-    cannot :manage, Campagne unless compte.administrateur?
     can :create, Campagne
-    can %i[update read destroy], Campagne, compte_id: compte.id
+    can :manage, Campagne, compte_id: compte.id
   end
 
   def droit_evaluation(compte)
-    cannot :manage, Evaluation unless compte.administrateur?
-    can :manage, Evaluation, campagne: { compte_id: compte.id }
+    cannot %i[create update], Evaluation
+    can %i[read destroy], Evaluation, campagne: { compte_id: compte.id }
   end
 
   def droit_evenement(compte)
     cannot %i[update create], Evenement
-    cannot :read, Evenement unless compte.administrateur?
     can :read, Evenement, evaluation: { campagne: { compte_id: compte.id } }
   end
 
-  def droit_situation(compte)
-    cannot :manage, Situation unless compte.administrateur?
+  def droit_restitution(compte)
+    can :manage, Restitution::Base, campagne: { compte_id: compte.id }
+  end
+
+  def droit_situation
     can :read, Situation
   end
 
-  def droit_questionnaire(compte)
-    cannot :manage, Questionnaire unless compte.administrateur?
+  def droit_questionnaire
     can :read, Questionnaire
     cannot :destroy, Questionnaire do |q|
       Campagne.where(questionnaire: q).present?
     end
   end
 
-  def droit_question(compte)
-    cannot :manage, Question unless compte.administrateur?
+  def droit_question
     can :read, Question
   end
 
-  def droits_generiques
-    can :manage, :all
+  def droits_generiques(compte)
+    can :manage, :all if compte.administrateur?
+    can :read, ActiveAdmin::Page, name: 'Dashboard', namespace_name: 'admin'
   end
 end
--- spec/models/ability_spec.rb
+++ spec/models/ability_spec.rb
@@ -4,26 +4,62 @@ require 'rails_helper'
 require 'cancan/matchers'
 
 describe Ability do
+  let(:compte_administrateur) { create :compte, role: 'administrateur' }
+  let(:compte_organisation) { create :compte, role: 'organisation' }
+
   subject(:ability) { Ability.new(compte) }
 
   context 'Compte administrateur' do
-    let(:compte) { build :compte, role: 'administrateur' }
+    let(:compte) { compte_administrateur }
 
     it { is_expected.to be_able_to(:manage, :all) }
-    it { is_expected.to be_able_to(%i[destroy create], Evaluation.new) }
-    it { is_expected.to_not be_able_to(:create, Evenement.new) }
+    it do
+      is_expected.to be_able_to(:read,
+                                ActiveAdmin::Page,
+                                name: 'Dashboard',
+                                namespace_name: 'admin')
+    end
+    it { is_expected.to be_able_to(%i[read destroy], Evaluation.new) }
+    it { is_expected.to_not be_able_to(%i[create update], Evaluation.new) }
+    it { is_expected.to_not be_able_to(%i[create update], Evenement.new) }
     it { is_expected.to be_able_to(:read, Evenement.new) }
+    it { is_expected.to be_able_to(:manage, Situation.new) }
     it { is_expected.to be_able_to(:manage, Campagne.new) }
     it { is_expected.to be_able_to(:manage, Questionnaire.new) }
+    it { is_expected.to be_able_to(:manage, Question.new) }
+    it { is_expected.to be_able_to(:manage, Restitution::Base.new(nil, nil)) }
   end
 
   context 'Compte organisation' do
-    let(:compte) { build :compte, role: 'organisation' }
+    let(:compte) { compte_organisation }
+    let(:campagne_compte) { Campagne.create(libelle: 'Nom', compte: compte) }
+    let(:campagne_administrateur) { Campagne.create(libelle: 'Nom', compte: compte_administrateur) }
+    let(:evaluation_autre_compte) { Evaluation.new(campagne: campagne_administrateur) }
+    let(:evaluation_compte) { Evaluation.create(campagne: campagne_compte) }
 
+    it { is_expected.to_not be_able_to(:manage, :all) }
+    it do
+      is_expected.to be_able_to(:read,
+                                ActiveAdmin::Page,
+                                name: 'Dashboard',
+                                namespace_name: 'admin')
+    end
     it { is_expected.to_not be_able_to(:manage, Compte.new) }
     it { is_expected.to_not be_able_to(%i[destroy create update], Situation.new) }
     it { is_expected.to_not be_able_to(%i[destroy create update], Question.new) }
     it { is_expected.to_not be_able_to(:manage, Questionnaire.new) }
+    it { is_expected.to_not be_able_to(:manage, Campagne.new) }
+    it { is_expected.to_not be_able_to(%i[create update], evaluation_compte) }
+    it { is_expected.to_not be_able_to(%i[read destroy], evaluation_autre_compte) }
+    it { is_expected.to_not be_able_to(:read, Evenement.new) }
+    it { is_expected.to_not be_able_to(:read, Evenement.new(evaluation: evaluation_autre_compte)) }
+    it { is_expected.to be_able_to(:read, Question.new) }
+    it { is_expected.to be_able_to(%i[read destroy], evaluation_compte) }
+    it { is_expected.to be_able_to(:read, Evenement.new(evaluation: evaluation_compte)) }
+    it { is_expected.to be_able_to(:create, Campagne.new) }
+    it { is_expected.to be_able_to(:manage, Campagne.new(compte: compte)) }
     it { is_expected.to be_able_to(:read, Questionnaire.new) }
+    it { is_expected.to be_able_to(:read, Situation.new) }
+    it { is_expected.to be_able_to(:manage, Restitution::Base.new(campagne_compte, nil)) }
   end
 end

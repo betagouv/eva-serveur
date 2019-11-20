79b2d7a22fab41626b738c1b31a758a90f6f3956
François de Metz <francois.de_metz@beta.gouv.fr>
2019-11-20 14:02:46 +0100

    Précompile le fichier  restitution_globale/restitution_globale_pdf.scss.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += %w[active_admin.css active_admin.js]
+Rails.application.config.assets.precompile += %w[active_admin.css active_admin.js restitution_globale/restitution_globale_pdf.css]

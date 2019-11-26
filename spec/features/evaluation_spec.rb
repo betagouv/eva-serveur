0e9161e9d46ae0f6bedf2b974f95f150781407ef
François de Metz <francois.de_metz@beta.gouv.fr>
2019-11-26 13:35:08 +0100

    Supprime l'onglet évaluation et redirige vers la campagne a la suppression.

--- spec/features/evaluation_spec.rb
+++ spec/features/evaluation_spec.rb
@@ -9,35 +9,6 @@ describe 'Admin - Evaluation', type: :feature do
     create :campagne, compte: Compte.first, libelle: 'Paris 2019', code: 'paris2019'
   end
 
-  context "en tant qu'organisation" do
-    let(:compte_rouen) { create :compte, role: 'organisation' }
-    let(:campagne_rouen) { create :campagne, compte: compte_rouen, libelle: 'Rouen 2019' }
-    let!(:evaluation) { create :evaluation, campagne: campagne_rouen }
-
-    let!(:mon_evaluation) { create :evaluation, campagne: ma_campagne, created_at: 3.days.ago }
-    let!(:mon_evaluation2) do
-      create :evaluation, campagne: ma_campagne, created_at: DateTime.now, nom: 'Jean'
-    end
-
-    it 'Je ne vois que les évaluations liées à mes campagnes' do
-      visit admin_evaluations_path
-      expect(page).to have_content 'Paris 2019'
-      expect(page).to_not have_content 'Rouen 2019'
-
-      within('#filters_sidebar_section') do
-        expect(page).to have_content 'Paris 2019'
-        expect(page).to_not have_content 'Rouen 2019'
-      end
-    end
-
-    it 'Trie les évaluations de la plus récentes à la plus vieille' do
-      visit admin_evaluations_path
-      within '.odd:first-of-type' do
-        expect(page).to have_content 'Jean'
-      end
-    end
-  end
-
   describe '#show' do
     let!(:mon_evaluation) { create :evaluation, campagne: ma_campagne, created_at: 3.days.ago }
     let(:situation) { build(:situation_inventaire) }
@@ -77,6 +48,7 @@ describe 'Admin - Evaluation', type: :feature do
     it do
       expect { click_on 'Supprimer' }.to(change { Evaluation.count }
                                      .and(change { Evenement.count }))
+      expect(page.current_url).to eql(admin_campagne_url(ma_campagne))
     end
   end
 end

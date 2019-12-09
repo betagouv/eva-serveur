8f290664d491945814765044e9bbf9e11d06c3b2
François de Metz <francois.de_metz@beta.gouv.fr>
2019-12-09 15:35:48 +0100

    Supprime un let inutilisé.

--- spec/models/partie_spec.rb
+++ spec/models/partie_spec.rb
@@ -3,8 +3,6 @@
 require 'rails_helper'
 
 describe Partie do
-  let(:partie) { described_class.new session_id: 'session_id' }
-
   it { should validate_presence_of(:session_id) }
   it { should belong_to(:evaluation) }
   it { should belong_to(:situation) }

b2ceba471619e6993845dd8f3825d799ff2c44cd
Stéphane Hanser <shanser@captive.fr>
2019-12-16 11:57:11 +0100

    remplace des références Compétences pro vers eva

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "competences_pro_serveur_production"
+  # config.active_job.queue_name_prefix = "eva_serveur_production"
 
 
   config.action_mailer.perform_caching = false

c21f1b73c83421880500f2f0ace647f8a2116e30
Étienne Charignon <etienne.charignon@gmail.com>
2019-12-23 13:43:28 +0100

    Ajoute la colonne `nom situation` dans l'export CSV des événements
    Co-authored-by: Clément Morisset <clement.morisset@beta.gouv.fr>
    Co-authored-by: François de Metz <francois.de_metz@beta.gouv.fr>

--- app/admin/evenements.rb
+++ app/admin/evenements.rb
@@ -7,7 +7,23 @@ ActiveAdmin.register Evenement do
   filter :session_id
   filter :date
 
+  colonnes_evenement = proc do
+    column(:situation) { |e| Partie.find_by(session_id: e.session_id).situation.libelle }
+    column :session_id
+    column :nom
+    column :donnees
+    column(:date) { |d| l(d.date, format: :date_heure) }
+    column(:created_at) { |c_a| l(c_a.created_at, format: :date_heure) }
+    column(:updated_at) { |u_a| l(u_a.updated_at, format: :date_heure) }
+  end
+
   index do
-    render 'index', context: self
+    selectable_column
+    instance_eval(&colonnes_evenement)
+    actions
+  end
+
+  csv do
+    instance_eval(&colonnes_evenement)
   end
 end

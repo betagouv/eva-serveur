e8cb4386bdd6fe0c3f179a2974fc4378512896dd
Clément MORISSET <cmorisset@captive.fr>
2020-01-13 11:29:33 +0100

    extraie les metriques persistées dans une classe à part

---
+++ app/models/restitution/metriques.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Restitution
+  class Metriques
+    SECURITE = %i[
+      nombre_reouverture_zone_sans_danger nombre_bien_qualifies
+      nombre_dangers_bien_identifies nombre_retours_deja_qualifies
+      nombre_dangers_bien_identifies_avant_aide_1 attention_visuo_spatiale
+      temps_ouvertures_zones_dangers temps_moyen_ouvertures_zones_dangers
+      temps_entrainement temps_total nombre_rejoue_consigne nombre_danger_mal_identifies
+    ].freeze
+  end
+end

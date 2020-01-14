858c2387b0146147136d1fa4b363b831559ed746
Clément MORISSET <cmorisset@captive.fr>
2020-01-14 13:00:09 +0100

    Une métrique par zone pour temps_recherche_zones_dangers
    Reste à régler le problème qu'une class et une méthode sont trop grandes
    
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

--- spec/models/restitution/securite_spec.rb
+++ spec/models/restitution/securite_spec.rb
@@ -219,7 +219,7 @@ describe Restitution::Securite do
       it { expect(restitution.temps_moyen_ouvertures_zones_dangers).to eq 60 }
     end
 
-    context 'deux zone danger ouverts' do
+    context 'deux zones danger ouverts' do
       let(:evenements) do
         [build(:evenement_demarrage, date: Time.local(2019, 10, 9, 10, 0)),
          build(:evenement_ouverture_zone,
@@ -251,6 +251,45 @@ describe Restitution::Securite do
          build(:evenement_qualification_danger, date: Time.local(2019, 10, 9, 10, 2))]
       end
       it { expect(restitution.temps_ouvertures_zones_dangers).to eq [60] }
+    end
+  end
+
+  describe '#temps_recherche_zones_dangers' do
+    context 'sans zone danger ouverte' do
+      let(:evenements) { [] }
+      it { expect(restitution.temps_recherche_zones_dangers).to eq [nil, nil, nil, nil, nil] }
+    end
+
+    context 'une zone danger ouverte' do
+      let(:evenements) do
+        [build(:evenement_demarrage, date: Time.local(2019, 10, 9, 10, 0)),
+         build(:evenement_ouverture_zone,
+               donnees: { danger: 'casque' }, date: Time.local(2019, 10, 9, 10, 1))]
+      end
+      it { expect(restitution.temps_recherche_zones_dangers).to eq [nil, 60, nil, nil, nil] }
+    end
+
+    context 'deux zones danger ouverts' do
+      let(:evenements) do
+        [build(:evenement_demarrage, date: Time.local(2019, 10, 9, 10, 0)),
+         build(:evenement_ouverture_zone,
+               donnees: { danger: 'casque' }, date: Time.local(2019, 10, 9, 10, 1)),
+         build(:evenement_qualification_danger, date: Time.local(2019, 10, 9, 10, 2)),
+         build(:evenement_ouverture_zone,
+               donnees: { danger: 'camion' }, date: Time.local(2019, 10, 9, 10, 4))]
+      end
+      it { expect(restitution.temps_recherche_zones_dangers).to eq [nil, 60, nil, 120, nil] }
+    end
+
+    context 'quand on ne finit pas par une identification' do
+      let(:evenements) do
+        [build(:evenement_demarrage, date: Time.local(2019, 10, 9, 10, 0)),
+         build(:evenement_ouverture_zone,
+               donnees: { danger: 'casque' }, date: Time.local(2019, 10, 9, 10, 1)),
+         build(:evenement_qualification_danger, date: Time.local(2019, 10, 9, 10, 2))]
+      end
+
+      it { expect(restitution.temps_recherche_zones_dangers).to eq [nil, 60, nil, nil, nil] }
     end
   end
 

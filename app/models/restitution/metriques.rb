d3f345e7b687aca0a9d6f1b9f72b7ee9f3c10ed2
Étienne Charignon <etienne.charignon@gmail.com>
2020-01-14 16:04:20 +0000

    Extrait une class pour `temps_bonnes_qualifications_dangers`

--- app/models/restitution/metriques.rb
+++ app/models/restitution/metriques.rb
@@ -2,6 +2,16 @@
 
 module Restitution
   class Metriques
+    def self.temps_entre_couples(evenements)
+      les_temps = []
+      evenements.each_slice(2) do |e1, e2|
+        next if e2.blank?
+
+        les_temps << e2.date - e1.date
+      end
+      les_temps
+    end
+
     SECURITE = %i[
       nombre_reouverture_zone_sans_danger nombre_bien_qualifies
       nombre_dangers_bien_identifies nombre_retours_deja_qualifies
@@ -9,5 +19,9 @@ module Restitution
       temps_ouvertures_zones_dangers temps_moyen_ouvertures_zones_dangers
       temps_entrainement temps_total nombre_rejoue_consigne nombre_danger_mal_identifies
     ].freeze
+
+    REGLES_SECURITE = {
+      'temps_bonnes_qualifications_dangers' => Securite::TempsBonnesQualificationsDangers
+    }.freeze
   end
 end

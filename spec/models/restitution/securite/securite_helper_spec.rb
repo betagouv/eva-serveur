ce11b4b37223e028b72f4641a1a6b7656ceb09e4
Étienne Charignon <etienne.charignon@gmail.com>
2020-01-17 14:35:44 +0000

    Filtre les dangers par ordre alphabetique

---
+++ spec/models/restitution/securite/securite_helper_spec.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Restitution::Securite::SecuriteHelper do
+  describe '#filtre_par_danger' do
+    let(:e1) { Evenement.new donnees: { 'danger' => 'signalisation' } }
+    let(:e2) { Evenement.new donnees: { 'danger' => 'camion' } }
+
+    it 'trie les dangers par ordre alphabetique' do
+      expect(described_class.filtre_par_danger([e1, e2], &:present?).keys)
+        .to eql(%w[camion signalisation])
+    end
+  end
+end

fade5d59096f362cd12922f6c2188aae73498863
Étienne Charignon <etienne.charignon@gmail.com>
2020-01-20 11:05:54 +0000

    ajoute deux tests pour spécifier le comportement en cas de mauvaise qualification

--- spec/models/restitution/securite/temps_bonnes_qualification_dangers_spec.rb
+++ spec/models/restitution/securite/temps_bonnes_qualification_dangers_spec.rb
@@ -24,7 +24,7 @@ describe Restitution::Securite::TempsBonnesQualificationsDangers do
       it { expect(restitution.temps_bonnes_qualifications_dangers).to eq('casque' => 60) }
     end
 
-    context 'avec deux bonnes qualifications pour des danges différents' do
+    context 'avec deux bonnes qualifications pour des dangers différents' do
       let(:evenements) do
         [build(:evenement_demarrage, date: Time.local(2019, 10, 9, 10, 0)),
          build(:evenement_ouverture_zone,
@@ -75,6 +75,36 @@ describe Restitution::Securite::TempsBonnesQualificationsDangers do
       it { expect(restitution.temps_bonnes_qualifications_dangers).to eq({}) }
     end
 
+    context 'ignore les mauvaises qualifications précédant une bonne qualification' do
+      let(:evenements) do
+        [build(:evenement_demarrage, date: Time.local(2019, 10, 9, 10, 0)),
+         build(:evenement_ouverture_zone,
+               donnees: { danger: 'd1' }, date: Time.local(2019, 10, 9, 10, 1)),
+         build(:evenement_qualification_danger,
+               donnees: { reponse: 'mauvais', danger: 'd1' }, date: Time.local(2019, 10, 9, 10, 2)),
+         build(:evenement_ouverture_zone,
+               donnees: { danger: 'd1' }, date: Time.local(2019, 10, 9, 10, 3)),
+         build(:evenement_qualification_danger,
+               donnees: { reponse: 'bonne', danger: 'd1' }, date: Time.local(2019, 10, 9, 10, 5))]
+      end
+      it { expect(restitution.temps_bonnes_qualifications_dangers).to eq('d1' => 120) }
+    end
+
+    context 'ignore les mauvaises qualifications même suivant une bonne qualification' do
+      let(:evenements) do
+        [build(:evenement_demarrage, date: Time.local(2019, 10, 9, 10, 0)),
+         build(:evenement_ouverture_zone,
+               donnees: { danger: 'd1' }, date: Time.local(2019, 10, 9, 10, 1)),
+         build(:evenement_qualification_danger,
+               donnees: { reponse: 'bonne', danger: 'd1' }, date: Time.local(2019, 10, 9, 10, 2)),
+         build(:evenement_ouverture_zone,
+               donnees: { danger: 'd1' }, date: Time.local(2019, 10, 9, 10, 3)),
+         build(:evenement_qualification_danger,
+               donnees: { reponse: 'mauvais', danger: 'd1' }, date: Time.local(2019, 10, 9, 10, 5))]
+      end
+      it { expect(restitution.temps_bonnes_qualifications_dangers).to eq('d1' => 60) }
+    end
+
     context 'ouverture sans qualification' do
       let(:evenements) do
         [build(:evenement_demarrage, date: Time.local(2019, 10, 9, 10, 0)),
@@ -92,7 +122,7 @@ describe Restitution::Securite::TempsBonnesQualificationsDangers do
       it { expect(restitution.temps_bonnes_qualifications_dangers).to eq({}) }
     end
 
-    context 'ignore les zones non dangers ouverts' do
+    context 'ignore les zones non dangers ouvertes' do
       let(:evenements) do
         [build(:evenement_demarrage, date: Time.local(2019, 10, 9, 10, 0)),
          build(:evenement_ouverture_zone,

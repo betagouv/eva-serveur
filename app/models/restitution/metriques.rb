97b764752d3d7558f46072242c92cf55e8a1ed48
Étienne Charignon <etienne.charignon@gmail.com>
2020-01-31 17:07:45 +0000

    Transforme la liste SECURITE en map et supprime REGLES_SECURITE

--- app/models/restitution/metriques.rb
+++ app/models/restitution/metriques.rb
@@ -2,18 +2,22 @@
 
 module Restitution
   class Metriques
-    REGLES_SECURITE = {
+    SECURITE = {
       'temps_bonnes_qualifications_dangers' => Securite::TempsBonnesQualificationsDangers,
       'temps_recherche_zones_dangers' => Securite::TempsRechercheZonesDangers,
-      'temps_total_ouverture_zones_dangers' => Securite::TempsTotalOuvertureZonesDangers
+      'temps_total_ouverture_zones_dangers' => Securite::TempsTotalOuvertureZonesDangers,
+      'nombre_reouverture_zone_sans_danger' => Securite,
+      'nombre_bien_qualifies' => Securite,
+      'nombre_dangers_bien_identifies' => Securite,
+      'nombre_retours_deja_qualifies' => Securite,
+      'nombre_dangers_bien_identifies_avant_aide_1' => Securite,
+      'attention_visuo_spatiale' => Securite,
+      'delai_ouvertures_zones_dangers' => Securite,
+      'delai_moyen_ouvertures_zones_dangers' => Securite,
+      'temps_entrainement' => Securite,
+      'temps_total' => Securite,
+      'nombre_rejoue_consigne' => Securite,
+      'nombre_danger_mal_identifies' => Securite
     }.freeze
-
-    SECURITE = %i[
-      nombre_reouverture_zone_sans_danger nombre_bien_qualifies
-      nombre_dangers_bien_identifies nombre_retours_deja_qualifies
-      nombre_dangers_bien_identifies_avant_aide_1 attention_visuo_spatiale
-      delai_ouvertures_zones_dangers delai_moyen_ouvertures_zones_dangers
-      temps_entrainement temps_total nombre_rejoue_consigne nombre_danger_mal_identifies
-    ].freeze
   end
 end

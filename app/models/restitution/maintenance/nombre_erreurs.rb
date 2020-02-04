0333999ed5efcd015764f58314fc6bfa23d92ca4
Clément MORISSET <cmorisset@captive.fr>
2020-02-04 12:42:25 +0100

    refactore les métriques de Maintenance en ajoutant un decorator

--- app/models/restitution/maintenance/nombre_erreurs.rb
+++ app/models/restitution/maintenance/nombre_erreurs.rb
@@ -5,18 +5,12 @@ module Restitution
     class NombreErreurs
       attr_reader :evenements_situation
 
-      EVENEMENT = {
-        NOM_MOT: 'non-mot',
-        REPONSE_NON_FRANCAIS: 'pasfrancais',
-        REPONSE_FRANCAIS: 'francais'
-      }.freeze
-
       def initialize(evenements_situation)
         @evenements_situation = evenements_situation
       end
 
       def calcule
-        evenements_situation.keep_if do |e|
+        evenements_situation.select do |e|
           mauvaises_reponses(e)
         end.count
       end
@@ -29,13 +23,13 @@ module Restitution
       end
 
       def mot_pas_francais_reponse_francais?(evt)
-        (evt.donnees['type'] == EVENEMENT[:NOM_MOT]) &&
-          (evt.donnees['reponse'] == EVENEMENT[:REPONSE_FRANCAIS])
+        evt.type_non_mot &&
+          evt.reponse_francais
       end
 
       def mot_francais_reponse_non_francais?(evt)
-        (evt.donnees['type'] != EVENEMENT[:NOM_MOT]) &&
-          (evt.donnees['reponse'] == EVENEMENT[:REPONSE_NON_FRANCAIS])
+        evt.type_mot_francais &&
+          evt.reponse_non_francais
       end
     end
   end

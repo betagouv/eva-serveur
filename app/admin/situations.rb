5c4dfa950c24405271a335e580d57673da8696e1
François de Metz <francois.de_metz@beta.gouv.fr>
2020-02-10 11:25:42 +0100

    Spécifie l'ordre des champs pour éditer une situation.

--- app/admin/situations.rb
+++ app/admin/situations.rb
@@ -2,4 +2,15 @@
 
 ActiveAdmin.register Situation do
   permit_params :libelle, :nom_technique, :questionnaire_id, :questionnaire_entrainement_id
+
+  form do |f|
+    f.semantic_errors
+    inputs do
+      f.input :libelle
+      f.input :nom_technique
+      f.input :questionnaire
+      f.input :questionnaire_entrainement
+    end
+    f.actions
+  end
 end

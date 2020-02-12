95a0cffcf48daa019e72a968bb70a912fca24478
François de Metz <francois.de_metz@beta.gouv.fr>
2020-02-12 11:41:18 +0100

    Persiste les métriques de maintenance.

---
+++ spec/models/restitution/maintenance_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Restitution::Maintenance do
+  describe '#persiste' do
+    context "persiste l'ensemble des données de sécurité" do
+      let(:campagne) { Campagne.new }
+      let(:restitution) { described_class.new campagne, evenements }
+
+      let(:situation) { create :situation_maintenance }
+      let(:evaluation) { create :evaluation, campagne: campagne }
+      let!(:partie) { create :partie, situation: situation, evaluation: evaluation }
+      let(:evenements) do
+        [build(:evenement_demarrage)]
+      end
+
+      it do
+        expect(restitution).to receive(:nombre_erreurs).and_return 1
+        expect(restitution).to receive(:nombre_non_reponses).and_return 2
+        restitution.persiste
+        partie.reload
+        expect(partie.metriques['nombre_erreurs']).to eq 1
+        expect(partie.metriques['nombre_non_reponses']).to eq 2
+      end
+    end
+  end
+end

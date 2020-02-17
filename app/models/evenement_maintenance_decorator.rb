675e36cdc7d40742584aa725a29ebc54a643a1fe
Stéphane Hanser <shanser@captive.fr>
2020-02-17 19:08:16 +0100

    déplace la logique de non réponse dans le décorateur d'événement

--- app/models/evenement_maintenance_decorator.rb
+++ app/models/evenement_maintenance_decorator.rb
@@ -66,4 +66,8 @@ class EvenementMaintenanceDecorator < SimpleDelegator
   def mauvaise_identification
     identification_non_mot_incorrect || identification_mot_francais_incorrect
   end
+
+  def non_reponse
+    donnees['reponse'].nil? && identification_mot
+  end
 end
--- app/models/restitution/maintenance/nombre_non_reponses.rb
+++ app/models/restitution/maintenance/nombre_non_reponses.rb
@@ -10,16 +10,7 @@ module Restitution
       end
 
       def calcule
-        evenements_situation.select do |e|
-          non_reponse?(e)
-        end.count
-      end
-
-      private
-
-      def non_reponse?(evt)
-        evt.donnees['reponse'].nil? &&
-          evt.identification_mot
+        evenements_situation.select(&:non_reponse).count
       end
     end
   end

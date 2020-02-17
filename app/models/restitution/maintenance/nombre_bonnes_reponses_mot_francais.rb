36a30fb9af0bc94968339ea252cc143b43078eca
Stéphane Hanser <shanser@captive.fr>
2020-02-17 19:01:22 +0100

    utilise une méthode du décorateur d'événement pour filter les événements

--- app/models/restitution/maintenance/nombre_bonnes_reponses_mot_francais.rb
+++ app/models/restitution/maintenance/nombre_bonnes_reponses_mot_francais.rb
@@ -10,10 +10,7 @@ module Restitution
       end
 
       def calcule
-        evenements_situation.select do |e|
-          e.type_mot_francais &&
-            e.reponse_francais
-        end.count
+        evenements_situation.select(&:identification_mot_francais_correct).count
       end
     end
   end

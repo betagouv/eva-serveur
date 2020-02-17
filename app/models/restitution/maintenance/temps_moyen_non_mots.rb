671f18fdf6bb66ccf571621c6ff5e00abda88722
Clément MORISSET <cmorisset@captive.fr>
2020-02-17 18:09:06 +0100

    Retourne nil si pas d'évenements retenus pour la métrique TempsMoyenNonMot
    Co-authored-by: Stéphane Hanser <stephane.hanser@beta.gouv.fr>

--- app/models/restitution/maintenance/temps_moyen_non_mots.rb
+++ app/models/restitution/maintenance/temps_moyen_non_mots.rb
@@ -10,12 +10,12 @@ module Restitution
       end
 
       def calcule
-        return 0 if nombre_identifications_non_mot_correct.zero?
-
         temps = Restitution::MetriquesHelper.temps_entre_couples(
           apparitions_et_identifications_non_mot_correctes
         )
-        (temps.sum / nombre_identifications_non_mot_correct).round(4)
+        return nil if temps.count.zero?
+
+        (temps.sum / temps.count).round(4)
       end
 
       private
@@ -32,10 +32,6 @@ module Restitution
 
       def apparitions_et_identifications_non_mot
         evenements_situation.select(&:apparition_ou_identification_non_mot)
-      end
-
-      def nombre_identifications_non_mot_correct
-        evenements_situation.select(&:identification_non_mot_correcte).count
       end
     end
   end

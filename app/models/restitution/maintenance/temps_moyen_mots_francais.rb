4d1ed1b82c70d3388c1228720914cd1234122a1e
Clément MORISSET <cmorisset@captive.fr>
2020-02-17 18:50:36 +0100

    Met en place la métrique TempsMoyenMotsFrancais
    Co-authored-by: Clément Prodhomme <clement@captive.fr>

--- app/models/restitution/maintenance/temps_moyen_mots_francais.rb
+++ app/models/restitution/maintenance/temps_moyen_mots_francais.rb
@@ -2,7 +2,7 @@
 
 module Restitution
   class Maintenance
-    class TempsMoyenNonMots
+    class TempsMoyenMotsFrancais
       attr_reader :evenements_situation
 
       def initialize(evenements_situation)
@@ -11,27 +11,27 @@ module Restitution
 
       def calcule
         temps = Restitution::MetriquesHelper.temps_entre_couples(
-          apparitions_et_identifications_non_mot_correctes
+          apparitions_et_identifications_mot_francais_correctes
         )
         return nil if temps.count.zero?
 
-        (temps.sum / temps.count).round(4)
+        temps.sum / temps.count
       end
 
       private
 
-      def apparitions_et_identifications_non_mot_correctes
+      def apparitions_et_identifications_mot_francais_correctes
         evenements_retenus = []
-        apparitions_et_identifications_non_mot.each_slice(2) do |apparition, identification|
-          next unless identification.identification_non_mot_correct
+        apparitions_et_identifications_mot_francais.each_slice(2) do |apparition, identification|
+          next unless identification.identification_mot_francais_correct
 
           evenements_retenus << apparition << identification
         end
         evenements_retenus
       end
 
-      def apparitions_et_identifications_non_mot
-        evenements_situation.select(&:apparition_ou_identification_non_mot)
+      def apparitions_et_identifications_mot_francais
+        evenements_situation.select(&:type_mot_francais)
       end
     end
   end
---
+++ spec/models/restitution/maintenance/temps_moyen_mots_francais_spec.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Restitution::Maintenance::TempsMoyenMotsFrancais do
+  let(:metrique_moyenne_mots_francais) do
+    described_class.new(evenements_decores(evenements)).calcule
+  end
+
+  describe '#metrique metrique_moyenne_mots_francais' do
+    context "aucun événement d'identification" do
+      let(:evenements) do
+        [
+          build(:evenement_demarrage)
+        ]
+      end
+      it { expect(metrique_moyenne_mots_francais).to eq nil }
+    end
+
+    context "avec un événement d'identification mot français correcte" do
+      let(:evenements) do
+        [
+          build(:evenement_demarrage),
+          build(:evenement_apparition_mot, donnees: { type: 'neutre', reponse: 'francais' },
+                                           date: Time.local(2019, 10, 9, 10, 1, 21, 950_000)),
+          build(:evenement_identification_mot, donnees: { type: 'neutre', reponse: 'francais' },
+                                               date: Time.local(2019, 10, 9, 10, 1, 21, 960_000))
+        ]
+      end
+      it { expect(metrique_moyenne_mots_francais).to eq 0.01 }
+    end
+
+    context "avec un événement d'identification non mot correcte" do
+      let(:evenements) do
+        [
+          build(:evenement_demarrage),
+          build(:evenement_apparition_mot, donnees: { type: 'non-mot', reponse: 'pasfrancais' },
+                                           date: Time.local(2019, 10, 9, 10, 1, 21, 950_000)),
+          build(:evenement_identification_mot, donnees: { type: 'non-mot', reponse: 'pasfrancais' },
+                                               date: Time.local(2019, 10, 9, 10, 1, 21, 960_000))
+        ]
+      end
+      it { expect(metrique_moyenne_mots_francais).to eq nil }
+    end
+  end
+end

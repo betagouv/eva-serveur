a9857d4a316d18ed6a0e398b370b3b0183127630
Clément MORISSET <cmorisset@captive.fr>
2020-02-21 12:52:36 +0100

    enrichit le test de persistence des métriques de Maintenance

--- spec/models/restitution/maintenance_spec.rb
+++ spec/models/restitution/maintenance_spec.rb
@@ -17,9 +17,15 @@ describe Restitution::Maintenance do
 
       it do
         expect(restitution).to receive(:nombre_non_reponses).and_return 2
+        expect(restitution).to receive(:nombre_bonnes_reponses_non_mot).and_return 20
+        expect(restitution).to receive(:temps_moyen_mots_français).and_return 0.789
+        expect(restitution).to receive(:temps_moyen_non_mots).and_return 1.5
         restitution.persiste
         partie.reload
         expect(partie.metriques['nombre_non_reponses']).to eq 2
+        expect(partie.metriques['nombre_bonnes_reponses_non_mot']).to eq 20
+        expect(partie.metriques['temps_moyen_mots_français']).to eq 0.789
+        expect(partie.metriques['temps_moyen_non_mots']).to eq 1.5
       end
     end
   end

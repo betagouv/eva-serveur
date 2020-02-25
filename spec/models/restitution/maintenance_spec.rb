13a3339a9dc87d3b43e48b0c8af987f84c4ea861
Étienne Charignon <etienne.charignon@gmail.com>
2020-02-25 14:33:55 +0000

    Calcule un score normalisé

--- spec/models/restitution/maintenance_spec.rb
+++ spec/models/restitution/maintenance_spec.rb
@@ -33,10 +33,10 @@ describe Restitution::Maintenance do
 
   describe '#score' do
     def score_pour(nombre_francais, temps_francais, nombre_non_mots, temps_non_mots)
+      allow(restitution).to receive(:temps_moyen_normalise)
+        .and_return(temps_francais, temps_non_mots)
       allow(restitution).to receive_messages(nombre_bonnes_reponses_francais: nombre_francais,
-                                             temps_moyen_mots_francais: temps_francais,
-                                             nombre_bonnes_reponses_non_mot: nombre_non_mots,
-                                             temps_moyen_non_mots: temps_non_mots)
+                                             nombre_bonnes_reponses_non_mot: nombre_non_mots)
       restitution.score
     end
 
@@ -44,5 +44,27 @@ describe Restitution::Maintenance do
     it { expect(score_pour(0, nil, 5, 1)).to eq(5 / 1) }
     it { expect(score_pour(2, 1.5, 0, nil)).to eq(2 / 1.5) }
     it { expect(score_pour(2, 1.5, 5, 3)).to eq(2 / 1.5 + 5 / 3) }
+  end
+
+  describe '#temps_moyen_normalise' do
+    context 'calcule le temps moyen normalises pour les mots francais' do
+      let(:situation) { create :situation_maintenance }
+      let(:evaluation) { create :evaluation, campagne: campagne }
+      let!(:partie) { create :partie, situation: situation, evaluation: evaluation }
+      let(:mock_metrique_temps) { double }
+
+      it do
+        allow(partie).to receive(:moyenne_metrique)
+          .with(:temps_moyen_mots_francais).and_return(2.7)
+        allow(partie).to receive(:ecart_type_metrique)
+          .with(:temps_moyen_mots_francais).and_return(0.5)
+        allow(restitution).to receive(:partie).and_return(partie)
+
+        expect(mock_metrique_temps).to receive(:calcule).and_return([2.7, 3.7, 3.8])
+        temps_moyen_normalise = restitution.temps_moyen_normalise(:temps_moyen_mots_francais,
+                                                                  mock_metrique_temps)
+        expect(temps_moyen_normalise).to eq((2.7 + 3.7) / 2.0)
+      end
+    end
   end
 end
---
+++ spec/models/restitution/metriques/temps_normalises_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe Restitution::Metriques::TempsNormalises do
+  describe '#temps_coherents' do
+    let(:mock_metrique_temps) { double }
+    let(:metrique_temps_coherents) do
+      described_class.new(mock_metrique_temps, 2.7, 0.5)
+    end
+
+    it 'supprime une valeur trop grande' do
+      expect(mock_metrique_temps).to receive(:calcule).and_return([2.7, 3.7, 3.8])
+      expect(metrique_temps_coherents.calcule([], [])).to eql([2.7, 3.7])
+    end
+
+    it 'supprime une valeur trop petite' do
+      expect(mock_metrique_temps).to receive(:calcule).and_return([1.7, 1.8, 2.7])
+      expect(metrique_temps_coherents.calcule([], [])).to eql([1.8, 2.7])
+    end
+  end
+end

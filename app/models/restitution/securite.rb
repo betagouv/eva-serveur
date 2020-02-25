2a18144876344b72b8c1399f6fd0144c8beaa475
Étienne Charignon <etienne.charignon@gmail.com>
2020-02-25 17:53:33 +0000

    Metrique sécurité : Introduit un type dans les règles pour l'affichage

--- app/models/restitution/securite.rb
+++ app/models/restitution/securite.rb
@@ -6,22 +6,62 @@ module Restitution
   class Securite < AvecEntrainement
     ZONES_DANGER = %w[bouche-egout camion casque escabeau signalisation].freeze
     METRIQUES = {
-      'temps_total' => Base::TempsTotal.new,
-      'temps_entrainement' => AvecEntrainement::TempsEntrainement.new,
-      'nombre_dangers_bien_identifies' => Securite::NombreDangersBienIdentifies.new,
-      'nombre_dangers_bien_identifies_avant_aide_1' =>
-                                  Securite::NombreDangersBienIdentifiesAvantAide1.new,
-      'nombre_dangers_mal_identifies' => Securite::NombreDangersMalIdentifies.new,
-      'attention_visuo_spatiale' => Securite::AttentionVisuoSpaciale.new,
-      'temps_bonnes_qualifications_dangers' => Securite::TempsBonnesQualificationsDangers.new,
-      'temps_recherche_zones_dangers' => Securite::TempsRechercheZonesDangers.new,
-      'temps_total_ouverture_zones_dangers' => Securite::TempsTotalOuvertureZonesDangers.new,
-      'nombre_reouverture_zones_sans_danger' => Securite::NombreReouvertureZonesSansDanger.new,
-      'nombre_bien_qualifies' => Securite::NombreDangersBienQualifies.new,
-      'nombre_retours_deja_qualifies' => Securite::NombreRetoursDejaQualifies.new,
-      'delai_ouvertures_zones_dangers' => Securite::DelaiOuverturesZonesDangers.new,
-      'delai_moyen_ouvertures_zones_dangers' =>
-                                  Metriques::Moyenne.new(Securite::DelaiOuverturesZonesDangers.new)
+      'temps_total' => {
+        'type' => :temps,
+        'instance' => Base::TempsTotal.new
+      },
+      'temps_entrainement' => {
+        'type' => :temps,
+        'instance' => AvecEntrainement::TempsEntrainement.new
+      },
+      'nombre_dangers_bien_identifies' => {
+        'type' => :nombre,
+        'instance' => Securite::NombreDangersBienIdentifies.new
+      },
+      'nombre_dangers_bien_identifies_avant_aide_1' => {
+        'type' => :nombre,
+        'instance' => Securite::NombreDangersBienIdentifiesAvantAide1.new
+      },
+      'nombre_dangers_mal_identifies' => {
+        'type' => :nombre,
+        'instance' => Securite::NombreDangersMalIdentifies.new
+      },
+      'attention_visuo_spatiale' => {
+        'type' => :texte,
+        'instance' => Securite::AttentionVisuoSpaciale.new
+      },
+      'temps_bonnes_qualifications_dangers' => {
+        'type' => :map,
+        'instance' => Securite::TempsBonnesQualificationsDangers.new
+      },
+      'temps_recherche_zones_dangers' => {
+        'type' => :map,
+        'instance' => Securite::TempsRechercheZonesDangers.new
+      },
+      'temps_total_ouverture_zones_dangers' => {
+        'type' => :map,
+        'instance' => Securite::TempsTotalOuvertureZonesDangers.new
+      },
+      'nombre_reouverture_zones_sans_danger' => {
+        'type' => :nombre,
+        'instance' => Securite::NombreReouvertureZonesSansDanger.new
+      },
+      'nombre_bien_qualifies' => {
+        'type' => :nombre,
+        'instance' => Securite::NombreDangersBienQualifies.new
+      },
+      'nombre_retours_deja_qualifies' => {
+        'type' => :nombre,
+        'instance' => Securite::NombreRetoursDejaQualifies.new
+      },
+      'delai_ouvertures_zones_dangers' => {
+        'type' => :liste_temps,
+        'instance' => Securite::DelaiOuverturesZonesDangers.new
+      },
+      'delai_moyen_ouvertures_zones_dangers' => {
+        'type' => :temps,
+        'instance' => Metriques::Moyenne.new(Securite::DelaiOuverturesZonesDangers.new)
+      }
     }.freeze
 
     def initialize(campagne, evenements)
@@ -43,7 +83,7 @@ module Restitution
 
     METRIQUES.keys.each do |metrique|
       define_method metrique do
-        METRIQUES[metrique]
+        METRIQUES[metrique]['instance']
           .calcule(evenements_situation, evenements_entrainement)
       end
     end

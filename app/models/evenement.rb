7319c38a38dd50f491c71d51c4c032a1c92dd4e7
Clément MORISSET <cmorisset@captive.fr>
2020-02-25 12:27:16 +0100

    déplace la constante DECORATOR dans spec_helper car elle n'est utilisé que pour les specs

--- app/models/evenement.rb
+++ app/models/evenement.rb
@@ -1,14 +1,6 @@
 # frozen_string_literal: true
 
-require_relative '../decorators/evenement_maintenance'
-require_relative '../decorators/evenement_securite'
-
 class Evenement < ApplicationRecord
-  DECORATORS = {
-    maintenance: EvenementMaintenance,
-    securite: EvenementSecurite
-  }.freeze
-
   validates :nom, :date, presence: true
   belongs_to :partie, foreign_key: :session_id, primary_key: :session_id
   delegate :situation, :evaluation, to: :partie

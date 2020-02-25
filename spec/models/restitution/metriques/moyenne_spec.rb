a3ed493b536072fcd7b22114da2398bb3042044b
Étienne Charignon <etienne.charignon@gmail.com>
2020-02-25 13:15:12 +0000

    factorise la création de l'instance dans le test de la moyenne

--- spec/models/restitution/metriques/moyenne_spec.rb
+++ spec/models/restitution/metriques/moyenne_spec.rb
@@ -9,21 +9,25 @@ class MockMetriqueAMoyenner < Restitution::Metriques::Base
 end
 
 describe Restitution::Metriques::Moyenne do
+  let(:metrique_moyenne) do
+    described_class.new(MockMetriqueAMoyenner.new)
+  end
+
   describe '#moyenne' do
     it "rend nil s'il n'y a pas de valeur" do
-      expect(described_class.new(MockMetriqueAMoyenner.new).calcule([], [])).to eql(nil)
+      expect(metrique_moyenne.calcule([], [])).to eql(nil)
     end
 
     it "calcul la moyenne d'une valeur" do
-      expect(described_class.new(MockMetriqueAMoyenner.new).calcule([1], [])).to eql(1.0)
+      expect(metrique_moyenne.calcule([1], [])).to eql(1.0)
     end
 
     it 'calcul la moyenne de deux valeurs entières' do
-      expect(described_class.new(MockMetriqueAMoyenner.new).calcule([1, 2], [])).to eql(1.5)
+      expect(metrique_moyenne.calcule([1, 2], [])).to eql(1.5)
     end
 
     it 'arrondi à quatre chiffres après la virgule' do
-      expect(described_class.new(MockMetriqueAMoyenner.new).calcule([1, 1, 2], [])).to eql(1.3333)
+      expect(metrique_moyenne.calcule([1, 1, 2], [])).to eql(1.3333)
     end
   end
 end

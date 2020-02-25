eac127c9a97d06edd7d63f25bde9b0bb1025b6d6
Étienne Charignon <etienne.charignon@gmail.com>
2020-02-25 13:15:44 +0000

    Ajoute une metrique de normalisation d'une liste de temps

---
+++ app/models/restitution/metriques/temps_coherents.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+module Restitution
+  module Metriques
+    class TempsCoherents < Restitution::Metriques::Base
+      def initialize(metriques_temps, moyenne, ecart_type)
+        @metriques_temps = metriques_temps
+        @moyenne = moyenne
+        @ecart_type = ecart_type
+      end
+
+      def calcule(evenements_situation, evenements_entrainement)
+        les_temps = @metriques_temps.calcule(evenements_situation, evenements_entrainement)
+
+        bornes = [@moyenne - 2 * @ecart_type, @moyenne + 2 * @ecart_type]
+
+        les_temps.select do |temps|
+          temps.between?(bornes.first, bornes.last)
+        end
+      end
+    end
+  end
+end

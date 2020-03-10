d89f4fad96676dfc42330dfc444d498bf62cb049
Étienne Charignon <etienne.charignon@gmail.com>
2020-03-10 10:46:20 +0000

    Extrait le score de maintenance dans une metrique
    Co-authored-by: Clément Morisset <clement.morisset@beta.gouv.fr>

--- spec/models/restitution/maintenance_spec.rb
+++ spec/models/restitution/maintenance_spec.rb
@@ -30,40 +30,4 @@ describe Restitution::Maintenance do
       end
     end
   end
-
-  describe '#score' do
-    def score_pour(nombre_francais, temps_francais, nombre_non_mots, temps_non_mots)
-      allow(restitution).to receive(:temps_moyen_normalise)
-        .and_return(temps_francais, temps_non_mots)
-      allow(restitution).to receive_messages(nombre_bonnes_reponses_francais: nombre_francais,
-                                             nombre_bonnes_reponses_non_mot: nombre_non_mots)
-      restitution.score
-    end
-
-    it { expect(score_pour(0, nil, 0, nil)).to eq(0) }
-    it { expect(score_pour(2, 1.5, 0, nil)).to eq(0) }
-    it { expect(score_pour(2, 1.5, 5, 3)).to eq((2 / 1.5) * (5 / 3)) }
-  end
-
-  describe '#temps_moyen_normalise' do
-    context 'calcule le temps moyen normalises pour les mots francais' do
-      let(:situation) { create :situation_maintenance }
-      let(:evaluation) { create :evaluation, campagne: campagne }
-      let!(:partie) { create :partie, situation: situation, evaluation: evaluation }
-      let(:mock_metrique_temps) { double }
-
-      it do
-        allow(partie).to receive(:moyenne_metrique)
-          .with(:temps_moyen_mots_francais).and_return(2.7)
-        allow(partie).to receive(:ecart_type_metrique)
-          .with(:temps_moyen_mots_francais).and_return(0.5)
-        allow(restitution).to receive(:partie).and_return(partie)
-
-        expect(mock_metrique_temps).to receive(:calcule).and_return([2.7, 3.7, 3.8])
-        temps_moyen_normalise = restitution.temps_moyen_normalise(:temps_moyen_mots_francais,
-                                                                  mock_metrique_temps)
-        expect(temps_moyen_normalise).to eq((2.7 + 3.7) / 2.0)
-      end
-    end
-  end
 end

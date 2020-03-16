286bc49aa899fb1ee89ba4c237cb8acc47a8e5ef
Stéphane Hanser <shanser@captive.fr>
2020-03-16 16:03:38 +0100

    Sait calculer et afficher un score de vocabulaire indéterminé
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

--- spec/models/restitution/maintenance/vocabulaire_spec.rb
+++ spec/models/restitution/maintenance/vocabulaire_spec.rb
@@ -13,6 +13,7 @@ describe Restitution::Maintenance::Vocabulaire do
     described_class.new(restitution)
   end
 
+  it { expect(niveau_pour_score(nil)).to evalue_a(Competence::NIVEAU_INDETERMINE) }
   it { expect(niveau_pour_score(-0.39)).to evalue_a(Competence::NIVEAU_1) }
   it { expect(niveau_pour_score(-0.4)).to evalue_a(Competence::NIVEAU_2) }
   it { expect(niveau_pour_score(-1.99)).to evalue_a(Competence::NIVEAU_2) }

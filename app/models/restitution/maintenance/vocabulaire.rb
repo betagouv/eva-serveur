e82b47d6827341096bad7cc807f5807c07ca88af
Stéphane Hanser <shanser@captive.fr>
2020-03-16 16:03:38 +0100

    Sait calculer et afficher un score de vocabulaire indéterminé
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

--- app/models/restitution/maintenance/vocabulaire.rb
+++ app/models/restitution/maintenance/vocabulaire.rb
@@ -5,6 +5,8 @@ module Restitution
     class Vocabulaire < Restitution::Competence::Base
       def niveau
         score = @restitution.partie.cote_z_metriques['score_vocabulaire']
+        return ::Competence::NIVEAU_INDETERMINE if score.blank?
+
         if score > -0.4 then ::Competence::NIVEAU_1
         elsif score > -2 then ::Competence::NIVEAU_2
         else ::Competence::NIVEAU_3

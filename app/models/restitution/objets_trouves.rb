36424f3de49e5630c9e719ea65cb80ee20df92c2
Stéphane Hanser <shanser@captive.fr>
2020-04-18 00:04:29 +0200

    Complète la restitution pour les objets trouvés

--- app/models/restitution/objets_trouves.rb
+++ app/models/restitution/objets_trouves.rb
@@ -1,6 +1,52 @@
 # frozen_string_literal: true
 
+require_relative '../../decorators/evenement_objets_trouves'
+
 module Restitution
-  class ObjetsTrouves < Base
+  class ObjetsTrouves < AvecEntrainement
+    METRIQUES = {
+      'nombre_bonnes_reponses_numeratie' => {
+        'type' => :nombre,
+        'metacompetence' => 'numeratie',
+        'instance' => Livraison::NombreBonnesReponses.new
+      },
+      'nombre_bonnes_reponses_ccf' => {
+        'type' => :nombre,
+        'metacompetence' => 'ccf',
+        'instance' => Livraison::NombreBonnesReponses.new
+      },
+      'nombre_bonnes_reponses_memorisation' => {
+        'type' => :nombre,
+        'metacompetence' => 'memorisation',
+        'instance' => Livraison::NombreBonnesReponses.new
+      },
+      'temps_moyen_bonnes_reponses_numeratie' => {
+        'type' => :nombre,
+        'metacompetence' => 'numeratie',
+        'instance' => Metriques::Moyenne.new(Livraison::TempsBonnesReponses.new)
+      },
+      'temps_moyen_bonnes_reponses_ccf' => {
+        'type' => :nombre,
+        'metacompetence' => 'ccf',
+        'instance' => Metriques::Moyenne.new(Livraison::TempsBonnesReponses.new)
+      },
+      'temps_moyen_bonnes_reponses_memorisation' => {
+        'type' => :nombre,
+        'metacompetence' => 'memorisation',
+        'instance' => Metriques::Moyenne.new(Livraison::TempsBonnesReponses.new)
+      }
+    }.freeze
+
+    def initialize(campagne, evenements)
+      evenements = evenements.map { |e| EvenementObjetsTrouves.new e }
+      super(campagne, evenements)
+    end
+
+    METRIQUES.keys.each do |metrique|
+      define_method metrique do
+        METRIQUES[metrique]['instance']
+          .calcule(evenements_situation, METRIQUES[metrique]['metacompetence'])
+      end
+    end
   end
 end

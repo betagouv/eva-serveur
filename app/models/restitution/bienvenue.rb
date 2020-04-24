a1bb68cbff39c6cc9d2e0dc48153b7ebb7d9cbdf
Stéphane Hanser <shanser@captive.fr>
2020-04-24 12:44:33 +0200

    Affiche les réponses de la Bienvenue dans la restitution globale Co-authored-by: Clément Morisset <clement.morisset@beta.gouv.fr>

--- app/models/restitution/bienvenue.rb
+++ app/models/restitution/bienvenue.rb
@@ -2,5 +2,37 @@
 
 module Restitution
   class Bienvenue < Base
+    EVENEMENT = {
+      REPONSE: 'reponse'
+    }.freeze
+
+    def questions_et_reponses
+      questions_repondues
+        .map do |question|
+        {
+          question: question,
+          reponse: trouve_reponse(question.id)
+        }
+      end
+    end
+
+    def reponses
+      evenements.find_all { |e| e.nom == EVENEMENT[:REPONSE] }
+    end
+
+    def questions
+      situation.questionnaire.questions
+    end
+
+    def questions_repondues
+      questions_ids = reponses.collect { |r| r.donnees['question'] }
+      questions.where(id: questions_ids)
+    end
+
+    def trouve_reponse(question_id)
+      reponses.find do |evenement|
+        evenement.donnees['question'] == question_id
+      end.donnees['reponse']
+    end
   end
 end

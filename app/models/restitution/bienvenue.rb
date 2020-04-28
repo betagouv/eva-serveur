89fc96ed5845ae1630f8d6fef03fd5cafe94cd6e
Clément MORISSET <cmorisset@captive.fr>
2020-04-28 12:47:30 +0200

    Selectionne directement la restitution d'autopositionnement plutot que de boucler sur les restitutions
    Co-authored-by: Marine Dominé <marine@captive.fr>

--- app/models/restitution/bienvenue.rb
+++ app/models/restitution/bienvenue.rb
@@ -2,6 +2,8 @@
 
 module Restitution
   class Bienvenue < Base
+    NOM_TECHNIQUE = 'bienvenue'
+
     def questions_et_reponses
       qr = QuestionsReponses.new(evenements, situation.questionnaire)
       qr.questions_et_reponses

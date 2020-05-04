ad7fb13e45ae1ac5aa50d2be53caac0f98a94343
Stéphane Hanser <shanser@captive.fr>
2020-05-04 17:56:32 +0200

    Affiche le nombre d'évaluations par structure
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

--- app/admin/structures.rb
+++ app/admin/structures.rb
@@ -6,6 +6,9 @@ ActiveAdmin.register Structure do
   index do
     column :nom
     column :code_postal
+    column :nombre_evaluations do |structure|
+      Evaluation.joins(campagne: :compte).where('comptes.structure_id' => structure).count
+    end
     column :created_at
     actions
   end

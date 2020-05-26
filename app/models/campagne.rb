aef652b1e839741161058fedcb195c69f7db79bc
Étienne Charignon <etienne.charignon@gmail.com>
2020-05-26 13:03:59 +0000

    Nouvelle structure : Initialise la campagne avec des situations

--- app/models/campagne.rb
+++ app/models/campagne.rb
@@ -1,6 +1,17 @@
 # frozen_string_literal: true
 
 class Campagne < ApplicationRecord
+  SITUATIONS_PAR_DEFAUT = %i[
+    bienvenue
+    maintenance
+    inventaire
+    livraison
+    tri
+    controle
+    securite
+    objets_trouves
+  ].freeze
+
   has_many :situations_configurations, -> { order(position: :asc) }, dependent: :destroy
   has_many :situations, through: :situations_configurations
   belongs_to :questionnaire, optional: true
@@ -14,11 +25,22 @@ class Campagne < ApplicationRecord
   accepts_nested_attributes_for :situations_configurations, allow_destroy: true
   accepts_nested_attributes_for :compte
 
+  attr_accessor :initialise_situations
+
+  before_create :initialise_situations_par_defaut, if: :initialise_situations
+
   def display_name
     libelle
   end
 
   private
+
+  def initialise_situations_par_defaut
+    SITUATIONS_PAR_DEFAUT.each do |nom_technique|
+      situation = Situation.find_by nom_technique: nom_technique
+      situations << situation unless situation.nil?
+    end
+  end
 
   def genere_code_unique
     return if self[:code].present?

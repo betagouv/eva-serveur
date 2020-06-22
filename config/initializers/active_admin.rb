1a7f1522bc9ede0086f299953485c2cd9f745cfc
Stéphane Hanser <shanser@captive.fr>
2020-06-22 18:43:26 +0200

    Permet de faire des exports XLS depuis l'admin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -251,7 +251,7 @@ ActiveAdmin.setup do |config|
   #     admin.download_links = proc { can?(:view_download_links) }
   #
   #   end
-  config.download_links = [:csv]
+  config.download_links = %i[csv xls]
 
   # == Pagination
   #

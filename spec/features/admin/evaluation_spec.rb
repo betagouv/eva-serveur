4dcf973ce35972a021b60e773602e251bcdc0181
Étienne Charignon <etienne.charignon@gmail.com>
2020-08-05 11:25:20 +0200

    Restitution illetrisme socle clea

--- spec/features/admin/evaluation_spec.rb
+++ spec/features/admin/evaluation_spec.rb
@@ -29,14 +29,6 @@ describe 'Admin - Evaluation', type: :feature do
       Restitution::Bienvenue.new(mon_evaluation_bienvenue, [evenement_bienvenue])
     end
 
-    let(:restitution_globale) do
-      double(Restitution::Globale,
-             date: DateTime.now,
-             utilisateur: 'Roger',
-             efficience: 5,
-             restitutions: [restitution])
-    end
-
     it 'sans auto_positionnement' do
       visit admin_campagne_evaluation_path(ma_campagne, mon_evaluation)
       expect(page).to_not have_content 'Auto-positionnement'
@@ -49,17 +41,51 @@ describe 'Admin - Evaluation', type: :feature do
       expect(page).to have_content 'Roger'
     end
 
-    it "affiche l'évaluation en pdf" do
-      competences = [{ Competence::ORGANISATION_METHODE => Competence::NIVEAU_4 }]
-      expect(restitution_globale).to receive(:niveaux_competences).and_return(competences)
-      allow(restitution_globale).to receive(:scores).and_return({})
-      allow(restitution_globale).to receive(:interpretations_niveau1).and_return([])
-      expect(FabriqueRestitution).to receive(:restitution_globale).and_return(restitution_globale)
-      visit admin_campagne_evaluation_path(ma_campagne, mon_evaluation, format: :pdf)
-      path = page.save_page
+    describe 'en moquant restitution_globale :' do
+      let(:restitution_globale) do
+        double(Restitution::Globale,
+               date: DateTime.now,
+               utilisateur: 'Roger',
+               efficience: 5,
+               restitutions: [restitution])
+      end
 
-      reader = PDF::Reader.new(path)
-      expect(reader.page(1).text).to include('Roger')
+      before do
+        competences = [{ Competence::ORGANISATION_METHODE => Competence::NIVEAU_4 }]
+        expect(restitution_globale).to receive(:niveaux_competences).and_return(competences)
+        expect(FabriqueRestitution).to receive(:restitution_globale).and_return(restitution_globale)
+      end
+
+      it 'affiche le niveau litteratie et numératie' do
+        expect(restitution_globale).to receive(:interpretations_niveau1)
+          .and_return([{ litteratie: :a1 }, { numeratie: :x1 }])
+        visit admin_campagne_evaluation_path(ma_campagne, mon_evaluation_bienvenue)
+        expect(page).to have_content 'Niveau A1'
+        expect(page).to have_content 'Niveau X1'
+      end
+
+      it "affiche que le score n'a pas pu être calculé" do
+        expect(restitution_globale).to receive(:interpretations_niveau1)
+          .and_return([{ litteratie: nil }])
+        visit admin_campagne_evaluation_path(ma_campagne, mon_evaluation_bienvenue)
+        expect(page).to have_content "Votre score n'a pas pu être calculé"
+      end
+
+      it "Socle cléa en cours d'acquisition" do
+        expect(restitution_globale).to receive(:interpretations_niveau1)
+          .and_return([{ socle_clea: :atteint }])
+        visit admin_campagne_evaluation_path(ma_campagne, mon_evaluation_bienvenue)
+        expect(page).to have_content 'Socle Cléa'
+      end
+
+      it "affiche l'évaluation en pdf" do
+        allow(restitution_globale).to receive(:interpretations_niveau1).and_return([])
+        visit admin_campagne_evaluation_path(ma_campagne, mon_evaluation, format: :pdf)
+        path = page.save_page
+
+        reader = PDF::Reader.new(path)
+        expect(reader.page(1).text).to include('Roger')
+      end
     end
   end
 

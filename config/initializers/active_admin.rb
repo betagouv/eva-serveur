45eef1dc62e32e8ea812508bd5bf1b671f778d1f
Étienne Charignon <etienne.charignon@gmail.com>
2020-09-08 14:12:36 +0000

    Ajoute le code de tracking hotjar

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -196,11 +196,11 @@ ActiveAdmin.setup do |config|
   #   config.register_stylesheet 'my_stylesheet.css'
   #
   # You can provide an options hash for more control, which is passed along to stylesheet_link_tag():
-    config.register_stylesheet 'restitution_globale/restitution_globale.css'
+  config.register_stylesheet 'restitution_globale/restitution_globale.css'
 
   #
   # To load a javascript file:
-  #   config.register_javascript 'my_javascript.js'
+  config.register_javascript 'hotjar.js'
 
   # == CSV options
   #

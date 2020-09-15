d89386a6695564f010a99a1b54eddd329d37ba61
Étienne Charignon <etienne.charignon@gmail.com>
2020-09-15 16:19:30 +0200

    Ajoute une tache de recalcule des métriques
    Cette tache prend enormement de temps et ce n'était plus vraiment
    gérable de faire ça avec un bouton sur l'interface d'admin

---
+++ spec/tasks/recalcule_metriques_spec.rb
@@ -0,0 +1,61 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'nettoyage:recalcule_metriques' do
+  include_context 'rake'
+  let(:logger) { RakeLogger.logger }
+
+  let!(:tri) { create :situation_tri, libelle: 'Situation Tri' }
+  let(:evaluation) { create :evaluation }
+  let!(:partie) do
+    create :partie,
+           evaluation: evaluation,
+           situation: tri,
+           metriques: { test: 'test' }
+  end
+
+  context 'appelé avec un nom de situation valide' do
+    before do
+      allow(logger).to receive :info
+      allow(FabriqueRestitution).to receive(:instancie).with(partie.id).and_return restitution
+      ENV['SITUATION'] = 'tri'
+    end
+
+    context 'persiste les restitutions terminées' do
+      let(:restitution) { double(termine?: true) }
+
+      it do
+        expect(restitution).to receive(:persiste)
+        subject.invoke
+      end
+    end
+
+    context 'ignore les restitutions non terminees' do
+      let(:restitution) { double(termine?: false) }
+
+      it do
+        expect(restitution).to_not receive(:persiste)
+        subject.invoke
+      end
+    end
+  end
+
+  context 'appellé avec une situation inconnue' do
+    it do
+      ENV['SITUATION'] = 'inconnue'
+      expect(logger).to receive(:error).with('Situation "inconnue" non trouvé')
+      subject.invoke
+    end
+  end
+
+  context 'appellé sans situation' do
+    it do
+      expect(logger).to receive(:error)
+        .with("La variable d'environnement SITUATION est marquante")
+      expect(logger).to receive(:error)
+        .with('Usage : rake nettoyage:recalcule_metriques SITUATION=<nom_technique>')
+      subject.invoke
+    end
+  end
+end

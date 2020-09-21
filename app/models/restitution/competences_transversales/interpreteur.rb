d0a3137df78b1e1ae4dba03de982fd6a4afae805
Étienne Charignon <etienne.charignon@gmail.com>
2020-09-21 17:21:21 +0200

    Ajoute l'interpreteur de niveau de compétences transversales

---
+++ app/models/restitution/competences_transversales/interpreteur.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Restitution
+  module CompetencesTransversales
+    class Interpreteur
+      def initialize(niveaux_competences)
+        @niveaux_competences = niveaux_competences
+      end
+
+      def interpretations
+        @niveaux_competences.map do |competence, niveau|
+          [competence, interprete(niveau)]
+        end
+      end
+
+      def interprete(niveau)
+        case niveau
+        when 4 then 3
+        when 1 then 1
+        else 2
+        end
+      end
+    end
+  end
+end

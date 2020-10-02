7a48bba0760054c9289a02320149c400927e2432
Marine Dominé <marine@captive.fr>
2020-10-02 14:02:00 +0200

    Corrige la ligne blanche sur le bord droit du pdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -21,5 +21,7 @@ WickedPdf.config = {
   margin: { top:    0,
             bottom: 0,
             left:   0,
-            right:  0 }
+            right:  0 },
+  page_width: 212,
+  page_height: 297
 }

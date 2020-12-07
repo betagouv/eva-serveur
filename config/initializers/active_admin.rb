a61c393bac024790e838b43ca47a758b22e48e22
Stéphane Hanser <shanser@captive.fr>
2020-12-07 17:26:52 +0100

    Revert "Ajoute un lien vers la vidéo explicative pour conseiller"
    This reverts commit d05920a59335c6e97aaa89b59df4ac2285b06bb9.

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -227,11 +227,11 @@ ActiveAdmin.setup do |config|
   #
   # If you wanted to add a static menu item to the default menu provided:
   #
-    config.namespace :admin do |admin|
-      admin.build_menu :default do |menu|
-        menu.add label: "Aide ▶️", url: 'https://youtu.be/WC9nICmMgbY', priority: 99, html_options: { target: :blank }
-      end
-    end
+  #   config.namespace :admin do |admin|
+  #     admin.build_menu :default do |menu|
+  #       menu.add label: "My Great Website", url: "http://www.mygreatwebsite.com", html_options: { target: :blank }
+  #     end
+  #   end
 
   # == Download Links
   #

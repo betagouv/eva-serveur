c9794e18240de1a850591a5b4158aeca8924d691
Étienne Charignon <etienne.charignon@gmail.com>
2020-12-08 12:08:59 +0100

    Ajout de deux nouvelles sources d'aide

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,3 +24,15 @@ SourceAide.find_or_create_by(titre: 'Exemples de restitution') do |source_aide|
   source_aide.categorie= :prise_en_main
   source_aide.type_document= :repertoire
 end
+SourceAide.find_or_create_by(titre: 'Vidéo introductive pour les candidats et les candidates') do |source_aide|
+  source_aide.description="Vidéo accessible sur Youtube\nDurée : 1mn50"
+  source_aide.url= 'https://youtu.be/T7Gh0sMZBqY'
+  source_aide.categorie= :animer_restituer
+  source_aide.type_document= :video
+end
+SourceAide.find_or_create_by(titre: 'Document d’interprétation des résultats eva en français et mathématiques') do |source_aide|
+  source_aide.description= "Document consultable en ligne"
+  source_aide.url= 'https://docs.google.com/document/d/e/2PACX-1vQ0mYR22VK-y_vX0buYtxTQBb6VuLZbMKfDuU0O7p1pi_y5nsbP1HOhiW5MPysbAhszxNIQ4lIin8s6/pub'
+  source_aide.categorie= :animer_restituer
+  source_aide.type_document= :web_doc
+end

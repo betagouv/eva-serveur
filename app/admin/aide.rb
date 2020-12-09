9c7a159a8a21e326a28902d96ac344327e7e352c
Étienne Charignon <etienne.charignon@gmail.com>
2020-12-09 17:30:39 +0100

    Ajout des faq sur la page d'aide

--- app/admin/aide.rb
+++ app/admin/aide.rb
@@ -7,5 +7,6 @@ ActiveAdmin.register_page 'Aide' do
     SourceAide.sources_par_categorie.each do |categorie, sources|
       render 'aide', sources_aide: sources, titre: categorie
     end
+    render 'faq', questions_frequentes: Aide::QuestionFrequente.all
   end
 end

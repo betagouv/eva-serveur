4f875145b2b9e10bcbf6f8a5e23779d31d7febe1
Marine Dominé <marine@captive.fr>
2020-12-18 14:39:18 +0100

    Aide : Invitation contact

--- app/admin/aide.rb
+++ app/admin/aide.rb
@@ -4,6 +4,7 @@ ActiveAdmin.register_page 'Aide' do
   menu priority: 99
 
   content do
+    render 'contact'
     SourceAide.sources_par_categorie.each do |categorie, sources|
       render 'aide', sources_aide: sources, titre: categorie
     end

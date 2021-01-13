bae125f8ee05061f80b9af6062fb0f48251724f2
Stéphane Hanser <shanser@captive.fr>
2021-01-13 14:30:29 +0100

    Change le lien matomo avec un `/` à la fin pour corriger l'erreur de chargement

--- app/assets/javascripts/matomo.js
+++ app/assets/javascripts/matomo.js
@@ -1,4 +1,4 @@
 // Matomo Tracking Code for https://api.app.eva.beta.gouv.fr
-tarteaucitron.user.matomoHost = 'https://stats.data.gouv.fr';
+tarteaucitron.user.matomoHost = 'https://stats.data.gouv.fr/';
 tarteaucitron.user.matomoId = 149;
 (tarteaucitron.job = tarteaucitron.job || []).push('matomohightrack');

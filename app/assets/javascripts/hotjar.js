ee146daadfdfdbecb354195d4f39e8ee96101239
Étienne Charignon <etienne.charignon@gmail.com>
2021-01-19 18:35:43 +0100

    Défini les ids matomo et hotjar avec des variables d'environnement
    De façon à permettre une configuration différente entre la production et
    la pré-production

--- app/assets/javascripts/hotjar.js
+++
@@ -1,3 +0,0 @@
-// Hotjar Tracking Code for https://api.app.eva.beta.gouv.fr
-(tarteaucitron.job = tarteaucitron.job || []).push('hotjar');
-tarteaucitron.user.hotjarId = 1981301;tarteaucitron.user.HotjarSv = 6;

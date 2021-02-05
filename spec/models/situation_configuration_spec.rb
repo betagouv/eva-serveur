afe6656ce983f062197093ef558b8477710b3e89
Stéphane Hanser <shanser@captive.fr>
2021-02-05 14:30:18 +0100

    Enlève à la campagne la responsabilité de connaitre ses questionnaires

--- spec/models/situation_configuration_spec.rb
+++ spec/models/situation_configuration_spec.rb
@@ -4,4 +4,22 @@ require 'rails_helper'
 
 describe SituationConfiguration do
   it { should validate_uniqueness_of(:situation_id).scoped_to(:campagne_id).case_insensitive }
+
+  describe '#questionnaire_utile' do
+    let(:situation_configuree) { described_class.new }
+    let(:questionnaire_situation) { double }
+    let(:situation) { double questionnaire: questionnaire_situation }
+    before { allow(situation_configuree).to receive(:situation).and_return situation }
+
+    context 'sans surcharge' do
+      before { situation_configuree.questionnaire = nil }
+      it { expect(situation_configuree.questionnaire_utile).to eq questionnaire_situation }
+    end
+
+    context 'avec surcharge' do
+      let(:questionnaire_surcharge) { Questionnaire.new }
+      before { situation_configuree.questionnaire = questionnaire_surcharge }
+      it { expect(situation_configuree.questionnaire_utile).to eq questionnaire_surcharge }
+    end
+  end
 end

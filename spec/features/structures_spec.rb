d8213a4173a3938e8a7377b179f813bb34b4c235
Stéphane Hanser <shanser@captive.fr>
2021-03-15 16:43:10 +0100

    Permet de trouver une structure par code postal

--- spec/features/structures_spec.rb
+++ spec/features/structures_spec.rb
@@ -6,9 +6,12 @@ describe 'Structures', type: :feature do
   describe '#index liste les structures du même code code postal' do
     let!(:structure_paris) { create :structure, nom: 'MILO Paris', code_postal: '75012' }
     let!(:structure_lyon) { create :structure, nom: 'MILO Lyon', code_postal: '69000' }
-    before { visit structures_path(code_postal: '75012') }
 
     it do
+      visit structures_path
+      expect(page).to_not have_content 'MILO Paris'
+      fill_in :code_postal, with: '75012'
+      click_on 'chercher'
       expect(page).to have_content 'MILO Paris'
       expect(page).to_not have_content 'MILO Lyon'
     end

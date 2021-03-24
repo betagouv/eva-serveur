b7a330044d5339903678004e706c1a0d6541caac
Stéphane Hanser <shanser@captive.fr>
2021-03-24 14:20:16 +0100

    Retire le bloc contact du tableau de bord

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -5,13 +5,11 @@ ActiveAdmin.register_page 'Dashboard' do
 
   content title: proc { I18n.t('active_admin.dashboard') } do
     evaluations = Evaluation.accessible_by(current_ability).order(created_at: :desc).limit(10)
-    contacts = Contact.where(saisi_par: current_compte)
     actualites = Actualite.order(created_at: :desc).first(4)
 
     render partial: 'dashboard',
            locals: {
              evaluations: evaluations,
-             contacts: contacts,
              actualites: actualites
            }
   end

21c6ec4ab03c916fe46a193e00b80833aff9782d
Clément Prod'homme <clement@captive.fr>
2021-04-02 15:18:22 +0200

    Ne plus permettre d'accéder à la page /sign_up sans structure
    Si on tente d'accéder à la page /admin/sign_up sans paramètre `structure_id`, on est redirigé vers /structures afin de trouver d'abbord une structure ici.

--- app/controllers/eva/devise/registrations_controller.rb
+++ app/controllers/eva/devise/registrations_controller.rb
@@ -4,6 +4,8 @@ module Eva
   module Devise
     class RegistrationsController < ActiveAdmin::Devise::RegistrationsController
       def new
+        return redirect_to structures_path if params[:structure_id].blank?
+
         super do |resource|
           resource.structure = Structure.find_by id: params[:structure_id]
         end

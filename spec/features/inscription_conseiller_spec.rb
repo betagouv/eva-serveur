06f80838d42b08397720aa675b33194481b3896e
Clément Prod'homme <clement@captive.fr>
2021-04-02 15:18:22 +0200

    Ne plus permettre d'accéder à la page /sign_up sans structure
    Si on tente d'accéder à la page /admin/sign_up sans paramètre `structure_id`, on est redirigé vers /structures afin de trouver d'abbord une structure ici.

--- spec/features/inscription_conseiller_spec.rb
+++ spec/features/inscription_conseiller_spec.rb
@@ -8,22 +8,10 @@ describe 'Création de compte conseiller', type: :feature do
   context 'sans structure précisée' do
     before do
       visit new_compte_registration_path
-      fill_in :compte_prenom, with: 'Julia'
-      fill_in :compte_nom, with: 'Robert'
-      fill_in :compte_email, with: 'monemail@eva.fr'
-      fill_in :compte_telephone, with: '01 02 03 04 05'
-      fill_in :compte_password, with: 'Pass123'
-      fill_in :compte_password_confirmation, with: 'Pass123'
-      select 'Ma structure'
     end
 
-    it do
-      expect { click_on "S'inscrire" }.to change(Compte, :count).by 1
-
-      nouveau_compte = Compte.find_by email: 'monemail@eva.fr'
-      expect(nouveau_compte.validation_en_attente?).to be true
-      expect(nouveau_compte.structure).to eq structure
-      expect(nouveau_compte.telephone).to eq '01 02 03 04 05'
+    it "redirige l'utilisateur vers la page pour trouver une structure" do
+      expect(page).to have_current_path(structures_path)
     end
   end
 

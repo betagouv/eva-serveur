0daaab4b92fc8738180234534e89c1c7cb574694
Stéphane Hanser <shanser@captive.fr>
2021-04-26 17:01:05 +0200

    Retire du code mort
    Co-authored-by: Clément Prod'Homme <clement@captive.fr>
    Co-authored-by: Benjamin Bartholet <hello@trab.design>

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -196,7 +196,6 @@ ActiveAdmin.setup do |config|
   #   config.register_stylesheet 'my_stylesheet.css'
   #
   # You can provide an options hash for more control, which is passed along to stylesheet_link_tag():
-  config.register_stylesheet 'restitution_globale/restitution_globale.css'
 
   #
   # To load a javascript file:

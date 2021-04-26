012c0a836946f4d28817cdcac72d6af26f547d7c
Stéphane Hanser <shanser@captive.fr>
2021-04-26 18:42:54 +0200

    Renomme menu aide en menu transverse
    Co-authored-by: Marine Dominé <marine.domine@beta.gouv.fr>

--- app/assets/javascripts/aide.js
+++ app/assets/javascripts/aide.js
@@ -11,7 +11,7 @@ const onIntersection = (entries) => {
   var sections_visibles = sections.filter(s => {
     return isInViewport(document.querySelector(s))
   });
-  document.querySelectorAll('.menu_aide ul li a').forEach(el => {
+  document.querySelectorAll('.menu-transverse ul li a').forEach(el => {
     if(el.hash === sections_visibles[0]) {
       el.classList.add('active');
     } else {
@@ -22,7 +22,7 @@ const onIntersection = (entries) => {
 
 document.addEventListener("DOMContentLoaded", () => {
   const observer = new IntersectionObserver(onIntersection);
-  document.querySelectorAll('.menu_aide ul li a').forEach(el => {
+  document.querySelectorAll('.menu-transverse ul li a').forEach(el => {
     observer.observe(document.querySelector(el.hash));
     sections.push(el.hash);
   });

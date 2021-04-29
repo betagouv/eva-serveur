8d9e61e7c71530dbd8f812d857fa61bef28d2424
Clément Prod'homme <clement@captive.fr>
2021-04-29 14:58:35 +0200

    Permet de filtrer les structures inactives et abandonnistes
    Et corrige la requête des pas vraiment utilisatrices
    + Affiche la dernière évaluation dans la liste des structures
    
    Co-authored-by: Stéphane Hanser <shanser@captive.fr>

--- app/models/structure.rb
+++ app/models/structure.rb
@@ -19,15 +19,35 @@ class Structure < ApplicationRecord
 
   after_validation :geocode, if: ->(s) { s.code_postal.present? and s.code_postal_changed? }
 
-  scope :par_nombre_d_evaluations, lambda { |condition_nb_evaluations|
+  scope :joins_evaluations_et_groupe, lambda {
     joins('INNER JOIN comptes ON structures.id = comptes.structure_id')
       .joins('INNER JOIN campagnes ON comptes.id = campagnes.compte_id')
+      .joins('INNER JOIN evaluations ON campagnes.id = evaluations.campagne_id')
       .group('structures.id')
-      .having("SUM(campagnes.nombre_evaluations) #{condition_nb_evaluations}")
   }
-  scope :pas_vraiment_utilisatrices, -> { par_nombre_d_evaluations '= 0' }
+  scope :par_nombre_d_evaluations, lambda { |condition_nb_evaluations|
+    joins_evaluations_et_groupe
+      .having("COUNT(evaluations.id) #{condition_nb_evaluations}")
+  }
+  scope :par_derniere_evaluation, lambda { |*condition_date|
+    condition_date[0] = "MAX(evaluations.created_at) #{condition_date[0]}"
+    joins_evaluations_et_groupe
+      .having(*condition_date)
+  }
+
+  scope :pas_vraiment_utilisatrices, lambda {
+    ids = Evaluation.joins(campagne: { compte: :structure })
+                    .select('structures.id')
+    where.not(id: ids)
+  }
+
   scope :non_activees, -> { par_nombre_d_evaluations 'BETWEEN 1 AND 3' }
-  scope :actives, -> { par_nombre_d_evaluations '> 3' }
+  scope :activees, -> { par_nombre_d_evaluations '> 3' }
+  scope :actives, -> { activees.par_derniere_evaluation('> ?', 2.months.ago) }
+  scope :inactives, lambda {
+    activees.par_derniere_evaluation('BETWEEN ? AND ?', 6.months.ago, 2.months.ago)
+  }
+  scope :abandonnistes, -> { activees.par_derniere_evaluation('< ?', 6.months.ago) }
 
   def display_name
     nom

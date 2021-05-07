c6c63ee6c627373ee72bb553ead1d4ff371d1f30
Clément Prod'homme <clement@captive.fr>
2021-05-07 16:13:06 +0200

    N'affiche pas les parties des superadmins dans le back-office

--- spec/features/admin/parties_spec.rb
+++ spec/features/admin/parties_spec.rb
@@ -18,5 +18,18 @@ describe 'Admin - Parties', type: :feature do
       expect(page).to have_content('evaluation 1')
       expect(page).to have_content('evaluation 2')
     end
+
+    it 'exclue les parties des superadmin' do
+      superadmin = create :compte_superadmin
+      campagne = create :campagne, compte: superadmin
+      evaluation_superadmin = create :evaluation, nom: 'evaluation_superadmin', campagne: campagne
+      create :partie, situation: situation, evaluation: evaluation_superadmin
+
+      visit admin_situation_parties_path(situation)
+
+      within('#index_table_parties') do
+        expect(page).not_to have_content('evaluation_superadmin')
+      end
+    end
   end
 end

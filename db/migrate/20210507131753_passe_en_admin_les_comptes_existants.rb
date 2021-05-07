6c15150e7842e99dea780dfb22d867df479599a0
Clément Prod'homme <clement@captive.fr>
2021-05-07 17:45:37 +0200

    Fix les migrations quand le model Compte évolue

--- db/migrate/20210507131753_passe_en_admin_les_comptes_existants.rb
+++ db/migrate/20210507131753_passe_en_admin_les_comptes_existants.rb
@@ -1,4 +1,6 @@
 class PasseEnAdminLesComptesExistants < ActiveRecord::Migration[6.1]
+  class Compte < ApplicationRecord; end
+
   def up
     Compte.where(role: 'organisation').update_all(role: 'admin')
   end

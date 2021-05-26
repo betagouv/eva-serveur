563e162d69381e1330c204374d23b01d8a608087
Marine Dominé <marine@captive.fr>
2021-05-26 12:58:35 +0200

    Met à jour le lien du pas à pas

--- app/lib/eva.rb
+++ app/lib/eva.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Eva
-  DOCUMENT_PRISE_EN_MAIN = 'https://drive.google.com/file/d/1bsNfv0viYwM9-uOSPdwVOi7yr_hRzI7C/view'
+  DOCUMENT_PRISE_EN_MAIN = 'https://docs.google.com/presentation/d/e/2PACX-1vSXUf1hvRGRne4GwuFDAc-0ALYMD_c6DT1Wpz3SkPaNTRmHFgu_pa6QDpVs7R_phy8YFqKHLXZj5fiy/pub?start=false&loop=false&delayms=3000'
   EMAIL_CONTACT = 'contact@eva.beta.gouv.fr'
   LIEN_LIVESTORM = 'https://app.livestorm.co/eva-diagnostic-cptc/presentation-et-demonstration-eva-session?type=detailed'
 end

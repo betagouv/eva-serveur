261972340fb43297dc2ab5444e9fce5656ebbb87
Clément Prod'homme <clement@captive.fr>
2021-06-16 18:20:47 +0200

    Permet de créer une structure eva et un compte "eva Bot" via le seed

--- app/lib/eva.rb
+++ app/lib/eva.rb
@@ -3,5 +3,6 @@
 module Eva
   DOCUMENT_PRISE_EN_MAIN = 'https://docs.google.com/presentation/d/e/2PACX-1vSXUf1hvRGRne4GwuFDAc-0ALYMD_c6DT1Wpz3SkPaNTRmHFgu_pa6QDpVs7R_phy8YFqKHLXZj5fiy/pub?start=false&loop=false&delayms=3000'
   EMAIL_CONTACT = 'contact@eva.beta.gouv.fr'
+  EMAIL_SUPPORT = 'support@eva.beta.gouv.fr'
   LIEN_LIVESTORM = 'https://app.livestorm.co/eva-diagnostic-cptc/presentation-et-demonstration-eva-session?type=detailed'
 end

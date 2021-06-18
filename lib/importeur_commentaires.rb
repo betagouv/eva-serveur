73dffd18e68dc5664e5ef592e2f5119861385d5a
Étienne Charignon <etienne.charignon@gmail.com>
2021-06-18 17:00:59 +0200

    Import Airtable : Ajout des logs, n'importe pas les lignes vides, ajoute le nom du champs dans le commentaire

--- lib/importeur_commentaires.rb
+++ lib/importeur_commentaires.rb
@@ -5,19 +5,25 @@ require 'rake_logger'
 class ImporteurCommentaires
   class << self
     def importe(ligne, auteur)
+      return if ligne[:notes].blank? && ligne[:utilisation_eva].blank?
+
       destinataire = Compte.find_by email: ligne[:mail]
 
+      donnees = "#{ligne[:mail]},#{ligne[:utilisation_eva]},#{ligne[:notes]}"
       if destinataire.blank?
-        RakeLogger.logger.info "Commentaire ignoré pour le compte inconnu : #{ligne[:mail]}"
+        RakeLogger.logger.warn "Commentaire ignoré pour le compte inconnu : #{donnees}"
       else
+        RakeLogger.logger.info "Importe : #{donnees}"
         cree_commentaires(auteur, destinataire, ligne)
       end
     end
 
     def cree_commentaires(auteur, destinataire, ligne)
       %i[utilisation_eva notes].each do |champ|
+        next if ligne[champ].blank?
+
         ActiveAdmin::Comment.where(author: auteur,
-                                   body: ligne[champ],
+                                   body: "#{champ} : #{ligne[champ]}",
                                    resource: destinataire.structure,
                                    namespace: :admin)
                             .first_or_create
--- spec/lib/importeur_commentaires_spec.rb
+++ spec/lib/importeur_commentaires_spec.rb
@@ -5,24 +5,40 @@ require 'importeur_commentaires'
 
 describe ImporteurCommentaires do
   let(:evabot) { create :compte, :superadmin, email: Eva::EMAIL_SUPPORT }
+  let(:logger) { RakeLogger.logger }
 
   context 'commentaire sur un compte existant' do
     let(:structure) { create :structure }
     let!(:compte) { create :compte, email: 'mon-conseiller@milo.fr', structure: structure }
     let(:ligne) { { utilisation_eva: 'Mon utilisation', mail: 'mon-conseiller@milo.fr' } }
 
+    before do
+      allow(logger).to receive(:info)
+    end
+
     it do
+      expect(logger).to receive(:info)
+        .with('Importe : mon-conseiller@milo.fr,Mon utilisation,')
       expect { ImporteurCommentaires.importe(ligne, evabot) }
         .to(change { ActiveAdmin::Comment.count })
       commentaire = ActiveAdmin::Comment.last
       expect(commentaire.resource).to eq structure
-      expect(commentaire.body).to eq 'Mon utilisation'
+      expect(commentaire.body).to eq 'utilisation_eva : Mon utilisation'
     end
 
     context 'importe les notes' do
       let(:ligne) { { notes: 'Mes notes', mail: 'mon-conseiller@milo.fr' } }
       before { ImporteurCommentaires.importe(ligne, evabot) }
-      it { expect(ActiveAdmin::Comment.last.body).to eq 'Mes notes' }
+      it { expect(ActiveAdmin::Comment.last.body).to eq 'notes : Mes notes' }
+    end
+
+    context 'ignore les lignes sans aucun commentaire' do
+      let(:ligne) { { notes: ' ', utilisation_eva: '', mail: 'mon-conseiller@milo.fr' } }
+      it do
+        expect(logger).to receive(:info).exactly(0).times
+        expect { ImporteurCommentaires.importe(ligne, evabot) }
+          .to_not(change { ActiveAdmin::Comment.count })
+      end
     end
 
     context 'ne duplique pas les commentaires pour permettre de réimporter si besoin' do
@@ -35,12 +51,11 @@ describe ImporteurCommentaires do
   end
 
   context 'ignore un compte inexistant' do
-    let(:logger) { RakeLogger.logger }
-    let(:ligne) { { utilisation_eva: 'Mon usage', mail: 'inconnu@milo.fr' } }
+    let(:ligne) { { utilisation_eva: 'Mon usage', notes: 'une note', mail: 'inconnu@milo.fr' } }
 
     it do
-      expect(logger).to receive(:info)
-        .with('Commentaire ignoré pour le compte inconnu : inconnu@milo.fr')
+      expect(logger).to receive(:warn)
+        .with('Commentaire ignoré pour le compte inconnu : inconnu@milo.fr,Mon usage,une note')
       expect { ImporteurCommentaires.importe(ligne, evabot) }
         .to_not(change { ActiveAdmin::Comment.count })
     end

49bd0afb43c2369b9419fde285e87e413be30009
Étienne Charignon <etienne.charignon@gmail.com>
2021-06-18 17:26:43 +0200

    Rapport le code campagne si la migration de mise en majuscule échoue

--- db/migrate/20210604145905_defini_le_code_des_campagnes_existantes_en_majuscule.rb
+++ db/migrate/20210604145905_defini_le_code_des_campagnes_existantes_en_majuscule.rb
@@ -1,7 +1,9 @@
 class DefiniLeCodeDesCampagnesExistantesEnMajuscule < ActiveRecord::Migration[6.1]
   def up
     Campagne.find_each do |campagne|
-      campagne.update!(code: campagne.code.upcase)
+      unless campagne.update(code: campagne.code.upcase)
+        raise "erreur, #{campagne.code.upcase} : #{campagne.errors.full_messages}"
+      end
     end
   end
 end

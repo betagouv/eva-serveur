7a222f44f1069da5c13808246ad0793b8e2c6e80
Stéphane Hanser <shanser@captive.fr>
2021-07-30 12:02:22 +0200

    Cache les actions dans le menu du haut et remet les flashes à leur place initiale
    Co-authored-by: Marine Dominé <marine.domine@beta.gouv.fr>

--- app/assets/javascripts/flashes.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const flashes = document.querySelector('.flashes');
-  const titlebar_left = document.querySelector('#titlebar_left');
-  titlebar_left.after(flashes);
-});

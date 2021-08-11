513393fc486ccedf55e2f128e565347547d6007a
Stéphane Hanser <shanser@captive.fr>
2021-08-11 16:14:23 +0200

    Ajoute une tâche rake pour importer les numéros de tel

---
+++ lib/importeur_telephone.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rake_logger'
+
+class ImporteurTelephone
+  class << self
+    def importe(ligne)
+      compte = Compte.find_by email: ligne[:mail]
+      return unless import_possible?(ligne, compte)
+
+      RakeLogger.logger.info "Importe : #{ligne[:mail]},#{ligne[:telephone]}"
+      compte.telephone = ligne[:telephone]
+      compte.save!
+    end
+
+    private
+
+    def import_possible?(ligne, compte)
+      if compte.blank?
+        RakeLogger.logger.warn 'Téléphone ignoré pour le compte inconnu : ' \
+                               "#{ligne[:mail]} - #{ligne[:telephone]}"
+        return false
+      elsif compte.telephone.present?
+        RakeLogger.logger.warn "#{compte.email}: téléphone #{compte.telephone} déjà présent;" \
+                               " #{ligne[:telephone]} ignoré"
+        return false
+      end
+
+      true
+    end
+  end
+end
--- lib/tasks/importe.rake
+++ lib/tasks/importe.rake
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'importeur_commentaires'
+require 'importeur_telephone'
 
 namespace :importe do
   desc 'Importe les commentaires Airtable'
@@ -12,6 +13,13 @@ namespace :importe do
     end
     CSV.parse($stdin, headers: true, header_converters: :symbol).each do |row|
       ImporteurCommentaires.importe row.to_hash, eva_bot
+    end
+  end
+
+  desc 'Importe les numéros de téléphone des conseillers connus'
+  task telephones: :environment do
+    CSV.parse($stdin, headers: true, header_converters: :symbol).each do |row|
+      ImporteurTelephone.importe row.to_hash
     end
   end
 end

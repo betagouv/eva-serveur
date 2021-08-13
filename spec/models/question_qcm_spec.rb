553f41c579da31291a0b3b47425d370f6299f86b
Clément Prod'homme <clement@captive.fr>
2021-08-13 10:38:24 +0200

    Empèche de modifier l'illustration d'une question dans l'admin
    On fera une seconde PR pour supprimer l'ensemble des illustrations des Questions dans un second déploiement

--- spec/models/question_qcm_spec.rb
+++ spec/models/question_qcm_spec.rb
@@ -11,7 +11,8 @@ RSpec.describe QuestionQcm, type: :model do
     it 'serialise les champs' do
       json = subject.as_json
       expect(json.keys)
-        .to match_array(%w[choix description id intitule type metacompetence type_qcm])
+        .to match_array(%w[choix description id intitule type metacompetence
+                           type_qcm])
       expect(json['type']).to eql('qcm')
     end
 

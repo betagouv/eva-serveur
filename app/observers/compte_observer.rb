6d1f2a9d26c3768f4ec43d07567d080734d0d785
Clément Prod'homme <clement@captive.fr>
2021-08-16 11:01:45 +0200

    Quand le compte n'est pas en attente, n'envoie pas d'alerte aux admins
    cela fait suite au problème remonté :
    
    > Gaëlle Ottan Aujourd’hui à 10 h 17
    Petite feedback de ce matin, quand je crée une nouvelle structure, je reçois un email pour valider mon accès, à moins que je rate un truc je pense que c'est une erreur et ça génère pas mal de confusion puisque je suis le premier utilisateur de ma structure (et je reçois deux mails ensuite qui contredisent ce premier)

--- app/observers/compte_observer.rb
+++ app/observers/compte_observer.rb
@@ -13,16 +13,20 @@ class CompteObserver < ActiveRecord::Observer
   def envoie_emails(compte)
     CompteMailer.with(compte: compte).nouveau_compte.deliver_later
 
-    compte.find_admins.each do |admin|
-      CompteMailer.with(compte: compte, compte_admin: admin)
-                  .alerte_admin
-                  .deliver_later
-    end
+    alerte_admins(compte) if compte.validation_en_attente?
   end
 
   def programme_email_relance(compte)
     RelanceUtilisateurPourNonActivationJob
       .set(wait: Compte::DELAI_RELANCE_NON_ACTIVATION)
       .perform_later(compte.id)
+  end
+
+  def alerte_admins(compte)
+    compte.find_admins.each do |admin|
+      CompteMailer.with(compte: compte, compte_admin: admin)
+                  .alerte_admin
+                  .deliver_later
+    end
   end
 end

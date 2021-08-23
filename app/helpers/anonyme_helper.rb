f98582702f4d81287202cc9822d231c474f54d56
Stéphane Hanser <shanser@captive.fr>
2021-08-23 17:22:41 +0200

    Retire des html_safe

--- app/helpers/anonyme_helper.rb
+++ app/helpers/anonyme_helper.rb
@@ -2,18 +2,7 @@
 
 module AnonymeHelper
   def nom_pour_evaluation(evaluation)
-    prefixe_anonyme = evaluation.anonyme? ? "#{svg_anonyme} ".html_safe : ''
+    prefixe_anonyme = evaluation.anonyme? ? inline_svg_tag('anonyme.svg') : ''
     prefixe_anonyme + evaluation.nom
-  end
-
-  private
-
-  def svg_anonyme
-    file_path = "#{Rails.root}/app/assets/images/anonyme.svg"
-    if File.exist?(file_path)
-      File.read(file_path).html_safe
-    else
-      ''
-    end
   end
 end

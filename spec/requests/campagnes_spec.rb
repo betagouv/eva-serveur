2c22a0e33a2f145bcd889c4056b7da5d68f808ea
Clément Prod'homme <clement@captive.fr>
2021-08-25 16:21:30 +0200

    les api campagne et evaluation retournent des listes de questions et non des objets Questionnaire

--- spec/requests/campagnes_spec.rb
+++ spec/requests/campagnes_spec.rb
@@ -40,34 +40,65 @@ describe 'Campagne API', type: :request do
       expect(response).to have_http_status(404)
     end
 
-    it 'retourne les situations de la campagne' do
-      questionnaire = create :questionnaire
-      questionnaire_entrainement = create :questionnaire
-      questionnaire_surcharge = create :questionnaire
-      situation_controle = create :situation_controle
-      situation_inventaire = create :situation_inventaire,
-                                    libelle: 'Inventaire',
-                                    questionnaire_entrainement: questionnaire_entrainement,
-                                    questionnaire: questionnaire
+    context 'quand la campagne a des situations' do
+      let(:question_entrainement) { create :question_qcm, intitule: 'Ma question entrainement' }
+      let(:questionnaire_entrainement) do
+        create :questionnaire,
+               questions: [question_entrainement]
+      end
+      let(:questionnaire_surcharge) { create :questionnaire, questions: [question_entrainement] }
+      let(:situation_bienvenue) do
+        create :situation_bienvenue,
+               libelle: 'Bienvenue',
+               questionnaire_entrainement: questionnaire_entrainement,
+               questionnaire: questionnaire
+      end
+      let(:situation_livraison) do
+        create :situation_livraison,
+               libelle: 'Livraison',
+               questionnaire_entrainement: questionnaire_entrainement,
+               questionnaire: questionnaire
+      end
+      let(:situation_inventaire) { create :situation_inventaire }
 
-      campagne.situations_configurations.create situation: situation_controle,
-                                                position: 2,
-                                                questionnaire: questionnaire_surcharge
-      campagne.situations_configurations.create situation: situation_inventaire, position: 1
-      get '/api/campagnes/ete21'
+      before do
+        campagne.situations_configurations.create situation: situation_bienvenue,
+                                                  position: 2,
+                                                  questionnaire: questionnaire_surcharge
+        campagne.situations_configurations.create situation: situation_livraison, position: 1
+        campagne.situations_configurations.create situation: situation_inventaire
+      end
 
-      reponse_json = JSON.parse(response.body)
+      it 'retourne les situations de la campagne' do
+        get '/api/campagnes/ete21'
 
-      expect(reponse_json['situations'].size).to eql(2)
-      expect(reponse_json['situations'][0]['libelle']).to eql('Inventaire')
-      expect(reponse_json['situations'][0]['nom_technique']).to eql('inventaire')
-      expect(reponse_json['situations'][0]['id']).to eql(situation_inventaire.id)
-      expect(reponse_json['situations'][0]['questionnaire_entrainement_id'])
-        .to eql(questionnaire_entrainement.id)
-      expect(reponse_json['situations'][0]['questionnaire_id'])
-        .to eql(questionnaire.id)
-      expect(reponse_json['situations'][1]['questionnaire_id'])
-        .to eql(questionnaire_surcharge.id)
+        reponse_json = JSON.parse(response.body)
+
+        expect(reponse_json['situations'].size).to eql(3)
+        premiere_situation = reponse_json['situations'][0]
+        expect(premiere_situation['libelle']).to eql('Livraison')
+        expect(premiere_situation['nom_technique']).to eql('livraison')
+        expect(premiere_situation['id']).to eql(situation_livraison.id)
+        expect(premiere_situation['questionnaire_id']).to eql(questionnaire.id)
+        expect(premiere_situation['questions'][0]['intitule'])
+          .to eql('Ma question')
+        expect(premiere_situation['questionnaire_entrainement_id'])
+          .to eql(questionnaire_entrainement.id)
+        expect(premiere_situation['questions_entrainement'][0]['intitule'])
+          .to eql('Ma question entrainement')
+
+        seconde_situation = reponse_json['situations'][1]
+        expect(seconde_situation['libelle']).to eql('Bienvenue')
+        expect(seconde_situation['questionnaire_id'])
+          .to eql(questionnaire_surcharge.id)
+
+        troisieme_situation = reponse_json['situations'][2]
+        expect(troisieme_situation['libelle']).to eql('Inventaire')
+        expect(troisieme_situation['questionnaire_id']).to be_nil
+        expect(troisieme_situation['questions']).to be_nil
+        expect(troisieme_situation['questionnaire_entrainement_id']).to be_nil
+        expect(troisieme_situation['questions_entrainement']).to be_nil
+      end
     end
   end
 end

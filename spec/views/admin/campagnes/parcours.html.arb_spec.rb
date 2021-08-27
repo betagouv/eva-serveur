55c19c2e20ced54803196f25336ae797e436ceaf
Stéphane Hanser <shanser@captive.fr>
2021-08-27 11:47:57 +0200

    Affiche un message de parcours personnalisé quand la campagne n'a pas de parcours type

---
+++ spec/views/admin/campagnes/parcours.html.arb_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'admin/campagnes/_parcours.html.arb' do
+  before { assign(:campagne, Campagne.new(parcours_type: parcours_type)) }
+  before { render }
+
+  context 'sans parcours type' do
+    let(:parcours_type) { nil }
+    it { expect(rendered).to match(/Parcours personnalisé/) }
+  end
+
+  context 'avec parcours_type' do
+    let(:parcours_type) { ParcoursType.new libelle: 'Mon libellé' }
+    it { expect(rendered).to match(/Mon libellé/) }
+  end
+end

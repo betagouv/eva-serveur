04ec10f86af51fc8eed04934973669fda1d9f952
Stéphane Hanser <shanser@captive.fr>
2021-09-17 12:30:54 +0200

    Permet de préfixer les url par /pro
    ce qui servira pour le proxy
    
    Co-authored-by: Clément Prod’homme <clement@captive.fr>

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,26 @@
 Rails.application.routes.draw do
-  devise_for :comptes, ActiveAdmin::Devise.config.deep_merge(controllers: {registrations: 'eva/devise/registrations'})
-  get '/admin', to: redirect('/admin/dashboard')
+  scope '(pro)' do
+    devise_for :comptes, ActiveAdmin::Devise.config.deep_merge(controllers: {registrations: 'eva/devise/registrations'})
+    get '/admin', to: redirect('/pro/admin/dashboard')
 
-  ActiveAdmin.routes(self)
+    ActiveAdmin.routes(self)
 
-  authenticate :compte, ->(o) { o.superadmin? } do
-    mount Sidekiq::Web => '/sidekiq'
-  end
-
-  root to: redirect('/admin/dashboard')
-
-  resource :nouvelle_structure, only: [:create, :show]
-  resources :structures, only: :index
-
-  namespace :api do
-    resources :campagnes, only: :show, param: :code_campagne
-    resources :evaluations, only: [:create, :update] do
-      resource :fin, only: [:create], controller: 'evaluations/fins'
+    authenticate :compte, ->(o) { o.superadmin? } do
+      mount Sidekiq::Web => '/sidekiq'
     end
-    resources :questionnaires, only: [:show]
-    resources :evenements
+
+    root to: redirect('/pro/admin/dashboard')
+
+    resource :nouvelle_structure, only: [:create, :show]
+    resources :structures, only: :index
+
+    namespace :api do
+      resources :campagnes, only: :show, param: :code_campagne
+      resources :evaluations, only: [:create, :update] do
+        resource :fin, only: [:create], controller: 'evaluations/fins'
+      end
+      resources :questionnaires, only: [:show]
+      resources :evenements
+    end
   end
 end

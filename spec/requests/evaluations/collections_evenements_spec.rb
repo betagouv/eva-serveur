7a4c1ee8adde4b984268369198501cfa5e8e35cc
Étienne Charignon <etienne.charignon@gmail.com>
2021-09-20 17:10:06 +0200

    Test qu'on a une 422 si l'évaluation n'existe pas

--- spec/requests/evaluations/collections_evenements_spec.rb
+++ spec/requests/evaluations/collections_evenements_spec.rb
@@ -4,43 +4,57 @@ require 'rails_helper'
 
 describe 'API Collections Evenements', type: :request do
   describe 'POST /evaluations/:id/collections_evenements' do
+    let!(:situation_inventaire) { create :situation_inventaire }
+    let!(:situation_livraison) { create :situation_livraison }
+    let(:evenement1) do
+      {
+        date: 1_631_719_609_975,
+        donnees: {},
+        nom: 'demarrage',
+        position: 0,
+        session_id: 'abbd368d-dc2d-49a4-ae8a-face8fefbc90',
+        situation: situation_inventaire.nom_technique
+      }
+    end
+    let(:evenement2) do
+      {
+        date: 1_631_891_672_641,
+        donnees: { contenant: '16' },
+        nom: 'ouvertureContenant',
+        position: 1,
+        session_id: 'abbd368d-dc2d-49a4-ae8a-face8fefbc90',
+        situation: situation_inventaire.nom_technique
+      }
+    end
+    let(:evenement3) do
+      {
+        date: 1_631_891_703_815,
+        donnees: {},
+        nom: 'demarrageEntrainement',
+        position: 0,
+        session_id: '184e1079-3bb9-4229-921e-4c2574d94934',
+        situation: situation_livraison.nom_technique
+      }
+    end
+    let(:donnees_evenements) do
+      { evenements: [evenement1, evenement2, evenement3] }
+    end
+
+    context 'avec une évaluation inexistante' do
+      it do
+        expect do
+          post '/api/evaluations/evaluation_inconnue/collections_evenements',
+               params: donnees_evenements
+        end.to change(Evenement, :count)
+          .by(0)
+          .and change(Partie, :count)
+          .by(0)
+        expect(response).to have_http_status(422)
+      end
+    end
+
     context 'avec une évaluation existante' do
-      let!(:situation_inventaire) { create :situation_inventaire }
-      let!(:situation_livraison) { create :situation_livraison }
       let(:evaluation) { create :evaluation }
-      let(:evenement1) do
-        {
-          date: 1_631_719_609_975,
-          donnees: {},
-          nom: 'demarrage',
-          position: 0,
-          session_id: 'abbd368d-dc2d-49a4-ae8a-face8fefbc90',
-          situation: situation_inventaire.nom_technique
-        }
-      end
-      let(:evenement2) do
-        {
-          date: 1_631_891_672_641,
-          donnees: { contenant: '16' },
-          nom: 'ouvertureContenant',
-          position: 1,
-          session_id: 'abbd368d-dc2d-49a4-ae8a-face8fefbc90',
-          situation: situation_inventaire.nom_technique
-        }
-      end
-      let(:evenement3) do
-        {
-          date: 1_631_891_703_815,
-          donnees: {},
-          nom: 'demarrageEntrainement',
-          position: 0,
-          session_id: '184e1079-3bb9-4229-921e-4c2574d94934',
-          situation: situation_livraison.nom_technique
-        }
-      end
-      let(:donnees_evenements) do
-        { evenements: [evenement1, evenement2, evenement3] }
-      end
 
       it do
         expect do

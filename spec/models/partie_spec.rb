83700f8667534f6aae31788d2f25a8687a5b547f
Clément Prod'homme <clement@captive.fr>
2021-09-24 17:08:59 +0200

    Fix : erreur rollbar # 163
    > PG::UniqueViolation: ERROR: duplicate key value violates unique constraint "index_parties_on_session_id" #390
    
    Aujourd'hui le front peut créer deux events en même temps, et le back tente de créer la partie en même temps. PG retourne alors cette erreur qui provoque une erreur 500.
    
    Pour corriger cette erreur 500, j'ai rajouté une validation activerecord qui viendra catcher l'erreur et donc retourner une 422.
    Le fait de retourner une 422 n'est pas un problème pour le front qui retentra l'appel quelques instants après et donc passera.

--- spec/models/partie_spec.rb
+++ spec/models/partie_spec.rb
@@ -4,6 +4,7 @@ require 'rails_helper'
 
 describe Partie do
   it { is_expected.to validate_presence_of(:session_id) }
+  it { is_expected.to validate_uniqueness_of(:session_id) }
   it { is_expected.to belong_to(:evaluation) }
   it { is_expected.to belong_to(:situation) }
 end

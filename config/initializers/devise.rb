b9adeb675d6f7b9d0d5aae3a31a5345f9bf6aa7a
Marine Sourin <marinesourin@live.fr>
2021-10-01 12:35:20 +0200

    Met à jour l'expéditeur des emails

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,8 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = ENV['EMAIL_DEFAULT_FROM']
-
+  config.mailer_sender = "L'équipe eva <#{Eva::EMAIL_CONTACT}>"
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 

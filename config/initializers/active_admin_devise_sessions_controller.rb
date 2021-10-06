958ec2a04116eba87ce430607df0de86fba95f76
Marine Sourin <marinesourin@live.fr>
2021-10-06 16:38:28 +0200

    Renvoie vers la page de confirmation si le compte n'est pas confirmé et qu'il a été créé il y a plus de 2 ans

--- config/initializers/active_admin_devise_sessions_controller.rb
+++ config/initializers/active_admin_devise_sessions_controller.rb
@@ -1,4 +1,6 @@
 class ActiveAdmin::Devise::SessionsController
+  before_action :check_compte_confirmation, only: :create
+
   def create
     self.resource = warden.authenticate!(auth_options)
     if !resource.confirmed?
@@ -9,5 +11,12 @@ class ActiveAdmin::Devise::SessionsController
     sign_in(resource_name, resource)
     yield resource if block_given?
     respond_with resource, location: after_sign_in_path_for(resource)
+  end
+
+private
+
+  def check_compte_confirmation
+    compte = Compte.find_by_email(params[:compte][:email])
+    redirect_to new_compte_confirmation_path unless compte && compte.active_for_authentication?
   end
 end

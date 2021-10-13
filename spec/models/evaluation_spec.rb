2dd54ac25c9cf8cebd2cfa3b9b802b6ace6593e1
Clément Prod'homme <clement@captive.fr>
2021-10-13 14:36:23 +0200

    Rend obligatoire la date de début pour une évaluation

--- spec/models/evaluation_spec.rb
+++ spec/models/evaluation_spec.rb
@@ -4,5 +4,6 @@ require 'rails_helper'
 
 describe Evaluation do
   it { is_expected.to validate_presence_of :nom }
+  it { is_expected.to validate_presence_of :debutee_le }
   it { is_expected.to belong_to :campagne }
 end

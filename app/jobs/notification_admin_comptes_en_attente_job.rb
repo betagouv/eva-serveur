92791c08a4bd08d20b352f2b30d60a9b86f7d489
Marie Leuliette <marie.leuliette@gmail.com>
2021-10-29 18:06:15 +0200

    Creation tests et job pour notifier les comptes en attente a l'admin
    Co-authored-by: Clément Prod'homme <clement@captive.fr>

---
+++ app/jobs/notification_admin_comptes_en_attente_job.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class NotificationAdminComptesEnAttenteJob < ApplicationJob
+  queue_as :default
+
+  def perform(*_args)
+    comptes_en_attente = Compte.validation_en_attente
+    structures = Structure.where(id: comptes_en_attente.select(:structure_id))
+    admins = Compte.where(role: Compte::ADMIN_ROLES).where(structure_id: structures)
+
+    admins.find_each do |admin|
+      comptes = comptes_en_attente.where(structure_id: admin.structure_id)
+      CompteMailer.with(compte_admin: admin, comptes: comptes)
+                  .comptes_a_autoriser
+                  .deliver_now
+    end
+  end
+end
---
+++ spec/jobs/notification_admin_comptes_en_attente_job_spec.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe NotificationAdminComptesEnAttenteJob, type: :job do
+  context "quand l'admin a toujours des comptes en attente de validation" do
+    let!(:structure) { create :structure }
+    let!(:compte) { create :compte_admin, structure: structure }
+    let!(:compte) { create :compte, statut_validation: :en_attente, structure: structure }
+
+    it "envoie un mail hebdomadairement pour relancer l'admin" do
+      expect do
+        NotificationAdminComptesEnAttenteJob.perform_now
+      end.to change { ActionMailer::Base.deliveries.count }.by(1)
+    end
+  end
+end

73dba30d67ca4602e859d5fa628d985084e2b54c
Marie Leuliette <marie.leuliette@gmail.com>
2021-11-04 19:31:26 +0100

    Retouche style css et refactorisation

--- app/admin/structures.rb
+++ app/admin/structures.rb
@@ -55,7 +55,10 @@ ActiveAdmin.register Structure do
     before_action :trouve_comptes, :trouve_campagnes, only: :show
 
     def trouve_comptes
-      @comptes = Compte.where(structure: resource)
+      comptes = Compte.where(structure: resource).order(:prenom, :nom)
+      @comptes_en_attente = comptes.validation_en_attente
+      @comptes_refuses = comptes.validation_refusee
+      @comptes_acceptes = comptes.validation_acceptee
     end
 
     def trouve_campagnes

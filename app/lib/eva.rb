01d6f0d5335bc0c414603f336425dd2c4d370a60
Clément Prod'homme <clement@captive.fr>
2021-11-29 14:34:49 +0100

    Modifie le texte de l'étape 3

--- app/lib/eva.rb
+++ app/lib/eva.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module Eva
-  DOCUMENT_PRISE_EN_MAIN = 'https://docs.google.com/presentation/d/e/2PACX-1vSXUf1hvRGRne4GwuFDAc-0ALYMD_c6DT1Wpz3SkPaNTRmHFgu_pa6QDpVs7R_phy8YFqKHLXZj5fiy/pub?start=false&loop=false&delayms=3000'
+  DOCUMENT_PRISE_EN_MAIN = 'https://docs.google.com/presentation/d/e/2PACX-1vRbrOAP6ojsMldL0YZaLLWH_nrluOd9kkp_0XACQsn7lateomMATUC5uEn_CZMLni--pjpfSJ366ppf/pub?start=true&loop=false&delayms=3000'
   EMAIL_CONTACT = 'contact@eva.beta.gouv.fr'
   EMAIL_SUPPORT = 'support@eva.beta.gouv.fr'
   LIEN_LIVESTORM = 'https://app.livestorm.co/eva-diagnostic-cptc/presentation-et-demonstration-eva-session?type=detailed'

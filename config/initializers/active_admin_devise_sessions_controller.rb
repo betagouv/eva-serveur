1bf7b044e9451eafbfe6270611d5819daa270987
Stéphane Hanser <shanser@captive.fr>
2021-12-08 16:06:13 +0100

    Fait passer rubocop sur le fichier active_admin_devise_sessions_controller.rb
    Co-authored-by: Marie Leuliette <marie.leuliette@gmail.com>

--- config/initializers/active_admin_devise_sessions_controller.rb
+++ config/initializers/active_admin_devise_sessions_controller.rb
@@ -1,23 +1,34 @@
-class ActiveAdmin::Devise::SessionsController
-  before_action :check_compte_confirmation, only: :create
+# frozen_string_literal: true
 
-  def create
-    self.resource = warden.authenticate!(auth_options)
-    if !resource.confirmed?
-      set_flash_message! :alert, :"signed_in_but_#{resource.inactive_message}"
-    else
-      set_flash_message!(:notice, :signed_in)
+module ActiveAdmin
+  module Devise
+    class SessionsController
+      before_action :check_compte_confirmation, only: :create
+
+      def create
+        self.resource = warden.authenticate!(auth_options)
+        assigne_message_flash resource
+        sign_in(resource_name, resource)
+        yield resource if block_given?
+        respond_with resource, location: after_sign_in_path_for(resource)
+      end
+
+      private
+
+      def assigne_message_flash(resource)
+        if resource.confirmed?
+          set_flash_message!(:notice, :signed_in)
+        else
+          set_flash_message! :alert, :"signed_in_but_#{resource.inactive_message}"
+        end
+      end
+
+      def check_compte_confirmation
+        compte = Compte.find_by_email(params[:compte][:email])
+        return flash.now[:alert] if compte.blank?
+
+        redirect_to new_compte_confirmation_path unless compte.active_for_authentication?
+      end
     end
-    sign_in(resource_name, resource)
-    yield resource if block_given?
-    respond_with resource, location: after_sign_in_path_for(resource)
-  end
-
-private
-
-  def check_compte_confirmation
-    compte = Compte.find_by_email(params[:compte][:email])
-    return flash.now[:alert] if compte.blank?
-    redirect_to new_compte_confirmation_path unless compte.active_for_authentication?
   end
 end

1b88d7ce37e7e648230d43b480d50b3f1b6e3fb6
Étienne Charignon <etienne.charignon@gmail.com>
2022-01-06 08:55:02 +0100

    Corrige le chemin du fichier css du PDF

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += %w[active_admin.css active_admin.js restitution_globale/restitution_globale_pdf.css]
+Rails.application.config.assets.precompile += %w[active_admin.css active_admin.js admin/pages/restitution_globale/restitution_globale_pdf.css]

4e61120f963342d9bfd9e01cc6265de6abec2daa
Étienne Charignon <etienne.charignon@gmail.com>
2022-01-31 16:48:04 +0100

    Ne limite plus l'affichage des parties en supprimant les parties des campagnes des superadmins
    Co-authored-by: Marie Leuliette <marie.leuliette@captive.fr>

--- spec/features/admin/parties_spec.rb
+++ spec/features/admin/parties_spec.rb
@@ -5,31 +5,26 @@ require 'rails_helper'
 describe 'Admin - Parties', type: :feature do
   before { se_connecter_comme_superadmin }
   let!(:situation) { create :situation }
+  let!(:autre_situation) { create :situation_inventaire }
 
   describe 'index' do
     let(:evaluation1) { create :evaluation, nom: 'evaluation 1' }
     let(:evaluation2) { create :evaluation, nom: 'evaluation 2' }
+    let(:evaluation3) { create :evaluation, nom: 'evaluation inventaire' }
     let!(:partie1) { create :partie, situation: situation, evaluation: evaluation1 }
     let!(:partie2) { create :partie, situation: situation, evaluation: evaluation2 }
+    let!(:partie_inventaire) do
+      create :partie,
+             situation: autre_situation,
+             evaluation: evaluation3
+    end
 
     before { visit admin_situation_parties_path(situation) }
 
     it do
       expect(page).to have_content('evaluation 1')
       expect(page).to have_content('evaluation 2')
-    end
-
-    it 'exclue les parties des superadmin' do
-      superadmin = create :compte_superadmin
-      campagne = create :campagne, compte: superadmin
-      evaluation_superadmin = create :evaluation, nom: 'evaluation_superadmin', campagne: campagne
-      create :partie, situation: situation, evaluation: evaluation_superadmin
-
-      visit admin_situation_parties_path(situation)
-
-      within('#index_table_parties') do
-        expect(page).not_to have_content('evaluation_superadmin')
-      end
+      expect(page).not_to have_content('evaluation inventaire')
     end
   end
 end

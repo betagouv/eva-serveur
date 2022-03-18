96948712fb41608e7edce2790796ef7902af1c01
Étienne Charignon <etienne.charignon@gmail.com>
2022-03-18 14:06:56 +0100

    Persiste la restitution à chaque fois qu'on persiste les métriques d'une partie

--- spec/jobs/persiste_metriques_partie_job_spec.rb
+++ spec/jobs/persiste_metriques_partie_job_spec.rb
@@ -3,12 +3,14 @@
 require 'rails_helper'
 
 describe PersisteMetriquesPartieJob, type: :job do
-  let(:partie) { double }
+  let(:evaluation) { create :evaluation }
+  let(:partie) { create :partie, evaluation: evaluation }
   let(:restitution) { double }
 
   it "persiste les événéments d'une partie" do
     expect(FabriqueRestitution).to receive(:instancie).with(partie).and_return restitution
     expect(restitution).to receive(:persiste)
-    PersisteMetriquesPartieJob.perform_now(partie)
+    expect { PersisteMetriquesPartieJob.perform_now(partie) }
+      .to have_enqueued_job(PersisteRestitutionJob).exactly(1)
   end
 end

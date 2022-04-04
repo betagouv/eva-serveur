f7e2b1a1e75754939eb9115da52dcc6ed47c7768
Clément Prod'homme <clement@captive.fr>
2022-04-04 16:39:27 +0200

    Fix les preview des mailers
    Il manquait un type aux structures instanciées
    
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

--- spec/mailers/previews/compte_mailer_preview.rb
+++ spec/mailers/previews/compte_mailer_preview.rb
@@ -3,13 +3,14 @@
 # Preview all emails at http://localhost:3000/rails/mailers/compte_mailer
 class CompteMailerPreview < ActionMailer::Preview
   def nouveau_compte
-    structure = Structure.new nom: 'Ma Super Structure', code_postal: '75012'
+    structure = StructureLocale.new nom: 'Ma Super Structure', code_postal: '75012'
     compte = Compte.new prenom: 'Paule', email: 'debut@test.com', structure: structure
     CompteMailer.with(compte: compte).nouveau_compte
   end
 
   def alerte_admin
-    structure = Structure.new id: SecureRandom.uuid, nom: 'Ma Super Structure', code_postal: '75012'
+    structure = StructureLocale.new id: SecureRandom.uuid, nom: 'Ma Super Structure',
+                                    code_postal: '75012'
     admin = Compte.new prenom: 'Admin', email: 'admin@test.com', structure: structure, role: 'admin'
     compte = Compte.new prenom: 'Paule',
                         nom: 'Delaporte',
@@ -20,15 +21,15 @@ class CompteMailerPreview < ActionMailer::Preview
   end
 
   def relance
-    structure = Structure.new type_structure: 'mission_locale'
+    structure = StructureLocale.new type_structure: 'mission_locale'
     compte = Compte.new prenom: 'Lucas', structure: structure, email: 'lucas.dupont@example.com'
 
     CompteMailer.with(compte: compte).relance
   end
 
   def comptes_a_autoriser
-    structure = Structure.new id: SecureRandom.uuid, nom: 'Ma structure', code_postal: '92100',
-                              type_structure: 'mission_locale'
+    structure = StructureLocale.new id: SecureRandom.uuid, nom: 'Ma structure',
+                                    code_postal: '92100', type_structure: 'mission_locale'
     compte_admin = Compte.new prenom: 'Admin', structure: structure, role: 'admin'
     compte1 = Compte.new prenom: 'Jean', nom: 'Bon', email: 'compte_1@gmail.com',
                          structure: structure

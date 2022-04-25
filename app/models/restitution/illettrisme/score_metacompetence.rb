74ba21d5bcb909617715e3c1908bbbc55320c1f7
Clément Prod'homme <clement@captive.fr>
2022-04-25 18:01:27 +0200

    Revert "Supprime classes inutiles de l'algo V1"
    This reverts commit aed09f204762399f6295fcfe780409a90d78cc50.

---
+++ app/models/restitution/illettrisme/score_metacompetence.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Restitution
+  module Illettrisme
+    class ScoreMetacompetence
+      def calcule(evenements, metacompetence)
+        nombre_bonnes_reponses = nombre_bonnes_reponses(evenements, metacompetence)
+        return 0 if nombre_bonnes_reponses.zero?
+
+        temps_moyen = temps_moyen_bonnes_reponses(evenements, metacompetence)
+        return if temps_moyen.nil? || temps_moyen.zero?
+
+        nombre_bonnes_reponses.fdiv(temps_moyen)
+      end
+
+      private
+
+      def nombre_bonnes_reponses(evenements, metacompetence)
+        Illettrisme::NombreBonnesReponses.new.calcule(evenements, metacompetence)
+      end
+
+      def temps_moyen_bonnes_reponses(evenements, metacompetence)
+        metrique = Illettrisme::TempsBonnesReponses.new
+        Metriques::Moyenne.new(metrique).calcule(evenements, metacompetence)
+      end
+    end
+  end
+end

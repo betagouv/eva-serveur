90c4a3a32a76ec8012255b41f2dcbef282e7578e
Clément Prod'homme <clement@captive.fr>
2022-04-27 18:19:27 +0200

    Supprime la migration qui vient ajouté une unicité sur les évènements

--- app/models/evenement.rb
+++ app/models/evenement.rb
@@ -2,7 +2,9 @@
 
 class Evenement < ApplicationRecord
   validates :nom, :date, :session_id, presence: true
+  # rubocop:disable Rails/UniqueValidationWithoutIndex
   validates :position, uniqueness: { scope: :session_id }
+  # rubocop:enable Rails/UniqueValidationWithoutIndex
   belongs_to :partie, foreign_key: :session_id, primary_key: :session_id
   delegate :situation, :evaluation, to: :partie
 

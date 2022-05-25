47c6465948f04bc68783ee666d7280ace839d2eb
Étienne Charignon <etienne.charignon@gmail.com>
2022-05-25 14:16:13 +0200

    ajoute un test d'intégration pour ids_situations

--- spec/models/restitution/completude_spec.rb
+++ spec/models/restitution/completude_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 describe Restitution::Completude do
-  let(:completude_helper) do
+  let(:completude) do
     Restitution::Completude.new(evaluation, restitutions)
   end
 
@@ -18,11 +18,11 @@ describe Restitution::Completude do
 
     describe 'quand la campagne est complete' do
       before do
-        allow(completude_helper)
+        allow(SituationConfiguration)
           .to receive(:ids_situations).with(evaluation.campagne_id,
                                             Evaluation::SITUATION_COMPETENCES_TRANSVERSALES)
                                       .and_return([controle.id])
-        allow(completude_helper)
+        allow(SituationConfiguration)
           .to receive(:ids_situations).with(evaluation.campagne_id,
                                             Evaluation::SITUATION_COMPETENCES_BASE)
                                       .and_return([livraison.id])
@@ -31,7 +31,7 @@ describe Restitution::Completude do
       context "quand aucune situations n'a été complétée" do
         let(:restitutions) { [] }
 
-        it { expect(completude_helper.calcule).to eq :incomplete }
+        it { expect(completude.calcule).to eq :incomplete }
       end
 
       context 'quand toutes les situations de la campagne ont été complétées' do
@@ -44,7 +44,7 @@ describe Restitution::Completude do
           ]
         end
 
-        it { expect(completude_helper.calcule).to eq :complete }
+        it { expect(completude.calcule).to eq :complete }
       end
 
       context "quand une situations non évaluante n'est pas terminée" do
@@ -57,7 +57,7 @@ describe Restitution::Completude do
           ]
         end
 
-        it { expect(completude_helper.calcule).to eq :complete }
+        it { expect(completude.calcule).to eq :complete }
       end
 
       context "quand une même situation n'est pas terminée" do
@@ -71,7 +71,7 @@ describe Restitution::Completude do
           ]
         end
 
-        it { expect(completude_helper.calcule).not_to eq :complete }
+        it { expect(completude.calcule).not_to eq :complete }
       end
 
       context 'quand une même situation se termine après plusieurs essai' do
@@ -85,7 +85,7 @@ describe Restitution::Completude do
           ]
         end
 
-        it { expect(completude_helper.calcule).to eq :complete }
+        it { expect(completude.calcule).to eq :complete }
       end
 
       context 'quand situation competence transversale non terminée' do
@@ -98,7 +98,7 @@ describe Restitution::Completude do
           ]
         end
 
-        it { expect(completude_helper.calcule).to eq :competences_transversales_incompletes }
+        it { expect(completude.calcule).to eq :competences_transversales_incompletes }
       end
 
       context 'quand situation competence de base non terminée' do
@@ -111,17 +111,17 @@ describe Restitution::Completude do
           ]
         end
 
-        it { expect(completude_helper.calcule).to eq :competences_de_base_incompletes }
+        it { expect(completude.calcule).to eq :competences_de_base_incompletes }
       end
     end
 
     describe 'quand la campagne est avec competences de base seulement' do
       before do
-        allow(completude_helper)
+        allow(SituationConfiguration)
           .to receive(:ids_situations).with(evaluation.campagne_id,
                                             Evaluation::SITUATION_COMPETENCES_TRANSVERSALES)
                                       .and_return([])
-        allow(completude_helper)
+        allow(SituationConfiguration)
           .to receive(:ids_situations).with(evaluation.campagne_id,
                                             Evaluation::SITUATION_COMPETENCES_BASE)
                                       .and_return([
@@ -140,7 +140,7 @@ describe Restitution::Completude do
           ]
         end
 
-        it { expect(completude_helper.calcule).to eq :complete }
+        it { expect(completude.calcule).to eq :complete }
       end
 
       context "quand une situations de la campagne n'a pas été complétée" do
@@ -152,19 +152,19 @@ describe Restitution::Completude do
           ]
         end
 
-        it { expect(completude_helper.calcule).to eq :incomplete }
+        it { expect(completude.calcule).to eq :incomplete }
       end
     end
 
     describe 'quand la campagne est avec competences transversales seulement' do
       before do
-        allow(completude_helper)
+        allow(SituationConfiguration)
           .to receive(:ids_situations).with(evaluation.campagne_id,
                                             Evaluation::SITUATION_COMPETENCES_TRANSVERSALES)
                                       .and_return([
                                                     controle.id
                                                   ])
-        allow(completude_helper)
+        allow(SituationConfiguration)
           .to receive(:ids_situations).with(evaluation.campagne_id,
                                             Evaluation::SITUATION_COMPETENCES_BASE)
                                       .and_return([])
@@ -177,7 +177,7 @@ describe Restitution::Completude do
           ]
         end
 
-        it { expect(completude_helper.calcule).to eq :complete }
+        it { expect(completude.calcule).to eq :complete }
       end
 
       context "quand une situations de la campagne n'a pas été complétée" do
@@ -187,7 +187,7 @@ describe Restitution::Completude do
           ]
         end
 
-        it { expect(completude_helper.calcule).to eq :incomplete }
+        it { expect(completude.calcule).to eq :incomplete }
       end
     end
   end

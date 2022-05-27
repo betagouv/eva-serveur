af8231ee445633fdbec8370f18ec038d10ca68db
Étienne Charignon <etienne.charignon@gmail.com>
2022-05-27 15:29:26 +0200

    Refactoring score orientation : donne la responsabilité des noms de question à EvenementEvacob

--- app/models/restitution/illettrisme/score_orientation.rb
+++ app/models/restitution/illettrisme/score_orientation.rb
@@ -4,22 +4,16 @@ module Restitution
   module Illettrisme
     class ScoreOrientation
       def calcule(evenements, _)
-        evenements_lodi = recupere_evenements_reponses_lodi_avec_score(evenements)
-        score_total = 0
-        evenements_lodi.each do |reponse|
-          score_total += reponse.donnees['score']
-        end
-
-        score_total
+        evenements_reponses_lodi_avec_score(evenements).sum(&:score_reponse)
       end
 
       private
 
-      def recupere_evenements_reponses_lodi_avec_score(evenements)
+      def evenements_reponses_lodi_avec_score(evenements)
         evenements.select do |evenement|
           evenement.nom == MetriquesHelper::EVENEMENT[:REPONSE] &&
-            evenement.donnees['question'].start_with?(MetriquesHelper::QUESTION[:LODI]) &&
-            evenement.donnees['score']
+            evenement.parcours?(:orientation) &&
+            evenement.score_reponse
         end
       end
     end

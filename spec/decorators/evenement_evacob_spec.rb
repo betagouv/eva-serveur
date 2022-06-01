6d0ff11fb9180913e9c019cd599e78465f25a172
Étienne Charignon <etienne.charignon@gmail.com>
2022-06-01 16:23:24 +0200

    Ajoute la métrique 'Score de lecture' de café de la place

---
+++ spec/decorators/evenement_evacob_spec.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe EvenementEvacob do
+  describe '#module?' do
+    def evenement(id_question)
+      described_class.new Evenement.new nom: 'reponse', donnees: {
+        question: id_question
+      }
+    end
+
+    it "retourn vrai s'il est du module orientation" do
+      expect(evenement('LOdi1').module?(:orientation)).to be true
+      expect(evenement('ALrd1').module?(:orientation)).to be false
+    end
+
+    it "retourn vrai s'il est du module lecture_complet" do
+      expect(evenement('ALrd1').module?(:lecture_complet)).to be true
+      expect(evenement('ALrd14').module?(:lecture_complet)).to be true
+      expect(evenement('LOdi2').module?(:lecture_complet)).to be true
+      expect(evenement('LOdi4').module?(:lecture_complet)).to be true
+      expect(evenement('LOdi6').module?(:lecture_complet)).to be true
+      expect(evenement('LOdi1').module?(:lecture_complet)).to be false
+      expect(evenement('LOdi3').module?(:lecture_complet)).to be false
+    end
+  end
+end

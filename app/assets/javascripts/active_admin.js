c88d259931898f51f89f4d72ed52732ac43985bc
Étienne Charignon <etienne.charignon@gmail.com>
2022-06-24 15:14:05 +0200

    Affichage du menu quand il n'y a pas de réponse à la recherche

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -29,20 +29,26 @@ document.addEventListener('DOMContentLoaded', () => {
         data: { q: request.term },
         dataType: "json",
         success: function (data) {
-          try {
-            response($.map(data.features, function (item) {
-              return { label: item.properties.label, value: item.properties.label };
-            }))
-          } catch (err) {
-            console.log(err);
-          }
+          response($.map(data.features, function (item) {
+            return { label: item.properties.label, value: item.properties.label };
+          }))
         },
         error: function () {
           response([]);
         }
       });
     },
-    autoFocus: true,
+    response: function(event, ui) {
+      if (!ui.content.length) {
+        const recherche = $(".champ-recherche").val();
+        const noResult = {
+          value: '',
+          label: `Aucun résultat ne correspond à la recherche "${recherche}"`
+        };
+        ui.content.push(noResult);
+      }
+    },
+    autoFocus: false,
     minLength: 2,
     delay: 100
   });

241ed7eafe2b3a4f22da66c23e8fc8dee74a7de2
guillaume imary <guillaume.imary@gmail.com>
2022-06-24 11:45:34 +0200

    Pour cacher les blocs rouges d'erreur on n'affiche dans le bloc rouge que les erreurs qui n'apparaissent pas sur les champs affichés

---
+++ spec/helpers/erreur_helper_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ErreurHelper do
+  describe '#erreurs_generales' do
+    let(:erreurs) { double }
+    let(:messages) { { email: 'doit exister', nom: "n'est pas unique" } }
+
+    it 'Retourne les erreurs qui ne concerne pas les champs affichés' do
+      allow(erreurs).to receive(:messages).and_return(messages)
+      allow(erreurs).to receive(:full_messages_for).with(:nom).and_return(["nom n'est pas unique"])
+      expect(erreurs_generales(erreurs, [:email])).to eql(["nom n'est pas unique"])
+    end
+
+    it "Retourne aucune erreur s'il n'y a pas d'erreur" do
+      allow(erreurs).to receive(:messages).and_return({})
+      expect(erreurs_generales(erreurs, [])).to eql([])
+    end
+  end
+end

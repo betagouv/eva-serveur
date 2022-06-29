397df2aeb6349ed42b86d164b79a93eb4336ff26
Clément Prod'homme <clement@captive.fr>
2022-06-29 12:13:03 +0200

    Corrige migration bloqué en production

--- db/migrate/20220620103206_nom_de_structures_est_unique.rb
+++ db/migrate/20220620103206_nom_de_structures_est_unique.rb
@@ -1,5 +1,5 @@
 class NomDeStructuresEstUnique < ActiveRecord::Migration[7.0]
   def change
-    add_index :structures, :nom, unique: true
+    add_index :structures, [:nom, :code_postal], unique: true
   end
 end

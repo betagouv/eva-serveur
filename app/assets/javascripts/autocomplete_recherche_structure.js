0bab3d80efa16f3f2ee4b5393b99b7a85d4235b2
Clément Prod'homme <clement@captive.fr>
2022-07-18 12:52:15 +0200

    ✨ Utilise l'api geo.api.gouv.fr pour l'autocomplete à l'inscription
    On n'appel pas l'api si le champs comprend entre 1 et 4 nombre car l'api ne retourne une réponse que si le code postal est complet.

--- app/assets/javascripts/autocomplete_recherche_structure.js
+++ app/assets/javascripts/autocomplete_recherche_structure.js
@@ -1,21 +1,36 @@
+function estUnCodePostal(texte) {
+  return texte.match(/^\d{5}$/);
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   $( ".champ-recherche" ).autocomplete({
     source: function (request, response) {
       $('#bouton-chercher').prop("disabled", true);
-      $.ajax({
-        url: "https://api-adresse.data.gouv.fr/search/",
-        data: { q: request.term, limit: 6, type: 'municipality', autocomplete: 1 },
-        dataType: "json",
-        success: function (data) {
-          response($.map(data.features, function (item) {
-            const label = `${item.properties.city} (${item.properties.postcode})`;
-            return { label: label, value: label, code_postal: item.properties.postcode };
-          }))
-        },
-        error: function () {
-          response([]);
+      if (!request.term.match(/^\d{1,4}$/)) {
+        let data = { limit: 6, type: 'commune-actuelle,arrondissement-municipal' };
+        if (estUnCodePostal(request.term)) {
+          data.codePostal = request.term 
+        } else {
+          data.nom = request.term;
+          data.boost = 'population';
         }
-      });
+        $.ajax({
+          url: "https://geo.api.gouv.fr/communes",
+          data: data,
+          dataType: "json",
+          success: function (data) {
+            response($.map(data, function (item) {
+              const ville = item.nom;
+              const codePostal = item.codesPostaux[0];
+              const label = `${ville} (${codePostal})`;
+              return { label: label, value: label, code_postal: codePostal };
+            }))
+          },
+          error: function () {
+            response([]);
+          }
+        });
+      }
     },
     response: function(event, ui) {
       if (!ui.content.length) {
@@ -34,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
       $('#bouton-chercher').prop("disabled", disabled);
     },
     autoFocus: false,
-    minLength: 2,
+    minLength: 3,
     delay: 100
   });
 });

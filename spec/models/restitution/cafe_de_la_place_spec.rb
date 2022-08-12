2640299e9f412fa1cf62345d2c60db3188a67668
Marie Leuliette <marie.leuliette@gmail.com>
2022-08-12 17:07:38 +0200

    Retourne le profil en fonction des scores obtenus au parcours haut

--- spec/models/restitution/cafe_de_la_place_spec.rb
+++ spec/models/restitution/cafe_de_la_place_spec.rb
@@ -17,6 +17,11 @@ describe Restitution::CafeDeLaPlace do
                                 donnees: { succes: true,
                                            question: 'LOdi1',
                                            score: 1 },
+                                partie: partie),
+                          build(:evenement_reponse,
+                                donnees: { succes: true,
+                                           question: 'HPar1',
+                                           score: 1 },
                                 partie: partie)
                         ])
   end
@@ -50,6 +55,40 @@ describe Restitution::CafeDeLaPlace do
                       production: :profil4
                     })
       expect(restitution.parcours_bas).to equal(::Competence::NIVEAU_INDETERMINE)
+    end
+  end
+
+  describe '#parcours_haut' do
+    context 'quand le score total est < 16 points' do
+      it 'retourne le profil aberrant' do
+        expect(restitution).to receive(:scores_parcours_haut)
+          .and_return({ hpar: 3, hgac: 3, hcvf: 3, hpfb: 3 })
+        expect(restitution.parcours_haut).to equal(::Competence::PROFIL_ABERRANT)
+      end
+    end
+
+    context 'quand le score total est >= 16 et < 26' do
+      it 'retourne le profil 4H' do
+        expect(restitution).to receive(:scores_parcours_haut)
+          .and_return({ hpar: 4, hgac: 4, hcvf: 4, hpfb: 4 })
+        expect(restitution.parcours_haut).to equal(::Competence::PROFIL_4H)
+      end
+    end
+
+    context 'quand le score total est >= 26 et < 33' do
+      it 'retourne le profil 4H+' do
+        expect(restitution).to receive(:scores_parcours_haut)
+          .and_return({ hpar: 7, hgac: 7, hcvf: 7, hpfb: 7 })
+        expect(restitution.parcours_haut).to equal(::Competence::PROFIL_4H_PLUS)
+      end
+    end
+
+    context 'quand le score total est >= 33' do
+      it 'retourne le profil 4H++' do
+        expect(restitution).to receive(:scores_parcours_haut)
+          .and_return({ hpar: 9, hgac: 9, hcvf: 9, hpfb: 9 })
+        expect(restitution.parcours_haut).to equal(::Competence::PROFIL_4H_PLUS_PLUS)
+      end
     end
   end
 

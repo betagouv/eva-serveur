195e8ac6aab38061f928269ce29583ced0171b33
Hadrien Blanc <blanc.hadrien@gmail.com>
2022-08-17 16:46:01 +0200

    ajout du bénéficiaire sur le détail de l'évaluation
    modifie aussi automatiquement le nom du bénéficiaire au changement du nom de l'évaluation

--- spec/models/evaluation_spec.rb
+++ spec/models/evaluation_spec.rb
@@ -7,4 +7,24 @@ describe Evaluation do
   it { is_expected.to validate_presence_of :debutee_le }
   it { is_expected.to belong_to :campagne }
   it { is_expected.to have_one :condition_passation }
+
+  describe "verifie la génération du bénéficiaire" do
+    let!(:evaluation) { create :evaluation, nom: 'NOM' }
+
+    context "quand nous n'avons pas de bénéficiaire" do
+      it 'génére un bénéficiaire du même nom' do
+        evaluation.beneficiaire_create_or_update_nom!
+        expect(evaluation.beneficiaire.nom).to eq('NOM')
+      end
+    end
+
+    context "quand nous avons déjà un bénéficiaire" do
+      it 'mets à jour le nom du bénéficiaire' do
+        evaluation.beneficiaire_create_or_update_nom!
+        evaluation.nom = 'nouveau NOM'
+        evaluation.beneficiaire_create_or_update_nom!
+        expect(evaluation.beneficiaire.nom).to eq('nouveau NOM')
+      end
+    end
+  end
 end

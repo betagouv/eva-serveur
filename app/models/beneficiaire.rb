77b2805e46324fe02ad3c1e708c67547c77b957d
Hadrien Blanc <blanc.hadrien@gmail.com>
2022-08-19 11:38:13 +0200

    beneficiaire : has_one evaluation --> has_many

--- app/models/beneficiaire.rb
+++ app/models/beneficiaire.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Beneficiaire < ApplicationRecord
-  has_one :evaluation, dependent: :nullify
+  has_many :evaluation, dependent: :nullify
 
   def display_name
     nom

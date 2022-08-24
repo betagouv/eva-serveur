3ce5aa3a742102fb6f779ff591f27f7805046cfc
Clément Prod'homme <clement@captive.fr>
2022-08-24 11:11:41 +0200

    ♻️ Utilise un accept_nested_attributes_for pour l'évaluation et le bénéficiaire

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -15,10 +15,10 @@ class Evaluation < ApplicationRecord
   validates :nom, :debutee_le, presence: true
   belongs_to :campagne, counter_cache: :nombre_evaluations
   belongs_to :beneficiaire, optional: true
+  accepts_nested_attributes_for :beneficiaire
   attr_accessor :code_campagne
 
   before_validation :trouve_campagne_depuis_code
-  after_save :ajout_du_beneficiaire_avec_nom!
   validate :code_campagne_connu
   has_one :condition_passation, dependent: :destroy
   accepts_nested_attributes_for :condition_passation
@@ -51,10 +51,6 @@ class Evaluation < ApplicationRecord
 
   def anonyme?
     anonymise_le.present?
-  end
-
-  def ajout_du_beneficiaire_avec_nom!
-    create_beneficiaire!(nom: nom) if beneficiaire.blank? && nom.present?
   end
 
   private
--- spec/models/evaluation_spec.rb
+++ spec/models/evaluation_spec.rb
@@ -7,24 +7,4 @@ describe Evaluation do
   it { is_expected.to validate_presence_of :debutee_le }
   it { is_expected.to belong_to :campagne }
   it { is_expected.to have_one :condition_passation }
-
-  describe 'verifie la génération du bénéficiaire' do
-    let!(:evaluation) { create :evaluation, nom: 'NOM' }
-
-    context "quand nous n'avons pas de bénéficiaire" do
-      it 'génére un bénéficiaire du même nom' do
-        evaluation.ajout_du_beneficiaire_avec_nom!
-        expect(evaluation.beneficiaire.nom).to eq('NOM')
-      end
-    end
-
-    context 'quand nous avons déjà un bénéficiaire' do
-      it 'ne change pas le nom du bénéficiaire' do
-        evaluation.ajout_du_beneficiaire_avec_nom!
-        evaluation.nom = 'nouveau NOM'
-        evaluation.ajout_du_beneficiaire_avec_nom!
-        expect(evaluation.beneficiaire.nom).to eq('NOM')
-      end
-    end
-  end
 end

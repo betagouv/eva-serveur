d4aa5fc9b9c32f7dd55279242a4fba682d5ec59f
Hadrien Blanc <blanc.hadrien@gmail.com>
2022-09-02 10:34:57 +0200

    config rails 7 : action_view --> i18n

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,7 +58,7 @@ Rails.application.configure do
   config.hosts << "api.eva-dev"
 
   # Raises error for missing translations.
-  # config.action_view.raise_on_missing_translations = true
+  # config.i18n.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.

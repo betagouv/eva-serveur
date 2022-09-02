0c60f620991c105ab4f17712777453d98d27a9cf
Hadrien Blanc <blanc.hadrien@gmail.com>
2022-09-02 11:40:38 +0200

    ajout de test pour should accept_nested_attributes_for

--- spec/models/evaluation_spec.rb
+++ spec/models/evaluation_spec.rb
@@ -6,5 +6,6 @@ describe Evaluation do
   it { is_expected.to validate_presence_of :nom }
   it { is_expected.to validate_presence_of :debutee_le }
   it { is_expected.to belong_to :campagne }
+  it { should accept_nested_attributes_for :beneficiaire }
   it { is_expected.to have_one :condition_passation }
 end

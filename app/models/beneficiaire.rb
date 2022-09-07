146d7bf7811751aa7f230814eb2fc26941cbb8d9
Hadrien Blanc <blanc.hadrien@gmail.com>
2022-09-07 15:46:19 +0200

    ajout de l'icon anonymisé sur les beneficaire + message flash

--- app/models/beneficiaire.rb
+++ app/models/beneficiaire.rb
@@ -4,6 +4,10 @@ class Beneficiaire < ApplicationRecord
   has_many :evaluations, dependent: :nullify
   validates :nom, presence: true
 
+  def anonyme?
+    anonymise_le.present?
+  end
+
   def display_name
     nom
   end

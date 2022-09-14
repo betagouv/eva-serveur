e1ae71fb5a866d8a571a0e0f428f55244d1dedaa
Hadrien Blanc <blanc.hadrien@gmail.com>
2022-09-14 17:19:42 +0200

    enlever has_many: parties

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -15,7 +15,6 @@ class Evaluation < ApplicationRecord
   validates :nom, :debutee_le, presence: true
   belongs_to :campagne, counter_cache: :nombre_evaluations
   belongs_to :beneficiaire
-  has_many :parties, class_name: :Partie, dependent: :destroy
 
   accepts_nested_attributes_for :beneficiaire, update_only: true
   attr_accessor :code_campagne

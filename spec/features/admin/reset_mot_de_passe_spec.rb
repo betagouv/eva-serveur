1ebe41412b44eb1ffa18de8d36c002d659e07fa5
Clément Prod'homme <clement@captive.fr>
2022-10-12 17:42:22 +0200

    ✨ Arrivée sur la réinitialisation de mot de passe avec un mauvais lien

--- spec/features/admin/reset_mot_de_passe_spec.rb
+++ spec/features/admin/reset_mot_de_passe_spec.rb
@@ -13,15 +13,34 @@ describe 'Reset mot de passe', type: :feature do
   end
 
   describe 'page modification du mot de passe' do
-    before do
-      compte = create :compte
-      raw_token, hashed_token = Devise.token_generator.generate(Compte, :reset_password_token)
-      compte.update(reset_password_token: hashed_token, reset_password_sent_at: Time.now.utc)
-      visit edit_compte_password_path(compte, reset_password_token: raw_token)
+    let(:compte) { create :compte }
 
-      click_on 'Valider mon nouveau mot de passe'
+    context 'avec un token valide' do
+      before do
+        raw_token, hashed_token = Devise.token_generator.generate(Compte, :reset_password_token)
+        compte.update(reset_password_token: hashed_token, reset_password_sent_at: Time.now.utc)
+        visit edit_compte_password_path(compte, reset_password_token: raw_token)
+
+        click_on 'Valider mon nouveau mot de passe'
+      end
+
+      it { expect(page.has_css?('#error_explanation')).to eq false }
     end
 
-    it { expect(page.has_css?('#error_explanation')).to eq false }
+    context 'avec un token invalide' do
+      before do
+        visit edit_compte_password_path(compte, reset_password_token: 'invalide')
+      end
+
+      it do
+        texte = I18n.t('active_admin.devise.reset_password.saisir_a_nouveau')
+        expect(page).to have_content(texte)
+      end
+
+      it do
+        texte = I18n.t('active_admin.devise.passwords.edit.token_invalide')
+        expect(page).to have_content(texte)
+      end
+    end
   end
 end

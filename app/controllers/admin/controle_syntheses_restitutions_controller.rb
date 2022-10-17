abd369cd570de826303e4c60f169977815f73bd3
Étienne Charignon <etienne.charignon@gmail.com>
2022-10-17 14:04:15 +0200

    Ajoute un page de controle des bloques de synthèses

---
+++ app/controllers/admin/controle_syntheses_restitutions_controller.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module Admin
+  class ControleSynthesesRestitutionsController < ApplicationController
+    def index
+      @syntheses_pre_positionnement = syntheses_pre_positionnement
+      @syntheses_avancees = syntheses_avancees
+    end
+
+    def syntheses_pre_positionnement
+      [
+        nil,
+        'illettrisme_potentiel',
+        'ni_ni',
+        'socle_clea'
+      ]
+    end
+
+    def syntheses_avancees
+      [[::Competence::NIVEAU_INDETERMINE, nil],
+       [::Competence::PROFIL_ABERRANT, 'aberrant'],
+       [::Competence::PROFIL_1, 'illettrisme_potentiel'],
+       [::Competence::PROFIL_2, 'illettrisme_potentiel'],
+       [::Competence::PROFIL_3, 'ni_ni'],
+       [::Competence::PROFIL_4, 'socle_clea'],
+       [::Competence::PROFIL_4H, 'socle_clea'],
+       [::Competence::PROFIL_4H_PLUS, 'socle_clea'],
+       [::Competence::PROFIL_4H_PLUS_PLUS, 'socle_clea']]
+    end
+  end
+end

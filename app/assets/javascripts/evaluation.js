ad5b9b78762886396d0f75ba8ae05a076f5fcde8
Clément Prod'homme <clement@captive.fr>
2022-11-14 17:13:03 +0100

    ✨ Conseiller, je peux modifier ma réponse sur l'accompagnement illettrisme

--- app/assets/javascripts/evaluation.js
+++ app/assets/javascripts/evaluation.js
@@ -1,6 +1,13 @@
 function afficheReponse(reponse) {
-  $('.mise-en-action').addClass('hidden');
+  $('.mise-en-action').removeClass('d-flex').addClass('hidden');
   $(`div[data-reponse='${reponse}']`).removeClass('hidden');
+  $('.card__banner--illettrisme').addClass('card__banner--illettrisme-avec-reponse');
+}
+
+function modifieReponseAccompagnementIllettrisme() {
+  $('.mise-en-action').addClass('hidden');
+  $("div[data-reponse='vide']").removeClass('hidden').addClass('d-flex');
+  $('.card__banner--illettrisme').removeClass('card__banner--illettrisme-avec-reponse');
 }
 
 function enregistreReponseAccompagnementIllettrisme(evaluationId, reponse) {
@@ -25,4 +32,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const reponse = $(this).data('reponse');
     enregistreReponseAccompagnementIllettrisme(evaluationId, reponse)
   });
+
+  $( ".modifier-mise-en-action" ).click(function(e) {
+    e.preventDefault();
+    modifieReponseAccompagnementIllettrisme()
+  })
 });

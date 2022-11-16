efe94af20de21787a1dd20b3212f7b1fdf7e9fc7
Étienne Charignon <etienne.charignon@gmail.com>
2022-11-16 16:12:24 +0100

    Migre le schema de la base en deux temps à l'init des reviewapps

--- bin/reviewappdeploy.sh
+++ bin/reviewappdeploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-bundle exec rails db:migrate
+bundle exec rails db:migrate VERSION=20221102151638
 bundle exec rake reviewapp:initdb
+bundle exec rails db:migrate
 bundle exec rails db:seed
 bundle exec rake reviewapp:seed

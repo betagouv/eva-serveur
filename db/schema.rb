c79ef58ca462b303e458ee14163610501bfba06a
guillaume imary <guillaume.imary@gmail.com>
2023-01-03 10:51:24 +0100

    🗃️ Ajoute Mode tutoriel pour persister lorsqu'il est terminé/quitté ou non

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_25_162847) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_02_144809) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -138,6 +138,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_25_162847) do
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
     t.datetime "deleted_at"
+    t.boolean "mode_tutoriel", default: true
     t.index ["confirmation_token"], name: "index_comptes_on_confirmation_token", unique: true
     t.index ["deleted_at"], name: "index_comptes_on_deleted_at"
     t.index ["email"], name: "index_comptes_on_email", unique: true

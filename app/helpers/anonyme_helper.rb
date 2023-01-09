34eb129d20fbd9a3f49ee1071da044da59e2f49f
Marie Leuliette <marie.leuliette@gmail.com>
2023-01-09 13:08:48 +0100

    Refacto carte avec nom et création d'un composant
    Style sur le nom au hover de la carte

--- app/helpers/anonyme_helper.rb
+++ app/helpers/anonyme_helper.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 module AnonymeHelper
-  def nom_pour_ressource(ressource)
+  def nom_pour_ressource(ressource, nom: nil)
     prefixe_anonyme = ressource.anonyme? ? inline_svg_tag('anonyme.svg') : ''
-    prefixe_anonyme + ressource.nom
+    nom = nom.presence || ressource.nom
+    prefixe_anonyme + nom
   end
 end

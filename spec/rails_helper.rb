37c60084956c3a8d3c3800b9d7bee79b0873bc23
Étienne Charignon <etienne.charignon@gmail.com>
2023-02-10 16:34:50 +0100

    Ignore la demande si l'on tente d'ajouter un responsable de suivi inconnu
    Co-authored-by: Marie Leuliette <marie.leuliette@captive.fr>

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -41,6 +41,7 @@ RSpec.configure do |config|
   config.include Capybara::DSL, type: :feature
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include Devise::Test::IntegrationHelpers, type: :request
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.

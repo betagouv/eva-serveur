69442615a261e50920106d7607a41464b87a8563
Étienne Charignon <etienne.charignon@gmail.com>
2023-02-15 09:47:51 +0100

    Uniformisation du nommage des profils 4H…

--- app/components/referentiel_anlci_component.rb
+++ app/components/referentiel_anlci_component.rb
@@ -10,7 +10,7 @@ class ReferentielAnlciComponent < ViewComponent::Base
 
   def badges_referentiel
     commun = %w[profil1-blanc profil2-blanc profil3-blanc profil4-blanc]
-    officiel = %w[profil4_h profil4_h_plus profil4_h_plus_plus]
+    officiel = %w[profil_4h-blanc profil_4h_plus-blanc profil_4h_plus_plus-blanc]
     version_eva = %w[profil4_plus-blanc profil4_plus_plus-blanc]
     return (commun << officiel).flatten if @type == 'officiel'
 

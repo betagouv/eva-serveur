ba1f1d7d09294c6601585fe749dd96ad5d78fb04
Marie Leuliette <marie.leuliette@gmail.com>
2023-02-17 11:11:02 +0100

    créé des enums pour les dispositifs de remédiation

--- app/models/mise_en_action.rb
+++ app/models/mise_en_action.rb
@@ -1,12 +1,17 @@
 # frozen_string_literal: true
 
 class MiseEnAction < ApplicationRecord
+  DISPOSITIFS_REMEDIATION = %w[formation_competences_de_base formation_metier
+                               dispositif_remobilisation levee_freins_peripheriques].freeze
+
   belongs_to :evaluation
 
   validates :evaluation_id, uniqueness: true
   validates :effectuee, absence: false, inclusion: { in: [true, false] }
 
   before_save :enregistre_date
+
+  enum :dispositif_de_remediation, DISPOSITIFS_REMEDIATION.zip(DISPOSITIFS_REMEDIATION).to_h
 
   acts_as_paranoid
 

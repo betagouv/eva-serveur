b34e85628589a4dd7201827826972341c49963c4
Marie Leuliette <marie.leuliette@gmail.com>
2023-02-17 11:11:02 +0100

    créé des enums pour les dispositifs de remédiation

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -25,6 +25,7 @@ ActiveSupport::Inflector.inflections do |inflect|
   inflect.irregular 'donnee_sociodemographique', 'donnees_sociodemographiques'
   inflect.irregular 'type_de_programme', 'types_de_programme'
   inflect.irregular 'mise_en_action', 'mises_en_action'
+  inflect.irregular 'dispositif_de_remediation', 'dispositifs_de_remediation'
 end
 
 # These inflection rules are supported but not enabled by default:

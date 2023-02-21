5eb85fdb4d5384914ae4da1a4fa8ec353f873d68
Marie Leuliette <marie.leuliette@gmail.com>
2023-02-21 12:30:27 +0100

    :recycle: Propage l'utilisation du viewcomponent tag et fix erreur traductionx
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

--- app/components/tag.rb
+++ app/components/tag.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Tag < ViewComponent::Base
-  def initialize(contenu, supprimable: false, classes: nil, image_path: nil)
+  def initialize(contenu, supprimable: false, classes: nil, image_path: nil, url: nil)
     @contenu = contenu
     @supprimable = supprimable
     @classes = classes
     @image_path = image_path
+    @url = url
   end
 end

8981646d94861e78bfc6fbb18ea8da1e92ce91ad
Marie Leuliette <marie.leuliette@gmail.com>
2023-03-02 10:51:37 +0100

    :bug: Réinitialise les boutons de mise en action lorsque je souhaite modifier ma  mise  en action

--- app/helpers/evaluation_helper.rb
+++ app/helpers/evaluation_helper.rb
@@ -10,15 +10,25 @@ module EvaluationHelper
     liste_filtree_illettrisme_potentiel ? false : true
   end
 
+  def effectuee_avec_dispositif_remediation?(ressource)
+    ressource&.mise_en_action&.effectuee_avec_dispositif_remediation?
+  end
+
   def effectuee_sans_dispositif_remediation?(ressource)
-    ressource&.mise_en_action&.effectuee_sans_dispositif_remediation?
+    ressource&.mise_en_action&.effectuee &&
+      ressource&.mise_en_action&.dispositif_de_remediation.blank?
   end
 
   def non_effectuee_sans_difficulte?(ressource)
-    ressource&.mise_en_action&.non_effectuee_sans_difficulte?
+    ressource&.mise_en_action&.effectuee == false &&
+      ressource&.mise_en_action&.difficulte.blank?
   end
 
-  def mise_en_action_sans_qualification?(ressource)
-    effectuee_sans_dispositif_remediation?(ressource) || non_effectuee_sans_difficulte?(ressource)
+  def non_effectuee_avec_difficulte?(ressource)
+    ressource&.mise_en_action&.non_effectuee_avec_difficulte?
+  end
+
+  def mise_en_action_avec_qualification?(ressource)
+    effectuee_avec_dispositif_remediation?(ressource) || non_effectuee_avec_difficulte?(ressource)
   end
 end

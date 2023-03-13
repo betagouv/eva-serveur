5a3f1f444f8c48d09f1ac8add0f5d7e5147ab036
Étienne Charignon <etienne.charignon@gmail.com>
2023-03-13 19:05:17 +0100

    Ajoute les logos des financeurs en pied de page

---
+++ app/admin/footer.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module ActiveAdmin
+  module Views
+    class Footer < Component
+      def build(_namespace)
+        render partial: 'components/footer'
+      end
+    end
+  end
+end

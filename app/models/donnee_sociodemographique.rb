855ff5e32df6fdf8425b45170499047731216ee6
Étienne Charignon <etienne.charignon@gmail.com>
2023-03-24 15:03:58 +0100

    Ajoute les nouvelles colonnes de données sociodemographiques
    Et aligne quelques nom_techniques des questions
    
    retour arrière renomage quel_age et niveau_etude

--- app/models/donnee_sociodemographique.rb
+++ app/models/donnee_sociodemographique.rb
@@ -7,10 +7,10 @@ class DonneeSociodemographique < ApplicationRecord
 
   GENRES = %w[homme femme autre].freeze
   enum :genre, GENRES.zip(GENRES).to_h
-  NIVEAUX_ETUDES = %w[college cfg_dnb_bepc cap_bep bac
+  NIVEAUX_ETUDES = %w[pas_etude college cfg_dnb cap_bep bac
                       bac_plus2 superieur_bac_plus2].freeze
   enum :dernier_niveau_etude, NIVEAUX_ETUDES.zip(NIVEAUX_ETUDES).to_h
-  SITUATIONS = %w[scolarisation formation_professionnelle alternance emploi
+  SITUATIONS = %w[scolarisation formation_professionnelle alternance en_emploi
                   sans_emploi].freeze
   enum :derniere_situation, SITUATIONS.zip(SITUATIONS).to_h
 
@@ -20,6 +20,8 @@ class DonneeSociodemographique < ApplicationRecord
     [
       age,
       genre,
+      langue_maternelle,
+      lieu_scolarite,
       dernier_niveau_etude,
       derniere_situation
     ].compact.join(' | ')

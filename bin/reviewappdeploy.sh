f783935158ac8cbd66795fd82bd8d45f77e03ad5
Étienne Charignon <etienne.charignon@gmail.com>
2023-04-03 16:42:48 +0200

    Actualisation des données des reviewapp

--- bin/reviewappdeploy.sh
+++ bin/reviewappdeploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-bundle exec rails db:migrate VERSION=20221125162847
+bundle exec rails db:migrate VERSION=20230329085445
 bundle exec rake reviewapp:initdb
 bundle exec rails db:migrate
 bundle exec rails db:seed

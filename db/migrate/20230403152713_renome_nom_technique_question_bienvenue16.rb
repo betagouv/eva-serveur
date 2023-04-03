43c11544b764c479c9cd78acd81a15d98f0a697a
Étienne Charignon <etienne.charignon@gmail.com>
2023-04-03 18:48:18 +0200

    migre le nom technique de la question bienvenu16

---
+++ db/migrate/20230403152713_renome_nom_technique_question_bienvenue16.rb
@@ -0,0 +1,37 @@
+class RenomeNomTechniqueQuestionBienvenue16 < ActiveRecord::Migration[7.0]
+  def up
+    bienvenue_16 = QuestionQcm.find_by(nom_technique: 'bienvenue_16')
+    return unless bienvenue_16.present?
+
+    question = QuestionQcm.find_by(nom_technique: 'lieu_scolarite')
+    if question.present?
+      QuestionnaireQuestion.where(question: question).update(question_id: bienvenue_16.id)
+      question.really_destroy!
+    end
+
+    bienvenue_16.update(nom_technique: 'lieu_scolarite')
+    Choix
+      .where(question_id: bienvenue_16.id, nom_technique: 'bienvenue_16_reponse_1')
+      .update(nom_technique: 'france')
+    Choix
+      .where(question_id: bienvenue_16.id, nom_technique: 'bienvenue_16_reponse_2')
+      .update(nom_technique: 'etranger')
+    Choix
+      .where(question_id: bienvenue_16.id, nom_technique: 'bienvenue_non')
+      .update(nom_technique: 'non')
+  end
+
+  def down
+    bienvenue_16 = QuestionQcm.find_by(nom_technique: 'lieu_scolarite')
+    bienvenue_16.update(nom_technique: 'bienvenue_16')
+    Choix
+      .where(question_id: bienvenue_16.id, nom_technique: 'france')
+      .update(nom_technique: 'bienvenue_16_reponse_1')
+    Choix
+      .where(question_id: bienvenue_16.id, nom_technique: 'etranger')
+      .update(nom_technique: 'bienvenue_16_reponse_2')
+    Choix
+      .where(question_id: bienvenue_16.id, nom_technique: 'non')
+      .update(nom_technique: 'bienvenue_16_reponse_3')
+  end
+end

cca372145c5726a925dcd4a1993e5149b5595b52
Marie Leuliette <marie.leuliette@gmail.com>
2023-04-07 16:56:30 +0200

    :card_file_box: Ajoute les deux sous consignes au questionnaire de bienvenue socio et autopositionnement
    Co-authored-by: Étienne Charignon <etienne.charignon@beta.gouv.fr>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_06_135310) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_07_134617) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -281,7 +281,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_06_135310) do
   end
 
   create_table "questions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "intitule"
+    t.text "intitule"
     t.datetime "created_at", precision: nil, null: false
     t.datetime "updated_at", precision: nil, null: false
     t.string "type"

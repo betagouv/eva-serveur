110a4f7623ed7eaec8fc830add0fd84f6856db1d
Étienne Charignon <etienne.charignon@gmail.com>
2023-04-12 15:13:36 +0200

    Actualise les données des reviewapp avec le questionnaire sociodemographique_autopos sans sous-consigne

--- bin/reviewappdeploy.sh
+++ bin/reviewappdeploy.sh
@@ -5,4 +5,3 @@ bundle exec rake reviewapp:initdb
 bundle exec rails db:migrate
 bundle exec rails db:seed
 bundle exec rake reviewapp:seed
-bundle exec rake questionnaire:complete_sociodemographique_autopositionnement

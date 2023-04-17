9b0b85c0a5694a15e7dedb55dfd791d82c655e75
Étienne Charignon <etienne.charignon@gmail.com>
2023-04-17 15:36:40 +0200

    WIP : migration vers storybook 7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,13 @@
-module.exports = {
-  stories: ["../spec/components/**/*.stories.json"],
-  addons: ["@storybook/addon-controls"],
+/** @type { import('@storybook/server-webpack5').StorybookConfig } */
+const config = {
+  stories: ["../spec/components/stories/**/*.mdx", "../spec/components/stories/*.stories.json"],
+  addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
+  framework: {
+    name: "@storybook/server-webpack5",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
 };
+export default config;

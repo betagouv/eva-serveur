eea708d2ee23b9b6bfc05e89be0fff63d86a6c98
Étienne Charignon <etienne.charignon@gmail.com>
2023-04-24 10:52:32 +0200

    Traite les cas particuliers de la recherche de région

--- app/models/geoloc_helper.rb
+++ app/models/geoloc_helper.rb
@@ -3,16 +3,23 @@
 class GeolocHelper
   class << self
     def cherche_region(code_postal)
-      departement = code_postal.match(/^97|^98/) ? code_postal[0, 3] : code_postal[0, 2]
-      departement = '2A' if departement == '20'
+      return if code_postal.nil?
+
       begin
-        reponse = RestClient.get("https://geo.api.gouv.fr/departements/#{departement}")
+        reponse = RestClient.get("https://geo.api.gouv.fr/departements/#{departement(code_postal)}")
         code_region = JSON.parse(reponse)['codeRegion']
         reponse = RestClient.get("https://geo.api.gouv.fr/regions/#{code_region}")
         JSON.parse(reponse)['nom']
       rescue RestClient::NotFound
         Rails.logger.warn "Region introuvable pour le code postal #{code_postal}"
+        nil
       end
+    end
+
+    def departement(code_postal)
+      departement = code_postal.match(/^97|^98/) ? code_postal[0, 3] : code_postal[0, 2]
+      departement = '2A' if departement == '20'
+      departement
     end
   end
 end

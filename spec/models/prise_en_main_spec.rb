3349937fa9fec22e6d6a40d629f941093f88ebaf
Étienne Charignon <etienne.charignon@gmail.com>
2023-07-05 16:28:33 +0200

    Ajoute une étape pour rejoindre la structure sur le dashbord

--- spec/models/prise_en_main_spec.rb
+++ spec/models/prise_en_main_spec.rb
@@ -19,49 +19,64 @@ describe PriseEnMain do
       it { expect(prise_en_main.etape_en_cours).to eq 'creation_campagne' }
     end
 
-    context "quand il n'y a pas d'évaluation et qu'il y a une campagne" do
+    context 'quand il y a une campagne' do
       let(:nombre_campagnes) { 1 }
-      let(:nombre_evaluations) { 0 }
 
-      it { expect(prise_en_main.etape_en_cours).to eq 'test_campagne' }
-    end
+      context "et qu'il n'y a pas d'évaluation" do
+        let(:nombre_evaluations) { 0 }
 
-    context "quand il y a moins de 4 évaluations, qu'il y a une campagne
-      mais que le compte n'est pas confirmé" do
-      let(:nombre_campagnes) { 1 }
-      let(:nombre_evaluations) { 3 }
+        it { expect(prise_en_main.etape_en_cours).to eq 'test_campagne' }
+      end
 
-      it { expect(prise_en_main.etape_en_cours).to eq 'confirmation_email' }
-    end
+      context "et qu'il y a au moins une évaluation" do
+        let(:nombre_evaluations) { 1 }
 
-    context 'quand le compte est confirmé' do
-      let(:nombre_campagnes) { 1 }
-      let(:nombre_evaluations) { 3 }
-      let(:compte) { Compte.new(confirmed_at: Time.zone.now) }
+        context "et que le compte n'est pas confirmé" do
+          it { expect(prise_en_main.etape_en_cours).to eq 'confirmation_email' }
+        end
 
-      it { expect(prise_en_main.etape_en_cours).to eq 'passations' }
+        context 'et que le compte est confirmé' do
+          let(:compte) { Compte.new(confirmed_at: Time.zone.now) }
+
+          context "et que le compte n'a pas de structure" do
+            it { expect(prise_en_main.etape_en_cours).to eq 'rejoindre_structure' }
+          end
+
+          context 'et que le compte a rejoint une structure' do
+            let(:structure) { Structure.new }
+
+            before do
+              compte.structure = structure
+            end
+
+            context "et qu'il y a moins de 4 passations" do
+              let(:nombre_evaluations) { 3 }
+
+              it { expect(prise_en_main.etape_en_cours).to eq 'passations' }
+            end
+
+            context "et qu'il y au moins 4 passations" do
+              let(:nombre_evaluations) { 4 }
+
+              it { expect(prise_en_main.etape_en_cours).to eq 'fin' }
+            end
+          end
+        end
+      end
     end
   end
 
   describe '#terminee?' do
-    context "quand il n'y a pas d'évaluation" do
-      let(:nombre_evaluations) { 0 }
-
-      it { expect(prise_en_main.terminee?).to be false }
-    end
-
-    context 'quand il y a moins de 4 évaluations' do
-      let(:nombre_evaluations) { 3 }
-
-      it { expect(prise_en_main.terminee?).to be false }
-    end
-
-    context 'quand il y a au moins quatre évaluations et que le mode tutoriel est désactivé' do
-      let(:nombre_evaluations) { 4 }
-
+    context 'quand le mode tutoriel est désactivé' do
       before { compte.update(mode_tutoriel: false) }
 
       it { expect(prise_en_main.terminee?).to be true }
+    end
+
+    context 'quand le mode tutoriel est activé' do
+      before { compte.update(mode_tutoriel: true) }
+
+      it { expect(prise_en_main.terminee?).to be false }
     end
   end
 end

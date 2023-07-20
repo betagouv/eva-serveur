fca5f15dc9d41c61faa044eb4c4839ef690730c0
Étienne Charignon <etienne.charignon@gmail.com>
2023-07-20 12:06:07 +0200

    Création de story de test du composant RechercheStructure et de ses sous-composants

--- app/components/recherche_structure_component.rb
+++ app/components/recherche_structure_component.rb
@@ -5,9 +5,13 @@ class RechercheStructureComponent < ViewComponent::Base
                  ville_ou_code_postal: nil, code_postal: nil)
     @recherche_url = recherche_url
     @current_compte = current_compte
+    @ville_ou_code_postal = ville_ou_code_postal
+    @code_postal = code_postal
+    @button_disabled = true
 
     return if ville_ou_code_postal.blank?
 
+    @button_disabled = false
     @structures_code_postal = StructureLocale.where(code_postal: code_postal)
     @structures = StructureLocale.near("#{ville_ou_code_postal}, FRANCE")
                                  .where.not(id: @structures_code_postal)
--- spec/components/stories/recherche_structure_component_stories.rb
+++ spec/components/stories/recherche_structure_component_stories.rb
@@ -4,4 +4,17 @@ class RechercheStructureComponentStories < ViewComponent::Storybook::Stories
   story :sans_compte_courrant_ni_params do
     constructor(recherche_url: '/structures')
   end
+
+  story :sans_compte_courrant do
+    constructor(recherche_url: '/structures',
+                ville_ou_code_postal: 'Strasbourg (67200)',
+                code_postal: '67200')
+  end
+
+  story :avec_compte_courrant do
+    constructor(recherche_url: '/structures',
+                current_compte: 'compte_id',
+                ville_ou_code_postal: 'Strasbourg (67200)',
+                code_postal: '67200')
+  end
 end

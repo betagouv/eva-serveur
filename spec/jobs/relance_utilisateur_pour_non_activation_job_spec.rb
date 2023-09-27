d8bb7d95ffb29f290a24b76f4b4d88be2c7bf3b0
Étienne Charignon <etienne.charignon@gmail.com>
2023-09-27 15:12:38 +0200

    Ne fait pas de relance pour les comptes sans structure
    pour éviter l'erreur rollbar
    https://app.rollbar.com/a/eva-betagouv/fix/item/eva/828?utm_campaign=new_item_message&utm_medium=slack&utm_source=rollbar-notification
    undefined method `cible_evaluation' for nil:NilClass
    at
    relance(/app/app/mailers/compte_mailer.rb:20)

--- spec/jobs/relance_utilisateur_pour_non_activation_job_spec.rb
+++ spec/jobs/relance_utilisateur_pour_non_activation_job_spec.rb
@@ -35,7 +35,26 @@ describe RelanceUtilisateurPourNonActivationJob, type: :job do
     end
   end
 
+  context 'quand le compte a une campagne sans passations et pas de structure' do
+    let(:campagne) { create :campagne, compte: compte }
+
+    before do
+      compte.update(structure_id: nil)
+    end
+
+    it 'ne fais rien' do
+      expect do
+        RelanceUtilisateurPourNonActivationJob.perform_now(compte.id)
+      end.to change { ActionMailer::Base.deliveries.count }.by(0)
+    end
+  end
+
   context 'quand le compte a été supprimé' do
-    it { RelanceUtilisateurPourNonActivationJob.perform_now(1) }
+    it do
+      id_compte_supprime = 1
+      expect do
+        RelanceUtilisateurPourNonActivationJob.perform_now(id_compte_supprime)
+      end.to change { ActionMailer::Base.deliveries.count }.by(0)
+    end
   end
 end

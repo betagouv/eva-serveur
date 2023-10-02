346ecd07a8475a27333feadaa3779b3d57b9b734
Étienne Charignon <etienne.charignon@gmail.com>
2023-10-02 16:58:51 +0200

    Ajoute le bouton inclusion connect sur l'écran de login

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,9 @@ Rails.application.routes.draw do
     end
     get '/admin', to: redirect('/pro/admin/dashboard')
 
+    get "inclusion_connect/auth" => "inclusion_connect#auth"
+    get "inclusion_connect/callback" => "inclusion_connect#callback"
+
     ActiveAdmin.routes(self)
     get '/admin/structures/:id', to: 'structures#show'
 

cb18f623127d43218da4c6d6f73056550430469c
Étienne Charignon <etienne.charignon@gmail.com>
2024-01-31 12:19:48 +0100

    mise à jour storybook vers 7.6.11

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,9 @@
 module.exports = {
   stories: ["../spec/components/**/*.stories.json"],
   addons: ["@storybook/addon-controls"],
+
+  framework: {
+    name: "@storybook/server-webpack5",
+    options: {}
+  }
 };

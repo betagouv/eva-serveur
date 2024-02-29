6eb807592ab5fb2d793f67ebffc7797c976f89a3
Étienne Charignon <etienne.charignon@gmail.com>
2024-02-29 16:48:47 +0100

    Actualisation du format de la configuration de wicked_pdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -8,7 +8,7 @@
 #
 # https://github.com/mileszs/wicked_pdf/blob/master/README.md
 
-WickedPdf.config = {
+WickedPdf.configure do |c|
   # Path to the wkhtmltopdf executable: This usually isn't needed if using
   # one of the wkhtmltopdf-binary family of gems.
   # exe_path: '/usr/local/bin/wkhtmltopdf',
@@ -17,11 +17,11 @@ WickedPdf.config = {
 
   # Layout file to be used for all PDFs
   # (but can be overridden in `render :pdf` calls)
-  layout: 'pdf',
-  # show_as_html: true,
-  margin: { top:    0,
-            bottom: 0,
-            left:   0,
-            right:  0 },
-  page_size: 'A4'
-}
+  c.layout =  'pdf'
+  # c.show_as_html =  true
+  c.margin = { top:    0,
+               bottom: 0,
+               left:   0,
+               right:  0 }
+  c.page_size = 'A4'
+end

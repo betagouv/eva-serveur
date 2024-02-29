4641fbda81ba3ca1b4ab1acb9c2501dc51750199
Étienne Charignon <etienne.charignon@gmail.com>
2024-02-29 16:47:31 +0100

    Ajout d'une tache rake pour la (ré-)initialisatio du compte de démonstration

--- app/lib/eva.rb
+++ app/lib/eva.rb
@@ -4,6 +4,7 @@ module Eva
   DOCUMENT_PRISE_EN_MAIN = 'https://docs.google.com/presentation/d/e/2PACX-1vRbrOAP6ojsMldL0YZaLLWH_nrluOd9kkp_0XACQsn7lateomMATUC5uEn_CZMLni--pjpfSJ366ppf/pub?start=true&loop=false&delayms=3000'
   EMAIL_CONTACT = 'contact@eva.beta.gouv.fr'
   EMAIL_SUPPORT = 'support@eva.beta.gouv.fr'
+  EMAIL_DEMO = 'demo@eva.beta.gouv.fr'
   LIEN_LIVESTORM = 'https://app.livestorm.co/eva-diagnostic-cptc/presentation-et-demonstration-eva-session?type=detailed'
   LIEN_DEMANDE_ACCOMPAGNEMENT_ILLETTRISME = 'https://evabetagouv.typeform.com/to/oWr4IPEq'
 end

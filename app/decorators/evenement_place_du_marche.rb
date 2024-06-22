c9d9a1ae30ddf56327a239c538785c1d9583bb19
Marie Leuliette <marie.leuliette@captive.fr>
2024-06-22 16:37:38 +0900

    Caclcule et affiche les scores de numératie

---
+++ app/decorators/evenement_place_du_marche.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class EvenementPlaceDuMarche < SimpleDelegator
+  PREFIX_QUESTIONS = {
+    NumeratieN1: 'NumeratieN1',
+    NumeratieN2: 'NumeratieN2',
+    NumeratieN3: 'NumeratieN3'
+  }.freeze
+
+  def module?(nom_module)
+    donnees['question'].start_with?(PREFIX_QUESTIONS[nom_module])
+  end
+
+  def score_reponse
+    donnees['score'] || 0
+  end
+end
---
+++ app/models/restitution/place_du_marche.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require_relative '../../decorators/evenement_place_du_marche'
+
+module Restitution
+  class PlaceDuMarche < Base
+    def initialize(campagne, evenements)
+      evenements = evenements.map { |e| EvenementPlaceDuMarche.new e }
+      super
+    end
+
+    def score_niveau1
+      Evacob::ScoreModule.new.calcule(evenements, :NumeratieN1)
+    end
+
+    def score_niveau2
+      Evacob::ScoreModule.new.calcule(evenements, :NumeratieN2)
+    end
+
+    def score_niveau3
+      Evacob::ScoreModule.new.calcule(evenements, :NumeratieN3)
+    end
+
+    def synthese
+      { numeratie_niveau1: score_niveau1,
+        numeratie_niveau2: score_niveau2,
+        numeratie_niveau3: score_niveau3 }
+    end
+  end
+end

b1b25f68805c0911776fc6bcc5418f314b23ed54
Étienne Charignon <etienne.charignon@gmail.com>
2024-06-27 17:38:11 +0200

    Supprime tous les comptes en attente de la structure de démo ANLCI

--- app/helpers/structure_helper.rb
+++ app/helpers/structure_helper.rb
@@ -12,4 +12,25 @@ module StructureHelper
       [traduction_type_structure(type_structure), type_structure]
     end
   end
+
+  def cree_structure_demo
+    StructureLocale.transaction do
+      StructureLocale.where(nom: Eva::STRUCTURE_DEMO).first_or_create do |s|
+        s.type_structure = :autre
+        s.code_postal = '69003'
+      end
+    end
+  end
+
+  def vide_compte(compte)
+    Campagne.with_deleted.where(compte: compte).find_each do |campagne|
+      logger.info "destruction de la campagne #{campagne.libelle}"
+      Evaluation.with_deleted.where(campagne: campagne).find_each do |evaluation|
+        b = evaluation.beneficiaire
+        evaluation.really_destroy!
+        b.really_destroy! unless Evaluation.with_deleted.exists?(beneficiaire: b)
+      end
+      campagne.really_destroy!
+    end
+  end
 end

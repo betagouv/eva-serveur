fb9cb8e420aa4b9fb917dffa96728251ed78a2e0
Marie Leuliette <marie.leuliette@captive.fr>
2024-08-05 13:49:54 +0900

    Permet de consulter la liste des questions et d'en créer une nouvelle

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,10 +1,19 @@
 # frozen_string_literal: true
 
 class Question < ApplicationRecord
-  validates :intitule, :libelle, :nom_technique, presence: true
+  validates :libelle, :nom_technique, :type, presence: true
 
   CATEGORIE = %i[situation scolarite sante appareils].freeze
+  TYPES = %i[QuestionQcm QuestionSaisie].freeze
+  enum :type, TYPES.zip(TYPES.map(&:to_s)).to_h
   enum :categorie, CATEGORIE.zip(CATEGORIE.map(&:to_s)).to_h, prefix: true
+
+  has_many :choix, lambda {
+    order(position: :asc)
+  }, foreign_key: :question_id,
+     dependent: :destroy
+
+  accepts_nested_attributes_for :choix, allow_destroy: true
 
   acts_as_paranoid
 
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  it { is_expected.to validate_presence_of :intitule }
+  it { is_expected.to validate_presence_of :type }
   it { is_expected.to validate_presence_of :libelle }
   it { is_expected.to validate_presence_of :nom_technique }
 end

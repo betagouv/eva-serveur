22b8d22d556f46a0d128eedbb44371f8c1cd0ffd
Marie Leuliette <marie.leuliette@captive.fr>
2024-08-06 22:58:30 +0900

    Permet d'ajouter un audio au modèle Choix

--- app/models/choix.rb
+++ app/models/choix.rb
@@ -3,6 +3,7 @@
 class Choix < ApplicationRecord
   validates :intitule, :type_choix, :nom_technique, presence: true
   enum :type_choix, { bon: 0, mauvais: 1, abstention: 2 }
+  has_one_attached :audio
 
   acts_as_list scope: :question_id
 
--- spec/models/choix_spec.rb
+++ spec/models/choix_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Choix, type: :model do
   it { is_expected.to validate_presence_of :intitule }
   it { is_expected.to validate_presence_of :type_choix }
   it { is_expected.to validate_presence_of :nom_technique }
+  it { is_expected.to have_one_attached(:audio) }
 
   it do
     expect(subject).to define_enum_for(:type_choix)

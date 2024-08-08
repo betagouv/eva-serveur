2a16ebd9b3b823558db2f029180ecb567ec56dc4
Marie Leuliette <marie.leuliette@captive.fr>
2024-08-08 13:26:51 +0900

    migre les intitulés des questions

--- app/admin/questions.rb
+++ app/admin/questions.rb
@@ -3,7 +3,7 @@
 ActiveAdmin.register Question do
   menu parent: 'Parcours', if: proc { current_compte.superadmin? }
 
-  permit_params :libelle, :nom_technique, :intitule, :type, :modalite_reponse,
+  permit_params :libelle, :nom_technique, :type, :modalite_reponse,
                 choix_attributes: %i[id intitule audio type_choix _destroy nom_technique],
                 transcriptions_attributes: %i[id categorie ecrit audio _destroy]
 

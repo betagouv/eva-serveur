8eb382b7361df385180f835149465c6e9c0d6c40
Marie Leuliette <marie.leuliette@captive.fr>
2024-08-13 21:58:42 +0900

    Permet d'ajouter une bonne réponse à QuestionSaisie

--- app/admin/questions_saisie.rb
+++ app/admin/questions_saisie.rb
@@ -5,34 +5,12 @@ ActiveAdmin.register QuestionSaisie do
 
   permit_params :categorie, :libelle, :nom_technique, :message,
                 :suffix_reponse, :description, :reponse_placeholder,
-                transcriptions_attributes: %i[id categorie ecrit audio _destroy]
+                transcriptions_attributes: %i[id categorie ecrit audio _destroy],
+                choix_attributes: %i[id intitule audio type_choix _destroy nom_technique]
 
   filter :libelle
 
-  form do |f|
-    f.semantic_errors
-    f.inputs do
-      f.input :libelle
-      f.input :categorie, as: :select
-      f.input :nom_technique
-      f.input :description
-
-      if f.object.transcription_pour(:intitule).nil?
-        f.object.transcriptions.build(categorie: :intitule)
-      end
-
-      f.has_many :transcriptions, allow_destroy: false, new_record: false, heading: false do |t|
-        t.input :ecrit, label: t('.label.intitule')
-      end
-      f.input :suffix_reponse
-      f.input :reponse_placeholder
-      f.input :type_saisie
-    end
-    f.actions do
-      f.action :submit
-      annulation_formulaire(f)
-    end
-  end
+  form partial: 'form'
 
   index do
     column :libelle
--- spec/models/question_saisie_spec.rb
+++ spec/models/question_saisie_spec.rb
@@ -3,18 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe QuestionSaisie, type: :model do
+  it { is_expected.to have_one(:choix).with_foreign_key(:question_id) }
+
   describe '#as_json' do
     it 'serialise les champs' do
       question_saisie = create(:question_saisie, transcription_ecrit: 'Mon Intitulé')
+      reponse = create(:choix, :bon, question_id: question_saisie.id)
       question_saisie.update(reponse_placeholder: 'écrivez ici')
 
       json = question_saisie.as_json
       expect(json.keys).to match_array(%w[id intitule nom_technique suffix_reponse
-                                          description type sous_type placeholder])
+                                          description type sous_type placeholder reponse])
       expect(json['type']).to eql('saisie')
       expect(json['sous_type']).to eql('redaction')
       expect(json['placeholder']).to eql('écrivez ici')
       expect(json['intitule']).to eql('Mon Intitulé')
+      expect(json['reponse']).to eql(reponse.intitule)
     end
   end
 end

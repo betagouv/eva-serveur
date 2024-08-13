56c0931feea4b08d7f41623ae04ae3d7f241a78f
Marie Leuliette <marie.leuliette@captive.fr>
2024-08-13 21:58:42 +0900

    Permet d'ajouter une bonne réponse à QuestionSaisie

--- app/controllers/api/questionnaires_controller.rb
+++ app/controllers/api/questionnaires_controller.rb
@@ -4,7 +4,7 @@ module Api
   class QuestionnairesController < Api::BaseController
     def show
       questionnaire = Questionnaire.find(params[:id])
-      render json: questionnaire.questions
+      render json: questionnaire.questions.includes([:choix])
     end
   end
 end
--- app/models/question_saisie.rb
+++ app/models/question_saisie.rb
@@ -4,6 +4,11 @@ class QuestionSaisie < Question
   QUESTION_REDACTION = 'redaction_note'
   enum :type_saisie, { redaction: 0, numerique: 1 }
 
+  has_one :choix, foreign_key: :question_id,
+                  dependent: :destroy
+
+  accepts_nested_attributes_for :choix, allow_destroy: true
+
   def as_json(_options = nil)
     transcription = Transcription.find_by(categorie: :intitule, question_id: id)
     json = slice(:id, :nom_technique, :suffix_reponse, :description)
@@ -11,6 +16,7 @@ class QuestionSaisie < Question
     json['intitule'] = transcription&.ecrit
     json['sous_type'] = type_saisie
     json['placeholder'] = reponse_placeholder
+    json['reponse'] = choix&.intitule
     json
   end
 end

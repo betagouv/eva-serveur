3d8eaa5baa6c24c70296ce9e28e48ffc3cb8189f
Clément Prod'homme <clement.prod-homme@captive.fr>
2024-08-14 17:48:56 +0200

    ✨ Empêche de créer une question avec un nom technique déjà existant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_09_101217) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_14_154218) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -305,6 +305,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_09_101217) do
     t.integer "type_saisie", default: 0
     t.string "categorie"
     t.index ["deleted_at"], name: "index_questions_on_deleted_at"
+    t.index ["libelle"], name: "index_questions_on_libelle"
+    t.index ["nom_technique"], name: "index_questions_on_nom_technique", unique: true
   end
 
   create_table "questions_frequentes", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|

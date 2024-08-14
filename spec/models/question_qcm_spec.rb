7ef6c835c96ff0fdc56de381716dd5bbaa93c6ca
Marion Velard <marion.velard@captive.fr>
2024-08-14 15:09:51 +0200

    ✨ Passe l'url de l'image a envoyer a l'api pour l'afficher dans le jeu

--- spec/models/question_qcm_spec.rb
+++ spec/models/question_qcm_spec.rb
@@ -7,14 +7,20 @@ RSpec.describe QuestionQcm, type: :model do
 
   describe '#as_json' do
     it 'serialise les champs' do
-      question_qcm = create(:question_qcm, transcription_ecrit: 'Mon Intitulé')
+      question_qcm = create(:question_qcm, transcription_ecrit: 'Mon Intitulé',
+                                           illustration: Rack::Test::UploadedFile.new(
+                                             Rails.root.join('spec/support/programme_tele.png')
+                                           ))
 
       json = question_qcm.as_json
       expect(json.keys)
         .to match_array(%w[choix description id intitule nom_technique type metacompetence
-                           type_qcm])
+                           type_qcm illustration])
       expect(json['type']).to eql('qcm')
       expect(json['intitule']).to eql('Mon Intitulé')
+      expect(json['illustration']).to eql(Rails.application.routes.url_helpers.url_for(
+                                            question_qcm.illustration
+                                          ))
     end
   end
 end

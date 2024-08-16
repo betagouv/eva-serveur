38a283701c4b52678b4416ac5466a195257ea796
Marie Leuliette <marie.leuliette@captive.fr>
2024-08-16 16:25:44 +0900

    Restraint aux formats mp3 et mp4 l'enregistrement en base d'audio car on interprête volontairement uniquement ceux-ci côté front

--- app/models/choix.rb
+++ app/models/choix.rb
@@ -5,9 +5,18 @@ class Choix < ApplicationRecord
   enum :type_choix, { bon: 0, mauvais: 1, abstention: 2 }
   has_one_attached :audio
 
+  validate :audio_type
+
   acts_as_list scope: :question_id
 
   def as_json(_options = nil)
     slice(:id, :intitule, :type_choix, :nom_technique)
+  end
+
+  def audio_type
+    return unless audio.attached? && !audio.content_type.in?(%w[audio/mpeg audio/mp4])
+
+    errors.add(:audio, 'doit être un fichier MP3 ou MP4')
+    audio.purge
   end
 end
--- spec/models/transcription_spec.rb
+++ spec/models/transcription_spec.rb
@@ -5,4 +5,24 @@ require 'rails_helper'
 RSpec.describe Transcription, type: :model do
   it { is_expected.to have_one_attached :audio }
   it { is_expected.to define_enum_for(:categorie).with_values(%i[intitule modalite_reponse]) }
+
+  describe 'validations' do
+    let(:transcription) { Transcription.new }
+    it 'ne valide pas un audio de type wav' do
+      transcription.audio.attach(io: Rails.root.join('spec/support/bravo.wav').open,
+                                 filename: 'bravo.wav')
+      expect(transcription.valid?).to be(false)
+      expect(transcription.errors[:audio]).to include('doit être un fichier MP3 ou MP4')
+      transcription.save
+      expect(transcription.audio).to_not be_attached
+    end
+
+    it 'valide un audio de type mp3' do
+      transcription.audio.attach(io: Rails.root.join('spec/support/alcoolique.mp3').open,
+                                 filename: 'alcoolique.mp3')
+      expect(transcription.valid?).to be(true)
+      transcription.save
+      expect(transcription.audio).to be_attached
+    end
+  end
 end

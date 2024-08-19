a0ef658e50a2ac34cd402c20022577653b3fe6bd
Étienne Charignon <etienne.charignon@gmail.com>
2024-08-19 12:56:52 +0200

    Fixe d'anciennes migrations qui ne passaient plus suite à certaines mises à jours de dépendences

--- db/migrate/20230403152713_renome_nom_technique_question_bienvenue16.rb
+++ db/migrate/20230403152713_renome_nom_technique_question_bienvenue16.rb
@@ -23,15 +23,17 @@ class RenomeNomTechniqueQuestionBienvenue16 < ActiveRecord::Migration[7.0]
 
   def down
     bienvenue_16 = QuestionQcm.find_by(nom_technique: 'lieu_scolarite')
-    bienvenue_16.update(nom_technique: 'bienvenue_16')
-    Choix
-      .where(question_id: bienvenue_16.id, nom_technique: 'france')
-      .update(nom_technique: 'bienvenue_16_reponse_1')
-    Choix
-      .where(question_id: bienvenue_16.id, nom_technique: 'etranger')
-      .update(nom_technique: 'bienvenue_16_reponse_2')
-    Choix
-      .where(question_id: bienvenue_16.id, nom_technique: 'non')
-      .update(nom_technique: 'bienvenue_16_reponse_3')
+    if bienvenue_16.present?
+      bienvenue_16.update(nom_technique: 'bienvenue_16')
+      Choix
+        .where(question_id: bienvenue_16.id, nom_technique: 'france')
+        .update(nom_technique: 'bienvenue_16_reponse_1')
+      Choix
+        .where(question_id: bienvenue_16.id, nom_technique: 'etranger')
+        .update(nom_technique: 'bienvenue_16_reponse_2')
+      Choix
+        .where(question_id: bienvenue_16.id, nom_technique: 'non')
+        .update(nom_technique: 'bienvenue_16_reponse_3')
+    end
   end
 end
--- db/migrate/20230515100936_change_colonne_collation_ancestry_pour_structures.rb
+++ db/migrate/20230515100936_change_colonne_collation_ancestry_pour_structures.rb
@@ -2,7 +2,7 @@ class ChangeColonneCollationAncestryPourStructures < ActiveRecord::Migration[7.0
   class Structure < ApplicationRecord
     has_ancestry
   end
-  def change
+  def up
     model = Structure
     # set all child nodes
     model.where.not(model.arel_table[model.ancestry_column].eq(nil)).update_all("#{model.ancestry_column} = CONCAT('#{model.ancestry_delimiter}', #{model.ancestry_column}, '#{model.ancestry_delimiter}')")
@@ -10,5 +10,15 @@ class ChangeColonneCollationAncestryPourStructures < ActiveRecord::Migration[7.0
     model.where(model.arel_table[model.ancestry_column].eq(nil)).update_all("#{model.ancestry_column} = '#{model.ancestry_root}'")
 
     change_column :structures, :ancestry, :string, collation: 'C', null: false
+  end
+
+  def down
+    # Cette fonction n'a jamais été implémenté mais ça ne se voyait pas car
+    # nous utilisions la méthode "change" au lieu de "up" et "down".
+    #
+    # This migration used change_column, which is not automatically reversible.
+    # To make the migration reversible you can either:
+    # 1. Define #up and #down methods in place of the #change method.
+    # 2. Use the #reversible method to define reversible behavior.
   end
 end

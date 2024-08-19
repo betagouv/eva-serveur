24cdb9a507e2139bb814a00c4f7bb9ac6685f038
Étienne Charignon <etienne.charignon@gmail.com>
2024-08-19 15:12:59 +0200

    Ajoute les classes locales dans les migrations pour éviter les effets d'évolution de modèles dans d'autres migrations quand on les enchaine

--- db/migrate/20240807151045_migre_intitule_question.rb
+++ db/migrate/20240807151045_migre_intitule_question.rb
@@ -1,4 +1,7 @@
 class MigreIntituleQuestion < ActiveRecord::Migration[7.0]
+  class ::Question < ApplicationRecord; end
+  class ::Transcription < ApplicationRecord; end
+
   def change
     Question.where.not(intitule: nil).find_each do |question|
       Transcription.find_or_create_by(ecrit: question.intitule, question_id: question.id)

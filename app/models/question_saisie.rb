d20c2d1eec2864c5fce0e7cf974ca23e6d5afa6c
Marie Leuliette <marie.leuliette@captive.fr>
2024-08-22 13:22:17 +0900

    Ajoute nouveau champs d'une question saisie à l'object json envoyé au front

--- app/models/question_saisie.rb
+++ app/models/question_saisie.rb
@@ -9,12 +9,50 @@ class QuestionSaisie < Question
   accepts_nested_attributes_for :bonne_reponse, allow_destroy: true
 
   def as_json(_options = nil)
-    json = slice(:id, :nom_technique, :suffix_reponse, :description)
-    json['type'] = 'saisie'
-    json['intitule'] = transcription_intitule&.ecrit
-    json['sous_type'] = type_saisie
-    json['placeholder'] = reponse_placeholder
-    json['reponse'] = bonne_reponse&.intitule
-    json
+    json = base_json_object
+    json.merge!(additional_json_fields(transcription_intitule, transcription_modalite_reponse))
+  end
+
+  private
+
+  def base_json_object
+    slice(:id, :nom_technique, :suffix_reponse, :description,
+          :illustration).tap do |json|
+      json['type'] = 'saisie'
+      json['illustration'] = cdn_for(illustration)
+      json['sous_type'] = type_saisie
+      json['placeholder'] = reponse_placeholder
+      json['description'] = description
+    end
+  end
+
+  def additional_json_fields(intitule, modalite)
+    fields = { 'intitule' => intitule&.ecrit,
+               'modalite_reponse' => modalite&.ecrit,
+               'audio_url' => question_audio_principal(intitule, modalite),
+               'reponse' => { 'textes' => bonne_reponse&.intitule,
+                              'bonneReponse' => bonne_reponse&.type_choix == 'bon' } }
+    if question_audio_secondaire(intitule)
+      fields['intitule_audio'] = question_audio_secondaire(intitule)
+    end
+    fields
+  end
+
+  def question_audio_principal(intitule, modalite)
+    if intitule&.ecrit.present? && intitule.audio.attached?
+      cdn_for(intitule.audio)
+    elsif modalite&.ecrit.present? && modalite.audio.attached?
+      cdn_for(modalite.audio)
+    end
+  end
+
+  def cdn_for(attachment)
+    ApplicationController.helpers.cdn_for(attachment) if attachment.attached?
+  end
+
+  def question_audio_secondaire(intitule)
+    return unless intitule&.ecrit.blank? && intitule.audio.attached?
+
+    cdn_for(intitule.audio)
   end
 end

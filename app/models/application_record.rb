bca2702e99d678016b4b248b352436ac57348c62
Clément Prod'homme <clement.prod-homme@captive.fr>
2024-08-30 09:18:47 +0200

    ♻️ Supprime la duplication l'audio de la transcription

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -9,4 +9,12 @@ class ApplicationRecord < ActiveRecord::Base
       "activerecord.attributes.#{model_name.i18n_key}.#{enum_name.to_s.pluralize}.#{enum_value}"
     )
   end
+
+  private
+
+  def cdn_for(attachment)
+    return unless attachment.attached?
+
+    ApplicationController.helpers.cdn_for(attachment)
+  end
 end

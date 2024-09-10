fe309be86689fe2535f4a7cc870a07f1212b3fb6
Marie Leuliette <marie.leuliette@captive.fr>
2024-09-10 16:40:04 +0900

    ✨ Permet d'ajouter une réponse au form de la question glisser déposer et l'afficher dans la show

--- app/models/question_glisser_deposer.rb
+++ app/models/question_glisser_deposer.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class QuestionGlisserDeposer < Question
-  has_many :reponse, class_name: 'Choix', foreign_key: :question_id, dependent: :destroy
-  accepts_nested_attributes_for :reponse, allow_destroy: true
+  has_many :reponses, class_name: 'Choix', foreign_key: :question_id, dependent: :destroy
+  accepts_nested_attributes_for :reponses, allow_destroy: true
 
   def as_json(_options = nil)
     slice(:id, :libelle, :nom_technique, :description)

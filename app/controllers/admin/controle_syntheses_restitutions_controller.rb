6195dbc80053670f1a018c44db8a03f5cfe127ce
Étienne Charignon <etienne.charignon@gmail.com>
2024-09-18 15:38:24 +0200

    accessibilité restitution

--- app/controllers/admin/controle_syntheses_restitutions_controller.rb
+++ app/controllers/admin/controle_syntheses_restitutions_controller.rb
@@ -2,23 +2,36 @@
 
 module Admin
   class ControleSynthesesRestitutionsController < ApplicationController
-    SOUS_COMPETENCES = [['lecture', ::Competence::PROFIL_1],
-                        ['lecture', ::Competence::PROFIL_2],
-                        ['lecture', ::Competence::PROFIL_3],
-                        ['lecture', ::Competence::PROFIL_4],
-                        ['comprehension_ecrite', ::Competence::PROFIL_1],
-                        ['comprehension_ecrite', ::Competence::PROFIL_2],
-                        ['comprehension_ecrite', ::Competence::PROFIL_3],
-                        ['comprehension_ecrite', ::Competence::PROFIL_4],
-                        ['production_ecrite', ::Competence::PROFIL_1],
-                        ['production_ecrite', ::Competence::PROFIL_2],
-                        ['production_ecrite', ::Competence::PROFIL_3],
-                        ['production_ecrite', ::Competence::PROFIL_4]].freeze
+    SOUS_COMPETENCES_POSITIONNEMENT = [
+      ['lecture', ::Competence::PROFIL_1],
+      ['lecture', ::Competence::PROFIL_2],
+      ['lecture', ::Competence::PROFIL_3],
+      ['lecture', ::Competence::PROFIL_4],
+      ['comprehension_ecrite', ::Competence::PROFIL_1],
+      ['comprehension_ecrite', ::Competence::PROFIL_2],
+      ['comprehension_ecrite', ::Competence::PROFIL_3],
+      ['comprehension_ecrite', ::Competence::PROFIL_4],
+      ['production_ecrite', ::Competence::PROFIL_1],
+      ['production_ecrite', ::Competence::PROFIL_2],
+      ['production_ecrite', ::Competence::PROFIL_3],
+      ['production_ecrite', ::Competence::PROFIL_4]
+    ].freeze
+    SOUS_COMPETENCES_PREPOSITIONNEMENT = [
+      ['litteratie', :pre_A1],
+      ['litteratie', :A1],
+      ['litteratie', :A2],
+      ['litteratie', :B1],
+      ['numeratie', :pre_X1],
+      ['numeratie', :X1],
+      ['numeratie', :X2],
+      ['numeratie', :Y1]
+    ].freeze
 
     def index
       @syntheses_pre_positionnement = syntheses_pre_positionnement
       @syntheses_positionnement = syntheses_positionnement
-      @sous_competences_positionnement = SOUS_COMPETENCES
+      @sous_competences_prepositionnement = SOUS_COMPETENCES_PREPOSITIONNEMENT
+      @sous_competences_positionnement = SOUS_COMPETENCES_POSITIONNEMENT
     end
 
     def syntheses_pre_positionnement

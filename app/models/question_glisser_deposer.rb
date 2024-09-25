5b3652065d0e1673e615d060bb174a6561ea7332
guillaume imary <guillaume.imary@gmail.com>
2024-09-25 15:30:58 +0200

    🗃️ Ajoute la zone de dépot url dans glisser deposer

--- app/models/question_glisser_deposer.rb
+++ app/models/question_glisser_deposer.rb
@@ -7,6 +7,16 @@ class QuestionGlisserDeposer < Question
            dependent: :destroy
   accepts_nested_attributes_for :reponses, allow_destroy: true
 
+  has_many_attached :zones_depot_url
+
+  validates :zones_depot_url,
+            blob: { content_type: 'image/svg+xml' }
+
+  attr_accessor :supprimer_zones_depot_url
+
+  before_save :valide_zones_depot_url_avec_reponse
+  after_update :supprime_zones_depot_url
+
   def as_json(_options = nil)
     json = base_json
     json.merge!(json_audio_fields, reponses_fields)
@@ -31,5 +41,40 @@ class QuestionGlisserDeposer < Question
       )
     end
     { 'reponsesNonClassees' => reponses_non_classees }
+  end
+
+  def supprime_zones_depot_url
+    zones_depot_url.purge_later if zones_depot_url.attached? && supprimer_zones_depot_url == '1'
+  end
+
+  def valide_zones_depot_url_avec_reponse
+    return unless zones_depot_url_attached?
+    return if zones_depot_url_changes_nil?
+
+    attachment_changes['zones_depot_url'].attachable.each do |file|
+      doc = Nokogiri::XML(file.download, nil, 'UTF-8')
+
+      if zones_depot_invalid?(doc)
+        add_invalid_zones_depot_error
+        throw(:abort)
+      end
+    end
+  end
+
+  def zones_depot_url_attached?
+    zones_depot_url.attached?
+  end
+
+  def zones_depot_url_changes_nil?
+    attachment_changes['zones_depot_url'].nil?
+  end
+
+  def zones_depot_invalid?(doc)
+    doc.css('.zone-depot').empty? || doc.css(".zone-depot--#{reponse.nom_technique}").empty?
+  end
+
+  def add_invalid_zones_depot_error
+    errors.add(:zones_depot_url,
+               "doit contenir les classes 'zone-depot' et 'zone-depot--#{reponse.nom_technique}'")
   end
 end

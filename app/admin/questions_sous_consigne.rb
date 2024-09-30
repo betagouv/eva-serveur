8aa7d9765eb3262b8f8a59e587341a21041d8ceb
Marie Leuliette <marie.leuliette@captive.fr>
2024-09-30 22:26:29 +0900

    ✨ Permet à l'admin d'ajouter une illustration et un intitulé audio à QuestionSousConsigne

--- app/admin/questions_sous_consigne.rb
+++ app/admin/questions_sous_consigne.rb
@@ -3,7 +3,8 @@
 ActiveAdmin.register QuestionSousConsigne do
   menu parent: 'Parcours', if: proc { current_compte.superadmin? }
 
-  permit_params :libelle, :nom_technique,
+  permit_params :libelle, :nom_technique, :illustration, :supprimer_illustration,
+                :supprimer_audio_intitule,
                 transcriptions_attributes: %i[id categorie ecrit audio _destroy]
 
   filter :libelle
@@ -14,10 +15,16 @@ ActiveAdmin.register QuestionSousConsigne do
     f.inputs do
       f.input :libelle
       f.input :nom_technique
+      render partial: 'admin/questions/input_illustration', locals: { f: f }
+
       f.object.transcriptions.build(categorie: :intitule) if f.object.transcription_intitule.nil?
 
-      f.has_many :transcriptions, allow_destroy: false, new_record: false, heading: false do |t|
-        t.input :ecrit, label: t('.label.intitule')
+      render partial: 'admin/transcriptions/input_ecrit_audio',
+             locals: { f: f, transcription_categorie: :intitule }
+
+      if f.object.transcription_intitule&.audio&.attached?
+        f.input :supprimer_audio_intitule, as: :boolean,
+                                           hint: tag_audio(f.object.transcription_intitule)
       end
     end
     f.actions do
---
+++ spec/models/question_sous_consigne_spec.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe QuestionSousConsigne, type: :model do
+  describe '#as_json' do
+    let(:question_sous_consigne) do
+      create(:question_sous_consigne,
+             illustration: Rack::Test::UploadedFile.new(
+               Rails.root.join('spec/support/programme_tele.png')
+             ))
+    end
+    let(:json) { question_sous_consigne.as_json }
+
+    it 'serialise les champs' do
+      expect(json.keys).to match_array(%w[id intitule audio_url nom_technique illustration type])
+      expect(json['type']).to eql('sous-consigne')
+    end
+
+    it "retourne l'illustration" do
+      expect(json['illustration']).to eql(Rails.application.routes.url_helpers.url_for(
+                                            question_sous_consigne.illustration
+                                          ))
+    end
+  end
+end

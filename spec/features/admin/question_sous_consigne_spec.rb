fd47d81689d4e3ffc6261a7fd2895411b252aaac
Marie Leuliette <marie.leuliette@captive.fr>
2024-10-30 22:22:28 +0900

    🥅 Envoie des messages d'erreurs sur les validations du format du xls

--- spec/features/admin/question_sous_consigne_spec.rb
+++ spec/features/admin/question_sous_consigne_spec.rb
@@ -23,7 +23,7 @@ describe 'Admin - Question Sous Consigne', type: :feature do
       within('.action-items-sidebar') do
         click_on 'Importer question sous consigne'
       end
-      expect(page).to have_content 'Importer question'
+      expect(page).to have_content 'Importer une question'
     end
   end
 end

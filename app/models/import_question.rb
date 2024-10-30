ea2ef43073240d1b33eb233ec73fc37dff3483ef
Marie Leuliette <marie.leuliette@captive.fr>
2024-10-30 22:40:35 +0900

    ✨ Mets à jour les champs spécifiques des questions Sous consigne, glisser déposer, clic dans image, saisie et qcm

--- app/models/import_question.rb
+++ app/models/import_question.rb
@@ -7,10 +7,10 @@ class ImportQuestion
   HEADERS_QCM = %i[type_qcm].freeze
   HEADERS_SAISIE = %i[suffix_reponse reponse_placeholder type_saisie bonne_reponse_intitule
                       bonne_reponse_nom_technique].freeze
-  HEADERS_SOUS_CONSIGNE = %i[libelle nom_technique intitule_ecrit intitule_audio
-                             illustration].freeze
-  HEADERS_COMMUN = %i[libelle nom_technique description intitule_ecrit intitule_audio
-                      consigne_ecrit consigne_audio illustration].freeze
+  HEADERS_SOUS_CONSIGNE = %i[libelle nom_technique illustration intitule_ecrit
+                             intitule_audio].freeze
+  HEADERS_COMMUN = %i[libelle nom_technique illustration intitule_ecrit intitule_audio
+                      consigne_ecrit consigne_audio description].freeze
 
   def initialize(type)
     @type = type
@@ -18,9 +18,11 @@ class ImportQuestion
 
   def remplis_donnees(file)
     sheet = Spreadsheet.open(file.path).worksheet(0)
-    raise Error, message_erreur_headers unless headers_valides?(sheet.rows[0])
+    @data = sheet.rows[1]
+    @headers = sheet.rows[0]
+    raise Error, message_erreur_headers unless headers_valides?
 
-    creation_question(sheet.rows[1])
+    creation_question
   rescue ActiveRecord::RecordInvalid => e
     raise Error, message_erreur_validation(e)
   rescue Down::Error
@@ -43,9 +45,9 @@ class ImportQuestion
     end.join(', '))
   end
 
-  def headers_valides?(headers)
-    headers_serialises = headers.map { |header| header.parameterize.underscore.to_sym if header }
-    headers_serialises == headers_attendus
+  def headers_valides?
+    headers_serialises = @headers.map { |header| header.parameterize.underscore.to_sym if header }
+    headers_serialises[0, headers_attendus.length] == headers_attendus
   end
 
   def headers_attendus
@@ -53,21 +55,24 @@ class ImportQuestion
       'QuestionClicDansImage' => HEADERS_COMMUN + HEADERS_CLIC_DANS_IMAGE,
       'QuestionGlisserDeposer' => HEADERS_COMMUN + HEADERS_GLISSER_DEPOSER,
       'QuestionQcm' => HEADERS_COMMUN + HEADERS_QCM,
-      'QuestionSaisie' => HEADERS_SAISIE + HEADERS_SAISIE,
+      'QuestionSaisie' => HEADERS_COMMUN + HEADERS_SAISIE,
       'QuestionSousConsigne' => HEADERS_SOUS_CONSIGNE
     }
 
     headers[@type]
   end
 
-  def creation_question(data)
+  def creation_question
     ActiveRecord::Base.transaction do
-      @question = Question.create!(type: @type, libelle: data[0],
-                                   nom_technique: data[1], description: data[2])
-      telecharge_illustration(data[7])
-      cree_transcription(:intitule, data[4], data[3])
-      cree_transcription(:modalite_reponse, data[6], data[5])
-      cree_champs_specifiques(data)
+      @question = Question.create!(type: @type, libelle: @data[0],
+                                   nom_technique: @data[1], description: @data[7])
+      telecharge_illustration(@data[2])
+      cree_transcription(:intitule, @data[4], @data[3])
+      unless @type == 'QuestionSousConsigne'
+        cree_transcription(:modalite_reponse, @data[6],
+                           @data[5])
+      end
+      update_champs_specifiques
     end
 
     @question
@@ -87,45 +92,108 @@ class ImportQuestion
     @question.illustration.attach(telecharge_fichier(url))
   end
 
-  def cree_champs_specifiques(data)
+  def update_champs_specifiques
+    case @type
+    when 'QuestionClicDansImage'
+      update_clic_dans_image
+    when 'QuestionGlisserDeposer'
+      update_glisser_deposer
+    when 'QuestionQcm'
+      update_qcm
+    when 'QuestionSaisie'
+      update_saisie
+    end
   end
 
-  def cree_reponses(data)
-    extraie_colonne_reponses.each do |data_reponse|
+  def update_clic_dans_image
+    @question.zone_cliquable.attach(telecharge_fichier(@data[8]))
+    @question.image_au_clic.attach(telecharge_fichier(@data[9]))
+  end
+
+  def update_glisser_deposer
+    @question.zone_depot.attach(telecharge_fichier(@data[8]))
+    cree_reponses
+  end
+
+  def update_qcm
+    @question.update!(type_qcm: @data[8])
+    cree_choix
+  end
+
+  def update_saisie
+    @question.update!(suffix_reponse: @data[8], reponse_placeholder: @data[9],
+                      type_saisie: @data[10])
+    cree_bonne_reponse
+  end
+
+  def cree_choix
+    extraie_colonne('choix').each_value do |data_choix|
+      cree_chaque_choix(data_choix)
+    end
+  end
+
+  def cree_reponses
+    extraie_colonne('reponse').each_value do |data_reponse|
       cree_reponse(data_reponse)
     end
   end
 
-  def cree_reponse(data_reponse)
-    Choix.create!
+  def cree_bonne_reponse
+    Choix.create!(
+      intitule: @data[11],
+      nom_technique: @data[12],
+      question_id: @question.id,
+      type_choix: 'bon'
+    )
+  end
+
+  def cree_chaque_choix(data)
+    choix = Choix.create!(
+      intitule: data['intitule'],
+      nom_technique: data['nom_technique'],
+      question_id: @question.id,
+      type_choix: data['type_choix']
+    )
+    choix.audio.attach(telecharge_fichier(data['audio']))
+  end
+
+  def cree_reponse(data)
+    reponse = Choix.create!(
+      nom_technique: data['nom_technique'],
+      position_client: data['position_client'],
+      type_choix: data['type_choix'],
+      question_id: @question.id
+    )
+    reponse.illustration.attach(telecharge_fichier(data['illustration']))
   end
 
   def telecharge_fichier(url)
     @current_download = url
+    return unless url
+
     fichier = Down.download(url)
+    content_type = Marcel::MimeType.for(fichier.path, name: fichier.original_filename)
+
     {
       io: fichier,
       filename: fichier.original_filename,
-      content_type: fichier.content_type
+      content_type: content_type
     }
   end
 
-  def extraie_colonne_reponses
-    # je parcours les colonnes
-    # je prends que celle qui commence par "reponse_"
-    # je regroupe les colonnes par ID
+  def extraie_colonne(reponse)
+    headers_data = {}
 
-    # /reponse_(\d+)_/
+    @headers.each_with_index do |header, index|
+      next unless header.to_s.match(/#{reponse}_(\d+)_/)
 
-    # je retourne un objet du type :
-    # {
-    #   1: {
-    #     libelle:
-    #     nom_technique
-    #   },
-    #   2: {
-    #     ...
-    #   }
-    # }
+      item = header.match(/#{reponse}_(\d+)_/)[1].to_i
+      data_type = header.match(/#{reponse}_\d+_(.*)/)[1]
+
+      headers_data[item] = {} if headers_data[item].nil?
+      headers_data[item][data_type] = @data[index]
+    end
+
+    headers_data
   end
 end
--- spec/models/import_question_spec.rb
+++ spec/models/import_question_spec.rb
@@ -3,14 +3,14 @@
 require 'rails_helper'
 
 describe ImportQuestion do
-  subject(:service) do
-    described_class.new('QuestionClicDansImage')
-  end
+  describe 'pour toutes les questions' do
+    subject(:service) do
+      described_class.new('QuestionClicDansImage')
+    end
 
-  describe '#remplis_donnees' do
     describe 'avec un fichier valide' do
       let(:file) do
-        fixture_file_upload('spec/support/import_question.xls', 'text/xls')
+        fixture_file_upload('spec/support/import_question_clic.xls', 'text/xls')
       end
 
       it 'importe une nouvelle question' do
@@ -36,6 +36,7 @@ describe ImportQuestion do
         question = Question.last
         expect(question.nom_technique).to eq 'N1Pse5'
         expect(question.libelle).to eq 'N1Pse5'
+        expect(question.description).to eq 'Ceci est une description'
         expect(question.illustration.attached?).to be true
       end
     end
@@ -52,6 +53,132 @@ describe ImportQuestion do
           service.remplis_donnees(file)
         end.to raise_error(ImportQuestion::Error, message)
       end
+    end
+  end
+
+  describe 'pour une question de type qcm' do
+    subject(:service) do
+      described_class.new('QuestionQcm')
+    end
+
+    let(:file) do
+      fixture_file_upload('spec/support/import_question_qcm.xls', 'text/xls')
+    end
+
+    it 'importe les données spécifiques' do
+      service.remplis_donnees(file)
+      question = Question.last
+      expect(question.type_qcm).to eq 'standard'
+    end
+
+    it 'crée les choix' do
+      service.remplis_donnees(file)
+      choix = Question.last.choix
+      expect(choix.count).to eq 2
+      choix1 = Choix.first
+      expect(choix1.intitule).to eq 'Choix1'
+      expect(choix1.nom_technique).to eq 'Choix1'
+      expect(choix1.type_choix).to eq 'bon'
+      expect(choix1.audio.attached?).to be true
+      choix2 = Choix.last
+      expect(choix2.intitule).to eq 'Choix2'
+      expect(choix2.nom_technique).to eq 'Choix2'
+      expect(choix2.type_choix).to eq 'mauvais'
+      expect(choix2.audio.attached?).to be true
+    end
+  end
+
+  describe 'pour une question de type glisser deposer' do
+    subject(:service) do
+      described_class.new('QuestionGlisserDeposer')
+    end
+
+    let(:file) do
+      fixture_file_upload('spec/support/import_question_glisser.xls', 'text/xls')
+    end
+
+    it 'importe les données spécifiques' do
+      service.remplis_donnees(file)
+      question = Question.last
+      expect(question.zone_depot.attached?).to eq true
+    end
+
+    it 'crée les réponses' do
+      service.remplis_donnees(file)
+      reponses = Question.last.reponses
+      expect(reponses.count).to eq 2
+      reponse1 = reponses.first
+      expect(reponse1.nom_technique).to eq 'reponse1'
+      expect(reponse1.position_client).to eq 2
+      expect(reponse1.type_choix).to eq 'bon'
+      expect(reponse1.illustration.attached?).to be true
+      reponse2 = reponses.last
+      expect(reponse2.nom_technique).to eq 'reponse2'
+      expect(reponse2.position_client).to eq 1
+      expect(reponse1.type_choix).to eq 'bon'
+      expect(reponse2.illustration.attached?).to be true
+    end
+  end
+
+  describe 'pour une question de type clic dans image' do
+    subject(:service) do
+      described_class.new('QuestionClicDansImage')
+    end
+
+    let(:file) do
+      fixture_file_upload('spec/support/import_question_clic.xls', 'text/xls')
+    end
+
+    it 'importe les données spécifiques' do
+      service.remplis_donnees(file)
+      question = Question.last
+      expect(question.image_au_clic.attached?).to eq true
+    end
+  end
+
+  describe 'pour une question de type saisie' do
+    subject(:service) do
+      described_class.new('QuestionSaisie')
+    end
+
+    let(:file) do
+      fixture_file_upload('spec/support/import_question_saisie.xls', 'text/xls')
+    end
+
+    it 'importe les données spécifiques' do
+      service.remplis_donnees(file)
+      question = Question.last
+      expect(question.suffix_reponse).to eq 'suffixe'
+      expect(question.reponse_placeholder).to eq 'placeholder'
+      expect(question.type_saisie).to eq 'numerique'
+      expect(question.bonne_reponse.intitule).to eq '9'
+      expect(question.bonne_reponse.nom_technique).to eq '9'
+    end
+  end
+
+  describe 'pour une question de type sous consigne' do
+    subject(:service) do
+      described_class.new('QuestionSousConsigne')
+    end
+
+    let(:file) do
+      fixture_file_upload('spec/support/import_question_consigne.xls', 'text/xls')
+    end
+
+    it 'importe les données' do
+      service.remplis_donnees(file)
+      question = Question.last
+      expect(question.nom_technique).to eq 'N1Pse5'
+      expect(question.libelle).to eq 'N1Pse5'
+    end
+
+    it "créé la transcription de li'intitulé seulement" do
+      service.remplis_donnees(file)
+      transcriptions = Question.last.transcriptions
+      expect(transcriptions.count).to eq 1
+      expect(transcriptions.first.categorie).to eq 'intitule'
+      expect(transcriptions.first.ecrit).to eq 'Ceci est un intitulé'
+      expect(transcriptions.first.audio.attached?).to be true
     end
   end
 end

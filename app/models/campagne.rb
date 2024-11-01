3b656a25958f855da833b3e4c853bbdf93fd21f9
Marie Leuliette <marie.leuliette@captive.fr>
2024-11-01 17:28:55 +0900

    ♻️ Refacto de campagne avec positionnement

--- app/models/campagne.rb
+++ app/models/campagne.rb
@@ -59,16 +59,12 @@ class Campagne < ApplicationRecord
     configuration_inclus?(&:competences_transversales?)
   end
 
-  def avec_numeratie?
-    configuration_inclus?(&:numeratie?)
-  end
-
   def avec_pre_positionnement?
     configuration_inclus?(&:pre_positionnement?)
   end
 
-  def avec_litteratie?
-    configuration_inclus?(&:litteratie?)
+  def avec_positionnement?(competence)
+    configuration_inclus?(&:"#{competence}?")
   end
 
   def genere_code_unique

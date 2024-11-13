52bfa7eb8616d9d8d348d2efb06ca07507d0f5a7
guillaume imary <guillaume.imary@gmail.com>
2024-11-13 17:11:04 +0100

    ✨ Change l'export afin d'exporter unz ou plusieurs à la fois

--- app/admin/questions.rb
+++ app/admin/questions.rb
@@ -5,7 +5,9 @@ ActiveAdmin.register Question do
 
   member_action :export_xls, method: :get do
     question = Question.find(params[:id])
-    export = ImportExport::Questions::ImportExportDonnees.new(question: question).exporte_donnees
+    export =
+      ImportExport::Questions::ImportExportDonnees.new(questions: [question], type: question.type)
+                                                  .exporte_donnees
     send_data export[:xls],
               content_type: export[:content_type],
               filename: export[:filename]
--- app/models/import_export/questions/export.rb
+++ app/models/import_export/questions/export.rb
@@ -3,9 +3,9 @@
 module ImportExport
   module Questions
     class Export < ::ImportExport::ExportXls
-      def initialize(question, headers)
+      def initialize(questions, headers)
         super()
-        @question = question
+        @questions = questions
         @headers = headers
       end
 
@@ -16,31 +16,35 @@ module ImportExport
         retourne_le_contenu_du_xls
       end
 
-      def nom_du_fichier
-        genere_fichier(@question.nom_technique)
+      def nom_du_fichier(type)
+        genere_fichier(type)
       end
 
       private
 
       def remplie_la_feuille
-        @headers.each { |_valeur| remplis_champs_commun }
+        @questions.each_with_index do |question, index|
+          @question = question
+          @ligne = index + 1
+          remplis_champs_commun
+        end
       end
 
       def remplis_champs_commun
-        @sheet[1, 0] = @question.libelle
-        @sheet[1, 1] = @question.nom_technique
-        @sheet[1, 2] = @question.illustration_url
-        @sheet[1, 3] = @question.transcription_intitule&.ecrit
-        @sheet[1, 4] = @question.transcription_intitule&.audio_url
+        @sheet[@ligne, 0] = @question.libelle
+        @sheet[@ligne, 1] = @question.nom_technique
+        @sheet[@ligne, 2] = @question.illustration_url
+        @sheet[@ligne, 3] = @question.transcription_intitule&.ecrit
+        @sheet[@ligne, 4] = @question.transcription_intitule&.audio_url
         remplis_champs_additionnels
       end
 
       def remplis_champs_additionnels
         return if @question.sous_consigne?
 
-        @sheet[1, 5] = @question.transcription_modalite_reponse&.ecrit
-        @sheet[1, 6] = @question.transcription_modalite_reponse&.audio_url
-        @sheet[1, 7] = @question.description
+        @sheet[@ligne, 6] = @question.transcription_modalite_reponse&.audio_url
+        @sheet[@ligne, 5] = @question.transcription_modalite_reponse&.ecrit
+        @sheet[@ligne, 7] = @question.description
         remplis_champs_specifiques
       end
 
@@ -54,43 +58,43 @@ module ImportExport
       end
 
       def remplis_champs_clic_dans_image
-        @sheet[1, 8] = @question.zone_cliquable_url
-        @sheet[1, 9] = @question.image_au_clic_url
+        @sheet[@ligne, 8] = @question.zone_cliquable_url
+        @sheet[@ligne, 9] = @question.image_au_clic_url
       end
 
       def remplis_champs_glisser_deposer
-        @sheet[1, 8] = @question.zone_depot_url
-        @question.reponses.each_with_index { |choix, index| ajoute_reponses(choix, 1, index) }
+        @sheet[@ligne, 8] = @question.zone_depot_url
+        @question.reponses.each_with_index { |choix, index| ajoute_reponses(choix, index) }
       end
 
       def remplis_champs_saisie
-        @sheet[1, 8] = @question.suffix_reponse
-        @sheet[1, 9] = @question.reponse_placeholder
-        @sheet[1, 10] = @question.type_saisie
+        @sheet[@ligne, 8] = @question.suffix_reponse
+        @sheet[@ligne, 9] = @question.reponse_placeholder
+        @sheet[@ligne, 10] = @question.type_saisie
         return unless @question.bonne_reponse
 
-        @sheet[1, 11] = @question.bonne_reponse.intitule
-        @sheet[1, 12] = @question.bonne_reponse.nom_technique
+        @sheet[@ligne, 11] = @question.bonne_reponse.intitule
+        @sheet[@ligne, 12] = @question.bonne_reponse.nom_technique
       end
 
       def remplis_champs_qcm
-        @sheet[1, 8] = @question.type_qcm
-        @question.choix.each_with_index { |choix, index| ajoute_choix(choix, 1, index) }
+        @sheet[@ligne, 8] = @question.type_qcm
+        @question.choix.each_with_index { |choix, index| ajoute_choix(choix, index) }
       end
 
-      def ajoute_choix(choix, ligne, index)
+      def ajoute_choix(choix, index)
         columns = %w[intitule nom_technique type_choix audio]
         columns.each_with_index do |col, i|
           @sheet[0, 9 + (index * 4) + i] = "choix_#{index + 1}_#{col}"
-          @sheet[ligne, 9 + (index * 4) + i] = choix.send(col)
+          @sheet[@ligne, 9 + (index * 4) + i] = choix.send(col)
         end
       end
 
-      def ajoute_reponses(choix, ligne, index)
+      def ajoute_reponses(choix, index)
         columns = %w[nom_technique position_client type_choix illustration]
         columns.each_with_index do |col, i|
           @sheet[0, 9 + (index * 4) + i] = "reponse_#{index + 1}_#{col}"
-          @sheet[ligne, 9 + (index * 4) + i] = choix.send(col)
+          @sheet[@ligne, 9 + (index * 4) + i] = choix.send(col)
         end
       end
     end

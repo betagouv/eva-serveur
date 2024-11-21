f9df40477424a0a588dee1e5c4c0c70b55804647
Étienne Charignon <etienne.charignon@gmail.com>
2024-11-21 16:18:08 +0100

    ajoute la colonne comptes.siret_pro_connect

--- db/schema.rb
+++ db/schema.rb
@@ -141,6 +141,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_22_062224) do
     t.string "id_inclusion_connect"
     t.boolean "cgu_acceptees"
     t.string "id_pro_connect"
+    t.string "siret_pro_connect"
     t.index ["confirmation_token"], name: "index_comptes_on_confirmation_token", unique: true
     t.index ["deleted_at"], name: "index_comptes_on_deleted_at"
     t.index ["email"], name: "index_comptes_on_email", unique: true, where: "(deleted_at IS NULL)"

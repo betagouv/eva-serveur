593557d7b48778991bbf54d691e553ef2cce6dc8
Marie Leuliette <marie.leuliette@captive.fr>
2024-11-22 15:36:24 +0900

    🗃️ Créé les questions numératie du niveau 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_15_120642) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_22_062224) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"

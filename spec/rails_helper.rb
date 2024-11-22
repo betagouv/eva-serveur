3e84cf9d8ff885f425e3c46f38e846e753649411
Clément Prod'homme <clement.prod-homme@captive.fr>
2024-11-22 12:18:06 +0100

    🗑️ Corrige deprecation warning avec Rails 7.2

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,7 +38,7 @@ rescue ActiveRecord::PendingMigrationError => e
 end
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = Rails.root.join('spec/fixtures')
+  config.fixture_paths = [Rails.root.join('spec/fixtures')]
   config.include Capybara::DSL, type: :feature
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::ControllerHelpers, type: :controller

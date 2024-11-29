a3526b26397f3882235fccff8d085aa93c67e7aa
Étienne Charignon <etienne.charignon@gmail.com>
2024-11-29 17:56:50 +0100

    Accessibilité du composant lien, dans le cas externe
    Au passage,
    - generalise le style des liens externe entre l'écran de login et le composant
    - Supprime fontawesome

--- app/components/lien_component.rb
+++ app/components/lien_component.rb
@@ -5,6 +5,11 @@ class LienComponent < ViewComponent::Base
     @body = body
     @url = url
     @externe = externe
-    @params = { target: '_blank' } if externe
+    return unless externe
+
+    @params = { target: '_blank',
+                title: I18n.t('.lien_externe', texte_du_lien: body),
+                rel: 'noopener',
+                class: 'lien-externe' }
   end
 end

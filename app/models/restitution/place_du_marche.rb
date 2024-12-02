8a96459223b2f77fec247ce62a396264caf63486
Clément Prod'homme <clement.prod-homme@captive.fr>
2024-12-02 17:26:56 +0100

    ♻️ Simplifie le calcul de la note sur Place du marché

--- app/models/restitution/place_du_marche.rb
+++ app/models/restitution/place_du_marche.rb
@@ -43,7 +43,6 @@ module Restitution
         profil_numeratie: profil_numeratie }
     end
 
-    # rubocop:disable Metrics/CyclomaticComplexity
     def niveau_numeratie
       niveau = 0
       n1 = pourcentage_de_reussite_pour(:N1)
@@ -53,13 +52,12 @@ module Restitution
       n3 = pourcentage_de_reussite_pour(:N3)
 
       niveau = 1
-      niveau = 2 if n2 && n1 > SEUIL_MINIMUM
-      niveau = 3 if n3 && n2 > SEUIL_MINIMUM
+      niveau = 2 if n2
+      niveau = 3 if n3
       niveau = 4 if n3 && n3 > SEUIL_MINIMUM
 
       niveau
     end
-    # rubocop:enable Metrics/CyclomaticComplexity
 
     def profil_numeratie
       return ::Competence::NIVEAU_INDETERMINE if niveau_numeratie.zero?

002262abf9b6f0335a2f9d763ee2b2200addc134
Marie Leuliette <marie.leuliette@captive.fr>
2024-12-02 13:32:30 +0900

    ✨ Ajoute la valeur prix_avec_centimes au champs type_saisie

--- app/models/question_saisie.rb
+++ app/models/question_saisie.rb
@@ -4,7 +4,7 @@ class QuestionSaisie < Question
   QUESTION_REDACTION = 'redaction_note'
   QUESTION_TYPE = 'QuestionSaisie'
 
-  enum :type_saisie, { redaction: 0, numerique: 1, texte: 2 }
+  enum :type_saisie, { redaction: 0, numerique: 1, texte: 2, prix_avec_centimes: 3 }
 
   has_many :reponses, class_name: 'Choix', foreign_key: :question_id, dependent: :destroy
   accepts_nested_attributes_for :reponses, allow_destroy: true

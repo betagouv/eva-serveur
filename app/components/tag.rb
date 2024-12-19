b7843f3224aa3b06486e8e8b313f825694d3d005
Étienne Charignon <etienne.charignon@gmail.com>
2024-12-19 18:09:27 +0100

    ♿️ Description alternatives composant tag
    Suppression de l'icone qui n'était plus utilisée

--- app/components/tag.rb
+++ app/components/tag.rb
@@ -1,11 +1,10 @@
 # frozen_string_literal: true
 
 class Tag < ViewComponent::Base
-  def initialize(contenu, supprimable: false, classes: nil, image_path: nil, url: nil)
+  def initialize(contenu, supprimable: false, classes: nil, url: nil)
     @contenu = contenu
     @supprimable = supprimable
     @classes = classes
-    @image_path = image_path
     @url = url
   end
 end

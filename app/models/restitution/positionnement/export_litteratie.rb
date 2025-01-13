994670f7af0b15bfbf52fe289d17066f8703b34b
Marion Velard <marion.velard@captive.fr>
2025-01-13 12:19:51 +0100

    ✨ Permet d'afficher le temps total sur l'export Litteratie

--- app/models/restitution/positionnement/export_litteratie.rb
+++ app/models/restitution/positionnement/export_litteratie.rb
@@ -20,10 +20,18 @@ module Restitution
         @sheet.row(ligne).replace([evenement.donnees['question'],
                                    evenement.donnees['intitule'],
                                    evenement.reponse_intitule,
+                                   calcule_temps_passe(evenement),
                                    evenement.donnees['score'],
                                    evenement.donnees['scoreMax'],
                                    evenement.donnees['metacompetence']])
         ligne + 1
+      end
+
+      def calcule_temps_passe(evenement)
+        evenement_debut = evenement.recupere_evenement_affichage_question_qcm
+        temps_total = evenement_debut.nil? ? 0 : evenement.date - evenement_debut.date
+
+        Restitution::Base::TempsTotal.format_temps_total(temps_total)
       end
     end
   end

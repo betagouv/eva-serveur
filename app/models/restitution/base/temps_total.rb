2e6a98bebbfb368dab57f1925d7be75809db8331
Marion Velard <marion.velard@captive.fr>
2025-01-17 10:51:43 +0100

    ♻️  Refactorise la facon de calculer le temps passé dans les exports de litteratie et numeratie

--- app/models/restitution/base/temps_total.rb
+++ app/models/restitution/base/temps_total.rb
@@ -9,12 +9,6 @@ module Restitution
 
         evenements.last.date - evenements.first.date
       end
-
-      def self.format_temps_total(temps_total)
-        minutes = temps_total / 60
-        seconds = temps_total % 60
-        format('%<minutes>02d:%<seconds>02d', minutes: minutes, seconds: seconds)
-      end
     end
   end
 end

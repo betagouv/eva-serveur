55180636ea1e382f21a5320dbca524320f315b64
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-01-23 15:44:47 +0100

    🐛 Corrige la traduction pour le composant BadgeProfilComponent

--- app/components/badge_profil_component.rb
+++ app/components/badge_profil_component.rb
@@ -1,7 +1,20 @@
 # frozen_string_literal: true
 
 class BadgeProfilComponent < ViewComponent::Base
+  TYPES = %i[acquis non_acquis].freeze
+
   def initialize(type)
     @type = type
+
+    validation
+  end
+
+  private
+
+  def validation
+    return if TYPES.include? @type.to_sym
+
+    valeurs = TYPES.join(', ')
+    raise "Le type #{@type} est invalide pour BadgeProfilComponent. Valeurs possible : #{valeurs}"
   end
 end

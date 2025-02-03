133fd86b5a7fa216914d355638a6fd8272495df0
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-02-03 16:01:25 +0100

    ✨ Ajoute une méthode acquis? sur les critères

--- spec/models/restitution/critere/numeratie_spec.rb
+++ spec/models/restitution/critere/numeratie_spec.rb
@@ -17,4 +17,26 @@ describe Restitution::Critere::Numeratie do
   it do
     expect(critere_numeratie).to have_attributes(attributes)
   end
+
+  describe '#acquis?' do
+    let(:critere) { described_class.new attributes.merge(pourcentage_reussite: pourcentage) }
+
+    context 'quand le pourcentage de réussite est 75' do
+      let(:pourcentage) { 75 }
+
+      it { expect(critere.acquis?).to be(true) }
+    end
+
+    context 'quand le pourcentage de réussite est 74' do
+      let(:pourcentage) { 74 }
+
+      it { expect(critere.acquis?).to be(false) }
+    end
+
+    context 'quand le pourcentage de réussite est nil' do
+      let(:pourcentage) { nil }
+
+      it { expect(critere.acquis?).to be(false) }
+    end
+  end
 end

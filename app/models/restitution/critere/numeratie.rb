320b1250fde8c841605df2957a74745081330da9
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-02-03 16:57:16 +0100

    EVA-290 - Le conseiller peut consulter si l'évalué a acquis le critère évalué (#1854)
    * ✨ Ajoute une méthode acquis? sur les critères
    
    * ✨ Le conseiller peut consulter si l'évalué a acquis le critère évalué
    
    EVA-290
    
    https://captive-team.atlassian.net/browse/EVA-290

--- app/models/restitution/critere/numeratie.rb
+++ app/models/restitution/critere/numeratie.rb
@@ -6,11 +6,28 @@ module Restitution
       include ActiveModel::Model
       include ActiveModel::Attributes
 
+      # Exemple : 2.1 doit avoir un score >= 75% pour être acquis
+      RESULTATS = {
+        '2.1': 75,
+        '2.2': 50,
+        '2.3': 75,
+        '2.4': 100,
+        '2.5': 66
+      }.freeze
+
       attribute :libelle, :string
       attribute :code_clea, :string
       attribute :nombre_tests_proposes, :integer
       attribute :nombre_tests_proposes_max, :integer
       attribute :pourcentage_reussite, :integer
+
+      def acquis?
+        minimum_pourcentage = RESULTATS[code_clea[0, 3].to_sym]
+        return false if minimum_pourcentage.nil?
+        return false if pourcentage_reussite.nil?
+
+        pourcentage_reussite >= minimum_pourcentage
+      end
     end
   end
 end

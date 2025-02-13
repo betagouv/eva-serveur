7dc414eefc7a2ac3cdd91a320d4f08bc1a941809
Étienne Charignon <etienne.charignon@gmail.com>
2025-02-13 10:14:06 +0100

    Réparer l'initialisation des reviewapps

--- bin/reviewappdeploy.sh
+++ bin/reviewappdeploy.sh
@@ -5,6 +5,8 @@ bundle exec rails db:migrate VERSION=20230406135310
 bundle exec rake reviewapp:ignore_migrations[20230419084251,20230407134617]
 bundle exec rails db:migrate VERSION=20240809100708
 bundle exec rake reviewapp:initdb
+bundle exec rails db:migrate VERSION=20241206052500
+bundle exec rake reviewapp:ignore_migrations[20241206064741,20250103165108,20250103172005]
 bundle exec rails db:migrate
 bundle exec rails db:seed
 bundle exec rake reviewapp:seed

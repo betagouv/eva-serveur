5391ae8fa241501ac8fadb9b878636448dd71af6
Étienne Charignon <etienne.charignon@gmail.com>
2025-02-14 16:28:15 +0100

    Ajoute le parcours type Numératie dans les données des reviewapps

--- bin/reviewappdeploy.sh
+++ bin/reviewappdeploy.sh
@@ -2,7 +2,7 @@
 
 bundle exec rails db:migrate VERSION=20210305110556
 bundle exec rails db:migrate VERSION=20230406135310
-bundle exec rake reviewapp:ignore_migrations[20230419084251,20230407134617]
+bundle exec rake reviewapp:ignore_migrations[20230419084251,20230407134617,20240624120736]
 bundle exec rails db:migrate VERSION=20240809100708
 bundle exec rake reviewapp:initdb
 bundle exec rails db:migrate VERSION=20241206052500

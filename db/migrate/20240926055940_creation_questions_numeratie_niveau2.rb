626804b05e9d0a48644dcf03fd73cc6e9dff6b61
Étienne Charignon <etienne.charignon@gmail.com>
2025-02-21 16:07:35 +0100

    Ajoute les questions N2Prh1 et N2Prh2 dans les données des reviewapp

--- db/migrate/20240926055940_creation_questions_numeratie_niveau2.rb
+++ db/migrate/20240926055940_creation_questions_numeratie_niveau2.rb
@@ -9,7 +9,7 @@ class CreationQuestionsNumeratieNiveau2 < ActiveRecord::Migration[7.0]
       'QuestionClicDansImage' => ['N2Plp1', 'N2Pod1', 'N2Pod2'],
       'QuestionQcm' => ['N2Plp2', 'N2Rlp1', 'N2Rlp2', 'N2Put1', 'N2Put2', 'N2Rut1', 'N2Rut2', 'N2Ptg2', 'N2Rtg1', 'N2Rtg2', 'N2Ppl1', 'N2Rpl1', 'N2Rpl2'],
       'QuestionSaisie' => ['N2Ppe1', 'N2Ppe2', 'N2Rpe1', 'N2Rpe2', 'N2Psu1', 'N2Psu2', 'N2Rsu1', 'N2Rsu2', 'N2Pom1', 'N2Pom2', 'N2Rom1', 'N2Rom2', 'N2Rod1', 'N2Rod2', 'N2Ptg1', 'N2Ppl2'],
-      'QuestionGlisserDeposer' => ['N2Pon1', 'N2Pon2', 'N2Ron1', 'N2Ron2']
+      'QuestionGlisserDeposer' => ['N2Pon1', 'N2Pon2', 'N2Ron1', 'N2Ron2', 'N2Prh1', 'N2Prh2']
     }.each do |type, nom_techniques|
       nom_techniques.each do |nom_technique|
         question = Question.find_or_initialize_by(nom_technique: nom_technique)

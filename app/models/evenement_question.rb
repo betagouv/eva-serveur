7913eff18f2300e7e0042992a5d3cfdd2f363f82
guillaume imary <guillaume.imary@gmail.com>
2025-02-26 11:09:40 +0100

    🐛 Refacto le calcul de score pour CLEA

--- app/models/evenement_question.rb
+++ app/models/evenement_question.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class EvenementQuestion
+class EvenementQuestion # rubocop:disable Metrics/ClassLength
   def initialize(question:, evenement: nil)
     @evenement = evenement || Evenement.new(donnees: {})
     @question = question || Question.new(nom_technique: evenement.donnees['question'])
@@ -66,31 +66,6 @@ class EvenementQuestion
     questions.any? { |q| q.nom_technique == nom_technique }
   end
 
-  def self.prises_en_compte_pour_calcul_score_clea(evenements_questions) # rubocop:disable all
-    resultat = evenements_questions.flatten
-
-    evenements_questions_groupes = evenements_questions.group_by do |evenement_question|
-      evenement_question.nom_technique[0, 5]
-    end
-
-    evenements_questions_groupes.each do |nom_technique, groupe|
-      next unless groupe.all? do |evenement_question|
-        evenement_question.est_principale? && evenement_question.score.positive?
-      end
-
-      nom_technique_rattrapage =
-        Restitution::Evacob::ScoreModule::NUMERATIE_METRIQUES[nom_technique]
-
-      next unless nom_technique_rattrapage
-
-      resultat.reject! do |evenement_question|
-        evenement_question.nom_technique.start_with? nom_technique_rattrapage
-      end
-    end
-
-    resultat
-  end
-
   def self.pourcentage_pour_groupe(evenements_questions)
     scores = evenements_questions.map do |evenement_question|
       next if evenement_question.score_max.nil?
@@ -121,5 +96,68 @@ class EvenementQuestion
     evenements_questions.select do |evenement_question|
       metacompetences.include? evenement_question.metacompetence
     end
+  end
+
+  def self.prises_en_compte_pour_calcul_score_clea(evenements_questions)
+    resultat = evenements_questions.flatten
+    dernier_niveau_joue = dernier_niveau_joue(resultat)
+
+    return resultat if dernier_niveau_joue.nil?
+
+    resultat = exclure_niveaux_superieurs(resultat, dernier_niveau_joue)
+    groupes_clea = regrouper_par_critere_clea(resultat)
+
+    selectionner_questions_a_prendre(groupes_clea)
+  end
+
+  def self.dernier_niveau_joue(evenements_questions)
+    niveaux_joues = evenements_questions
+                    .select(&:a_ete_repondue?)
+                    .map { |eq| eq.nom_technique.match(/^N(\d)/)&.captures&.first.to_i }
+                    .compact
+                    .uniq
+    niveaux_joues.max || 1
+  end
+
+  def self.exclure_niveaux_superieurs(evenements_questions, dernier_niveau_joue)
+    evenements_questions.reject do |eq|
+      niveau_question = eq.nom_technique.match(/N(\d)/)&.captures&.first.to_i
+      niveau_question > dernier_niveau_joue
+    end
+  end
+
+  def self.regrouper_par_critere_clea(evenements_questions)
+    evenements_questions.group_by(&:code_clea)
+  end
+
+  def self.selectionner_questions_a_prendre(groupes_clea) # rubocop:disable all
+    questions_a_prendre = []
+
+    groupes_clea.each_value do |questions|
+      questions_principales = questions.select(&:est_principale?)
+      questions_rattrapage = questions.reject(&:est_principale?)
+
+      questions_principales.each do |question_principale|
+        questions_a_prendre << question_principale
+        question_rattrapage = questions_rattrapage.find do |q|
+          q.nom_technique.gsub(/^N(\d)R/, 'N\\1P') == question_principale.nom_technique
+        end
+        next unless question_rattrapage
+
+        if doit_prendre_en_compte_rattrapage?(question_principale, question_rattrapage)
+          questions_a_prendre << question_rattrapage
+        end
+      end
+    end
+
+    questions_a_prendre
+  end
+
+  def self.doit_prendre_en_compte_rattrapage?(question_principale, question_rattrapage)
+    return false unless question_principale.a_ete_repondue?
+    return false if question_principale.score.positive?
+    return false if question_principale.score >= question_rattrapage.score_max
+
+    true
   end
 end
--- spec/models/evenement_question_spec.rb
+++ spec/models/evenement_question_spec.rb
@@ -3,54 +3,100 @@
 require 'rails_helper'
 
 describe EvenementQuestion, type: :model do
-  describe '.prises_en_compte_pour_calcul_score_clea(questions_repondues)' do
+  describe '.prises_en_compte_pour_calcul_score_clea' do
     let(:question_n1) { build(:question, :numeratie_niveau1, score: 1) }
+    let(:question_n2) { build(:question, :numeratie_niveau2, score: 2) }
+    let(:question_n3) { build(:question, :numeratie_niveau3, score: 0) }
     let(:question_rattrapage_n1) { build(:question, :numeratie_niveau1_rattrapage, score: 1) }
+    let(:question_rattrapage_n3) { build(:question, :numeratie_niveau3_rattrapage, score: 2) }
+
+    let(:evenement_n1) do
+      evenement = build(:evenement, donnees: { score: 1, question: question_n1.nom_technique })
+      allow(evenement).to receive(:persisted?).and_return(true)
+      evenement
+    end
+    let(:evenement_n2) do
+      evenement = build(:evenement, donnees: { score: 2, question: question_n2.nom_technique })
+      allow(evenement).to receive(:persisted?).and_return(true)
+      evenement
+    end
+    let(:evenement_n3) do
+      evenement = build(:evenement, donnees: { score: 0, question: question_n3.nom_technique })
+      allow(evenement).to receive(:persisted?).and_return(true)
+      evenement
+    end
+    let(:evenement_rattrapage_n3) do
+      evenement = build(:evenement,
+                        donnees: { score: 2, question: question_rattrapage_n3.nom_technique })
+      allow(evenement).to receive(:persisted?).and_return(true)
+      evenement
+    end
+
+    let(:evenement_question_n1) do
+      described_class.new(question: question_n1, evenement: evenement_n1)
+    end
+    let(:evenement_question_n2) do
+      described_class.new(question: question_n2, evenement: evenement_n2)
+    end
+    let(:evenement_question_n3) do
+      described_class.new(question: question_n3, evenement: evenement_n3)
+    end
+    let(:evenement_question_rattrapage_n3) do
+      described_class.new(question: question_rattrapage_n3, evenement: evenement_rattrapage_n3)
+    end
 
     let(:evenements_questions) { [] }
 
-    let(:resultat) do
-      described_class.prises_en_compte_pour_calcul_score_clea(evenements_questions)
-    end
+    context 'lorsque plusieurs niveaux ont été joués' do
+      let(:evenements_questions) { [evenement_question_n1, evenement_question_n2] }
 
-    context 'quand les questions sont pour la litteratie' do
-      it "retourne l'ensemble des questions" do
-        expect(resultat).to eq evenements_questions
-      end
-    end
-
-    context "quand il n'y a pas de rattrapage dans les questions répondues" do
-      let(:evenement_question_n1) do
-        described_class.new(question: question_n1,
-                            evenement: build(:evenement,
-                                             donnees: { score: 1,
-                                                        question: question_n1.nom_technique }))
-      end
-      let(:evenement_question_n1_rattrapage) do
-        described_class.new(question: question_rattrapage_n1)
-      end
-      let(:evenements_questions) { [evenement_question_n1, evenement_question_n1_rattrapage] }
-
-      it 'ne retourne pas les questions de rattrapage' do
-        expect(resultat).not_to include(evenement_question_n1_rattrapage)
-        expect(evenements_questions.size).to eq 2
+      it 'ne garde que les questions du dernier niveau atteint' do
+        resultat = described_class.prises_en_compte_pour_calcul_score_clea(evenements_questions)
+        expect(resultat).to contain_exactly(evenement_question_n1, evenement_question_n2)
       end
     end
 
-    context 'quand il y a une question principale échouée' do
-      let(:evenement_question_n1) do
-        described_class.new(question: question_n1,
-                            evenement: build(:evenement,
-                                             donnees: { score: 0,
-                                                        question: question_n1.nom_technique }))
-      end
-      let(:evenement_question_n1_rattrapage) do
-        described_class.new(question: question_rattrapage_n1)
-      end
-      let(:evenements_questions) { [evenement_question_n1, evenement_question_n1_rattrapage] }
+    context 'lorsqu\'aucun niveau n\'a été joué' do
+      let(:evenements_questions) { [evenement_question_n3] }
 
-      it 'retourne les questions de rattrapage N1Rrn' do
-        expect(resultat).to include(evenement_question_n1_rattrapage)
+      it 'retourne toutes les questions car aucun niveau ne peut être déterminé' do
+        resultat = described_class.prises_en_compte_pour_calcul_score_clea(evenements_questions)
+        expect(resultat).to contain_exactly(evenement_question_n3)
+      end
+    end
+
+    context 'quand une question principale est réussie' do
+      let(:evenements_questions) { [evenement_question_n1, evenement_question_rattrapage_n3] }
+
+      it 'ignore la question de rattrapage' do
+        resultat = described_class.prises_en_compte_pour_calcul_score_clea(evenements_questions)
+        expect(resultat).to contain_exactly(evenement_question_n1)
+        expect(resultat).not_to include(evenement_question_rattrapage_n3)
+      end
+    end
+
+    context 'quand une question principale a échoué' do
+      let(:evenements_questions) { [evenement_question_n3, evenement_question_rattrapage_n3] }
+
+      it 'prend en compte la question de rattrapage' do
+        resultat = described_class.prises_en_compte_pour_calcul_score_clea(evenements_questions)
+        expect(resultat).to include(evenement_question_n3, evenement_question_rattrapage_n3)
+      end
+    end
+
+    context 'quand on a joué uniquement le niveau 1 et 2 mais pas le niveau 3' do
+      let(:evenement_question_n3) do
+        described_class.new(question: question_n3, evenement: nil)
+      end
+      let(:evenements_questions) do
+        [evenement_question_n1, evenement_question_n2, evenement_question_n3]
+      end
+
+      it 'ne garde que les questions des niveaux 1 et 2 et exclut le niveau 3' do
+        resultat = described_class.prises_en_compte_pour_calcul_score_clea(evenements_questions)
+
+        expect(resultat).to include(evenement_question_n1, evenement_question_n2)
+        expect(resultat).not_to include(evenement_question_n3)
       end
     end
   end

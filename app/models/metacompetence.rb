67df24cfdfd59e223280bed52fc9490c4d22f973
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-03-05 19:40:54 +0100

    ✨ Corrige les noms des métacompétences

--- app/models/metacompetence.rb
+++ app/models/metacompetence.rb
@@ -6,7 +6,7 @@ class Metacompetence
       '2.1.1' => %w[operations_addition operations_soustraction operations_multiplication
                     operations_division],
       '2.1.2' => %w[denombrement],
-      '2.1.3' => %w[ordonner_nombres_entiers ordonner_nombres_decimaux operations_nombres_entiers],
+      '2.1.3' => %w[ordonner_nombres_decimaux operations_nombres_entiers],
       '2.1.4' => %w[estimation],
       '2.1.5' => %w[],
       '2.1.6' => %w[],
@@ -17,13 +17,13 @@ class Metacompetence
       '2.2.2' => %w[pourcentages]
     },
     '2.3' => {
-      '2.3.1' => %w[unites_temps unites_de_temps_conversions],
-      '2.3.2' => %w[plannings plannings_calculs],
+      '2.3.1' => %w[unites_de_temps unites_de_temps_conversions],
+      '2.3.2' => %w[plannings_lecture plannings_calculs],
       '2.3.3' => %w[renseigner_horaires],
       '2.3.4' => %w[unites_de_mesure instruments_de_mesure],
       '2.3.5' => %w[tableaux_graphiques],
       '2.3.6' => %w[],
-      '2.3.7' => %w[surfaces perimetres perimetres_surfaces volumes]
+      '2.3.7' => %w[surfaces perimetres volumes]
     },
     '2.4' => {
       '2.4.1' => %w[lecture_plan]
@@ -31,8 +31,7 @@ class Metacompetence
     '2.5' => {
       '2.5.1' => %w[],
       '2.5.2' => %w[],
-      '2.5.3' => %w[situation_dans_lespace reconnaitre_les_nombres reconaitre_les_nombres
-                    vocabulaire_numeracie]
+      '2.5.3' => %w[situation_dans_lespace reconnaitre_les_nombres vocabulaire_numeracie]
     }
   }.freeze
 

3f0b1ec6547f02250c55b12d5c62d4cd1c9b7f2f
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-03-10 15:52:24 +0100

    ✨ Les résultats sous-domaine de Place du marché sont consultables dans l'export PDF
    Passage wicked_pdf à Puppeteer

--- db/migrate/2021/20210305120035_valide_les_acces_des_comptes_existants.rb
+++ db/migrate/2021/20210305120035_valide_les_acces_des_comptes_existants.rb
@@ -1,8 +1,10 @@
 class ValideLesAccesDesComptesExistants < ActiveRecord::Migration[6.1]
-  class Compte < ApplicationRecord; end
+  class Compte < ApplicationRecord
+    enum :statut_validation, { en_attente: 0, acceptee: 1, refusee: 2 }, prefix: :validation
+  end
 
   def up
-    Compte.update_all statut_validation: :acceptee
+    Compte.update_all statut_validation: 1
   end
 
   def down

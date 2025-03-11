0c2a92f34c1606d2118b191b099aae41de3e5ad1
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-03-11 18:21:40 +0100

    ♻️ Extraie le component Evaluation::Numeratie

---
+++ app/components/evaluation/numeratie_component.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Evaluation
+  class NumeratieComponent < ViewComponent::Base
+    def initialize(restitution_globale:, place_du_marche:)
+      @restitution_globale = restitution_globale
+      @place_du_marche = place_du_marche
+      @profil = place_du_marche ? place_du_marche.profil_numeratie : 'indetermine'
+      @synthese = restitution_globale.synthese_positionnement_numeratie
+      @sous_competences = place_du_marche.competences_numeratie
+    end
+
+    def classes_panel
+      @place_du_marche ? 'panel panel--avec-references' : 'panel'
+    end
+  end
+end

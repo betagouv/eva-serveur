fefe083e51d47de7cf92825ed46e86d68c60d2e5
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-03-12 18:01:11 +0100

    ✅ Ajout d'un test sur le composant Numeratie

---
+++ spec/components/evaluation/numeratie_component_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Evaluation::NumeratieComponent, type: :component do
+  context "quand il n'y a pas de restitution Place du marché" do
+    let(:restitution_globale) do
+      instance_double(Restitution::Globale, synthese_positionnement_numeratie: nil)
+    end
+
+    it do
+      component = described_class.new restitution_globale: restitution_globale, place_du_marche: nil
+      component_rendu = render_inline(component).to_html
+
+      expect(component_rendu).to include('Calculer et raisonner - Numératie')
+      expect(component_rendu).to include("Votre score n'a pas pu être calculé.")
+      expect(component_rendu).to include('<img').and include('alt="Profil indéterminé"')
+    end
+  end
+end

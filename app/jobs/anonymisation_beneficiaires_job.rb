6e1ccf99ce63472c1f9e88aa4ec080c7e8af04c3
Étienne Charignon <etienne.charignon@gmail.com>
2025-03-13 14:54:06 +0100

    👔 N'anonymise les bénéficiares qu'au bout de 5 ans

--- app/jobs/anonymisation_beneficiaires_job.rb
+++ app/jobs/anonymisation_beneficiaires_job.rb
@@ -3,8 +3,10 @@
 class AnonymisationBeneficiairesJob < ApplicationJob
   queue_as :default
 
+  ANNEES_CONSERVEES = 5
+
   def perform
-    beneficiaire_avec_evaluations_de_plus_1_an.find_each do |beneficiaire|
+    beneficiaires_a_annonymiser.find_each do |beneficiaire|
       Anonymisation::Beneficiaire.new(beneficiaire).anonymise
       beneficiaire.evaluations.each do |evaluation|
         Anonymisation::Evaluation.new(evaluation).anonymise
@@ -14,12 +16,12 @@ class AnonymisationBeneficiairesJob < ApplicationJob
 
   private
 
-  def beneficiaire_avec_evaluations_de_plus_1_an
+  def beneficiaires_a_annonymiser
     Beneficiaire
-      .select('beneficiaires.*, MAX(evaluations.created_at) AS derniere_evaluation_date')
+      .select('beneficiaires.*')
       .where(anonymise_le: nil)
       .joins(:evaluations).group('beneficiaires.id').having(
-        'MAX(evaluations.created_at) < ?', 1.year.ago
+        'MAX(evaluations.created_at) < ?', ANNEES_CONSERVEES.year.ago
       )
   end
 end
--- spec/jobs/anonymisation_beneficiaires_job_spec.rb
+++ spec/jobs/anonymisation_beneficiaires_job_spec.rb
@@ -3,48 +3,69 @@
 require 'rails_helper'
 
 describe AnonymisationBeneficiairesJob, type: :job do
-  it "n'anonymise pas les bénéficiaires qui ont des évaluations de moins d'un an" do
-    beneficiaire = create :beneficiaire, nom: 'nom'
-    ancienne_evaluation = create :evaluation, created_at: 2.years.ago, beneficiaire: beneficiaire
-    evaluation_recente = create :evaluation, created_at: 11.months.ago, beneficiaire: beneficiaire
+  it "n'anonymise pas les bénéficiaires qui ont au moins une évaluations recente" do
+    date_anonymisation = Time.zone.local(2025, 1, 2, 12, 0, 0)
+    Timecop.freeze(date_anonymisation) do
+      beneficiaire = create :beneficiaire, nom: 'nom'
+      ancienne_evaluation = create :evaluation, created_at: Date.new(2020, 1, 1),
+                                                beneficiaire: beneficiaire
+      evaluation_recente = create :evaluation, created_at: Date.new(2020, 1, 3),
+                                               beneficiaire: beneficiaire
 
-    described_class.perform_now
+      described_class.perform_now
 
-    ancienne_evaluation.reload
-    evaluation_recente.reload
+      beneficiaire.reload
+      ancienne_evaluation.reload
+      evaluation_recente.reload
 
-    expect(ancienne_evaluation.anonymise_le).to be_nil
-    expect(evaluation_recente.anonymise_le).to be_nil
+      expect(beneficiaire.anonymise_le).to be_nil
+      expect(ancienne_evaluation.anonymise_le).to be_nil
+      expect(evaluation_recente.anonymise_le).to be_nil
+    end
   end
 
-  it "anonymise les bénéficiaires qui ont des évaluations de plus d'un an" do
-    beneficiaire = create :beneficiaire, nom: 'nom'
-    ancienne_evaluation = create :evaluation, created_at: 2.years.ago, beneficiaire: beneficiaire
-    evaluation_recente = create :evaluation, created_at: 16.months.ago, beneficiaire: beneficiaire
+  it "anonymise les bénéficiaires qui n'ont que des évaluations ancienne" do
+    date_anonymisation = Time.zone.local(2025, 1, 2, 12, 0, 0)
+    Timecop.freeze(date_anonymisation) do
+      beneficiaire = create :beneficiaire, nom: 'nom'
+      ancienne_evaluation = create :evaluation, created_at: Date.new(2020, 1, 1),
+                                                beneficiaire: beneficiaire
+      autre_evaluation_ancienne = create :evaluation, created_at: Date.new(2019, 1, 1),
+                                                      beneficiaire: beneficiaire
 
-    described_class.perform_now
+      described_class.perform_now
 
-    ancienne_evaluation.reload
-    evaluation_recente.reload
+      beneficiaire.reload
+      ancienne_evaluation.reload
+      autre_evaluation_ancienne.reload
 
-    expect(ancienne_evaluation.anonymise_le).not_to be_nil
-    expect(evaluation_recente.anonymise_le).not_to be_nil
+      expect(beneficiaire.anonymise_le).to eq date_anonymisation
+      expect(ancienne_evaluation.anonymise_le).to eq date_anonymisation
+      expect(autre_evaluation_ancienne.anonymise_le).to eq date_anonymisation
+    end
   end
 
   it "n'anonymise pas les bénéficiaires déjà annonymisé" do
-    date_anonymisation = 12.days.ago.beginning_of_minute
-    beneficiaire = create :beneficiaire, nom: 'nom', anonymise_le: date_anonymisation
-    ancienne_evaluation = create :evaluation, created_at: 2.years.ago, beneficiaire: beneficiaire,
-                                              anonymise_le: date_anonymisation
-    evaluation_recente = create :evaluation, created_at: 16.months.ago, beneficiaire: beneficiaire,
-                                             anonymise_le: date_anonymisation
+    date_anonymisation = Time.zone.local(2025, 1, 2, 12, 0, 0)
+    Timecop.freeze(date_anonymisation) do
+      annonymisiation_existante = Time.zone.local(2024, 1, 1, 0, 0, 0)
+      beneficiaire = create :beneficiaire, nom: 'nom', anonymise_le: annonymisiation_existante
+      ancienne_evaluation = create :evaluation, created_at: Date.new(2019, 1, 1),
+                                                beneficiaire: beneficiaire,
+                                                anonymise_le: annonymisiation_existante
+      evaluation_recente = create :evaluation, created_at: Date.new(2019, 1, 1),
+                                               beneficiaire: beneficiaire,
+                                               anonymise_le: annonymisiation_existante
 
-    described_class.perform_now
+      described_class.perform_now
 
-    ancienne_evaluation.reload
-    evaluation_recente.reload
+      beneficiaire.reload
+      ancienne_evaluation.reload
+      evaluation_recente.reload
 
-    expect(ancienne_evaluation.anonymise_le).to eq(date_anonymisation)
-    expect(evaluation_recente.anonymise_le).to eq(date_anonymisation)
+      expect(beneficiaire.anonymise_le).to eq annonymisiation_existante
+      expect(ancienne_evaluation.anonymise_le).to eq annonymisiation_existante
+      expect(evaluation_recente.anonymise_le).to eq annonymisiation_existante
+    end
   end
 end

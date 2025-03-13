e157a20b101e7f73f49511987bf24038b9f88764
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-03-13 17:16:21 +0100

    ✨ Les résultats sous-domaine de Place du marché sont consultables dans l'export PDF

---
+++ app/components/evaluation/table_criteres_component.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Evaluation
+  class TableCriteresComponent < ViewComponent::Base
+    def initialize(criteres)
+      @criteres = criteres
+    end
+  end
+end

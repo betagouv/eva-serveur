0e67fac39c1de07713b23c83ba9484581e35aca3
Étienne Charignon <etienne.charignon@gmail.com>
2025-03-19 11:53:20 +0100

    🐛 Utilise le composant lien pour les liens des métacompétences

--- app/components/lien_component.rb
+++ app/components/lien_component.rb
@@ -1,14 +1,24 @@
 # frozen_string_literal: true
 
 class LienComponent < ViewComponent::Base
-  def initialize(body, url, externe: false)
+  def initialize(body, url, externe: false, aria: nil)
     @body = body
     @url = url
     @externe = externe
-    return unless externe
+    @params = { aria: aria.dup }
+    ajoute_params_externe(body, aria) if externe
+  end
 
-    @params = { target: '_blank',
-                title: I18n.t('.lien_externe', texte_du_lien: body),
-                rel: 'noopener' }
+  def ajoute_params_externe(body, aria)
+    description = description_extern(body)
+    if aria.present? && aria[:label].present?
+      description = description_extern(aria[:label])
+      @params[:aria][:label] = description
+    end
+    @params = @params.merge({ target: '_blank', title: description, rel: 'noopener' })
+  end
+
+  def description_extern(description)
+    I18n.t('.lien_externe', texte_du_lien: description)
   end
 end

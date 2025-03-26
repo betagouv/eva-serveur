025815e5646a8168b4b387e9a801482c8d1861ef
Étienne Charignon <etienne.charignon@gmail.com>
2025-03-26 19:24:19 +0100

    Exporte les noms techniques des choix de questions qui n'ont pas d'intitulé

--- spec/models/question_qcm_spec.rb
+++ spec/models/question_qcm_spec.rb
@@ -2,8 +2,50 @@
 
 require 'rails_helper'
 
-RSpec.describe QuestionQcm, type: :model do
+describe QuestionQcm, type: :model do
   it { is_expected.to have_many(:choix).order(position: :asc).with_foreign_key(:question_id) }
+
+  describe "accesseurs des réponses pour l'export" do
+    let(:choix1) do
+      create :choix, :bon, nom_technique: 'choix_1', intitule: 'intitule1'
+    end
+
+    let(:choix2) { create :choix, :bon, nom_technique: 'choix_2', intitule: '' }
+    let(:choix3) { create :choix, :mauvais, nom_technique: 'choix_3', intitule: '' }
+
+    let(:question_qcm) { create(:question_qcm, choix: [choix1, choix2, choix3]) }
+    let(:question_qcm_sans_bonne_reponses) { create(:question_qcm, choix: [choix3]) }
+
+    describe '#reponses_possibles' do
+      it "retourne l'intitulé ou le nom technique a défaut" do
+        expect(question_qcm.reponses_possibles).to eq('intitule1 | choix_2 | choix_3')
+      end
+    end
+
+    describe '#bonnes_reponses' do
+      it "retourne l'intitulé ou le nom technique a défaut" do
+        expect(question_qcm.bonnes_reponses).to eq('intitule1 | choix_2')
+      end
+
+      it "retourne vide s'il n'y a pas de bonnes réponses" do
+        expect(question_qcm_sans_bonne_reponses.bonnes_reponses).to eq('')
+      end
+    end
+
+    describe '#intitule_reponse' do
+      it "retourne l'intitulé s'il est présent" do
+        expect(question_qcm.intitule_reponse(choix1.id)).to eq('intitule1')
+      end
+
+      it "retourne le nom technique s'il n'y a pas d'intitulé" do
+        expect(question_qcm.intitule_reponse(choix2.id)).to eq('choix_2')
+      end
+
+      it "retourne le parametre si ce n'est pas un identifiant" do
+        expect(question_qcm.intitule_reponse('texte reponse')).to eq('texte reponse')
+      end
+    end
+  end
 
   describe '#as_json' do
     let(:question_qcm) do

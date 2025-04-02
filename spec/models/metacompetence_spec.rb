c9c79d69ab26770127f8214d52b1d869f5ce31ee
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-04-02 16:25:24 +0200

    ⚰️ Supprime variable inutile dans les tests

--- spec/models/metacompetence_spec.rb
+++ spec/models/metacompetence_spec.rb
@@ -3,38 +3,6 @@
 require 'rails_helper'
 
 describe Metacompetence, type: :model do
-  let(:metacompetences) do
-    %w[numeratie
-       ccf
-       syntaxe-orthographe
-       operations_addition
-       operations_soustraction
-       operations_multiplication
-       operations_division
-       denombrement
-       ordonner_nombres_entiers
-       ordonner_nombres_decimaux
-       estimation
-       proportionnalite
-       resolution_de_probleme
-       pourcentages
-       unites_de_temps
-       unites_de_temps_conversions
-       plannings_lecture
-       plannings_calculs
-       renseigner_horaires
-       unites_de_mesure
-       instruments_de_mesure
-       tableaux_graphiques
-       surfaces
-       perimetres
-       volumes
-       lecture_plan
-       situation_dans_lespace
-       reconnaitre_les_nombres
-       vocabulaire_numeracie]
-  end
-
   describe '.code_clea_sous_domaine' do
     it "retourne le code cléa du sous domaine d'une métacompétence" do
       expect(described_class.code_clea_sous_domaine('operations_addition')).to eq('2.1')

9003007366dd7aab40b9141ef70374ab67458926
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-04-14 14:16:11 +0200

    ✨ Le conseiller peut consulter le référentiel utilisé pour évaluer la numératie

---
+++ app/components/evaluation/alert_interpreter_resultats_component.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class Evaluation
+  class AlertInterpreterResultatsComponent < ViewComponent::Base
+    def initialize
+      @badges = %i[profil1 profil2 profil3 profil4 profil4_plus]
+      @scope = 'admin.restitutions.numeratie'
+      @scope_badge = 'admin.restitutions.references'
+      @lien = 'https://eva.beta.gouv.fr/en-savoir-plus-sur-les-referentiels-devaluation/'
+    end
+  end
+end

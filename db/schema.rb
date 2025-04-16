4190c6c4b5f76510aaefb1fdae531bd30eb73aad
Étienne Charignon <etienne.charignon@gmail.com>
2025-04-16 14:42:30 +0200

    Renomme le type de structure pole_emploi en france_travail

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_04_09_101829) do
+ActiveRecord::Schema[7.2].define(version: 2025_04_16_123529) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"

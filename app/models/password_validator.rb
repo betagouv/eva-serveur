2d3dad413ac906da0466da545a4ee57056fff0c4
Étienne Charignon <etienne.charignon@gmail.com>
2025-04-24 18:16:19 +0200

    🐛 Corrige le message d'erreur de validation du mot de passe
    On affichait les deux règles moins de 8 et moins de 12 en même temps

--- app/models/password_validator.rb
+++ app/models/password_validator.rb
@@ -4,10 +4,16 @@ class PasswordValidator < ActiveModel::Validator
   def validate(compte)
     value = compte.password
     return if value.blank?
-    return unless compte.anlci?
-    return if PasswordValidator.est_avec_12_maj_min_num_et_symbol?(value)
-
-    compte.errors.add(:password, I18n.t(".creation_compte.regles_mot_de_passe_anlci"))
+    if compte.anlci?
+      unless PasswordValidator.est_avec_12_maj_min_num_et_symbol?(value)
+        compte.errors.add(:password, I18n.t(".creation_compte.regles_mot_de_passe_anlci"))
+      end
+    else
+      if value.length < 8
+        compte.errors.add(
+          :password, I18n.t(".creation_compte.regles_mot_de_passe", longueur_mot_de_passe: 8))
+      end
+    end
   end
 
   def self.est_avec_12_maj_min_num_et_symbol?(value)
--- spec/models/password_validator_spec.rb
+++ spec/models/password_validator_spec.rb
@@ -4,55 +4,69 @@ require 'rails_helper'
 
 describe PasswordValidator do
   describe '#validate' do
-    it 'quand le mot de passe contient bien des majuscule, minuscule, chiffre et symbol' do
-      compte = Compte.new(password: 'aA345678912$', role: :superadmin)
-      described_class.new.validate(compte)
-      expect(compte.errors.include?(:password)).to be false
-    end
-
-    it 'quand le mot de passe ne contient pas de majuscule' do
-      compte = Compte.new(password: 'aa345678912$', role: :superadmin)
-      described_class.new.validate(compte)
-      expect(compte.errors.include?(:password)).to be true
-    end
-
-    it 'quand le mot de passe ne contient pas de minuscule' do
-      compte = Compte.new(password: 'AA345678912$', role: :superadmin)
-      described_class.new.validate(compte)
-      expect(compte.errors.include?(:password)).to be true
-    end
-
-    it 'quand le mot de passe ne contient pas de chiffre' do
-      compte = Compte.new(password: 'aAAAAAAAAAA$', role: :superadmin)
-      described_class.new.validate(compte)
-      expect(compte.errors.include?(:password)).to be true
-    end
-
-    it 'quand le mot de passe ne contient pas de symbol' do
-      compte = Compte.new(password: 'aA3456789120', role: :superadmin)
-      described_class.new.validate(compte)
-      expect(compte.errors.include?(:password)).to be true
-    end
-
-    it 'quand le mot de passe ne contient pas assez de caracters' do
-      compte = Compte.new(password: 'aA34567891$', role: :superadmin)
-      described_class.new.validate(compte)
-      expect(compte.errors.include?(:password)).to be true
-    end
-
-    it 'ne fait aucune vérificaton pour les comptes non anlci' do
-      %i[conseiller admin compte_generique].each do |role|
-        compte = Compte.new(password: '123456', role: role)
+    context "compte anlci" do
+      it 'quand le mot de passe contient bien des majuscule, minuscule, chiffre et symbol' do
+        compte = Compte.new(password: 'aA345678912$', role: :superadmin)
         described_class.new.validate(compte)
         expect(compte.errors.include?(:password)).to be false
       end
-    end
 
-    it 'vérifie les comptes anlci' do
-      Compte::ANLCI_ROLES.each do |role|
-        compte = Compte.new(password: '123456', role: role)
+      it 'quand le mot de passe ne contient pas de majuscule' do
+        compte = Compte.new(password: 'aa345678912$', role: :superadmin)
         described_class.new.validate(compte)
         expect(compte.errors.include?(:password)).to be true
+      end
+
+      it 'quand le mot de passe ne contient pas de minuscule' do
+        compte = Compte.new(password: 'AA345678912$', role: :superadmin)
+        described_class.new.validate(compte)
+        expect(compte.errors.include?(:password)).to be true
+      end
+
+      it 'quand le mot de passe ne contient pas de chiffre' do
+        compte = Compte.new(password: 'aAAAAAAAAAA$', role: :superadmin)
+        described_class.new.validate(compte)
+        expect(compte.errors.include?(:password)).to be true
+      end
+
+      it 'quand le mot de passe ne contient pas de symbol' do
+        compte = Compte.new(password: 'aA3456789120', role: :superadmin)
+        described_class.new.validate(compte)
+        expect(compte.errors.include?(:password)).to be true
+      end
+
+      it 'quand le mot de passe ne contient pas assez de caracters' do
+        compte = Compte.new(password: 'aA34567891$', role: :superadmin)
+        described_class.new.validate(compte)
+        expect(compte.errors.include?(:password)).to be true
+      end
+
+      it 'vérifie les comptes anlci' do
+        Compte::ANLCI_ROLES.each do |role|
+          compte = Compte.new(password: '123456', role: role)
+          described_class.new.validate(compte)
+          expect(compte.errors.include?(:password)).to be true
+        end
+      end
+    end
+
+    context "compte standard, non anlci" do
+      it "Erreur si la taille est inférieur a 8 caracteres" do
+        %i[conseiller admin compte_generique].each do |role|
+          compte = Compte.new(password: '1234567', role: role)
+          described_class.new.validate(compte)
+          expect(compte.errors.include?(:password)).to be true
+          expect(compte.errors[:password])
+            .to eq [ "Votre mot de passe doit comporter au moins 8 caractères." ]
+        end
+      end
+
+      it "pas d'erreur si c'est 8 ou plus" do
+        %i[conseiller admin compte_generique].each do |role|
+          compte = Compte.new(password: '12345678', role: role)
+          described_class.new.validate(compte)
+          expect(compte.errors.include?(:password)).to be false
+        end
       end
     end
 

0d2705d373a06e9c45391e5459a3213f80494c14
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-04-29 16:34:34 +0200

    ♻️ Extrait le formulaire des questions sous_consigne dans une vue

--- app/admin/questions_sous_consigne.rb
+++ app/admin/questions_sous_consigne.rb
@@ -20,36 +20,7 @@ ActiveAdmin.register QuestionSousConsigne do
     link_to "Exporter la question en XLS", admin_question_export_xls_path(question_id: params[:id])
   end
 
-  form do |f|
-    f.semantic_errors
-    f.inputs do
-      f.input :libelle
-      f.input :nom_technique
-      render partial: "admin/questions/input_illustration", locals: { f: f }
-    end
-    f.object.transcriptions.build(categorie: :intitule) if f.object.transcription_intitule.nil?
-
-    f.has_many :transcriptions, allow_destroy: false, new_record: false,
-                                heading: false do |t|
-      t.input :id, as: :hidden
-      t.input :ecrit, label: t(".label.intitule"), input_html: { rows: 4 }
-      t.input :audio, as: :file, label: t(".label.intitule_audio"),
-                      input_html: { accept: Transcription::AUDIOS_CONTENT_TYPES.join(",") }
-      t.input :categorie, as: :hidden, input_html: { value: :intitule }
-    end
-
-    f.inputs do
-      if f.object.transcription_intitule&.audio&.attached?
-        f.input :supprimer_audio_intitule, as: :boolean,
-                                           hint: tag_audio(f.object.transcription_intitule)
-      end
-    end
-
-    f.actions do
-      f.action :submit
-      annulation_formulaire(f)
-    end
-  end
+  form partial: "form"
 
   index do
     column :libelle do |q|

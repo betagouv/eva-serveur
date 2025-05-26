213ddbde58fa2d39357f58c732fad986da483492
guillaume imary <guillaume.imary@gmail.com>
2025-05-26 10:00:33 +0200

    💄 Crée un toggle input

---
+++ app/inputs/toggle_input.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class ToggleInput < Formtastic::Inputs::BooleanInput
+  def to_html
+    input_wrapping do
+      template.render(ToggleComponent.new(
+        f: builder,
+        method: method,
+        label: label_text
+      ))
+    end
+  end
+end

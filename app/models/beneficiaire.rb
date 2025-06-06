06a074a15fd076e600465b64646f97ac5cf7cf2e
Marion Velard <marion.velard@captive.fr>
2025-06-06 15:01:50 +0200

    ✨ Permet de selectionner plusieurs beneficiaires depuis la liste afin de les fusionner

--- app/models/beneficiaire.rb
+++ app/models/beneficiaire.rb
@@ -3,8 +3,10 @@
 class Beneficiaire < ApplicationRecord
   has_many :evaluations, dependent: :destroy
   validates :nom, presence: true
-
   acts_as_paranoid
+
+  scope :le_plus_ancien, -> { order(created_at: :asc).first }
+  scope :sauf_pour, ->(id) { where.not(id: id) }
 
   def anonyme?
     anonymise_le.present?

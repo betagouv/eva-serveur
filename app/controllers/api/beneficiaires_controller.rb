e5cbcd1dea53c04f44c8c6fb80f9a468191e8937
guillaume imary <guillaume.imary@gmail.com>
2025-06-06 17:12:09 +0200

    ✨ Le bénéficiaire peut se connecter au jeu avec son identifiant

---
+++ app/controllers/api/beneficiaires_controller.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Api
+  class BeneficiairesController < Api::BaseController
+    before_action :trouve_beneficiaire
+
+    def show; end
+
+    private
+
+    def trouve_beneficiaire
+      @beneficiaire = Beneficiaire.find_by!(code: params[:code_beneficiaire])
+    end
+  end
+end

295c63802b7b24e7940190b6cf53187caf764d24
guillaume imary <guillaume.imary@gmail.com>
2025-06-25 13:03:23 +0200

    ✨ Refacto le composant mise en avant et ajoute une vue de démo du composant

---
+++ app/components/mise_en_avant_component.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class MiseEnAvantComponent < ViewComponent::Base
+  def initialize(titre:, description: nil, icone: nil, bouton_label: nil, bouton_attributes: {}, html_attributes: {})
+    @titre = titre
+    @description = description
+    @icone = icone
+    @bouton_label = bouton_label
+    @bouton_attributes = bouton_attributes
+    @html_attributes = html_attributes
+  end
+end

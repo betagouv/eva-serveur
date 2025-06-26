f6d8bd760d12f91b275b45a3d8f742c1859155f0
guillaume imary <guillaume.imary@gmail.com>
2025-06-26 09:51:12 +0200

    ✨ Ajoute les composants et l'index dans la section ui_kit

--- app/components/statut_campagne_component.rb
+++ app/components/statut_campagne_component.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 class StatutCampagneComponent < ViewComponent::Base
-  def initialize(campagne)
-    @campagne = campagne
+  def initialize(campagne_active: true, campagne_privee: false)
+    @campagne_active = campagne_active
+    @campagne_privee = campagne_privee
   end
 end

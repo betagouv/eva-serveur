9de9f3c0495f46ada32feb7bee8fc9bc1d22e97d
guillaume imary <guillaume.imary@gmail.com>
2025-07-09 10:28:40 +0200

    🔧 Modifie le helper traduis_acces pour afficher le statut de validation sans le rôle si le statut n'est pas 'acceptee'

--- app/helpers/compte_helper.rb
+++ app/helpers/compte_helper.rb
@@ -4,6 +4,10 @@ module CompteHelper
   def traduis_acces(statut_validation, role)
     statut = Compte.human_enum_name(:statut_validation, statut_validation)
     role = " - #{Compte.human_enum_name(:role, role)}" if role.present?
-    "#{statut}#{role}"
+    if statut_validation == "acceptee"
+      "#{statut}#{role}"
+    else
+      "#{statut}"
+    end
   end
 end

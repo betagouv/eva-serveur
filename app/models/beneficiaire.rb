2513a6728271f81891b8905f1a85e7c654b41c43
Marion Velard <marion.velard@captive.fr>
2025-07-25 15:12:06 +0200

    👔 Ajoute un scope positionnement dans le model Evaluation

--- app/models/beneficiaire.rb
+++ app/models/beneficiaire.rb
@@ -13,7 +13,7 @@ class Beneficiaire < ApplicationRecord
   scope :par_date_creation_asc, -> { order(created_at: :asc) }
   scope :sauf_pour, ->(id) { where.not(id: id) }
 
-  delegate :diagnostic, to: :evaluations, prefix: true
+  delegate :diagnostic, :positionnement, to: :evaluations, prefix: true
 
   def anonyme?
     anonymise_le.present?

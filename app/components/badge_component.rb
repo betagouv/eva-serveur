955f1709c950067cf8f9a1f416ea8086647c2289
Marion Velard <marion.velard@captive.fr>
2025-07-28 17:35:21 +0200

    💄 Permet de passer une taille au composant Badge

--- app/components/badge_component.rb
+++ app/components/badge_component.rb
@@ -1,10 +1,17 @@
 # frozen_string_literal: true
 
 class BadgeComponent < ViewComponent::Base
-  def initialize(contenu:, html_class:, display_icon: false)
+  def initialize(contenu:, html_class:, display_icon: false, taille: nil)
     @contenu = contenu
     @html_class = html_class
     @display_icon = display_icon
+    @taille = taille
+  end
+
+  def css_classes
+    classes = [ "fr-badge", @html_class, display_icon_class ]
+    classes << "fr-badge--#{@taille}" if @taille.present?
+    classes.compact.join(" ")
   end
 
   def display_icon_class

258957e958417f2db8db3e8288b72fc9dd3218f3
Marion Velard <marion.velard@captive.fr>
2025-07-31 12:07:39 +0200

    💄 Renomme html_class en class_couleur dans le component Badge

--- app/components/badge_component.rb
+++ app/components/badge_component.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
 class BadgeComponent < ViewComponent::Base
-  def initialize(contenu:, html_class:, display_icon: false, taille: nil)
+  def initialize(contenu:, class_couleur:, display_icon: false, taille: nil)
     @contenu = contenu
-    @html_class = html_class
+    @class_couleur = class_couleur
     @display_icon = display_icon
     @taille = taille
   end
 
   def css_classes
-    classes = [ "fr-badge", @html_class, display_icon_class ]
+    classes = [ "fr-badge", @class_couleur, display_icon_class ]
     classes << "fr-badge--#{@taille}" if @taille.present?
     classes.compact.join(" ")
   end

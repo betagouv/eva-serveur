5a72fe90e026c47e389a661745262207c8f4ad1a
Étienne Charignon <etienne.charignon@gmail.com>
2025-08-13 16:48:40 +0200

    ♻️ Liste evaluation bénéfiaciaire : sépare chaque mise à jour dans une fonction séparée

--- app/assets/javascripts/comparer_evaluations_positionnement.js
+++ app/assets/javascripts/comparer_evaluations_positionnement.js
@@ -1,9 +1,39 @@
-function updateHiddenInputs(checkboxes, formElement) {
-    // Remove existing hidden inputs
+function estTestee(row, competence) {
+    const donnees = row.querySelector(`div[data-${competence}]`);
+    return donnees && donnees.getAttribute(`data-${competence}`) === 'true';
+}
+
+function activationCheckbox(checkboxes) {
+    let litteratieCount = 0;
+    let numeratieCount = 0;
+
+    checkboxes
+        .filter(checkbox => checkbox.checked)
+        .forEach(checkbox => {
+            const row = checkbox.closest('tr');
+            if (estTestee(row, 'litteratie')) { litteratieCount++; }
+            if (estTestee(row, 'numeratie')) { numeratieCount++; }
+        });
+
+    checkboxes.forEach(checkbox => {
+        checkbox.disabled = false;
+        checkbox.title = "";
+        const row = checkbox.closest('tr');
+        if (!checkbox.checked &&
+            (estTestee(row, 'litteratie') && litteratieCount >= 2 ||
+             estTestee(row, 'numeratie') && numeratieCount >= 2)){
+            checkbox.disabled = true;
+            checkbox.title = I18n.t("admin.beneficiaires.liste_evaluations_positionnement.indication");
+        }
+    });
+}
+
+function ajouteIdSelectionnesAuFormulaire(checkboxes, formElement) {
+    // Supprime les champs cachés existants
     formElement.querySelectorAll('input[name="evaluation_ids[]"]').forEach(input => input.remove());
 
-    // Create new hidden inputs for each selected evaluation
-    Array.from(checkboxes).filter(checkbox => checkbox.checked)
+    // Crée de nouveaux champs cachés pour chaque évaluation sélectionnée
+    checkboxes.filter(checkbox => checkbox.checked)
         .forEach(checkbox => {
             const evaluationId = checkbox.closest('tr').getAttribute('data-id');
             const hiddenInput = document.createElement('input');
@@ -14,69 +44,40 @@ function updateHiddenInputs(checkboxes, formElement) {
         });
 }
 
-function updateButtonState(checkboxes, compareButton) {
-    let litteratieCount = 0;
-    let numeratieCount = 0;
-    const selectedEvaluationIds = [];
-
-    checkboxes.forEach(checkbox => {
-        const row = checkbox.closest('tr');
-        const litteratieDiv = row.querySelector('div[data-litteratie]');
-        const numeratieDiv = row.querySelector('div[data-numeratie]');
-        const evaluationId = row.getAttribute('data-id');
-
-        if (checkbox.checked) {
-            if (litteratieDiv && litteratieDiv.getAttribute('data-litteratie') === 'true') {
-                litteratieCount++;
-            }
-            if (numeratieDiv && numeratieDiv.getAttribute('data-numeratie') === 'true') {
-                numeratieCount++;
-            }
-            selectedEvaluationIds.push(evaluationId);
-        }
-    });
-
-    compareButton.disabled = selectedEvaluationIds.length === 0;
+function activationBoutonCompare(checkboxes) {
+    const compareButton = document.getElementById('comparer-evaluation');
+    const nombreEvaluationsSelectionnees = checkboxes.filter(checkbox => checkbox.checked).length;
+    compareButton.disabled = nombreEvaluationsSelectionnees === 0;
 
     const formElement = compareButton.closest('form');
-    updateHiddenInputs(checkboxes, formElement); // Update hidden input fields with selected IDs
+    ajouteIdSelectionnesAuFormulaire(checkboxes, formElement);
+}
 
-    const evaluationSelectedText = document.querySelector('.liste-evaluations-positionnement__nombre-element-selectionnes');
-    if (selectedEvaluationIds.length > 0) {
-        evaluationSelectedText.textContent =`${selectedEvaluationIds.length} ${evaluationSelectedText.dataset['1EvaluationSelectionnee']}`
-    } else {
-        evaluationSelectedText.textContent =`${selectedEvaluationIds.length} ${evaluationSelectedText.dataset['nEvaluationsSelectionnees']}`
-    }
-
-    checkboxes.forEach(checkbox => {
-        if (checkbox.checked) {
-            checkbox.disabled = false;
-            checkbox.title = "";
-        } else {
-            const row = checkbox.closest('tr');
-            const litteratieDiv = row.querySelector('div[data-litteratie]');
-            const numeratieDiv = row.querySelector('div[data-numeratie]');
-
-            const isLitteratie = litteratieDiv && litteratieDiv.getAttribute('data-litteratie') === 'true';
-            const isNumeratie = numeratieDiv && numeratieDiv.getAttribute('data-numeratie') === 'true';
-
-            checkbox.disabled = (isLitteratie && litteratieCount >= 2) || (isNumeratie && numeratieCount >= 2);
-            checkbox.title = checkbox.disabled ? "Vous ne pouvez pas sélectionner plus de 2 fois une même compétence" : "";
-        }
-    });
+function actualiseInformationSelection(checkboxes) {
+    const nombreEvaluationsSelectionnees = checkboxes.filter(checkbox => checkbox.checked).length;
+    const texte = (nombreEvaluationsSelectionnees <= 1) ?
+        I18n.t("admin.beneficiaires.liste_evaluations_positionnement.1-evaluation-selectionnee") :
+        I18n.t("admin.beneficiaires.liste_evaluations_positionnement.n-evaluations-selectionnees");
+    const node = document.querySelector('.liste-evaluations-positionnement__nombre-element-selectionnes');
+    node.textContent =`${nombreEvaluationsSelectionnees} ${texte}`
 }
 
 document.addEventListener('DOMContentLoaded', function () {
   const evaluationTable = document.querySelector('.liste-evaluations-positionnement__tableau');
 
   if (evaluationTable) {
-    const checkboxes = evaluationTable.querySelectorAll('.fr-checkbox-group input');
-    const compareButton = document.getElementById('comparer-evaluation');
+    const checkboxes = Array.from(evaluationTable.querySelectorAll('.fr-checkbox-group input'));
+
+    const cbCheckbox = () => {
+        activationCheckbox(checkboxes);
+        activationBoutonCompare(checkboxes);
+        actualiseInformationSelection(checkboxes);
+    }
 
     checkboxes.forEach(checkbox => {
-      checkbox.addEventListener('change', () => { updateButtonState(checkboxes, compareButton) });
+      checkbox.addEventListener('change', cbCheckbox);
     });
 
-    updateButtonState(checkboxes, compareButton);
+    cbCheckbox();
   }
 });

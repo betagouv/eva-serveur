af1d1e9938de9bbc80f5904b860fa4b448b2d73d
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-08-25 17:52:23 +0200

    ♻️ Extraie des logiques dans ComparaisonEvaluationHelper

---
+++ app/helpers/comparaison_evaluations_helper.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+module ComparaisonEvaluationsHelper
+  def restitution_litteratie(comparaison, numero_evaluation)
+    comparaison.restitutions_litteratie[numero_evaluation].litteratie
+  end
+
+  def profil_litteratie(comparaison, numero_evaluation)
+    restitution_litteratie = restitution_litteratie(comparaison, numero_evaluation)
+    restitution_litteratie ? restitution_litteratie.niveau_litteratie : "indetermine"
+  end
+
+  def sous_competences_litteratie(comparaison, numero_evaluation)
+    restitution_litteratie = restitution_litteratie(comparaison, numero_evaluation)
+    restitution_litteratie ? restitution_litteratie.competences_litteratie : []
+  end
+
+  def restitution_numeratie(comparaison, numero_evaluation)
+    comparaison.restitutions_numeratie[numero_evaluation].numeratie
+  end
+
+  def profil_numeratie(comparaison, numero_evaluation)
+    restitution_numeratie = restitution_numeratie(comparaison, numero_evaluation)
+    restitution_numeratie ? restitution_numeratie.profil_numeratie : "indetermine"
+  end
+
+  def sous_competences_numeratie(comparaison, numero_evaluation)
+    restitution_numeratie = restitution_numeratie(comparaison, numero_evaluation)
+    restitution_numeratie ? restitution_numeratie.competences_numeratie : []
+  end
+end

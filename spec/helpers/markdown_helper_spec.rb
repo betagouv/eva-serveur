4c1155f9d34b679be821ad0f1017f2aa4a06b791
Étienne Charignon <etienne.charignon@gmail.com>
2025-08-25 16:30:27 +0200

    🌐 Ajoute des targets blank à toutes les urls des emails
    Sinon ça ne s'ouvre pas toujours bien dans les webmails

--- spec/helpers/markdown_helper_spec.rb
+++ spec/helpers/markdown_helper_spec.rb
@@ -1,33 +1,27 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 describe MarkdownHelper do
-  describe '#md' do
-    context "quand il n'y a pas de contenu" do
-      let(:contenu) { nil }
-
-      it 'retourne une chaine vide' do
-        expect(helper.md(contenu)).to eq ''
-      end
+  describe "#md" do
+    it "retourne une chaine vide pour nil" do
+      expect(helper.md(nil)).to eq ""
     end
 
-    context 'quand il y a un contenu' do
-      let(:contenu) { '*évaluer les compétences*' }
-
-      it 'retourne le contenu traduit en html' do
-        expect(helper.md(contenu)).to match('<p><em>évaluer les compétences</em></p>')
-      end
+    it "retourne le contenu traduit en html" do
+      expect(helper.md("*évaluer les compétences*"))
+        .to match("<p><em>évaluer les compétences</em></p>")
     end
 
-    context 'quand il y a un contenu avec une phrase en gras' do
-      let(:contenu) { '**évaluer les compétences** transversales' }
+    it "retourne le contenu traduit en html avec une font-weight customisé" do
+      html =
+        '<p><strong class="fw-semi-bold">évaluer les compétences</strong> transversales</p>'
+      expect(helper.md("**évaluer les compétences** transversales")).to match(html)
+    end
 
-      it 'retourne le contenu traduit en html avec une font-weight customisé' do
-        html =
-          '<p><strong class="fw-semi-bold">évaluer les compétences</strong> transversales</p>'
-        expect(helper.md(contenu)).to match(html)
-      end
+    it "Les section de code ne traduisent pas les caractères markdown" do
+      expect(helper.md("`du code avec des caractères _markdown_`"))
+        .to match("<code>du code avec des caractères _markdown_</code>")
     end
   end
 end

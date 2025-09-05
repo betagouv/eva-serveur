05724d9d262bd32ea94b031f88a60186a9e33176
Marion Velard <marion.velard@captive.fr>
2025-09-05 09:29:03 +0200

    ✏️ Renomme l'action lier en fusionner dans la resource beneficiaire

--- app/admin/beneficiaire.rb
+++ app/admin/beneficiaire.rb
@@ -9,7 +9,7 @@ ActiveAdmin.register Beneficiaire do
   config.sort_order = "created_at_desc"
   config.batch_actions = true
 
-  batch_action :lier, if: proc { can?(:lier, Beneficiaire) } do |beneficiaire_ids|
+  batch_action :fusionner, if: proc { can?(:fusionner, Beneficiaire) } do |beneficiaire_ids|
     beneficiaires = Beneficiaire.where(id: beneficiaire_ids)
 
     if beneficiaires.size < 2
@@ -44,7 +44,7 @@ ActiveAdmin.register Beneficiaire do
   end
 
   index do
-    selectable_column if can?(:lier, Beneficiaire)
+    selectable_column if can?(:fusionner, Beneficiaire)
     column :nom do |beneficiaire|
       render partial: "nom_beneficiaire", locals: { beneficiaire: beneficiaire }
     end

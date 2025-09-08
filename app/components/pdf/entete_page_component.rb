ffe0a677c670da60ad08180d7d2b4b53994e24bc
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-09-08 16:18:03 +0200

    ✨ Le conseiller peut télécharger une version pdf de la comparaison des évaluations

---
+++ app/components/pdf/entete_page_component.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+class Pdf::EntetePageComponent < ViewComponent::Base
+  def initialize(nom_beneficiaire:, code_beneficiaire:, date: nil, structure: nil)
+    @nom_beneficiaire = nom_beneficiaire
+    @code_beneficiaire = code_beneficiaire
+    @date = date
+    @structure = structure
+  end
+end
---
+++ app/components/pdf/pied_page_component.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+class Pdf::PiedPageComponent < ViewComponent::Base
+end

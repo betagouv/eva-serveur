7b68dcb9a3eb43e783086712d74acc7fee898fd8
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-09-08 16:18:03 +0200

    ✨ Le conseiller peut télécharger une version pdf de la comparaison des évaluations

--- app/admin/comparaison.rb
+++ app/admin/comparaison.rb
@@ -2,14 +2,45 @@ ActiveAdmin.register_page "Comparaison" do
   belongs_to :beneficiaire
 
   content title: "Comparer les évaluations" do
-    evaluations = Evaluation.where(id: params[:evaluation_ids])
-                            .includes(campagne: { situations_configurations: :situation })
-    comparaison = ComparaisonEvaluations.new(evaluations)
     if !comparaison.valid?
       flash[:error] = I18n.t("admin.comparaison.errors.limite_evaluations")
       redirect_to admin_beneficiaire_path(beneficiaire)
     end
 
     render partial: "comparaison", locals: { comparaison: comparaison }
+  end
+
+  page_action :download_pdf, method: :get do
+    beneficiaire = Beneficiaire.find params[:beneficiaire_id]
+    html_content = render_to_string(
+      template: "admin/comparaison/pdf",
+      layout: "application",
+      locals: { comparaison: comparaison, beneficiaire: beneficiaire }
+    )
+
+    pdf_path = Html2Pdf.genere_pdf_depuis_html(html_content)
+    if pdf_path == false
+      flash[:error] = t(".erreur_generation_pdf")
+      redirect_to admin_beneficiaire_comparaison_path(evaluation_ids: params[:evaluation_ids])
+    else
+      send_file(pdf_path, filename: "#{beneficiaire.nom.parameterize}.pdf", type: "application/pdf",
+                          disposition: disposition)
+    end
+  end
+
+  controller do
+    helper_method :comparaison
+
+    def comparaison
+      @comparaison ||= begin
+        evaluations = Evaluation.where(id: params[:evaluation_ids])
+                              .includes(campagne: { situations_configurations: :situation })
+        ComparaisonEvaluations.new(evaluations)
+      end
+    end
+
+    def disposition
+      Rails.env.development? ? "inline" : "attachment"
+    end
   end
 end

fad87b0b9fade68570ee6968de50d5f1d669e3d1
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-09-09 10:13:39 +0200

    ♻️ Supprime la duplication des vues comparaisons

--- app/helpers/comparaison_evaluations_helper.rb
+++ app/helpers/comparaison_evaluations_helper.rb
@@ -1,6 +1,42 @@
 # frozen_string_literal: true
 
 module ComparaisonEvaluationsHelper
+  def tableau_comparaison_litteratie(comparaison)
+    tableau = []
+    evaluations = comparaison.evaluations_litteratie
+    return tableau if evaluations.blank?
+
+    2.times do |numero_evaluation|
+      evaluation = evaluations[numero_evaluation]
+      profil = evaluation ? profil_litteratie(comparaison, numero_evaluation) : nil
+      sous_competences = evaluation ? sous_competences_litteratie(comparaison, numero_evaluation) : nil
+      tableau << {
+        evaluation: evaluation,
+        profil: profil,
+        sous_competences: sous_competences
+      }
+    end
+    tableau
+  end
+
+  def tableau_comparaison_numeratie(comparaison)
+    tableau = []
+    evaluations = comparaison.evaluations_numeratie
+    return tableau if evaluations.blank?
+
+    2.times do |numero_evaluation|
+      evaluation = evaluations[numero_evaluation]
+      profil = evaluation ? profil_numeratie(comparaison, numero_evaluation) : nil
+      sous_competences = evaluation ? sous_competences_numeratie(comparaison, numero_evaluation) : nil
+      tableau << {
+        evaluation: evaluation,
+        profil: profil,
+        sous_competences: sous_competences
+      }
+    end
+    tableau
+  end
+
   def restitution_litteratie(comparaison, numero_evaluation)
     comparaison.restitutions_litteratie[numero_evaluation].litteratie
   end

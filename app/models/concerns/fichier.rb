c386d64d286df247c3d3b6af0f396e64d36f1144
Étienne Charignon <etienne.charignon@ut7.fr>
2025-09-11 18:38:58 +0200

    🐛 Le nom du fichier PDF est le nom du bénéficiaire précédé de la date d'évaluation
    et non le nom de l'évaluation

--- app/models/concerns/fichier.rb
+++ app/models/concerns/fichier.rb
@@ -6,4 +6,8 @@ module Fichier
     date = DateTime.current.strftime("%Y%m%d%H%M%S")
     "#{date}-#{titre}.#{extension}"
   end
+
+  def nom_fichier_date(date, titre, extension)
+    "#{date.strftime("%Y%m%d")}-#{titre}.#{extension}"
+  end
 end

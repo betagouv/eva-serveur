62156419ae7a280658dbb25dacc24c549ee07df9
Étienne Charignon <etienne.charignon@ut7.fr>
2025-09-17 14:46:34 +0200

    🔒️ Interdit d'utiliser l'email comme mot de passe

--- app/models/password_validator.rb
+++ app/models/password_validator.rb
@@ -2,25 +2,38 @@
 
 class PasswordValidator < ActiveModel::Validator
   def validate(compte)
-    value = compte.password
-    return if value.blank?
+    valeur = compte.password
+    return if valeur.blank?
+
+    valide_robustesse(compte, valeur)
+    valide_different_email(compte, valeur)
+  end
+
+  def valide_different_email(compte, valeur)
+    if valeur.strip.casecmp?(compte.email&.strip)
+      compte.errors.add(
+        :password, I18n.t(".creation_compte.mot_de_passe_email"))
+    end
+  end
+
+  def valide_robustesse(compte, valeur)
     if compte.anlci?
-      unless PasswordValidator.est_avec_12_maj_min_num_et_symbol?(value)
+      unless PasswordValidator.est_avec_12_maj_min_num_et_symbol?(valeur)
         compte.errors.add(:password, I18n.t(".creation_compte.regles_mot_de_passe_anlci"))
       end
     else
-      if value.length < 8
+      if valeur.length < 8
         compte.errors.add(
           :password, I18n.t(".creation_compte.regles_mot_de_passe", longueur_mot_de_passe: 8))
       end
     end
   end
 
-  def self.est_avec_12_maj_min_num_et_symbol?(value)
-    value.length >= 12 &&
-      value =~ /[A-Z]/ &&
-      value =~ /[a-z]/ &&
-      value =~ /[0-9]/ &&
-      value =~ /[^A-Za-z0-9]/
+  def self.est_avec_12_maj_min_num_et_symbol?(valeur)
+    valeur.length >= 12 &&
+      valeur =~ /[A-Z]/ &&
+      valeur =~ /[a-z]/ &&
+      valeur =~ /[0-9]/ &&
+      valeur =~ /[^A-Za-z0-9]/
   end
 end

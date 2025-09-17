a32a2acafcd758d6cb0a2b763525d113be7b2991
Étienne Charignon <etienne.charignon@ut7.fr>
2025-09-17 14:46:34 +0200

    🔒️ Interdit d'utiliser l'email comme mot de passe

--- spec/models/password_validator_spec.rb
+++ spec/models/password_validator_spec.rb
@@ -1,49 +1,49 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
 describe PasswordValidator do
-  describe '#validate' do
+  describe "#validate" do
     context "compte anlci" do
-      it 'quand le mot de passe contient bien des majuscule, minuscule, chiffre et symbol' do
-        compte = Compte.new(password: 'aA345678912$', role: :superadmin)
+      it "quand le mot de passe contient bien des majuscule, minuscule, chiffre et symbol" do
+        compte = Compte.new(password: "aA345678912$", role: :superadmin)
         described_class.new.validate(compte)
         expect(compte.errors.include?(:password)).to be false
       end
 
-      it 'quand le mot de passe ne contient pas de majuscule' do
-        compte = Compte.new(password: 'aa345678912$', role: :superadmin)
+      it "quand le mot de passe ne contient pas de majuscule" do
+        compte = Compte.new(password: "aa345678912$", role: :superadmin)
         described_class.new.validate(compte)
         expect(compte.errors.include?(:password)).to be true
       end
 
-      it 'quand le mot de passe ne contient pas de minuscule' do
-        compte = Compte.new(password: 'AA345678912$', role: :superadmin)
+      it "quand le mot de passe ne contient pas de minuscule" do
+        compte = Compte.new(password: "AA345678912$", role: :superadmin)
         described_class.new.validate(compte)
         expect(compte.errors.include?(:password)).to be true
       end
 
-      it 'quand le mot de passe ne contient pas de chiffre' do
-        compte = Compte.new(password: 'aAAAAAAAAAA$', role: :superadmin)
+      it "quand le mot de passe ne contient pas de chiffre" do
+        compte = Compte.new(password: "aAAAAAAAAAA$", role: :superadmin)
         described_class.new.validate(compte)
         expect(compte.errors.include?(:password)).to be true
       end
 
-      it 'quand le mot de passe ne contient pas de symbol' do
-        compte = Compte.new(password: 'aA3456789120', role: :superadmin)
+      it "quand le mot de passe ne contient pas de symbol" do
+        compte = Compte.new(password: "aA3456789120", role: :superadmin)
         described_class.new.validate(compte)
         expect(compte.errors.include?(:password)).to be true
       end
 
-      it 'quand le mot de passe ne contient pas assez de caracters' do
-        compte = Compte.new(password: 'aA34567891$', role: :superadmin)
+      it "quand le mot de passe ne contient pas assez de caracters" do
+        compte = Compte.new(password: "aA34567891$", role: :superadmin)
         described_class.new.validate(compte)
         expect(compte.errors.include?(:password)).to be true
       end
 
-      it 'vérifie les comptes anlci' do
+      it "vérifie les comptes anlci" do
         Compte::ANLCI_ROLES.each do |role|
-          compte = Compte.new(password: '123456', role: role)
+          compte = Compte.new(password: "123456", role: role)
           described_class.new.validate(compte)
           expect(compte.errors.include?(:password)).to be true
         end
@@ -53,7 +53,7 @@ describe PasswordValidator do
     context "compte standard, non anlci" do
       it "Erreur si la taille est inférieur a 8 caracteres" do
         %i[conseiller admin compte_generique].each do |role|
-          compte = Compte.new(password: '1234567', role: role)
+          compte = Compte.new(password: "1234567", role: role)
           described_class.new.validate(compte)
           expect(compte.errors.include?(:password)).to be true
           expect(compte.errors[:password])
@@ -63,17 +63,25 @@ describe PasswordValidator do
 
       it "pas d'erreur si c'est 8 ou plus" do
         %i[conseiller admin compte_generique].each do |role|
-          compte = Compte.new(password: '12345678', role: role)
+          compte = Compte.new(password: "12345678", role: role)
           described_class.new.validate(compte)
           expect(compte.errors.include?(:password)).to be false
         end
       end
     end
 
-    it 'ne valide rien si le mot de passe est vide' do
-      compte = Compte.new(password: '', role: :superadmin)
+    it "ne valide rien si le mot de passe est vide" do
+      compte = Compte.new(password: "", role: :conseiller)
       described_class.new.validate(compte)
       expect(compte.errors.include?(:password)).to be false
+    end
+
+    it "interdit d'utiliser son identifiant comme mot de passe" do
+      compte = Compte.new(email: " etienne@ut7.fr", password: "Etienne@ut7.fr ", role: :conseiller)
+      described_class.new.validate(compte)
+      expect(compte.errors.include?(:password)).to be true
+      expect(compte.errors[:password])
+        .to eq [ "Votre mot de passe ne doit pas être identique à votre adresse email." ]
     end
   end
 end

496ae3ffe8563d8d808869efea5b23fce7934138
Étienne Charignon <etienne.charignon@ut7.fr>
2025-09-17 09:50:40 +0200

    🧱 Ajoute la variable d'environement URL_SITE_VITRINE

--- app/components/evaluation/alert_annexe_component.rb
+++ app/components/evaluation/alert_annexe_component.rb
@@ -3,8 +3,8 @@
 class Evaluation
   class AlertAnnexeComponent < ViewComponent::Base
     def initialize
-      @lien_referentiel = "https://eva.anlci.gouv.fr/referentiels/"
-      @lien_positionnement = "https://eva.anlci.gouv.fr/positionnement/"
+      @lien_referentiel = "#{ENV['URL_SITE_VITRINE']}/referentiels/"
+      @lien_positionnement = "#{ENV['URL_SITE_VITRINE']}/positionnement/"
     end
   end
 end

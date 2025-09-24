d5f368ea5fee375d53a58117966c64a577d1bb79
Étienne Charignon <etienne.charignon@ut7.fr>
2025-09-24 12:45:35 +0200

    🐛 Corrige la traduction manquante pour le message de confirmation de suppresion d'une évaluation
    Généralie l'utilisation de MenuActionsComponent

--- app/components/menu_actions_component.rb
+++ app/components/menu_actions_component.rb
@@ -1,7 +1,12 @@
 # frozen_string_literal: true
 
 class MenuActionsComponent < ViewComponent::Base
-  def initialize(*actions)
+  include CanCan::ControllerAdditions
+
+  def initialize(ressource, *actions)
+    @ressource = ressource
     @actions = actions
   end
+
+  delegate :can?, :current_user, to: :helpers
 end

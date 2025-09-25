61a0a7a40e11e9d3ec75ac6f66ca419071db8f8c
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-09-25 11:54:13 +0200

    ✨ Ajoute la classe pour calculer le pourcentage de risque

---
+++ app/models/restitution/entreprises/pourcentage_risque.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Restitution
+  module Entreprises
+    class PourcentageRisque
+      # pourcentage => score_max
+      # soit 10% pour un score <= à 8
+      POURCENTAGE_RISQUE_PAR_SEUIL = {
+        10 => 8,
+        25 => 16,
+        50 => 24,
+        75 => 33
+      }.freeze
+
+      def calcule(evenements)
+        evenements_reponse = MetriquesHelper.filtre_evenements_reponses(evenements)
+
+        return if evenements_reponse.blank?
+
+        pourcentage = evenements_reponse&.sum { |e| e.donnees["score"] || 0 }
+
+        POURCENTAGE_RISQUE_PAR_SEUIL.each do |pourcentage_risque, seuil|
+          return pourcentage_risque if pourcentage <= seuil
+        end
+      end
+    end
+  end
+end

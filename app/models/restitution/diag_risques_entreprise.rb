604f05e760b4954f0adb509fca88e216dc890ec4
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-09-25 15:45:31 +0200

    ✨ Enregistre le pourcentage risque sur l'évaluation

--- app/models/restitution/diag_risques_entreprise.rb
+++ app/models/restitution/diag_risques_entreprise.rb
@@ -2,5 +2,18 @@
 
 module Restitution
   class DiagRisquesEntreprise < Base
+    METRIQUES = {
+      "pourcentage_risque" => {
+        "type" => :nombre,
+        "instance" => Restitution::Entreprises::PourcentageRisque.new
+      }
+    }
+
+    METRIQUES.each_key do |metrique|
+      define_method metrique do
+        METRIQUES[metrique]["instance"]
+          .calcule(@evenements)
+      end
+    end
   end
 end

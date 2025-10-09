06b13a91490869e2fade79e64c3cb746ddeaa1fd
Étienne Charignon <etienne.charignon@ut7.fr>
2025-10-09 18:59:06 +0200

    🔥 Supprime les traductions inutiles "Oui" et "Non"

--- app/helpers/opco_helper.rb
+++ app/helpers/opco_helper.rb
@@ -1,7 +1,7 @@
 module OpcoHelper
   def badge_financeur(opco)
     render BadgeComponent.new(
-      contenu: opco.financeur? ? "Oui" : "Non",
+      contenu: Opco.human_attribute_name(opco.financeur?),
       class_couleur: opco.financeur? ? "fr-badge--green-emeraude" : "fr-badge--grey-950",
       display_icon: false,
       taille: "sm"

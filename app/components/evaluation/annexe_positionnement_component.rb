65b057dc08981c058c404b0b955b0a88a3550f97
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-10-20 17:04:37 +0200

    ✨ Le conseiller peut consulter une explication sur le référentiel dans l'export pdf de la comparaison

--- app/components/evaluation/annexe_positionnement_component.rb
+++ app/components/evaluation/annexe_positionnement_component.rb
@@ -1,7 +1,10 @@
 class Evaluation
   class AnnexePositionnementComponent < ViewComponent::Base
-    def initialize(restitution_globale:)
-      @restitution_globale = restitution_globale
+    include MarkdownHelper
+
+    def initialize(entete_page_args)
+      @entete_page_args = entete_page_args
+
       @badges_numeratie = %i[profil1 profil2 profil3 profil4 profil4_plus]
       @badges_litteratie = %i[profil1 profil2 profil3 profil4 profil4_plus profil4_plus_plus]
       @scope_badge = "admin.restitutions.references"

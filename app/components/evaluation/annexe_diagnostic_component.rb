65815aaa2446501712de39dad1db7b87f57ccca8
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-10-20 17:04:37 +0200

    ✨ Le conseiller peut consulter une explication sur le référentiel dans l'export pdf de la comparaison

--- app/components/evaluation/annexe_diagnostic_component.rb
+++ app/components/evaluation/annexe_diagnostic_component.rb
@@ -1,7 +1,10 @@
 class Evaluation
   class AnnexeDiagnosticComponent < ViewComponent::Base
-    def initialize(restitution_globale:)
-      @restitution_globale = restitution_globale
+    include MarkdownHelper
+
+    def initialize(entete_page_args)
+      @entete_page_args = entete_page_args
+
       @badges_francais = %i[pre_A1 A1 A2 B1]
       @badges_maths = %i[pre_X1 X1 X2 Y1]
       @badges_francais_et_maths = %i[profil1 profil2 profil3 profil4 profil4_plus profil4_plus_plus]

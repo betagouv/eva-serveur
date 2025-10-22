d8e27fe7b91905c6132f86227921b618362f8992
Étienne Charignon <etienne.charignon@ut7.fr>
2025-10-22 14:23:49 +0200

    ✨ Ajoute la recherche sans accès au nom des structures

--- app/models/beneficiaire.rb
+++ app/models/beneficiaire.rb
@@ -1,4 +1,6 @@
 class Beneficiaire < ApplicationRecord
+  include RechercheSansAccent
+
   has_many :evaluations, dependent: :destroy
   validates :nom, presence: true
   validates :code_beneficiaire, presence: true, format: { with: /\A[A-Z0-9]+\z/ },
@@ -13,37 +15,8 @@ class Beneficiaire < ApplicationRecord
 
   delegate :diagnostic, :positionnement, to: :evaluations, prefix: true
 
-  def self.ransack(params = {}, options = {})
-    if params.is_a?(Hash)
-      transformed_params = transform_ransack_params(params)
-      super(transformed_params, options)
-    elsif params.is_a?(ActionController::Parameters)
-      hash_params = params.to_unsafe_h
-      transformed_params = transform_ransack_params(hash_params)
-      super(transformed_params, options)
-    else
-      super(params, options)
-    end
-  end
-
-  def self.transform_ransack_params(params)
-    params.transform_keys do |key|
-      key_str = key.to_s
-      if key_str == "nom_cont"
-        "nom_contains_unaccent"
-      else
-        key
-      end
-    end.transform_values do |value|
-      case value
-      when Hash
-        transform_ransack_params(value)
-      when Array
-        value.map { |v| v.is_a?(Hash) ? transform_ransack_params(v) : v }
-      else
-        value
-      end
-    end
+  def self.ransack_unaccent_attributes
+    %w[nom]
   end
 
   def anonyme?

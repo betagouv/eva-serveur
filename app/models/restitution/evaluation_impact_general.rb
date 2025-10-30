58cb34326a989fed45e5cfae465e9424605f207a
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-10-30 14:28:33 +0100

    🚚 Renomme performence en performance

--- app/models/restitution/evaluation_impact_general.rb
+++ app/models/restitution/evaluation_impact_general.rb
@@ -2,15 +2,15 @@ module Restitution
   class EvaluationImpactGeneral < Base
     def synthese
       {
-        performence_collective: calcule_performence_collective,
+        performance_collective: calcule_performance_collective,
         agilite_organisationnelle: calcule_agilite_organisationnelle,
         securite_qualite: calcule_securite_qualite,
         mobilite_professionnelle: calcule_mobilite_professionnelle
       }
     end
 
-    def calcule_performence_collective
-      Restitution::Entreprises::Impact::InterpretationScoring.new.calcule_performence_collective(@evenements)
+    def calcule_performance_collective
+      Restitution::Entreprises::Impact::InterpretationScoring.new.calcule_performance_collective(@evenements)
     end
 
     def calcule_agilite_organisationnelle
--- spec/models/restitution/evaluation_impact_general_spec.rb
+++ spec/models/restitution/evaluation_impact_general_spec.rb
@@ -3,14 +3,14 @@ require 'rails_helper'
 describe Restitution::EvaluationImpactGeneral do
   describe '#synthese' do
     let(:evaluation_impact_general) { described_class.new(nil, nil) }
-    let(:performence_collective_attendu) { :faible }
+    let(:performance_collective_attendu) { :faible }
     let(:agilite_organisationnelle_attendu) { :moyen }
     let(:securite_qualite_attendu) { :fort }
     let(:mobilite_professionnelle_attendu) { :tres_fort }
 
     before do
       allow(evaluation_impact_general).to receive_messages(
-        calcule_performence_collective: performence_collective_attendu,
+        calcule_performance_collective: performance_collective_attendu,
         calcule_agilite_organisationnelle: agilite_organisationnelle_attendu,
         calcule_securite_qualite: securite_qualite_attendu,
         calcule_mobilite_professionnelle: mobilite_professionnelle_attendu
@@ -19,7 +19,7 @@ describe Restitution::EvaluationImpactGeneral do
 
     it 'calculates pourcentage_risque and returns it in the synthese hash' do
       synthese = evaluation_impact_general.synthese
-      expect(synthese[:performence_collective]).to eq(performence_collective_attendu)
+      expect(synthese[:performance_collective]).to eq(performance_collective_attendu)
       expect(synthese[:agilite_organisationnelle]).to eq(agilite_organisationnelle_attendu)
       expect(synthese[:securite_qualite]).to eq(securite_qualite_attendu)
       expect(synthese[:mobilite_professionnelle]).to eq(mobilite_professionnelle_attendu)

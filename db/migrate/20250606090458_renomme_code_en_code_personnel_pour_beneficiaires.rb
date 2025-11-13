fb4698ea07462b12813492f60e1ea25328d4a026
Étienne Charignon <etienne.charignon@ut7.fr>
2025-11-13 16:30:17 +0100

    💚 Actualise les migrations pour permettre la créations de reviewapp

--- db/migrate/20250606090458_renomme_code_en_code_personnel_pour_beneficiaires.rb
+++ db/migrate/20250606090458_renomme_code_en_code_personnel_pour_beneficiaires.rb
@@ -1,4 +1,6 @@
 class RenommeCodeEnCodePersonnelPourBeneficiaires < ActiveRecord::Migration[7.2]
+  class Beneficiaire < ApplicationRecord; end
+
   def change
     rename_column :beneficiaires, :code, :code_personnel
   end

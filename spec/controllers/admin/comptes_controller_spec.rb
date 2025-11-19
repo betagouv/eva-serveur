58bb109a41b53acb2839e01f187e5e2968727fca
Étienne Charignon <etienne.charignon@ut7.fr>
2025-11-19 15:19:07 +0100

    ✨ Permet aux admin de SA de modifier la structure d'un compte

--- spec/controllers/admin/comptes_controller_spec.rb
+++ spec/controllers/admin/comptes_controller_spec.rb
@@ -45,12 +45,34 @@ describe Admin::ComptesController, type: :controller do
         sign_in compte_admin_administratif
       end
 
-      context "lors de l'édition d'un compte existant" do
+      context "lors de l'édition d'un compte existant sans campagne" do
+        let(:structure_locale_fille_1) do
+          create :structure_locale, structure_referente: structure_administrative
+        end
+        let(:structure_locale_fille_2) do
+          create :structure_locale, structure_referente: structure_administrative
+        end
+        let!(:admin_fille_1) { create :compte_admin, structure: structure_locale_fille_1 }
+        let!(:admin_fille_2) { create :compte_admin, structure: structure_locale_fille_2 }
+        let(:compte_existant) { create :compte_conseiller, structure: structure_locale_fille_1 }
+
+        before { get :edit, params: { id: compte_existant.id } }
+
+        it "le champ structure est éditable avec les structures filles" do
+          expect(response.body).to include('id="compte_structure_input"')
+          expect(response.body).not_to include('disabled="disabled"')
+          expect(response.body).to include(structure_locale_fille_1.nom)
+          expect(response.body).to include(structure_locale_fille_2.nom)
+        end
+      end
+
+      context "lors de l'édition d'un compte existant avec des campagnes" do
         let(:structure_locale_fille) do
           create :structure_locale, structure_referente: structure_administrative
         end
         let!(:admin_fille) { create :compte_admin, structure: structure_locale_fille }
         let(:compte_existant) { create :compte_conseiller, structure: structure_locale_fille }
+        let!(:campagne) { create :campagne, compte: compte_existant }
 
         before { get :edit, params: { id: compte_existant.id } }
 

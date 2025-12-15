94d5408ab50a24ab4553111ba4c203c1e9d2e51f
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-12-15 11:57:29 +0100

    ✨ Ajoute un logo pour les OPCOS

--- app/models/opco.rb
+++ app/models/opco.rb
@@ -1,6 +1,12 @@
 class Opco < ApplicationRecord
   validates :nom, presence: true
 
+  has_one_attached :logo do |attachable|
+    attachable.variant :defaut,
+                       resize_to_limit: [ 500, 500 ],
+                       preprocessed: true
+  end
+
   def display_name
     nom
   end
--- spec/models/opco_spec.rb
+++ spec/models/opco_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Opco, type: :model do
   it { is_expected.to validate_presence_of(:nom) }
+  it { is_expected.to have_one_attached(:logo) }
 end

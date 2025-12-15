3074d5e622178ff5771f04b87be0f26c28af1180
Clément Prod'homme <clement.prod-homme@captive.fr>
2025-12-15 11:57:29 +0100

    ✨ Ajoute un logo pour les OPCOS

--- app/admin/opcos.rb
+++ app/admin/opcos.rb
@@ -1,13 +1,22 @@
 ActiveAdmin.register Opco do
   menu parent: I18n.t(".menu_structure"), if: proc { current_compte.superadmin? }
 
-  permit_params :nom, :financeur
+  permit_params :nom, :financeur, :logo
 
   filter :nom
   filter :financeur
 
   index do
-    column :nom
+    column :nom do |opco|
+      libelle = opco.nom
+      if opco.logo.attached?
+        logo = image_tag(cdn_for(opco.logo.variant(:defaut)),
+                         alt: opco.logo.filename,
+                         class: "logo-opco-preview")
+        libelle = logo + " " + libelle
+      end
+      link_to libelle, admin_opco_path(opco)
+    end
     column :financeur do |opco|
       badge_financeur(opco)
     end
@@ -15,10 +24,15 @@ ActiveAdmin.register Opco do
     actions
   end
 
+  show do
+    render partial: "show"
+  end
+
   form do |f|
     f.inputs do
       f.input :nom, hint: I18n.t("formtastic.hints.opco.nom")
       f.input :financeur, as: :toggle
+      f.input :logo, as: :image_file
     end
     f.actions
   end

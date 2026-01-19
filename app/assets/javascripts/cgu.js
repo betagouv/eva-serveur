0772573c6c054aea48c96dd9b1bd302ab4c84206
guillaume imary <guillaume.imary@gmail.com>
2026-01-19 12:55:36 +0100

    ✨ Améliore la gestion de l'activation des boutons dans le formulaire d'informations de compte en ajoutant des vérifications pour les champs requis et en simplifiant le code de mise à jour.

--- app/assets/javascripts/cgu.js
+++ app/assets/javascripts/cgu.js
@@ -1,6 +1,6 @@
-function activation_bouton_submit(cgu_acceptees) {
+function activation_bouton_submit(peut_valider) {
   let bouton_submit = $('.valider-compte-bouton')
-  if(cgu_acceptees) {
+  if(peut_valider) {
     bouton_submit.removeClass('disabled');
     bouton_submit.removeAttr('disabled');
     bouton_submit.removeAttr('title');
@@ -12,12 +12,25 @@ function activation_bouton_submit(cgu_acceptees) {
   }
 }
 
-function cgu_activation_submit() {
-  let cgu_acceptees = $("#compte_cgu_acceptees").is(':checked');
+function champ_rempli(selector) {
+  return $(selector).val().trim() !== "";
+}
 
-  activation_bouton_submit(cgu_acceptees);
-  $('#compte_cgu_acceptees_input').on('change', () => {
-    cgu_acceptees = !cgu_acceptees
-    activation_bouton_submit(cgu_acceptees);
-  });
+function peut_valider_informations_compte() {
+  const cgu_acceptees = $("#compte_cgu_acceptees").is(":checked");
+  const nom_rempli = champ_rempli("#compte_nom");
+  const prenom_rempli = champ_rempli("#compte_prenom");
+  const email_rempli = champ_rempli("#compte_email");
+
+  return cgu_acceptees && nom_rempli && prenom_rempli && email_rempli;
+}
+
+function cgu_activation_submit() {
+  const rafraichir_bouton = () => {
+    activation_bouton_submit(peut_valider_informations_compte());
+  };
+
+  rafraichir_bouton();
+  $("#compte_cgu_acceptees_input").on("change", rafraichir_bouton);
+  $("#compte_nom, #compte_prenom, #compte_email").on("input", rafraichir_bouton);
 }

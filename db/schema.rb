0332b46c59295ff5c50252e1d51461d6e2c21620
Étienne Charignon <etienne.charignon@ut7.fr>
2026-01-22 10:22:33 +0100

    🗃️ Passe toutes les questions de 'numeratie_2024' à passable

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_21_150323) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_22_091642) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"

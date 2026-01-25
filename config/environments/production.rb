7dd52c7dd3564ac353b09cb8ce0bb0ad54dcffd7
Étienne Charignon <etienne.charignon@ut7.fr>
2026-01-25 08:44:12 +0100

    🔧 Désactive le tracking des liens par mailjet
    Le short link Mailjet en http est parfois bloqué.  On n'utilise pas
    cette information, les mails envoyés sont des mails utilitaires, comme
    les mails de réinitialisation de mots de passe ou de validation
    d'adresse e-mail.  J'ai gardé le tracking d'ouverture des email pour
    pouvoir suivre d'éventuels problèmes de lecture des mails par les
    utilisateurs.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,6 +63,9 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :mailjet_api
+  config.action_mailer.delivery_method_options = {
+    'TrackClicks' => 'disabled'
+  }
 
   config.action_mailer.default_url_options = { host: ENV['HOTE_SERVEUR'] }
 

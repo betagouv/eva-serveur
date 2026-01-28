a1fba54261d1ef8c9305a8fb3c3eddf570ade13b
Marion Velard <marion.velard@captive.fr>
2026-01-28 14:15:54 +0100

    💄Traite les retours sur le header

--- app/components/header_component.rb
+++ app/components/header_component.rb
@@ -1,9 +1,11 @@
 # frozen_string_literal: true
 
 class HeaderComponent < ViewComponent::Base
-  def initialize(logo:, logo_alt:, titre: nil)
+  def initialize(logo:, logo_alt:, titre: nil, tagline: nil, logo_url: nil)
     @logo = logo
     @logo_alt = logo_alt
+    @logo_url = logo_url
     @titre = titre
+    @tagline = tagline
   end
 end

951f5f13601747e0606b916d04e328753e050170
Marion Velard <marion.velard@captive.fr>
2026-01-29 11:51:35 +0100

    💄 Le nouvel inscrit suit son embarquement dans un espace visiblement connecté

--- app/components/header_component.rb
+++ app/components/header_component.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
 class HeaderComponent < ViewComponent::Base
-  def initialize(logo:, logo_alt:, titre: nil, tagline: nil, logo_url: nil)
+  def initialize(logo:, logo_alt:, titre: nil, tagline: nil, logo_url: nil, current_compte: nil,
+actions: [])
     @logo = logo
     @logo_alt = logo_alt
     @logo_url = logo_url
     @titre = titre
     @tagline = tagline
+    @current_compte = current_compte
+    @actions = actions
+  end
+
+  def affiche_actions_connexion?
+    @current_compte.present? && @actions.any?
+  end
+
+  def affiche_titre?
+    @titre.present?
   end
 end

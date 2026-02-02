6ca63914eb45e9e717ce504e7a2abec2b10668f8
Marion Velard <marion.velard@captive.fr>
2026-02-02 15:19:19 +0100

    💄 Change la présentation de la mesure des risques sur les evaluations

--- spec/helpers/evaluation_helper_spec.rb
+++ spec/helpers/evaluation_helper_spec.rb
@@ -13,4 +13,52 @@ describe EvaluationHelper do
       expect(niveau_bas?(:profil_autre)).to be false
     end
   end
+
+  describe '#palier_pourcentage_risque' do
+    context 'quand le pourcentage est entre 0 et 10' do
+      it 'retourne "A - Très bon" pour 0' do
+        expect(palier_pourcentage_risque(0)).to eq("A - Très bon")
+      end
+
+      it 'retourne "A - Très bon" pour 10' do
+        expect(palier_pourcentage_risque(10)).to eq("A - Très bon")
+      end
+    end
+
+    context 'quand le pourcentage est entre 11 et 25' do
+      it 'retourne "B - Bon" pour 11' do
+        expect(palier_pourcentage_risque(11)).to eq("B - Bon")
+      end
+
+      it 'retourne "B - Bon" pour 25' do
+        expect(palier_pourcentage_risque(25)).to eq("B - Bon")
+      end
+    end
+
+    context 'quand le pourcentage est entre 26 et 50' do
+      it 'retourne "C - Moyen" pour 26' do
+        expect(palier_pourcentage_risque(26)).to eq("C - Moyen")
+      end
+
+      it 'retourne "C - Moyen" pour 50' do
+        expect(palier_pourcentage_risque(50)).to eq("C - Moyen")
+      end
+    end
+
+    context 'quand le pourcentage est entre 51 et 100' do
+      it 'retourne "D - Mauvais" pour 51' do
+        expect(palier_pourcentage_risque(51)).to eq("D - Mauvais")
+      end
+
+      it 'retourne "D - Mauvais" pour 100' do
+        expect(palier_pourcentage_risque(100)).to eq("D - Mauvais")
+      end
+    end
+
+    context 'quand le pourcentage est hors limites' do
+      it 'retourne "D - Mauvais" pour une valeur supérieure à 100' do
+        expect(palier_pourcentage_risque(150)).to eq("D - Mauvais")
+      end
+    end
+  end
 end

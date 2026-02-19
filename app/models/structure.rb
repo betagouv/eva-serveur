d08418a1435644b9526d199bff800557d54ea093
guillaume imary <guillaume.imary@gmail.com>
2026-02-19 18:44:13 +0100

    ♻️ Structure: belongs_to :opco, suppression modèle StructureOpco

--- app/models/structure.rb
+++ app/models/structure.rb
@@ -8,25 +8,12 @@ class Structure < ApplicationRecord
 
   has_ancestry
   acts_as_paranoid
-  has_many :structure_opcos, dependent: :destroy, autosave: true
-  has_many :opcos, through: :structure_opcos
+  belongs_to :opco, optional: true
 
   alias structure_referente parent
   alias structure_referente= parent=
 
   attr_accessor :validation_inscription
-
-  def opco_id
-    opco_ids.first
-  end
-
-  def opco_id=(id)
-    if id.present?
-      self.opco_ids = [ id ]
-    else
-      self.opco_ids = []
-    end
-  end
 
   validates :nom, presence: true
   validates :nom, uniqueness: {
@@ -123,11 +110,7 @@ class Structure < ApplicationRecord
   end
 
   def opco_financeur
-    opcos.find(&:financeur?)
-  end
-
-  def opco
-    opcos.first
+    opco if opco&.financeur?
   end
 
   private
@@ -172,7 +155,7 @@ class Structure < ApplicationRecord
   end
 
   def doit_avoir_un_opco
-    return if opcos.any?
+    return if opco.present?
 
     errors.add(:opco_id, :must_be_present)
   end

d74052ab76c7e1a4a31d27a5cc828ae219196f58
Marion Velard <marion.velard@captive.fr>
2026-02-20 15:52:09 +0100

    ✨ Pemet a l'admin de modifier et de consulter l'url de contact d'un opco

--- app/admin/opcos.rb
+++ app/admin/opcos.rb
@@ -1,7 +1,7 @@
 ActiveAdmin.register Opco do
   menu parent: I18n.t(".menu_structure"), if: proc { current_compte.superadmin? }
 
-  permit_params :nom, :financeur, :logo, :telephone, :url, :email
+  permit_params :nom, :financeur, :logo, :telephone, :url, :email, :url_contact
 
   filter :nom
   filter :financeur
@@ -36,6 +36,7 @@ ActiveAdmin.register Opco do
       f.input :telephone
       f.input :email
       f.input :url
+      f.input :url_contact
     end
     f.actions
   end

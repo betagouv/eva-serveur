a54194db1470fd28a51a83e86212d6b4cb36511e
Étienne Charignon <etienne.charignon@ut7.fr>
2026-03-18 14:43:10 +0100

    ✨ Ne purge les comptes que s'ils ont été suprimés depuis plus d'un mois

--- spec/jobs/purge_comptes_supprimes_job_spec.rb
+++ spec/jobs/purge_comptes_supprimes_job_spec.rb
@@ -4,13 +4,21 @@ describe PurgeComptesSupprimesJob, type: :job do
   let(:structure) { create :structure_locale, :avec_admin }
 
   it 'detruit définitivement un compte supprimé sans évaluation et ses campagnes' do
-    compte = create :compte_conseiller, structure: structure, deleted_at: Time.zone.now
-    create :campagne, compte: compte, deleted_at: Time.zone.now
+    compte = create :compte_conseiller, structure: structure, deleted_at: 2.months.ago
+    create :campagne, compte: compte, deleted_at: 2.months.ago
 
     described_class.perform_now
 
     expect(Compte.with_deleted.find_by(id: compte.id)).to be_nil
     expect(Campagne.with_deleted.where(compte_id: compte.id)).to be_empty
+  end
+
+  it 'ne detruit pas un compte supprimé depuis moins d\'un mois' do
+    compte = create :compte_conseiller, structure: structure, deleted_at: 1.month.ago + 1.day
+
+    described_class.perform_now
+
+    expect(Compte.with_deleted.find_by(id: compte.id)).not_to be_nil
   end
 
   it 'ne detruit pas un compte supprimé qui a des évaluations' do
@@ -35,13 +43,23 @@ describe PurgeComptesSupprimesJob, type: :job do
   end
 
   it 'anonymise les comptes supprimés qui ont des évaluations' do
-    compte = create :compte_conseiller, structure: structure, deleted_at: Time.zone.now
+    compte = create :compte_conseiller, structure: structure, deleted_at: 1.month.ago
     campagne = create :campagne, compte: compte, deleted_at: Time.zone.now
     create :evaluation, campagne: campagne
 
     anonymisation = instance_double(Anonymisation::Compte)
     allow(Anonymisation::Compte).to receive(:new).with(compte).and_return(anonymisation)
     expect(anonymisation).to receive(:anonymise)
+
+    described_class.perform_now
+  end
+
+  it "n'anonymise pas un compte supprimé depuis moins d'un mois" do
+    compte = create :compte_conseiller, structure: structure, deleted_at: 1.month.ago + 1.day
+    campagne = create :campagne, compte: compte, deleted_at: 1.month.ago + 1.day
+    create :evaluation, campagne: campagne
+
+    expect(Anonymisation::Compte).not_to receive(:new)
 
     described_class.perform_now
   end
@@ -55,7 +73,7 @@ describe PurgeComptesSupprimesJob, type: :job do
   end
 
   it "detruit un compte supprimé responsable de suivi d'une évaluation et retire ce lien" do
-    compte = create :compte_conseiller, structure: structure, deleted_at: Time.zone.now
+    compte = create :compte_conseiller, structure: structure, deleted_at: 1.month.ago
     evaluation = create :evaluation, responsable_suivi: compte
 
     described_class.perform_now
@@ -65,7 +83,7 @@ describe PurgeComptesSupprimesJob, type: :job do
   end
 
   it 'detruit un compte supprimé référencé par un bénéficiaire et retire ce lien' do
-    compte = create :compte_conseiller, structure: structure, deleted_at: Time.zone.now
+    compte = create :compte_conseiller, structure: structure, deleted_at: 1.month.ago
     beneficiaire = create :beneficiaire, compte: compte
 
     described_class.perform_now
@@ -75,8 +93,8 @@ describe PurgeComptesSupprimesJob, type: :job do
   end
 
   it 'detruit un compte supprimé référencé par un bénéficiaire supprimé et retire ce lien' do
-    compte = create :compte_conseiller, structure: structure, deleted_at: Time.zone.now
-    beneficiaire = create :beneficiaire, compte: compte, deleted_at: Time.zone.now
+    compte = create :compte_conseiller, structure: structure, deleted_at: 1.month.ago
+    beneficiaire = create :beneficiaire, compte: compte, deleted_at: 1.month.ago
 
     described_class.perform_now
 
@@ -85,8 +103,8 @@ describe PurgeComptesSupprimesJob, type: :job do
   end
 
   it "detruit un compte supprimé responsable de suivi d'une évaluation supprimé et retire ce lien" do
-    compte = create :compte_conseiller, structure: structure, deleted_at: Time.zone.now
-    evaluation = create :evaluation, responsable_suivi: compte, deleted_at: Time.zone.now
+    compte = create :compte_conseiller, structure: structure, deleted_at: 1.month.ago
+    evaluation = create :evaluation, responsable_suivi: compte, deleted_at: 1.month.ago
 
     described_class.perform_now
 

816ee39639c136c36c67e4672b86fc4eaefe9f5f
guillaume imary <guillaume.imary@gmail.com>
2026-03-25 16:06:42 +0100

    🔧 Refactorise les présentateurs de risques et de coûts dans le modèle `DiagnosticPro` pour améliorer la gestion des évaluations. Remplace les anciennes méthodes par des classes dédiées et met à jour les vues pour utiliser ces nouvelles structures.

--- app/models/evaluations/passation_beneficiaire.rb
+++ app/models/evaluations/passation_beneficiaire.rb
@@ -11,6 +11,49 @@ module Evaluations
     def evapro?
       false
     end
+
+    def enregistre_mise_en_action(effectuee)
+      @evaluation.enregistre_mise_en_action(effectuee)
+    end
+
+    def a_mise_en_action?
+      @evaluation.a_mise_en_action?
+    end
+
+    def mise_en_action_presenter
+      MiseEnActionPresenter.new(evaluation)
+    end
+
+    class MiseEnActionPresenter
+      def initialize(evaluation)
+        @evaluation = evaluation
+      end
+
+      def a_mise_en_action?
+        @evaluation.a_mise_en_action?
+      end
+
+      def effectuee_avec_remediation?
+        @evaluation&.mise_en_action&.effectuee_avec_remediation?
+      end
+
+      def effectuee_sans_remediation?
+        @evaluation&.mise_en_action&.effectuee &&
+          @evaluation&.mise_en_action&.remediation.blank?
+      end
+
+      def non_effectuee_sans_difficulte?
+        @evaluation&.mise_en_action&.effectuee == false &&
+          @evaluation&.mise_en_action&.difficulte.blank?
+      end
+
+      def non_effectuee_avec_difficulte?
+        @evaluation&.mise_en_action&.non_effectuee_avec_difficulte?
+      end
+
+      def mise_en_action_avec_qualification?
+        effectuee_avec_remediation? || non_effectuee_avec_difficulte?
+      end
+    end
   end
 end
-
---
+++ spec/models/evaluations/passation_beneficiaire/mise_en_action_presenter_spec.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Evaluations::PassationBeneficiaire::MiseEnActionPresenter do
+  describe "#mise_en_action_avec_qualification?" do
+    it "retourne true quand la mise en action est effectuée avec remédiation" do
+      mise_en_action = instance_double(MiseEnAction, effectuee_avec_remediation?: true)
+      evaluation = instance_double(
+        Evaluation,
+        a_mise_en_action?: true,
+        mise_en_action: mise_en_action
+      )
+
+      presenter = described_class.new(evaluation)
+
+      expect(presenter.effectuee_avec_remediation?).to be(true)
+      expect(presenter.mise_en_action_avec_qualification?).to be(true)
+    end
+
+    it "retourne true quand la mise en action est non effectuée avec difficulté" do
+      mise_en_action = instance_double(MiseEnAction,
+                                       effectuee_avec_remediation?: false,
+                                       non_effectuee_avec_difficulte?: true)
+      evaluation = instance_double(
+        Evaluation,
+        a_mise_en_action?: true,
+        mise_en_action: mise_en_action
+      )
+
+      presenter = described_class.new(evaluation)
+
+      expect(presenter.non_effectuee_avec_difficulte?).to be(true)
+      expect(presenter.mise_en_action_avec_qualification?).to be(true)
+    end
+  end
+end

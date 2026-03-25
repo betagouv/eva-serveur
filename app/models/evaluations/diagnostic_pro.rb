c4fa6fe0242e055013f2d0bd01c31663a0d468c1
guillaume imary <guillaume.imary@gmail.com>
2026-03-25 10:20:10 +0100

    🔧 Refactorise les méthodes `opco` et `opco_financeur` dans le modèle `Evaluation` pour utiliser `diagnostic_pro`.

--- app/models/evaluations/diagnostic_pro.rb
+++ app/models/evaluations/diagnostic_pro.rb
@@ -12,6 +12,20 @@ module Evaluations
       true
     end
 
+    def opco_financeur
+      structure = evaluation.campagne&.compte&.structure
+      return if structure.blank?
+
+      structure.opco_financeur
+    end
+
+    def opco
+      structure = evaluation.campagne&.compte&.structure
+      return if structure.blank?
+
+      structure.opco
+    end
+
     def with_restitution_globale(restitution_globale)
       Restitution.new(restitution_globale: restitution_globale)
     end

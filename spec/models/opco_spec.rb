aba46d518dd1ac72da98153b41c394b1050737af
guillaume imary <guillaume.imary@gmail.com>
2026-03-26 08:14:50 +0100

    ✨ Ajoute la possibilité de supprimer le visuel de l'offre de services dans le modèle Opco

--- spec/models/opco_spec.rb
+++ spec/models/opco_spec.rb
@@ -1,9 +1,48 @@
 require "rails_helper"
 
 RSpec.describe Opco, type: :model do
+  include ActiveJob::TestHelper
+
   it { is_expected.to validate_presence_of(:nom) }
   it { is_expected.to have_one_attached(:logo) }
   it { is_expected.to have_one_attached(:visuel_offre_services) }
+
+  describe "#supprime_visuel_offre_services" do
+    let(:opco) { create(:opco) }
+
+    before do
+      opco.visuel_offre_services.attach(
+        io: Rails.root.join("spec/support/programme_tele.png").open,
+        filename: "visuel.png",
+        content_type: "image/png"
+      )
+    end
+
+    it "supprime le visuel lorsque la case est cochée" do
+      perform_enqueued_jobs do
+        opco.update(supprimer_visuel_offre_services: "1")
+      end
+
+      expect(opco.reload.visuel_offre_services.attached?).to be false
+    end
+
+    context "quand un nouveau visuel est envoyé en même temps que la suppression" do
+      it "conserve le nouveau fichier" do
+        nouvelle_image = Rack::Test::UploadedFile.new(
+          Rails.root.join("spec/support/programme_tele.png")
+        )
+
+        perform_enqueued_jobs do
+          opco.update(
+            visuel_offre_services: nouvelle_image,
+            supprimer_visuel_offre_services: "1"
+          )
+        end
+
+        expect(opco.reload.visuel_offre_services.attached?).to be true
+      end
+    end
+  end
 
   describe "validation url_offre_services" do
     context "quand url_offre_services est vide" do

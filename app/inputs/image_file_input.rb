fe10af2cb03ded8537c758552c9c9547406e8eae
guillaume imary <guillaume.imary@gmail.com>
2026-03-30 09:49:17 +0200

    🔧 Ajoute la suppression dans le file input

--- app/inputs/image_file_input.rb
+++ app/inputs/image_file_input.rb
@@ -24,6 +24,28 @@ class ImageFileInput < FileInput
               class: "image-preview")
   end
 
+  def file_input_html
+    input_html = get_input_html_options
+    field = builder.file_field(method, input_html)
+
+    template.content_tag(:div, class: "image-file-input-with-clear") do
+      template.safe_join([ field, clear_button_html ])
+    end
+  end
+
+  def clear_button_html
+    label = I18n.t("formtastic.inputs.image_file.retirer_fichier_selectionne")
+
+    template.content_tag(:button,
+      template.content_tag(:span, "", class: "fr-icon-close-line", "aria-hidden": "true"),
+      type: "button",
+      class: "image-file-input-with-clear__btn fr-btn fr-btn--tertiary-no-outline fr-btn--sm",
+      hidden: true,
+      title: label,
+      "aria-label": label,
+      "data-image-file-clear": "")
+  end
+
   def get_input_html_options
     input_html = super
     input_html[:accept] ||= "image/*"

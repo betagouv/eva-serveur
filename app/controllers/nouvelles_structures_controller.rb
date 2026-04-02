a52f27542833c4abda881c5e9da653fb7860e7ee
Étienne Charignon <etienne.charignon@ut7.fr>
2026-04-02 21:38:47 +0200

    🐛 Remet les captcha sur la page de connexion à la démo et création de structure

--- app/controllers/nouvelles_structures_controller.rb
+++ app/controllers/nouvelles_structures_controller.rb
@@ -10,7 +10,7 @@ class NouvellesStructuresController < ApplicationController
   def create
     @compte = Compte.new compte_parametres
     @compte.structure&.affecte_usage_entreprise_si_necessaire
-    if @compte.save
+    if verify_recaptcha(model: @compte) && @compte.save
       AffiliationOpcoService.new(@compte.structure).affilie_opcos
       envoie_emails
       sign_in @compte

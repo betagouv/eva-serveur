35d5d9eda8c83cd96a8eaa2234775f132aacf6a7
Marion Velard <marion.velard@captive.fr>
2026-04-14 18:46:17 +0200

    🐛  Corrige les url des restitutions eva pro
    - Corrige les urls constructys.frcontacts -> constructys.fr/contacts (manquait un /)
    - Supprime les derniers / en fin d'adresse pour ne pas avoir constructys.fr/contacts/ mais constructys.fr/contacts
    - Ajout de test pour UrlHelper

--- app/helpers/url_helper.rb
+++ app/helpers/url_helper.rb
@@ -1,6 +1,6 @@
 module UrlHelper
   # https://www.constructys.fr/ => constructys.fr
   def humanize_url(url)
-    url.gsub("https://", "").gsub("http://", "").gsub("www.", "").gsub("/", "")
+    url.gsub("https://", "").gsub("http://", "").gsub("www.", "").delete_suffix("/")
   end
 end

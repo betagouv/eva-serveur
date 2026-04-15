9aaa81ca974f08e8f4c3c9c1f565bcd510ccc9d2
Étienne Charignon <etienne.charignon@ut7.fr>
2026-04-15 19:23:27 +0200

    🔥 Suprime la page 'admin/sign_up'

--- spec/features/inscription_conseiller_spec.rb
+++ spec/features/inscription_conseiller_spec.rb
@@ -23,50 +23,7 @@ describe 'Création de compte conseiller', type: :feature do
     end
   end
 
-  context "structure précisée dans l'url" do
-    before do
-      visit new_compte_registration_path(structure_id: structure.id)
-      fill_in :compte_prenom, with: 'Julia'
-      fill_in :compte_nom, with: 'Robert'
-      fill_in :compte_email, with: 'monemail@eva.fr'
-      fill_in :compte_password, with: 'Pass5678'
-      fill_in :compte_password_confirmation, with: 'Pass5678'
-      check("J’accepte les conditions générales d’utilisation", allow_label_click: true)
-    end
-
-    it "crée un compte" do
-      expect do
-        click_on "S'inscrire"
-      end.to change(Compte, :count).by(1)
-                                   .and(change(Devise.mailer.deliveries, :count).by(1))
-      nouveau_compte = Compte.find_by email: 'monemail@eva.fr'
-      expect(nouveau_compte.validation_en_attente?).to be true
-      expect(nouveau_compte.structure).to eq structure
-      expect(nouveau_compte.cgu_acceptees).to be true
-    end
-  end
-
-  context 'structure sans admin' do
-    let!(:structure_sans_admin) { create :structure_locale, nom: 'Ma structure sans admin' }
-
-    before do
-      visit new_compte_registration_path(structure_id: structure_sans_admin.id)
-      fill_in :compte_prenom, with: 'Julia'
-      fill_in :compte_nom, with: 'Robert'
-      fill_in :compte_email, with: 'monemail@eva.fr'
-      fill_in :compte_password, with: 'Pass5678'
-      fill_in :compte_password_confirmation, with: 'Pass5678'
-    end
-
-    it 'donne le role admin' do
-      click_on "S'inscrire"
-      nouveau_compte = Compte.find_by email: 'monemail@eva.fr'
-      expect(nouveau_compte.validation_en_attente?).to be false
-      expect(nouveau_compte.role).to eq 'admin'
-    end
-  end
-
-  context "redirection de l'ancien lien d'inscription Devise vers l'embarquement v2" do
+  context "redirection de l’ancien lien d’inscription Devise vers l’embarquement v2" do
     let!(:invitation) do
       invitant = create(:compte_admin, structure: structure)
       create(:invitation,

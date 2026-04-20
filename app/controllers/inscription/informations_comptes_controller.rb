5037a549c0f0e6e67a57bd140421a1f958598347
Marion Velard <marion.velard@captive.fr>
2026-04-20 16:47:06 +0200

    🐛 Permet d'enregistrer le numero de telephone lors de la creation d'un compte lors de l'embarquement

--- app/controllers/inscription/informations_comptes_controller.rb
+++ app/controllers/inscription/informations_comptes_controller.rb
@@ -20,7 +20,7 @@ class Inscription::InformationsComptesController < ApplicationController
   private
 
   def compte_parametres
-    params.require(:compte).permit(:nom, :prenom, :email, :fonction,
+    params.require(:compte).permit(:nom, :prenom, :email, :fonction, :telephone,
 :service_departement, :cgu_acceptees)
   end
 

dfca7b79ff87a9d0f9576280115692a0249e2b4e
Clément Prod'homme <clement.prod-homme@captive.fr>
2026-05-04 12:40:59 +0200

    ♻️ Vérifie que les situations de Evapro sont complété pour vérifier la complétude des évaluations

--- spec/models/restitution/completude_spec.rb
+++ spec/models/restitution/completude_spec.rb
@@ -6,13 +6,29 @@ describe Restitution::Completude do
   end
 
   describe '#calcule' do
-    let(:evaluation) { double(campagne_id: SecureRandom.uuid, evapro?: false) }
+    let(:evapro?) { false }
+    let(:evaluation) { double(campagne_id: SecureRandom.uuid, evapro?: evapro?) }
     let(:bienvenue) { Situation.new id: SecureRandom.uuid }
     let(:plan_de_la_ville) { Situation.new id: SecureRandom.uuid }
     let(:controle) { Situation.new id: SecureRandom.uuid }
     let(:livraison) { Situation.new id: SecureRandom.uuid }
     let(:maintenance) { Situation.new id: SecureRandom.uuid }
     let(:objets_trouves) { Situation.new id: SecureRandom.uuid }
+
+    before do
+      allow(SituationConfiguration)
+        .to receive(:ids_situations).with(evaluation.campagne_id,
+                                          Evaluation::SITUATION_COMPETENCES_TRANSVERSALES)
+                                    .and_return([])
+      allow(SituationConfiguration)
+        .to receive(:ids_situations).with(evaluation.campagne_id,
+                                          Evaluation::SITUATION_COMPETENCES_BASE)
+                                    .and_return([])
+      allow(SituationConfiguration)
+            .to receive(:ids_situations).with(evaluation.campagne_id,
+                                              Evaluation::SITUATION_COMPETENCES_EVAPRO)
+                                        .and_return([])
+    end
 
     describe 'quand la campagne est complete' do
       before do
@@ -189,126 +205,50 @@ describe Restitution::Completude do
       end
     end
 
-    describe "quand l'evaluation est Evapro" do
-      let(:restitutions) { [] }
-      let(:evaluation) { double(campagne_id: SecureRandom.uuid, evapro?: true) }
+    context "quand l'evaluation est Evapro" do
+      let(:evapro?) { true }
+      let(:diag_risques_entreprise) { Situation.new id: SecureRandom.uuid }
+      let(:evaluation_impact_general) { Situation.new id: SecureRandom.uuid }
 
-      let(:situation_diag) do
-        instance_double(Situation).tap do |s|
-          allow(s).to receive(:a_pour_nom_technique?)
-            .with(Situation::DIAG_RISQUES_ENTREPRISE)
-            .and_return(true)
-          allow(s).to receive(:a_pour_nom_technique?)
-            .with(Situation::EVALUATION_IMPACT_GENERAL)
-            .and_return(false)
-        end
+      before do
+        allow(SituationConfiguration)
+          .to receive(:ids_situations).with(evaluation.campagne_id,
+                                            Evaluation::SITUATION_COMPETENCES_EVAPRO)
+                                      .and_return([ diag_risques_entreprise.id,
+evaluation_impact_general.id ])
       end
 
-      let(:situation_impact) do
-        instance_double(Situation).tap do |s|
-          allow(s).to receive(:a_pour_nom_technique?)
-            .with(Situation::DIAG_RISQUES_ENTREPRISE)
-            .and_return(false)
-          allow(s).to receive(:a_pour_nom_technique?)
-            .with(Situation::EVALUATION_IMPACT_GENERAL)
-            .and_return(true)
+      context "quand aucune situations n'a été complétée" do
+        let(:restitutions) do
+          [
+            double(situation: diag_risques_entreprise, termine?: false),
+            double(situation: evaluation_impact_general, termine?: false)
+          ]
         end
+
+        it { expect(completude.calcule).to eq :incomplete }
       end
 
-      let(:diag_restitution) do
-        double(situation: situation_diag, synthese: diag_synthese)
+      context 'quand toutes les situations de la campagne ont été complétées' do
+        let(:restitutions) do
+          [
+            double(situation: diag_risques_entreprise, termine?: true),
+            double(situation: evaluation_impact_general, termine?: true)
+          ]
+        end
+
+        it { expect(completude.calcule).to eq :complete }
       end
 
-      let(:impact_restitution) do
-        double(situation: situation_impact, synthese: impact_synthese)
-      end
-
-      let(:restitutions) { [ diag_restitution, impact_restitution ] }
-
-      context 'si pourcentage_risque, score_cout et toutes les metriques impact sont presents' do
-        let(:diag_synthese) { { pourcentage_risque: 25 } }
-        let(:impact_synthese) do
-          {
-            score_cout: 'moyen',
-            performance_collective: :faible,
-            agilite_organisationnelle: :moyen,
-            securite_qualite: :fort,
-            mobilite_professionnelle: :tres_fort
-          }
+      context "quand une situations de la campagne n'a pas été complétée" do
+        let(:restitutions) do
+          [
+            double(situation: diag_risques_entreprise, termine?: false),
+            double(situation: evaluation_impact_general, termine?: true)
+          ]
         end
 
-        it 'retourne complete' do
-          expect(described_class.new(evaluation, restitutions).calcule).to eq(:complete)
-        end
-      end
-
-      context 'si pourcentage_risque manque' do
-        let(:diag_synthese) { { pourcentage_risque: nil } }
-        let(:impact_synthese) do
-          {
-            score_cout: 'moyen',
-            performance_collective: :faible,
-            agilite_organisationnelle: :moyen,
-            securite_qualite: :fort,
-            mobilite_professionnelle: :tres_fort
-          }
-        end
-
-        it 'retourne incomplete' do
-          expect(described_class.new(evaluation, restitutions).calcule).to eq(:incomplete)
-        end
-      end
-
-      context 'si score_cout manque' do
-        let(:diag_synthese) { { pourcentage_risque: 25 } }
-        let(:impact_synthese) do
-          {
-            score_cout: nil,
-            performance_collective: :faible,
-            agilite_organisationnelle: :moyen,
-            securite_qualite: :fort,
-            mobilite_professionnelle: :tres_fort
-          }
-        end
-
-        it 'retourne incomplete' do
-          expect(described_class.new(evaluation, restitutions).calcule).to eq(:incomplete)
-        end
-      end
-
-      context 'si une metrique impact manque' do
-        let(:diag_synthese) { { pourcentage_risque: 25 } }
-        let(:impact_synthese) do
-          {
-            score_cout: 'moyen',
-            performance_collective: :faible,
-            agilite_organisationnelle: nil,
-            securite_qualite: :fort,
-            mobilite_professionnelle: :tres_fort
-          }
-        end
-
-        it 'retourne incomplete' do
-          expect(described_class.new(evaluation, restitutions).calcule).to eq(:incomplete)
-        end
-      end
-
-      context 'si diag ou impact restitution manque' do
-        let(:diag_synthese) { nil }
-        let(:impact_synthese) do
-          {
-            score_cout: 'moyen',
-            performance_collective: :faible,
-            agilite_organisationnelle: :moyen,
-            securite_qualite: :fort,
-            mobilite_professionnelle: :tres_fort
-          }
-        end
-
-        it 'retourne incomplete' do
-          restitutions = [ diag_restitution ].compact
-          expect(described_class.new(evaluation, restitutions).calcule).to eq(:incomplete)
-        end
+        it { expect(completude.calcule).to eq :incomplete }
       end
     end
   end

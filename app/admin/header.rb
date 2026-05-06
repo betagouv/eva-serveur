12f85a85b79473f1fd59ad6c6cdc3065ec716cbc
Marion Velard <marion.velard@captive.fr>
2026-05-06 11:44:23 +0200

    🐛 Ajoute le bon layout et Header pour les compte opco

--- app/admin/header.rb
+++ app/admin/header.rb
@@ -8,7 +8,7 @@ module ActiveAdmin
         return super unless current_compte.present?
 
         partial =
-          if current_compte.structure&.evapro?
+          if current_compte.utilisateur_entreprise? || current_compte.utilisateur_opco?
             "components/header"
           else
             "components/header_eva"

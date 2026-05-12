9ef66ce1ca1630584863e5b0a18c12e698d685e7
Clément Prod'homme <clement.prod-homme@captive.fr>
2026-05-12 11:56:10 +0200

    🧑‍💻 Cache les logs capybara lors de l'execution des tests
    avant Capybara affichait les logs suivant :
    
    Capybara starting Puma...
    * Version 6.6.1, codename: Return to Forever
    * Min threads: 0, max threads: 4
    * Listening on http://127.0.0.1:38429

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -12,6 +12,7 @@ end
 
 Capybara.javascript_driver = :selenium_chrome_headless
 Capybara.default_max_wait_time = 5
+Capybara.server = :puma, { Silent: true }
 
 WebMock.disable_net_connect!(allow_localhost: true)
 

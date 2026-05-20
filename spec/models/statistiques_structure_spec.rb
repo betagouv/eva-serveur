3652499c0129d8d857271ef4be7bc1e4b32a01f7
Étienne Charignon <etienne.charignon@ut7.fr>
2026-05-20 16:44:46 +0200

    ♻️ Création d'un composant metabase iframe pour les structures
    Pour factoriser le calcul de l'url et la création de l'iframe

--- spec/models/statistiques_structure_spec.rb
+++ spec/models/statistiques_structure_spec.rb
@@ -48,37 +48,4 @@ describe StatistiquesStructure do
       end
     end
   end
-
-  describe "#url" do
-   let(:structure) { create :structure_opco }
-
-    context "quand METABASE_SECRET_KEY est configurée" do
-      before do
-        allow(structure).to receive_messages(metabase_dashboard: 61,
-          metabase_query_params: { "opco_id" => [ 42 ] })
-        allow(ENV).to receive(:fetch).with("METABASE_SECRET_KEY", nil).and_return("secret")
-        allow(ENV).to receive(:fetch).with("METABASE_SITE_URL", nil).and_return("http://metabase.example")
-      end
-
-      it "construit une url embed signée et filtrée sur l'opco" do
-        statistiques_opco = described_class.new(structure)
-
-        url = statistiques_opco.url
-        token = url.match(%r{/embed/dashboard/([^#]+)})[1]
-        payload = JWT.decode(token, "secret", true, algorithm: "HS256").first
-
-        expect(url).to start_with("http://metabase.example/embed/dashboard/")
-        expect(url).to end_with("#bordered=false&titled=false")
-        expect(payload["resource"]).to eq("dashboard" => 61)
-        expect(payload["params"]).to eq("opco_id" => [ 42 ])
-        expect(payload["exp"]).to be_within(5).of(10.minutes.from_now.to_i)
-      end
-    end
-
-    context "quand METABASE_SECRET_KEY n'est pas configurée" do
-      it "retourne nil" do
-        expect(described_class.new(structure).url).to be_nil
-      end
-    end
-  end
 end

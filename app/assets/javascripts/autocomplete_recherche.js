db011087dd5dd80bd2edaa978b4635d18c5f017c
Marion Velard <marion.velard@captive.fr>
2026-05-27 12:55:04 +0200

    ✨ Restaure les fonctions d'autocomplétion pour la recherche de compte

--- app/assets/javascripts/autocomplete_recherche.js
+++ app/assets/javascripts/autocomplete_recherche.js
@@ -1,72 +1,9 @@
-function extraitCodePostal(texte) {
-  const match = texte.match(/(\d{5})/);
-  return match ? match[1] : null;
-}
-
-function escapeHTML(str) {
-  return str.replace(/[&<>'"]/g, tag => {
-    const entityMap = {
-      '&': '&amp;',
-      '<': '&lt;',
-      '>': '&gt;',
-      "'": '&#39;',
-      '"': '&quot;'
-    };
-    return entityMap[tag] || tag
-  });
-}
-
-function construitReponse(item, codePostalSaisi) {
-  const ville = item.nom;
-  let codePostal = '';
-  if (codePostalSaisi && item.codesPostaux.length > 1) {
-    codePostal = codePostalSaisi
-  } else {
-    codePostal = item.codesPostaux[0];
-  }
-  const label = `${ville} (${codePostal})`;
-  return { value: label, ville: ville, code_postal: codePostal };
-}
-
 function afficheRecherche(boutonAjout, formulaireRecherche, initAutocomplete) {
   boutonAjout.click(function () {
     boutonAjout.hide()
     formulaireRecherche.show()
     initAutocomplete();
   });
-}
-
-function requeteCommunes(requete, reponse, $) {
-  $('#bouton-chercher')
-    .prop("disabled", true)
-    .addClass("disabled");
-  if (!requete.match(/^\d{1,4}$/)) {
-    let data = { limit: 6, type: 'commune-actuelle,arrondissement-municipal' };
-    if (extraitCodePostal(requete)) {
-      data.codePostal = extraitCodePostal(requete);
-    } else {
-      data.nom = requete;
-      data.boost = 'population';
-    }
-    $.ajax({
-      url: "https://geo.api.gouv.fr/communes",
-      data: data,
-      dataType: "json",
-      success: function (datas) {
-        reponse($.map(datas, function (item) {
-          return construitReponse(item, data.codePostal);
-        }))
-      },
-      error: function () {
-        reponse([]);
-      }
-    });
-  }
-}
-
-function getUrlParameter(name) {
-  const urlParams = new URLSearchParams(window.location.search);
-  return urlParams.get(name);
 }
 
 function initialiseAutocomplete(options) {
@@ -83,9 +20,9 @@ function initialiseAutocomplete(options) {
       onConfirm
   } = options;
 
-  const autocomplete = accessibleAutocomplete({
+  accessibleAutocomplete({
     element: element,
-    id: id, // id de l'input créé a utiliser dans l'attribut for du label
+    id: id,
     name: name,
     defaultValue: defaultValue || '',
     showAllValues: false,
@@ -110,19 +47,17 @@ function initialiseAutocomplete(options) {
         }
       }
     },
-
     onConfirm: onConfirm,
-    tNoResults: () => I18n.t('recherche_structure_component.no_results'),
-    tStatusNoResults: () => I18n.t('recherche_structure_component.no_results'),
+    tNoResults: () => I18n.t('recherche_compte.no_results'),
+    tStatusNoResults: () => I18n.t('recherche_compte.no_results'),
     tStatusSelectedOption: (selectedOption, length, index) => `${selectedOption} ${index + 1} sur ${length} est sélectionné`,
-    tAssistiveHint: () => I18n.t('recherche_structure_component.assistive_hint'),
+    tAssistiveHint: () => I18n.t('recherche_compte.assistive_hint'),
     tStatusQueryTooShort: (minQueryLength) => `Entrez au moins ${minQueryLength} characters pour avoir un résultat`,
     tStatusResults: (length, contentSelectedOption) => {
         const words = {
             result: (length === 1) ? 'résultat' : 'résultats',
             is: (length === 1) ? 'disponible' : 'disponibles'
         };
-
         return `${length} ${words.result} ${words.is}. ${contentSelectedOption}`;
     }
   });
@@ -134,38 +69,3 @@ function initialiseAutocomplete(options) {
     input.focus();
   }
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-  const container = document.querySelector('#recherche-structure-autocomplete-container');
-  if(!container){ return; }
-
-  initialiseAutocomplete({
-    focus: !getUrlParameter('code_postal'),
-    element: container,
-    id: 'recherche-structure-autocomplete',
-    name: 'ville_ou_code_postal',
-    defaultValue: getUrlParameter('ville_ou_code_postal'),
-    placeholder: I18n.t('recherche_structure_component.placeholder_recherche'),
-    minLength: 3,
-    source: (requete, reponse) => { requeteCommunes(requete, reponse, $); },
-    templates: {
-      inputValue: (reponse) => reponse ? reponse.value : '',
-      suggestion: (reponse) => `
-      <div class="commune-suggestion">
-        <strong>${escapeHTML(reponse.ville)}</strong>
-        (${escapeHTML(reponse.code_postal)})
-      </div>
-      `
-    },
-    onConfirm: (selectedItem) => {
-      if (! selectedItem) return;
-
-      $('#code_postal').val(selectedItem.code_postal);
-
-      const disabled = selectedItem.value == '';
-      $('#bouton-chercher')
-        .prop("disabled", disabled)
-        .toggleClass('disabled', disabled);
-    },
-  });
-});

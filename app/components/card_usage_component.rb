5ea18330ce651c2e46e23824b0235250c8f55965
Étienne Charignon <etienne.charignon@ut7.fr>
2026-06-03 19:33:39 +0200

    ♻️ Retravail des cartes de choix d'usage
    * Alignement des logo
    * nottoyage du code inutile
    * factorisation des descriptions alternatives des logos

--- app/components/card_usage_component.rb
+++ app/components/card_usage_component.rb
@@ -1,17 +1,14 @@
 # frozen_string_literal: true
 
 class CardUsageComponent < ViewComponent::Base
-  def initialize(titre:, description: nil, image: nil, submit_value: nil, aria_label: nil,
-html_attributes: {})
+  def initialize(titre:, description: nil, image: nil, logo_alt: nil,
+    submit_value: nil, aria_label: nil, html_attributes: {})
     @titre = titre
     @description = description
     @image = image
+    @logo_alt = logo_alt
     @submit_value = submit_value
     @aria_label = aria_label
     @html_attributes = html_attributes
-  end
-
-  def submit?
-    @submit_value.present?
   end
 end

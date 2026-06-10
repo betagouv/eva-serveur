1707f76554d5e427b5cd548ff4d7990ab74f6a05
Étienne Charignon <etienne.charignon@ut7.fr>
2026-06-10 15:36:08 +0200

    🐛 Corrige le calcul de score_strategie qui se basait sur les données pour score_numerique

--- app/models/restitution/entreprises/impact/score_par_impact.rb
+++ app/models/restitution/entreprises/impact/score_par_impact.rb
@@ -2,48 +2,47 @@ module Restitution
   module Entreprises
     module Impact
       class ScoreParImpact
-        # Si cumul des scores >= 39, alors très fort
-        SEUILS_POUR_COUT = {
-          39 => :tres_fort,
-          26 => :fort,
-          13 => :moyen,
-          0 => :faible
+        SEUILS = {
+          score_cout: {
+            39 => :tres_fort,
+            26 => :fort,
+            13 => :moyen,
+            0 => :faible
+          },
+          score_strategies: {
+            27 => :tres_fort,
+            18 => :fort,
+            9 => :moyen,
+            0 => :faible
+          },
+          score_numerique: {
+            30 => :tres_fort,
+            20 => :fort,
+            10 => :moyen,
+            0 => :faible
+          }
         }.freeze
 
         def calcule_score_cout(evenements, pourcentage_risque)
-          calcule(evenements, "score_cout", SEUILS_POUR_COUT, pourcentage_risque)
+          calcule(evenements, :score_cout, pourcentage_risque)
         end
-
-        SEUILS_POUR_STRATEGIE = {
-          27 => :tres_fort,
-          18 => :fort,
-          9 => :moyen,
-          0 => :faible
-        }.freeze
 
         def calcule_score_strategie(evenements, pourcentage_risque)
-          calcule(evenements, "score_numerique", SEUILS_POUR_STRATEGIE, pourcentage_risque)
+          calcule(evenements, :score_strategies, pourcentage_risque)
         end
-
-        SEUILS_POUR_NUMERIQUE = {
-          30 => :tres_fort,
-          20 => :fort,
-          10 => :moyen,
-          0 => :faible
-        }.freeze
 
         def calcule_score_numerique(evenements, pourcentage_risque)
-          calcule(evenements, "score_numerique", SEUILS_POUR_NUMERIQUE, pourcentage_risque)
+          calcule(evenements, :score_numerique, pourcentage_risque)
         end
 
-        def calcule(evenements, type_score, seuils, pourcentage_risque)
+        def calcule(evenements, type_score, pourcentage_risque)
           evenements_reponse = MetriquesHelper.filtre_evenements_reponses(evenements)
           return if evenements_reponse.blank?
 
-          total = evenements_reponse&.sum { |e| e.donnees[type_score] || 0 }
+          total = evenements_reponse.sum { |e| e.donnees[type_score.to_s] || 0 }
           total += calcule_malus(pourcentage_risque)
 
-          seuils.each do |seuil, interpretation|
+          SEUILS[type_score].each do |seuil, interpretation|
             return interpretation if total >= seuil
           end
         end

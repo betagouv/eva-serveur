4f309a969f54966ebb162343a056576b450a60c2
Étienne Charignon <etienne.charignon@ut7.fr>
2026-06-11 15:59:44 +0200

    💬 Reprise des textes de l'écran de choix de l'usage

--- app/components/texte_intro_component.rb
+++ app/components/texte_intro_component.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class TexteIntroComponent < ViewComponent::Base
+  include ApplicationHelper
+
   def initialize(titre: nil, texte: nil, classes: "")
     @titre = titre
     @texte = texte

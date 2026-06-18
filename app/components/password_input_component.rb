a532f2423eddf1580adba6e37c1e8a59f93c6f89
Étienne Charignon <etienne.charignon@ut7.fr>
2026-06-18 14:58:49 +0200

    ♻️ Extrait le code commun de input_component et password_input_component

--- app/components/password_input_component.rb
+++ app/components/password_input_component.rb
@@ -1,26 +1,9 @@
 # frozen_string_literal: true
 
-class PasswordInputComponent < ViewComponent::Base
+class PasswordInputComponent < BaseInputComponent
   def initialize(id:, label:, hint: nil, form: nil, method: nil, required: false, autocomplete: nil)
-    @id = id
-    @label = label
-    @hint = hint
-    @form = form
-    @method = method
-    @required = required
-    @autocomplete = autocomplete
-  end
-
-  def input_id
-    if @form&.object_name.present? && @method.present?
-      "#{@form.object_name}_#{@method}"
-    else
-      @id
-    end
-  end
-
-  def messages_id
-    "#{input_id}-messages"
+    super(id: id, label: label, hint: hint, form: form, method: method,
+          required: required, autocomplete: autocomplete)
   end
 
   def show_id
@@ -39,31 +22,5 @@ class PasswordInputComponent < ViewComponent::Base
     attrs[:required] = true if @required
     attrs[:autocomplete] = @autocomplete if @autocomplete.present?
     attrs
-  end
-
-  def has_errors?
-    return false unless @form&.object.respond_to?(:errors)
-
-    @form.object.errors[@method].present?
-  end
-
-  def error_html
-    return "".html_safe unless has_errors?
-
-    @form.object.errors[@method].map { |error|
-      "<p class=\"fr-message fr-message--error\">#{error}</p>"
-    }.join.html_safe
-  end
-
-  def label_classes
-    classes = [ "fr-password__label", "fr-label" ]
-    classes << "fr-label--error" if has_errors?
-    classes.join(" ")
-  end
-
-  def required_asterisk
-    return "".html_safe unless @required
-
-    safe_join([ " ", tag.abbr("*", title: "required") ])
   end
 end

39044c428ea00b5c10f79b2de96bcc06161e7110
Étienne Charignon <etienne.charignon@ut7.fr>
2026-07-09 18:11:22 +0200

    🐛 Répare le formulaire de modification d'une évaluation

--- app/models/evaluation_eva.rb
+++ app/models/evaluation_eva.rb
@@ -1,2 +1,7 @@
 class EvaluationEva < Evaluation
+  def self.model_name
+    # Temporairement, en attendant la séparation des modeles,
+    # pour réparer le formulaire de modification
+    Evaluation.model_name
+  end
 end

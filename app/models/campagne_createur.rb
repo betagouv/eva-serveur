d7740ad78bcef4a6c389d92fba70e8fc1c7c5a59
Étienne Charignon <etienne.charignon@ut7.fr>
2026-07-30 16:27:47 +0200

    ✨ Simplifie le nom généré pour les campagne Evapro

--- app/models/campagne_createur.rb
+++ app/models/campagne_createur.rb
@@ -1,6 +1,4 @@
 class CampagneCreateur
-  NOM_CAMPAGNE_PAR_DEFAUT = "Diagnostic des risques"
-
   def initialize(structure, compte)
     @structure = structure
     @compte = compte
@@ -9,7 +7,12 @@ class CampagneCreateur
   def cree_campagne_opco!
     return unless doit_creer_campagne?
 
-    cree_campagnes_opco_financeur
+    parcours_types = @structure.opco.parcours_types
+    return if parcours_types.empty?
+
+    parcours_types.each_with_index do |parcours_type, index|
+      cree_campagne(parcours_type, libelle: libelle_campagne(index, parcours_types.size))
+    end
   end
 
   private
@@ -20,23 +23,12 @@ class CampagneCreateur
       @structure.opco.present?
   end
 
-  def cree_campagnes_opco_financeur
-    parcours_types = premier_opco_financeur.parcours_types
-    return if parcours_types.empty?
-
-    parcours_types.each do |parcours_type|
-      libelle = libelle_campagne_avec_parcours(parcours_type)
-      cree_campagne(parcours_type, libelle: libelle)
-    end
+  def libelle_campagne(index, nombre_parcours_types)
+    suffix = nombre_parcours_types > 1 ? " #{index + 1}" : ""
+    "Diagnostic#{suffix} : #{@structure.nom}"
   end
 
-  # Retourne l'OPCO financeur s'il existe (il ne devrait y en avoir qu'un seul),
-  # sinon retourne nil.
-  def premier_opco_financeur
-    @premier_opco_financeur ||= @structure.opco
-  end
-
-  def cree_campagne(parcours_type, libelle: libelle_campagne)
+  def cree_campagne(parcours_type, libelle:)
     campagne = Campagne.new(
       libelle: libelle,
       compte: @compte,
@@ -46,13 +38,5 @@ class CampagneCreateur
 
     campagne.save!
     campagne
-  end
-
-  def libelle_campagne
-    "#{NOM_CAMPAGNE_PAR_DEFAUT} : #{@structure.nom}"
-  end
-
-  def libelle_campagne_avec_parcours(parcours_type)
-    "#{NOM_CAMPAGNE_PAR_DEFAUT} : #{@structure.nom} - #{parcours_type.libelle}"
   end
 end

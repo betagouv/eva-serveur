39615bf3a88aa40ae712ab5954602b944d44bef6
Étienne Charignon <etienne.charignon@ut7.fr>
2026-08-27 19:00:13 +0200

    ♻️ Regroupe les requêtes du StandardisateurGlissant en une seule par situation
    Chaque affichage d'évaluation calculait moyenne et écart-type de chaque
    métrique via une requête SQL séparée (jusqu'à 18 scans de "parties" par
    partie affichée). Elles sont maintenant calculées en une seule requête.

--- app/models/restitution/standardisateur_glissant.rb
+++ app/models/restitution/standardisateur_glissant.rb
@@ -8,35 +8,68 @@ module Restitution
     end
 
     def moyennes_metriques
-      @moyennes_metriques ||= @metriques.index_with do |metrique|
-        moyenne_metrique(metrique)
-      end
+      @moyennes_metriques ||=
+        agregats_metriques.transform_values { |agregat| agregat[:average] }
     end
 
     def ecarts_types_metriques
-      @ecarts_types_metriques ||= @metriques.index_with do |metrique|
-        ecart_type_metrique(metrique)
-      end
+      @ecarts_types_metriques ||=
+        agregats_metriques.transform_values { |agregat| agregat[:stddev_pop] }
     end
 
     private
 
-    def moyenne_metrique(metrique)
-      aggrege_metrique(:average, metrique)
+    def agregats_metriques
+      @agregats_metriques ||= begin
+        figees, a_calculer = @metriques.partition { |metrique| figee?(metrique) }
+
+        resultats = figees.index_with { |metrique| standard_fige(metrique) }
+        resultats.merge!(calcule_metriques(a_calculer))
+
+        @metriques.index_with { |metrique| resultats[metrique] }
+      end
     end
 
-    def ecart_type_metrique(metrique)
-      aggrege_metrique(:stddev_pop, metrique)
+    def figee?(metrique)
+      @standards_figes&.key?(metrique.to_sym)
     end
 
-    def aggrege_metrique(fonction, metrique)
-      return @standards_figes[metrique.to_sym][fonction] if @standards_figes&.key?(metrique.to_sym)
+    def standard_fige(metrique)
+      @standards_figes[metrique.to_sym].slice(:average, :stddev_pop)
+    end
 
-      @collect_metriques
-        .call
-        .where.not(metriques: {})
-        .calculate(fonction, "(metriques ->> '#{metrique}')::numeric")
-        .to_f
+    def calcule_metriques(metriques)
+      return {} if metriques.empty?
+
+      ligne = @collect_metriques.call
+                                 .where.not(metriques: {})
+                                 .select(Arel.sql(colonnes_agregation(metriques)))
+                                 .take
+
+      resultats_agregation(metriques, ligne)
+    end
+
+    def colonnes_agregation(metriques)
+      metriques.each_with_index.flat_map do |metrique, index|
+        expression = "(metriques ->> #{connexion.quote(metrique.to_s)})::numeric"
+        [
+          "AVG(#{expression}) AS moyenne_#{index}",
+          "STDDEV_POP(#{expression}) AS ecart_type_#{index}"
+        ]
+      end.join(", ")
+    end
+
+    def resultats_agregation(metriques, ligne)
+      metriques.each_with_index.each_with_object({}) do |(metrique, index), resultats|
+        resultats[metrique] = {
+          average: ligne&.public_send("moyenne_#{index}").to_f,
+          stddev_pop: ligne&.public_send("ecart_type_#{index}").to_f
+        }
+      end
+    end
+
+    def connexion
+      ActiveRecord::Base.connection
     end
   end
 end

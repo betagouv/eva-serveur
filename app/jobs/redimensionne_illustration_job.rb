93d3177e065014878ed332959c0d31f224fc9f9e
Étienne Charignon <etienne.charignon@ut7.fr>
2026-09-03 18:55:25 +0200

    🐛 Limite la concurence des jobs de redimentionnement d'image
    Limite la concurrence : le traitement d'une image (téléchargement +
    redimensionnement vips + upload) est gourmand en mémoire, et en faire
    tourner plusieurs à la fois a déjà fait dépasser le quota mémoire du
    worker (OOM kill), perdant silencieusement les jobs en cours.

--- app/jobs/redimensionne_illustration_job.rb
+++ app/jobs/redimensionne_illustration_job.rb
@@ -1,5 +1,9 @@
 class RedimensionneIllustrationJob < ApplicationJob
+  include Sidekiq::Throttled::Job
+
   queue_as :default
+
+  sidekiq_throttle concurrency: { limit: 1 }
 
   def perform(question)
     question.illustration.variant(:defaut).processed

83af8f8ac2adc180f83f7eb0d73a4633108ff6b4
Étienne Charignon <etienne.charignon@ut7.fr>
2026-09-09 11:15:15 +0200

    🧑‍💻 Ferme l'instance Chrome en cas de rechargement de la class Browser

---
+++ config/initializers/pdf_browser_reload.rb
@@ -0,0 +1,8 @@
+# Pdf::Browser mémorise un navigateur Puppeteer dans une variable de classe
+# pour le réutiliser entre les générations de PDF. En développement, le
+# rechargement du code (Zeitwerk) réinitialise cette variable sans jamais
+# fermer le processus Chrome sous-jacent, qui reste alors orphelin.
+# On le ferme explicitement juste avant que la classe ne soit déchargée.
+Rails.autoloaders.main.on_unload("Pdf::Browser") do |klass, _abspath|
+  klass.reset!
+end

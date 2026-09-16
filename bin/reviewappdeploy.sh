c5550fe6eb8991ccc33566a35eb0ae88f9e7fe4f
Étienne Charignon <etienne.charignon@ut7.fr>
2026-09-16 15:48:03 +0200

    🧑‍💻 Rafraichi l'initialisation de la base des reviewapp

--- bin/reviewappdeploy.sh
+++ bin/reviewappdeploy.sh
@@ -11,13 +11,7 @@
 # avant Rails 8.
 bundle exec rails runner 'ActiveRecord::Base.connection_pool.schema_migration.create_table unless ActiveRecord::Base.connection_pool.schema_migration.table_exists?'
 
-bundle exec rails db:migrate VERSION=20210305110556
-bundle exec rails db:migrate VERSION=20230406135310
-bundle exec rake reviewapp:ignore_migrations[20230419084251,20230407134617,20240624120736]
-bundle exec rails db:migrate VERSION=20240809100708
 bundle exec rake reviewapp:initdb
-bundle exec rails db:migrate VERSION=20241206052500
-bundle exec rake reviewapp:ignore_migrations[20241206064741,20250103165108,20250103172005]
 bundle exec rails db:migrate
 bundle exec rails db:seed
 bundle exec rake reviewapp:seed
